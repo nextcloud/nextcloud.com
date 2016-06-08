85f1da5c6e454fd5ec590851f5b43bdd9ff50612
Jos Poortvliet <jospoortvliet@gmail.com>
2016-06-08 11:37:07 +0200

    stop contribook from breaking stuff

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Enables CONTRIBOOK (required mysql database)
-define(CONTRIBOOK, 1);
+define(CONTRIBOOK, 0);
 
 // Defines the author id of the official user posting annoucenments (used to produce rss feed)
 define(OFFICIALAUTHORID, 2);
