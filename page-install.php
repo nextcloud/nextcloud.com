e75080d38232b11e0efe425893e02efb94f4169b
Mike Babb <mail@mikebabb.com>
2016-06-12 18:07:38 +0100

    Replaced loader gif on Install page with text content and a link to the Contribute page

--- page-install.php
+++ page-install.php
@@ -1,13 +1,3 @@
-<div class="row">
-	<div class="col-md-6 col-md-offset-3 text-center">
-
-		<h1>Coming soon!</h1>
-	</div>
-	<div class="col-md-4 col-md-offset-4 text-center">
-		<img class="img-responsive" alt="animation from wikimedia commons" src="<?php bloginfo('template_directory'); ?>/assets/img/common/loading_big.gif" />
-	</div>
-	<div class="col-md-8 col-md-offset-2 text-center">
-		
-		<p>Over the coming weeks we'll provide stable and pre-release builds of Nextcloud right here.</p>
-	</div>
-</div>
+<h1>Coming soon!</h1>
+<p>We're working hard on desktop and mobile clients to bring Nextcloud to a variety of platforms. Download links will be added to this page as soon as they're ready.</p>
+<p>If you'd like to lend us a hand, <a href="/contribute/">get involved!</a></p>
