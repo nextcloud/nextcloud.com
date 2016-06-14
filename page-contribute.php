ef573363e2ed53cc4d3578cdba41111691fe20b2
Jos Poortvliet <jospoortvliet@gmail.com>
2016-06-14 11:59:15 +0200

    then we can also expand the contribute page a bit...

--- page-contribute.php
+++ page-contribute.php
@@ -1,14 +1,12 @@
 <h1>Get involved!</h1>
 <p>Nextcloud is an <strong>open source project</strong> and an open company. That means it is <em>yours</em> to make a difference!</p>
-<p>We are still starting up. We're defining our plans and will come with more <a href="/news">news</a> the coming days and weeks. We will start by delivering Nextcloud 9, adding bugfixes and Spreed.ME based on what currently is shipped in the Spreedbox. From there on we'll define and built the future, together with all of you!</p>
-
-<p>While we bootstrap our product, website and community infrastructure, we invite you to join the conversations on <a href="https://help.nextcloud.com">our forums</a> and our <a href="irc://#nextcloud-dev@freenode.net">IRC channel on freenode</a>. Interact with us on our <a href="/news">blogs and social media</a>, too!</p>
+<p>While we bootstrap our product, website and community infrastructure, we invite you to join our <a href="https://github.com/nextcloud" target="_blank">development efforts in github</a> and the conversations on <a href="https://help.nextcloud.com" target="_blank">our forums</a> and our <a href="irc://#nextcloud-dev@freenode.net" target="_blank">IRC channel on freenode</a>. Interact with us on our <a href="/news">blogs and social media</a>, too!</p>
 
 
-<!--<p>Do you want to get a certain <strong>improvement</strong> in Nextcloud? Did you find a <strong>problem</strong>?
+<p>Do you want to get a certain <strong>improvement</strong> in Nextcloud? Did you find a <strong>problem</strong>?
 Do you want to <strong>help</strong> translate, promote or document Nextcloud?</p>
 <h2>Become part of the Nextcloud Community and help us liberate data across the globe!</h2>
-<div class="row" style="text-align: center">
+<!--<div class="row" style="text-align: center">
   <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>testing/index.html" class="btn btn-lg btn-default">
   <i class="icon-lightbulb-o"></i> Join the Test Pilots!</a>
   <a href="/promote" class="btn btn-default btn-lg"><i class="icon-bullhorn"></i> Discuss Nextcloud with others</a>
@@ -30,7 +28,7 @@ Do you want to <strong>help</strong> translate, promote or document Nextcloud?</
 		</div>
 	</div>
 </div>-->
-<!--<h2>How to participate:</h2>
+<h2>How to participate:</h2>
 <div class="row">
 	<div class="col-md-6">
             <h3><i class="icon-thumbs-up"></i>&nbsp;&nbsp;Improve Nextcloud</h3>
@@ -38,43 +36,44 @@ Do you want to <strong>help</strong> translate, promote or document Nextcloud?</
             <li>Develop your own Nextcloud Server app (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>app">app development documentation</a>)</li>
             <li>Get a bug fixed (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>bugtracker">bugtracker documentation</a> and <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>general">general guidelines and setup</a>)</li>
             <li>Improve the Nextcloud Core (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>core">core development documentation</a>)</li>
-            <li>Learn to write Nextcloud code. (Try your hands on a <a href="https://github.com/nextcloud/core/issues?labels=Junior+Job&state=open">Junior Job</a>
-          and see the <a href="https://github.com/nextcloud/core/wiki/Project-ideas#coding">project ideas page</a>)
+            <li>Learn to write Nextcloud code. (Try your hands on a <a href="https://github.com/nextcloud/core/issues?labels=Junior+Job&state=open" target="_blank">Junior Job</a>
+          <!--and see the <a href="https://github.com/nextcloud/core/wiki/Project-ideas#coding">project ideas page</a>-->)
           </ul>
           <p>Talk to us <a href="irc://#nextcloud-dev@freenode.net">on IRC</a> and on our
-          <a href="https://mailman.nextcloud.org/mailman/listinfo/devel">mailing list</a>! We look forward to your code and thoughts.</p>
-          <iframe width="100%" height="360" src="https://www.youtube.com/embed/M_-PtACnnz4?rel=0" frameborder="0" allowfullscreen></iframe>
+          <a href="https://help.nextcloud.com" target="_blank">forum</a>! We look forward to your code and thoughts.</p>
+<!--           <iframe width="100%" height="360" src="https://www.youtube.com/embed/M_-PtACnnz4?rel=0" frameborder="0" allowfullscreen></iframe> -->
 	</div>
 	<div class="col-md-6">
             <h3><i class="icon-refresh"></i>&nbsp;&nbsp;Test Nextcloud</h3>
             <p>Testing upcoming Nextcloud Server releases is the best way of making sure the new release can do what you need it 
             to do. And of course, only if you report a problem to us we know about it and can fix it! If you have a bug to report,
             find the <a href="https://github.com/nextcloud/core/blob/master/CONTRIBUTING.md">issue submission guidelines here</a>.</p>
-            <p>Anybody interested in helping out with Nextcloud Server testing is invited to join the Nextcloud Test Pilot team.
-            See <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>testing">the Test Pilot documentation</a> for information!.</p>
+<!--             <p>Anybody interested in helping out with Nextcloud Server testing is invited to join the Nextcloud Test Pilot team. -->
+<!--             See <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>testing">the Test Pilot documentation</a> for information!.</p> -->
 	</div>
-    <div class="col-md-6">
+    <!--<div class="col-md-6">
         <h3><i class="icon-book"></i>&nbsp;&nbsp;Translate and document Nextcloud</h3>
          <p>Translate Nextcloud to your language. Go to <a href="https://www.transifex.com/projects/p/nextcloud/">Transifex</a> and get going!</p>
         <p>Help us improve the Nextcloud documentation! Find out how to make a difference <a href="https://doc.nextcloud.org/howto_contribute.html">in this contribution guide</a>!</p>
         <p><img src="<?php echo get_template_directory_uri(); ?>/assets/img/misc/contribute-doc.gif" style="width: 100%" alt="front door" class="aligncenter" /></p>
         <p>The animation shows the easiest way to contribute to the Nextcloud documentation. Go to the <a href="https://github.com/nextcloud/documentation">documentation in Github</a>, click the
         'edit' icon on a page, make the changes and create a pull request. This will notify the documentation maintainers of your proposed change and after review, it can be included!</p>
-    </div>
-    <div class="col-md-6">
+    </div>-->
+<!--    <div class="col-md-6">
         <h3><i class="icon-pencil"></i>&nbsp;&nbsp;Design Nextcloud</h3>
       	Help out with Nextcloud user interaction design or graphic design! Get involved on 
       	<a href="design/">the design page</a>!
         <h3><i class="icon-money"></i>&nbsp;&nbsp;Support Nextcloud development with bounties</h3>
         Support Nextcloud development by offering some money on features you would like to have!
         <a href="https://www.bountysource.com/teams/nextcloud">Check our bountysource page</a>!
-    </div>
+    </div>-->
     <div class="col-md-6">
     <h3><i class="icon-book"></i>&nbsp;&nbsp;Answer questions about Nextcloud use and development</h3>
         <p>Help answer questions users and developers have about Nextcloud!</p>
-        <ul><li>Join the <a href="https://forum.nextcloud.org">Nextcloud forums</a> and look for <a href="https://forum.nextcloud.org/search.php?search_id=unanswered">unanswered questions</a></li>
-        <li>Answer Nextcloud development questions on <a href="https://stackoverflow.com/questions/tagged/nextcloud">Stack Overflow</a> and <a href="https://community.spiceworks.com/">Spiceworks</a></li>
-        <li>Answer Nextcloud user questions on <a href="https://mailman.nextcloud.org/mailman/listinfo/user">the Nextcloud user mailing list</a></li></ul>
+        <ul><li>Join the <a href="https://help.nextcloud.com">Nextcloud forums</a>!</li>
+        <li>Answer Nextcloud development questions on <a href="https://stackoverflow.com/questions/tagged/nextcloud">Stack Overflow</a> and other places online.</li>
+<!--         <li>Answer Nextcloud user questions on <a href="https://mailman.nextcloud.org/mailman/listinfo/user">the Nextcloud user mailing list</a></li> -->
+		</ul>
     </div>
 </div>-->
 <br />
