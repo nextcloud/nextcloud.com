c9c4531bb514e288295a9062158ec29334d227a7
Jos Poortvliet <jospoortvliet@gmail.com>
2016-06-19 17:34:07 +0200

    how to get added? So!

--- page-apply.php
+++ page-apply.php
@@ -3,22 +3,41 @@
 </div>
 <div class="row providers">
     <div class="span12">
-        <p>To apply to be listed on the <a href="/providers">Nextcloud.org/providers</a> webpage as an open source
-            provider, please complete the following form.</p>
-        <script src="//app-ab13.marketo.com/js/forms2/js/forms2.min.js"></script>
-	<form id="mktoForm_1256"></form>
-	<script>MktoForms2.loadForm("//app-ab13.marketo.com", "038-KRL-592", 1256);</script>
+		<p>To apply to be listed on the <a href="/providers">Nextcloud.org/providers</a> webpage as an open source provider, please complete the following form.</p>
+		<form name="provider" method="post" action="providersubmit/">
+			<p><label for="organization">Company/Organization<br>
+			<input  type="text" name="organization" maxlength="80" size="60" placeholder=""></label></p>
+			<p><label for="yourname">Contact name<br>
+			<input  type="text" name="yourname" maxlength="60" size="60"></label></p>
+			<p><label for="email">Contact Email<br>
+			<input  type="text" name="email" maxlength="80" size="60"></label></p>
+			<p><label for="phone">Phone number<br>
+			<input  type="text" name="phone" maxlength="40" size="60" placeholder="Please include country code"></label></p>
+			<p><label for="target_countries">Countries you intend to service<br>
+			<input  type="text" name="target_countries" maxlength="40" size="60" placeholder="country codes separated by comma, like 'NL, FR, DE'"></label></p>
+			<p><label for="hosting">What audience do you target, home users, businesses or both?<br>
+			<select name="hosting">
+				<option value="both">Both</option>
+				<option value="organizations">Business</option>
+				<option value="consumers">Home users</option>
+			</select></label></p>
+			<p><label for="free">Do you offer a free, not time-limited hosting tier?<br>
+			<select name="free">
+				<option value="nofreeplan">No</option>
+				<option value="freeplan">Yes</option>
+			</select></label></p>
+			<p><label for="description">100 character description of your offering.<br />
+			<textarea  name="description" maxlength="100" cols="60" rows="3" placeholder="Introduce your organization and offering in max 100 characters!"></textarea></label></p>
+			<p><label for="image">URL to your logo, 600x150 pixels<br>
+			<input  type="text" name="image" maxlength="80" size="60"></label></p>
+			<td colspan="2" style="text-align:center">
+			<div class="g-recaptcha" data-sitekey="<?php echo RECAPTCHA_SITEKEY; ?>"></div>
+			<input type="submit" value=" Submit ">
+		</form>
+	</div>
+</div> 
 
-<!-- Old hubspot code 	 -->
-<!--        <script charset="utf-8" src="//js.hsforms.net/forms/current.js"></script>
-        <script>
-            hbspt.forms.create({
-                portalId: '328096',
-                formId: 'ecc0f964-1dd7-4159-913a-89172fbadfb6'
-            });
-        </script>-->
-        <p>If you are interested in becoming an enterprise provider with a support contract from
+
+<!--        <p>If you are interested in becoming an enterprise provider with a support contract from
             <a target="_blank" href="https://nextcloud.com">Nextcloud Inc</a> you can find more information
-            <a target="_blank" href="https://nextcloud.com/products/service-provider">here</a>.</p>
-    </div>
-</div>
+            <a target="_blank" href="https://nextcloud.com/products/service-provider">here</a>.</p>-->
