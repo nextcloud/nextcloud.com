0313ab8c6e6dd63981d3579a5b674596c9e4bdb4
Jos Poortvliet <jospoortvliet@gmail.com>
2016-06-30 13:50:18 +0200

    first spreed page

---
+++ page-spreedbox.php
@@ -0,0 +1,35 @@
+<!-- <?php get_template_part('templates/parts/title'); ?> -->
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<div class="page-header">
+	<h1>Spreedbox: Most private video chat and file exchange</h1>
+</div>
+<iframe width="100%" height="450" src="https://www.youtube.com/embed/egLqRqb4sHA?rel=0" frameborder="0" allowfullscreen></iframe>
+
+<h2>The Spreedbox</h2>
+<p>Rediscover private video chat, messaging and file exchange. The Spreedbox is a unique device for secure audio/video conferencing, text and video messaging and file sharing. The Spreedbox is your own conferencing, meeting and file exchange service on the Internet and puts the control and security of your data into your own hands.</p>
+
+<iframe width="100%" height="450" src="https://www.youtube.com/embed/Yc6avTVowc0?rel=0" frameborder="0" allowfullscreen></iframe>
+
+<h2>Cloud vs. Private Cloud</h2>
+<p>Today, most organizations use online services for communication and often have confidential data shared and stored with service providers. Just think of Google®, Skype®, WebEx®, GoToMeeting®, BlueJeans® and many others. You may already have used these services to video conference, share files, keep your address books and stay in touch. All great features, without a doubt. But where is your data? Who has access to it? Confidential conversations, files, videos and personal contacts are uploaded and shared with these service providers without having an adequate service and privacy agreement that meets your requirements in privacy and confidentiality.
+
+<h2>Your Data, Your Control</h2></p>
+<p>This is where the Spreedbox comes in. The Spreedbox allows you to take back ownership of your data. The Spreedbox empowers you to operate your own secure audio/video chat, messaging and file sharing service with the highest measurements for control and security of your own data. It is your own video meeting and file sharing service that can be available on computers, mobile phones and tablets through the Internet or limited to an Intranet. Your data always stays on your Spreedbox. Make a call, invite your friends and clients, and collaborate in closed groups through video/audio, text messaging, and document and file sharing. You can access your private data in an easy-to-use web interface with PC, Android and iOS devices.
+</p>
+<h2>Open Source</h2>
+<p>The Spreedbox runs Nextcloud, software which is free and published under open source AGPL license that gives you the right to examine, share and modify it. An international community of software engineers and volunteer contributors develops Nextcloud and <a href="/contribute">you are invited to get involved, too</a>.</p>
+
+<h2>Better-and-better-and-better Security</h2>
+<p>The Spreedbox is built on Linux as an operating system, providing world-leading security as transactional components with rigorous application isolation. With our secure algorithms, the high-speed ARM 4-core CPU and an off-the-silicon secure hardware key-generator (TRNG), the Spreedbox features an outstanding cryptographic budget well above any industry standard.</p>
+
+<h2>Use One, Deploy Many</h2>
+<p>The Spreedbox is instantly ready to use. You only need one Spreedbox to securely meet with up to 6 people in a session where you can use video/audio, share files and collaborate on documents. You can have up to 10 group sessions in parallel each with 6 attendees. This accounts for an awesome capacity of real-time meetings being held by up to 60 people at any given time – all with one Spreedbox.</p>
+
+<h2>One Last Thing</h2>
+<p>As the Spreedbox is your private cloud you can video chat and share globally 24 hours a day, 365 days a year. There are no fees, no subscriptions and no running costs.*</p>
+
+<p>*Electricity and Internet connectivity is on your account.
+
+<h2><a href="https://www.spreed.me/spreedbox/">LEARN MORE AND PURCHASE A SPREEDBOX</a></h2>
+
+
