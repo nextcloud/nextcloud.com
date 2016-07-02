bf0d55be2ae4e9af8a529135627e7bddc00bb79f
Marius Blüm <marius@lineone.io>
2016-07-02 10:53:44 +0200

    Fix Google Play - Beta Link

--- install-instructions.php
+++ install-instructions.php
@@ -201,7 +201,7 @@
 							<div class="row">
 								<div class="col-xs-6">
 									<a style="opacity: 0.9" target="_blank" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_ANDROID; ?>">
-									<img src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/googleplay.png"><br /><a target="_blank" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_FDROID_BETA; ?>">Beta client</a>
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/googleplay.png"><br /><a target="_blank" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_ANDROID_BETA; ?>">Beta client</a>
 									</a>
 								</div>
 								<div class="col-xs-6">
