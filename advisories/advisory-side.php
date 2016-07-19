b7e36a3ef4c6c5adca6189a199b2134cb4e61ead
Lukas Reschke <lukas@statuscode.ch>
2016-07-19 11:39:15 +0200

    Use proper link

--- advisories/advisory-side.php
+++ advisories/advisory-side.php
@@ -1,6 +1,6 @@
 <br/><p>Nextcloud server 9.0.52</p>
-<a href="/security/advisory?id=nc-sa-2016-001">Stored XSS in "gallery" application</a><br/>
-<a href="/security/advisory?id=nc-sa-2016-002">Log pollution can potentially lead to local HTML injection</a><br/>
-<a href="/security/advisory?id=nc-sa-2016-003">Content-Spoofing in "files" app</a><br/>
-<a href="/security/advisory?id=nc-sa-2016-004">Edit permission check not enforced on WebDAV COPY action</a><br/>
-<a href="/security/advisory?id=nc-sa-2016-005">Read-only share recipient can restore old versions of file</a><br/>
+<a href="/security/advisory/?id=nc-sa-2016-001">Stored XSS in "gallery" application</a><br/>
+<a href="/security/advisory/?id=nc-sa-2016-002">Log pollution can potentially lead to local HTML injection</a><br/>
+<a href="/security/advisory/?id=nc-sa-2016-003">Content-Spoofing in "files" app</a><br/>
+<a href="/security/advisory/?id=nc-sa-2016-004">Edit permission check not enforced on WebDAV COPY action</a><br/>
+<a href="/security/advisory/?id=nc-sa-2016-005">Read-only share recipient can restore old versions of file</a><br/>
