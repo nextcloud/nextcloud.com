c0df34d7074bfaf309b0734b4fd8d6c37e1bb2f3
Jos Poortvliet <jospoortvliet@gmail.com>
2016-07-19 15:17:58 +0200

    double it up...

---
+++ pressreleases/page-20160614.php
@@ -0,0 +1,16 @@
+<h1>Nextcloud 9 Released Ahead of Promised Date and Fully Commited to Open Source</h1>
+<p><strong>Stuttgart, Germany - June 14nd, 2016</strong> Ahead of a promised early July release, Nextcloud, the reliable solution for open source enterprise file sync and share, communication and collaboration, announces the availability of Nextcloud 9 with enterprise support options and the upcoming availability of enterprise functionality under an open source license.</p>
+
+<p>The first release delivers a capable, proven platform replacing existing cloud services for modern enterprises with a privacy-aware, secure solution under an open source license. Introducing often requested features like a file drop capability, a theming interface and enterprise grade logging, backed by long term, enterprise grade support, Nextcloud 9 provides a solid solution to migrate to for customers.</p>
+
+<p>"I'm proud to be part of a company with the confidence and long term vision to work fully in the open and facilitate an ecosystem", said Frank Karlitschek. "The immensely positive response to our initial announcement has enabled us to speed up our delivery through a collaborative approach, quickly introducing the capabilities modern enterprises demand"</p>
+
+<p>Nextcloud has started the public development of further enterprise grade features like Shibboleth authentication, Windows Network Drive storage integration, retention handling and fine grained file access control capabilities. These features will be made available as Nextcloud apps under an open source license and developed in the open, enabling customers, partners and users to participate and contribute to their development. *These features will gradually become available for support through our enterprise support subscriptions.</p>
+
+<p>"The transparency of developing these features in the open enables us to more closely track customer demands and needs", said Niels Mache. "By pursuing a business model which has worked for a large company like Red Hat we enable the establishment of a healthy ecosystem, benefiting users, customers, contributors and our own business alike"</p>
+
+<p>Nextcloud 9 is available immediately. Enterprises and organizations looking for quick deployments and reliable functioning backed by a qualified enterprise support team can find subscription options on the website.</p>
+
+<p>About Nextcloud<br />
+Enterprises managing confidential data and communication internally and externally rely on Nextcloud's secure, on-premise enterprise file access and communication platform. Nextcloud combines unprecedented universal access to data with next-generation secure communication and collaboration capabilities under direct control of IT and integrated with existing compliant infrastructure. Nextcloud's open, modular architecture, emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing assets within and across the borders of their organization. For more information, visit https://nextcloud.com</p>
+ 
