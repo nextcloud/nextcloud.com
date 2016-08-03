273a32101c07812e07fa55ed1ca3516820189ca3
Lukas Reschke <lukas@statuscode.ch>
2016-08-03 19:51:23 +0200

    Update piwik-tracking.php

--- templates/piwik-tracking.php
+++ templates/piwik-tracking.php
@@ -7,7 +7,7 @@
     var u="//stats.nextcloud.com/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 1]);
-    _paq.push(['addTracker', 'https://stats.nextcloud.opensourcesoftware.rocks/', 4]);
+    _paq.push(['addTracker', 'https://stats.nextcloud.opensourcesoftware.rocks/piwik.php', 4]);
 
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
