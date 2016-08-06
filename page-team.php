0039a9cf1db0dce6b011ffcd97a2963f7c63c377
Jos Poortvliet <jospoortvliet@gmail.com>
2016-08-06 12:51:37 +0200

    new pics, more details

--- page-team.php
+++ page-team.php
@@ -17,9 +17,7 @@
 	<div class="col-md-4">
 		<div class="teamp">
 		 &nbsp;
-			<div class="teamimg">
-				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/frank.jpg" />
-			</div>
+			<img class="img-responsive img-circle teamimg" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/frank.jpg" />
 			<h2 class="teamname">Frank Karlitschek</h2>
 			<div class="teammeta">
 				<h3 class="teamtitle">Managing Director</h3>
@@ -30,6 +28,40 @@
 			<p class="teamdescription">Frank founded the ownCloud project in 2010 to put people back in control of their data. To improve the company-community balance and accelerate the project he founded Nextcloud in 2016 and has been tirelessly working to realize his vision ever since.</p>
 		</div>
 	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/niels.jpg" />
+			</div>
+			<h2 class="teamname">Niels Mache</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Managing Director</h3>
+				<h4 class="teamexpertise">Founder</h4>
+				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
+				<p class="teamsocial"><!--<a href="https://spreed.me/"><i class="icon-rss icon"></i></a>--> <!--<a href="https://github.com/"><i class="icon-github-alt icon"></i></a>--> <a href="https://twitter.com/nielsmache"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">Currently managing director at <a href="http://www.struktur.de/">Struktur AG</a> and Nextcloud GmbH, Niels Mache was a co-founder of delix GmbH, one of the first Linux service providers, and of Red Hat Deutschland GmbH. During this time he led Red Hat Deutschland's development department as Director of Development, among other positions.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/andreas.jpg" />
+			</div>
+			<h2 class="teamname">Andreas Rode</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Sales Director</h3>
+				<h4 class="teamexpertise">Sales and Partnerships</h4>
+				<h4 class="teamlocation">lives in Hannover, Germany</h4>
+				<p class="teamsocial"><!--<a href="https://www.schilljs.com/"><i class="icon-rss icon"></i></a>--> <!--<a href="https://github.com/nickvergessen"><i class="icon-github-alt icon"></i></a>--> <!--<a href="https://twitter.com/nickvergessen"><i class="icon-twitter icon"></i></a>--> &nbsp; </p>
+			</div>
+			<p class="teamdescription">Andreas Rode is awesome.</p>
+		</div>
+	</div>
+</div>
+<div class="row">
 	<div class="col-md-4">
 		<div class="teamp">
 		 &nbsp;
@@ -55,11 +87,227 @@
 			<h2 class="teamname">Björn Schließle</h2>
 			<div class="teammeta">
 				<h3 class="teamtitle">Software Engineer</h3>
-				<h4 class="teamexpertise">Federation maintainer</h4>
+				<h4 class="teamexpertise">Federation and Encryption</h4>
 				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
 				<p class="teamsocial"><a href="https://schiessle.org/"><i class="icon-rss icon"></i></a> <a href="https://github.com/schiesbn"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/schiessle"><i class="icon-twitter icon"></i></a> </p>
 			</div>
 			<p class="teamdescription">Björn has been developing federated technology for Nextcloud since 2012. Computer Scientist, graduated at University of Stuttgart and Open Source and  Open Standards Evangelist for privacy respecting, distributed and federated networks, FSFE's Deputy Coordinator Germany has a deep understanding of the technical and social aspects of privacy-aware, distributed technology.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/joas.jpg" />
+			</div>
+			<h2 class="teamname">Joas Schilling</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Software Engineer</h3>
+				<h4 class="teamexpertise">Activity, Workflow and Notifications</h4>
+				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
+				<p class="teamsocial"><a href="https://www.schilljs.com/"><i class="icon-rss icon"></i></a> <a href="https://github.com/nickvergessen"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/nickvergessen"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">Joas has been involved in open source for over a decade, being a core member of the phpBB™ developer team and has been working on Nextcloud code since late 2013. He graduated from the University of Stuttgart and developed the Notifications and Activities technology in Nextcloud.</p>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/olivier.jpg" />
+			</div>
+			<h2 class="teamname">Olivier Paroz</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Senior Solutions Consultant</h3>
+				<h4 class="teamexpertise">Gallery app</h4>
+				<h4 class="teamlocation">lives in lausanne, Switzerland</h4>
+				<p class="teamsocial"><a href="https://www.linkedin.com/in/olivierparoz"><i class="icon-rss icon"></i></a> <a href="https://github.com/oparoz"><i class="icon-github-alt icon"></i></a><a href="https://twitter.com/oparoz"><i class="icon-twitter icon"></i></a></p>
+			</div>
+			<p class="teamdescription">TBD.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/patrick.jpg" />
+			</div>
+			<h2 class="teamname">Patrick Walther</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Sales Manager</h3>
+				<h4 class="teamexpertise">TBD</h4>
+				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
+				<p class="teamsocial"><!--<a href="https://www.schilljs.com/"><i class="icon-rss icon"></i></a>--> <!--<a href="https://github.com/nickvergessen"><i class="icon-github-alt icon"></i></a>--> <!--<a href="https://twitter.com/nickvergessen"><i class="icon-twitter icon"></i></a>--> &nbsp; </p>
+			</div>
+			<p class="teamdescription">TBD.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/jan.jpg" />
+			</div>
+			<h2 class="teamname">Jan-Christoph Borchardt</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Lead Designer</h3>
+				<h4 class="teamexpertise">User Interaction, design</h4>
+				<h4 class="teamlocation">lives in Ljubljan, Slovenia</h4>
+				<p class="teamsocial"><a href="https://jancborchardt.net/"><i class="icon-rss icon"></i></a> <a href="https://github.com/jancborchardt"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/jancborchardt"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">open source designer, adviser & educator - need more ;-)</p>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/jos.jpg" />
+			</div>
+			<h2 class="teamname">Jos Poortvliet</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Marketing Manager</h3>
+				<h4 class="teamexpertise">Marketing & communication</h4>
+				<h4 class="teamlocation">lives in Berlin, Germany</h4>
+				<p class="teamsocial"><a href="http://blog.jospoortvliet.com/"><i class="icon-rss icon"></i></a> <a href="https://github.com/jospoortvliet"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/jospoortvliet"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">Jos graduated in Psychology from the University of Utrecht and did project and service level management at Royal Bank of Scottland, KPN and the Dutch Department of Education. He has over a decade open source marketing and PR experience, previously working as Community Manager for SUSE Linux and ownCloud GmbH.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/robin.jpg" />
+			</div>
+			<h2 class="teamname">Robin Appelman</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Software Engineer</h3>
+				<h4 class="teamexpertise">Filesystem & scalability</h4>
+				<h4 class="teamlocation">lives in Utrecht, Netherlands</h4>
+				<p class="teamsocial"><a href="http://blog.jospoortvliet.com/"><i class="icon-rss icon"></i></a> <a href="https://github.com/jospoortvliet"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/jospoortvliet"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">Robin studied computer science and is highly experienced in scaling file storage and handling systems. He has been working on Nextcloud code for half a decade and has, at one point or another, touched code all over.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/vanessa.jpg" />
+			</div>
+			<h2 class="teamname">Vanessa Steeg</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Project Manager</h3>
+				<h4 class="teamexpertise">Marketing, Media design and project management</h4>
+				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
+				<p class="teamsocial"><!--<a href="http://blog.jospoortvliet.com/"><i class="icon-rss icon"></i></a>--> <!--<a href="https://github.com/jospoortvliet"><i class="icon-github-alt icon"></i></a>--> <!--<a href="https://twitter.com/vanesteeg"><i class="icon-twitter icon"></i></a>--> &nbsp;</p>
+			</div>
+			<p class="teamdescription">Vanessa has a degree in Media Design and qualifications as Executive Assistant in various languages (German, English and Spanish). Her experience ranges from project management to graphic design, marketing and communication.</p>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/roeland.jpg" />
+			</div>
+			<h2 class="teamname">Roeland Douma</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Software Engineer</h3>
+				<h4 class="teamexpertise">Desktop Client, sharing</h4>
+				<h4 class="teamlocation">lives in Zuidoostbeemster, Netherlands</h4>
+				<p class="teamsocial"><a href="http://rullzer.com/"><i class="icon-rss icon"></i></a> <a href="https://github.com/rullzer"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/rullzer"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">Roeland is awesome.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/simon.jpg" />
+			</div>
+			<h2 class="teamname">Simon Eisenmann</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Software Engineer</h3>
+				<h4 class="teamexpertise">Spreedbox, packaging, embedded</h4>
+				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
+				<p class="teamsocial"><a href="https://www.stdin.xyz/"><i class="icon-rss icon"></i></a> <a href="https://github.com/longsleep"><i class="icon-github-alt icon"></i></a> <!--<a href="https://twitter.com/rullzer"><i class="icon-twitter icon"></i></a>--> </p>
+			</div>
+			<p class="teamdescription">Simon is awesome.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/lukas.jpg" />
+			</div>
+			<h2 class="teamname">Lukas Reschke</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Security lead</h3>
+				<h4 class="teamexpertise">Security</h4>
+				<h4 class="teamlocation">lives in bla, Switzerland</h4>
+				<p class="teamsocial"><a href="https://statuscode.ch/"><i class="icon-rss icon"></i></a> <a href="https://github.com/LukasReschke"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/LukasReschke"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">Lukas Reschke is awesome.</p>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/morris.jpg" />
+			</div>
+			<h2 class="teamname">Morris Jobke</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Support and Software Engineer</h3>
+				<h4 class="teamexpertise">Supprt</h4>
+				<h4 class="teamlocation">lives in bla, Germany</h4>
+				<p class="teamsocial"><a href="http://morrisjobke.de/"><i class="icon-rss icon"></i></a> <a href="https://github.com/MorrisJobke"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/MorrisJbk"><i class="icon-twitter icon"></i></a> </p>
+			</div>
+			<p class="teamdescription">Morris Jobke is awesome.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/ivan.jpg" />
+			</div>
+			<h2 class="teamname">Ivan</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Software Engineer</h3>
+				<h4 class="teamexpertise">iOS, Monitoring</h4>
+				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
+				<p class="teamsocial"><!--<a href="https://www.stdin.xyz/"><i class="icon-rss icon"></i></a> <a href="https://github.com/longsleep"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/rullzer"><i class="icon-twitter icon"></i></a>--> &nbsp;</p>
+			</div>
+			<p class="teamdescription">Ivan is awesome.</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		 &nbsp;
+		<div class="teamp">
+			<div class="teamimg">
+				<img class="img-responsive img-circle" alt="What I look like when not eaten by a shark" src="<?php bloginfo('template_directory'); ?>/assets/img/people/brian.jpg" />
+			</div>
+			<h2 class="teamname">Brian</h2>
+			<div class="teammeta">
+				<h3 class="teamtitle">Support Engineer</h3>
+				<h4 class="teamexpertise">Spreed.ME</h4>
+				<h4 class="teamlocation">lives in Stuttgart, Germany</h4>
+				<p class="teamsocial"><!--<a href="https://www.stdin.xyz/"><i class="icon-rss icon"></i></a> <a href="https://github.com/longsleep"><i class="icon-github-alt icon"></i></a> <a href="https://twitter.com/rullzer"><i class="icon-twitter icon"></i></a>--> &nbsp;</p>
+			</div>
+			<p class="teamdescription">Brian is awesome.</p>
 		</div>
 	</div>
 </div>
