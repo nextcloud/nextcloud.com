b338df5681def95898dabb5e32984f878c4004aa
Jos Poortvliet <jospoortvliet@gmail.com>
2016-08-23 12:56:47 +0200

    feedback from Phil

--- page-workflow.php
+++ page-workflow.php
@@ -15,13 +15,12 @@
 		</div>
 		<div class="col-md-6">
 			<h2 class="featuretitle">Define automated Workflows and retention</h2>
-			<p class="">The Workflow engine expands these capabilities, enabling administrators to start any kind of actions based on these triggers, for example converting certain file types to PDF upon upload or emailing files put in a specified folder with a given tag to a given mail address.</p>
+			<p class="">The Workflow engine expands these capabilities, enabling administrators to start any kind of actions based on these triggers, for example converting document file types to PDF upon upload by members of a specified group or emailing files put in a specified folder with a given tag to a given mail address.</p>
 			<p>File retention and deletion can also be controlled based on tags set manually or automatically, ensuring legal or practical requirements for the longevity of data lifespans can be enforced.</p>
 		</div>
 	</div>
 </div>
 
-<a name="#federation"></a>
 	<h2 class="">Monitoring your Nextcloud server</h2>
 <div class="row">
 	<div class="col-md-4">
@@ -30,7 +29,7 @@
 	</div>
 	<div class="col-md-8">
 		<p>The Server Information app provides a way for admins to monitor the health and performance of a Nextcloud system. Besides the graphical UI, an API endpoint provided makes it possible for system administrators to import this data in their monitoring app so they can keep an eye on Nextcloud operations from the same place they monitor the rest of their infrastructure.</p>
-		<p>The <a href="http://opennms.org/">openNMS monitoring app</a> has already support for monitoring Nextcloud 10 systems and the template can be easily modified for other tools like Nagios.</p>
+		<p>The <a href="http://opennms.org/">openNMS monitoring app</a> already has support for monitoring Nextcloud 10 systems and the template can be easily modified for other tools like Nagios.</p>
 	</div>
 </div>
 
