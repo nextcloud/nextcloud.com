cd2cff75f257fd785a6808a9e3ee59fcd35bdb18
Jos Poortvliet <jospoortvliet@gmail.com>
2016-08-25 11:24:04 +0200

    one more!

--- page-workflow.php
+++ page-workflow.php
@@ -1,5 +1,5 @@
 <div class="featurerow">
-	<h1 class="featuretitle">File Access Control and Monitoring</h1>
+	<h1 class="featuretitle">File Access Control and Workflows</h1>
 	<div class="row">
 		<div class="col-md-6 featureblock">
 			<h2 class="featuretitle">Access on your terms</h2>
@@ -19,16 +19,28 @@
 			<p>File retention and deletion can also be controlled based on tags set manually or automatically, ensuring legal or practical requirements for the longevity of data lifespans can be enforced.</p>
 		</div>
 	</div>
-	<a name="monitoring">
-	<h2 class="">Monitoring your Nextcloud server</h2>
 	<div class="row">
-		<div class="col-md-4">
-	<!-- 		<a href="https://www.youtube.com/watch?v=9-JEmlH2DEg" target="_blank" style="border: none;"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/federation/federation.gif" alt="in action" /></a> -->
-			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/monitoring.png"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/monitoring-wee.png" alt="in action" /></a>
+		<div class="col-md-6">
+			<h2 class="featuretitle">File access control at home</h2>
+			<p class="">Home users will find that the File Access Control app and other workflow tools can be used to prevent accidental sharing of sensitive data, adding an additional layer of protection to Nextcloud.</p>
+			<p>The example on the right ensures that files with the "protected" tag are not accessible outside the home network nor synced by the clients. <a href="https://nextcloud.com/?p=869" target="_blank">Learn more in our blog</a>.</p>
 		</div>
+		<div class="col-md-6">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/FileAccessControl-at-home.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/FileAccessControl-at-home.png" class="img-responsive featureimg" /></a>
+		</div>
+	</div>
+</div>
+	<a name="monitoring"></a>
+<div class="featurerow">
+	<h1 class="featuretitle">Monitoring your Nextcloud server</h1>
+	<div class="row">
 		<div class="col-md-8">
 			<p>The Server Information app provides a way for admins to monitor the health and performance of a Nextcloud system. Besides the graphical UI, an API endpoint provided makes it possible for system administrators to import this data in their monitoring app so they can keep an eye on Nextcloud operations from the same place they monitor the rest of their infrastructure.</p>
 			<p>The <a href="http://opennms.org/" target="_blank">openNMS monitoring app</a> already has support for monitoring Nextcloud 10 systems and <a href="https://github.com/opennms-config-modules/nextcloud" target="_blank">the configuration module</a> can be easily modified for other tools like Nagios.</p>
+		</div>
+		<div class="col-md-4">
+	<!-- 		<a href="https://www.youtube.com/watch?v=9-JEmlH2DEg" target="_blank" style="border: none;"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/federation/federation.gif" alt="in action" /></a> -->
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/monitoring.png"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/monitoring-wee.png" alt="in action" /></a>
 		</div>
 	</div>
 	<p>The app lets you track:
@@ -41,8 +53,11 @@
 	</ul>
 </div>
 
+</div></div>
+
+<div class="wrap container featuregroup">
 <div class="featurerow">
-	<h2>screenshots</h2>
+	<h1 class="featuretitle">Screenshots</h1>
 	<div class="row">
 		<div class="col-md-6">
 			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/monitoring-full.png"><img class="img-responsive featureimg" src="<?php bloginfo('template_directory'); ?>/assets/img/features/monitoring-full.png" /></a>
