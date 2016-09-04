ad83111b9da9630031f7801e6db771df672c2086
Jos Poortvliet <jospoortvliet@gmail.com>
2016-09-04 21:57:35 +0200

    new anon

--- page-team.php
+++ page-team.php
@@ -195,7 +195,7 @@
 	</div>
 	<div class="col-md-4">
 		<div class="teamp">
-			<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
+			<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person2.jpg" />
 			<h2 class="teamname">Leon Klingele</h2>
 			<div class="teammeta">
 				<h3 class="teamtitle">Software Engineer</h3>
