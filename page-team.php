0a14ed88a6f1c4dac9438856a5eb3ddab245a8bc
Jos Poortvliet <jospoortvliet@gmail.com>
2016-09-04 22:14:21 +0200

    fix pic

--- page-team.php
+++ page-team.php
@@ -221,7 +221,7 @@
 	</div>
 	<div class="col-md-4">
 		<div class="teamp">
-			<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/morris.jpg" />
+			<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/joachim.jpg" />
 			<h2 class="teamname">Joachim</h2>
 			<div class="teammeta">
 				<h3 class="teamtitle">Software Engineer</h3>
