d94644438762252fdf833f9901297249ddb17867
Jos Poortvliet <jospoortvliet@gmail.com>
2016-09-15 20:26:14 +0200

    1st Nc Box page

---
+++ page-box.php
@@ -0,0 +1,65 @@
+<div class="row conf-header">
+	<div class="col-sm-8 col-sm-offset-2">
+		<img class="conf-img img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/pidrive/box.jpg" alt="" />
+		<h1 class="box-header text-center">Introducing the Nextcloud Box</h2>
+	</div>
+</div>
+</div>
+</div>
+<div class="featurerow">
+	<div class="wrap container featuregroup">
+	<div class="row conf-info">
+		<div class="col-sm-5">
+			<h1 class="text-center"><i class="icon-hdd-o"></i></br> Bring your data home</h1>
+			<p>The Nextcloud Box is your private cloud. It stores your data and enables you to sync between your devices and share with others.</p>
+			<img class="conf-img img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/features/serverwebui.png" alt="Web UI" />
+			<div class="text-center morebuttondiv">
+					<a href="/features" class="btn btn-primary morebutton" target="_blank">Learn more about Nextcloud! <i class="icon-arrow-circle-o-right icon"></i></a>
+			</div>
+		</div>
+		<div class="col-sm-5 col-sm-offset-2">
+			<h1 class="text-center"><i class="icon-code-fork"></i></br> Technology</h1>
+			<p>The Nextcloud box consists of the following parts:</p>
+			<ul>
+				<li>1 TB USB3 hard drive from WDLabs</li>
+				<li>Nextcloud case with room for the drive and a compute board</li>
+				<li>microUSB charger, cables and adapters, a screw driver and screws</li>
+				<li>microSD card with <a href="https://developer.ubuntu.com/en/snappy/" target="_blank">Snappy Ubuntu Core</a> as OS</li>
+				<li>Apache, MySQL and the latest Nextcloud 10 pre-installed and ready to go</li>
+			</ul>
+			<p>The Box is compatible with the Raspberry Pi 2, which you need to supply yourself. The Box can also fit the Raspberry Pi 3 and the oDroid C2. These will be supported by the software in a future release.</p>
+			<img class="conf-img img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/pidrive/box-open.jpg" alt="contents" />
+		</div>
+	</div>
+	</div>
+</div>
+<div class="text-center morebuttondiv">
+	<h1 class="text-center"><i class="icon-hdd-o"></i></br> Get the Box</h1>
+	<p>Currenly you can only purchase the Nextcloud Box in Europe and the USA. We're working on bringing the box to other regions!</p>
+					<a href="http://store.wdc.com/promo/4822802800" class="btn btn-primary morebutton" target="_blank">Buy in Europe <i class="icon-arrow-circle-o-right icon"></i></a>
+					<a href="http://store.wdc.com/promo/4822802900" class="btn btn-primary morebutton" target="_blank">Buy in the USA <i class="icon-arrow-circle-o-right icon"></i></a>
+</div>
+
+<div class="wrap container">
+<div class="row conf-partners-title">
+					<img class="conf-img img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/pidrive/wrap.jpg" alt="contents" />
+	<div class="col-md-12">
+		<h2>Partners</h2>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-12 col-sm-4 col-md-3">
+		<a target="_blank" href="https://developer.ubuntu.com/en/snappy/"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/pidrive/snappy.svg" alt="Snappy Ubuntu Core" /></a>
+	</div>
+	<div class="col-xs-12 col-sm-4 col-md-3">
+		<a target="_blank" href="http://wdlabs.wd.com/products/wd-pidrive-1tb-kit/"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/pidrive/wdlabs.png" alt="Visit lwn.net" /></a>
+	</div>
+</div>
+<div class="row conf-partners-title">
+	<div class="col-md-12">
+		<h2>How to Box</h2>
+		<p>More information about using the Nextcloud Box is coming soon! Stay tuned.</p>
+	</div>
+</div>
+<div class="row">
+</div>
