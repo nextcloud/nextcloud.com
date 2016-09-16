8654236e16172400770606ec47ae2dfe925c1f0b
Jos Poortvliet <jospoortvliet@gmail.com>
2016-09-16 12:54:12 +0200

    add Box press release

---
+++ page-pr20160916.php
@@ -0,0 +1,47 @@
+<h1>Nextcloud Box – a private cloud and IoT solution for home users – from Nextcloud, Canonical and WDLabs</h1>
+<h3>Companies launch Raspberry Pi-based device to enable consumers greater control over their data</h3>
+<p><strong>Stuttgart, Germany, September 16, 2016</strong> Nextcloud, Canonical and WDLabs, are today launching the Nextcloud Box, a secure, private, self-hosted cloud and Internet of Things (IoT) platform giving consumers a way to take back control over their personal data. Nextcloud Box makes hosting a personal cloud simple and cost effective whilst maintaining a secure private environment that can be expanded with additional features via apps.</p>
+
+<p>The Nextcloud Box consists of a hard drive and a case, complemented by a Raspberry Pi 2 or a similar credit-card sized computer. The pre-configured, easy-to-use platform handles file storage and syncing, communication and more, requires no maintenance and enables users to install more functionality through apps like Spreed, OpenHab and Collabora Online. The box offers 1TB of storage at the price point of Eur 70. For information on where to buy please visit nextcloud.com/box</a></p>
+
+<p><em>"WDLabs  is very fortunate to have connected with our Nextcloud and Canonical teammates, and have found it to be a perfect fit of complementary capabilities coupled with high-speed co-development,"</em>, said Dave Chew, Chief Engineer, WDLabs. <em>"We’ve combined our latest technologies to create an integrated hardware/software platform for the Nextcloud community focused on ease of use, extensibility and affordability."</em></p>
+
+<p><em>"We have always believed that collaboration brings out the best in communities and companies alike,"</em> said Jane Silber, CEO at Canonical. <em>"Together with WDLabs and Nextcloud we are able to bring the first Ubuntu Core-enabled device, as an app-enabled IoT gateway, to the market and to people’s homes."</em></p>
+
+<p><em>"It has been a great co-operation with amazingly agile teams at Canonical and WDLabs,"</em> said Frank Karlitschek, Founder and Managing Director at Nextcloud. <em>"Empowering home users to sync and share their data with a secure, privacy preserving solution built on a combination of cutting edge software and hardware platforms is no panacea and we’re proud to have delivered together!"</em></p>
+
+<p>Key WD components including the WD PiDrive and SanDisk SD card deliver a device with the flexibility and openness to enable learning and experimenting, with easy installation and allowing replacement of components, adding functionality and capabilities like more storage space, networking interfaces and other addons.</p>
+
+<p>The base with Ubuntu Core enables the device to act as an extensible IoT gateway at home, adding functionality and controlling other devices and connecting them with their owner while keeping the user secure through automatic, unattended updates.</p>
+
+<p>The Nextcloud software empowers users to sync and share their data while remaining in control and accessing and collaborating through a range of devices and platforms, from browsers and mobile phones to desktops and laptops. The federation capabilities make it possible to share between users on different Nextcloud servers, participating in and creating a network beyond the single device.</p>
+
+<p>As it is, the Nextcloud Box addresses one segment of the market. However, all three companies have technology and the ability to scale and integrate this to solution in other products and the team is looking for such opportunities.</p>
+
+<p>For more information on the product, please visit: <a href="https://nextcloud.com/box">nextcloud.com/box</a></p>
+
+<p>About Nextcloud</br />
+Nextcloud offers a unique-in-the-industry fully open source solution for on-premise data handling and communication with a uncompromising focus on security and privacy. Nextcloud brings together universal access to data with next-generation secure communication and collaboration capabilities under direct control of IT and integrated with existing compliant infrastructure. Nextcloud's open, modular architecture, emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing assets within and across the borders of their organization. For more information, visit https://nextcloud.com or follow @Nextclouders on Twitter.</p>
+
+<p>About Canonical</br />
+Canonical is the company behind Ubuntu, the leading Operating System for cloud and the Internet of Things. Most public cloud workloads are running Ubuntu, and most new smart gateways, self-driving cars and advanced humanoid robots are running Ubuntu as well. Canonical provides enterprise support and services for commercial users of Ubuntu.</p>
+
+<p>Canonical leads the development of the <a href="http://snapcraft.io/">snap universal Linux packaging system</a> for secure, transactional device updates and app stores. Ubuntu Core is an all-snap OS, perfect for devices and appliances. Established in 2004, Canonical is a privately held company.</p>
+
+<p>About WD Labs</br />
+<a href="http://wdlabs.wd.com/">WDLabs™</a> is a business growth incubator of storage solutions leader <a href="http://www.wdc.com/en/">Western Digital Corporation</a> (NASDAQ:WDC). Western Digital is an industry-leading provider of storage technologies and solutions that enable people to create, leverage, experience and preserve data. The company addresses ever-changing market needs by providing a full portfolio of compelling, high-quality storage solutions with customer-focused innovation, high efficiency, flexibility and speed. Our products are marketed under the HGST, SanDisk and WD brands to OEMs, distributors, resellers, cloud infrastructure providers and consumers. Financial and investor information is available on the company's Investor Relations website at <a href="https://investor.wdc.com">investor.wdc.com</a></p>
+
+<p><strong>Links:</strong><br />
+About Nextcloud: <a href="https://nextcloud.com/about">nextcloud.com/about</a><br />
+About the Nextcloud Box: <a href="https://nextcloud.com/box">nextcloud.com/box</a><br />
+About WDLabs: <a href="https://wdlabs.wd.com">wdlabs.wd.com</a><br />
+About Canonical: <a href="https://canonical.com">canonical.com</a><br />
+About Snapcraft: <a href="http://snapcraft.io/">snapcraft.io</a><br />
+<br />
+</p>
+<p><strong>Press contacts</strong><br />
+Nextcloud GmbH<br />
+Jos Poortvliet<br />
+Kronenstraße 22A<br />
+D-70173 Stuttgart, Germany<br />
+e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
