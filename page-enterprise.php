73d3e9b46e8585369b5648f03a1fbb987a57cd17
Jos Poortvliet <jospoortvliet@gmail.com>
2016-09-27 14:05:04 +0200

    Discounts and FAQ

--- page-enterprise.php
+++ page-enterprise.php
@@ -3,24 +3,22 @@
 		<div class="col-md-6 topheader">
 			<h1>Enterprise Support Subscriptions</h1>
 			<h2>An enterprise subscription provides you the confidence of knowing that your Nextcloud deployment is backed by the best available technical expertise and with the features you need for your organization</h2>
-			<p><a class="btn btn-primary btn-lg btn-absolute-icon" href="<?php echo home_url('install') ?>" role="button" id="get-nextcloud-button">Get Nextcloud <span class="icon-arrow"></span></a></p>
 		</div>
 	</div>
 </div>
 
 <div class="container pricing">
 	<div class="row">
-	<h1 class="header">Plans and Pricing</h1>
+		<h1 class="header">Plans and Pricing</h1>
 		<div class="col-md-4 table-column">
 			<div class="table-header basic">
 				<h1>Basic</h1>
-				<h2>From 1500E/year</h2>
+				<h2>From 1500€/year</h2>
 			</div>
 			<div class="table-content">
 				<h1>Supported Capabilities</h1>
 				<div class="line"></div>
 				<div class="list">
-				<ul>
 					<li class="check">File sync and share</li>
 					<li class="check">Mobile and desktop clients</li>
 					<li class="check">Enterprise capabilities</li>
@@ -29,100 +27,160 @@
 					<li class="nocheck">Secure WebRTC Conferencing</li>
 					<li class="nocheck">SIP Gateway</li>
 					<li class="nocheck">Collabora Online Office</li>
-				</ul>
 				</div>
 				<h1>Support</h1>
 				<div class="line"></div>
-				<div> class="list">
-				<ul>
-					<li class="check">File sync and share</li>
-					<li class="check">Mobile and desktop clients</li>
-					<li class="check">Enterprise capabilities</li>
-					<li class="check">File sync and share</li>
-					<li class="check">Mobile and desktop clients</li>
-					<li class="check">Enterprise capabilities</li>
-					<li class="check">File sync and share</li>
-				</ul>
-				</div>		
+				<div class="list">
+					<li class="check">Production Support SLA: 3 business days</li>
+					<li class="check">Portal support</li>
+					<li class="check">ownCloud™ migration support</li>
+					<li class="check">Email support</li>
+					<li class="nocheck">Phone support</li>
+					<li class="nocheck">Custom consulting</li>
+					<li class="nocheck">Clustering support</li>
+					<li class="check">Maintenance life cycle: 1 year</li>
+				</div>
+				<a class="btn btn-primary btn-lg btn-absolute-icon" href="/enterprise/buy" role="button" id="get-nextcloud-button">BUY IT <span class="icon-arrow"></span></a>
 			</div>
 		</div>
 		<div class="col-md-4 table-column">
 			<div class="table-header standart">
-				<h1>Standart</h1>
-				<h2>From 2500E/year</h2>
+				<h1>Standard</h1>
+				<h2>From 2500€/year</h2>
 			</div>
 			<div class="table-content">
 				<h1>Supported Capabilities</h1>
 				<div class="line"></div>
 				<div class="list">
-				<ul>
 					<li class="check">File sync and share</li>
 					<li class="check">Mobile and desktop clients</li>
 					<li class="check">Enterprise capabilities</li>
-					<li class="nocheck">Calendar and Contacts</li>
+					<li class="check">fully Open Source</li>
+					<li class="check">Calendar and Contacts</li>
 					<li class="nocheck">Secure WebRTC Conferencing</li>
 					<li class="nocheck">SIP Gateway</li>
 					<li class="nocheck">Collabora Online Office</li>
-				</ul>
 				</div>
 				<h1>Support</h1>
 				<div class="line"></div>
-				<div> class="list">
-				<ul>
-					<li class="check">File sync and share</li>
-					<li class="check">Mobile and desktop clients</li>
-					<li class="check">Enterprise capabilities</li>
-					<li class="check">File sync and share</li>
-					<li class="check">Mobile and desktop clients</li>
-					<li class="check">Enterprise capabilities</li>
-					<li class="check">File sync and share</li>
-				</ul>
-				</div>		
+				<div class="list">
+					<li class="check">Production Support SLA: 2 business days</li>
+					<li class="check">Portal support</li>
+					<li class="check">ownCloud™ migration support</li>
+					<li class="check">Email support: business hours</li>
+					<li class="check">Phone support: business hours</li>
+					<li class="check">Custom consulting</li>
+					<li class="nocheck">Clustering support</li>
+					<li class="check">Maintenance life cycle: 3 years</li>
+				</div>
+				<a class="btn btn-primary btn-lg btn-absolute-icon" href="/enterprise/buy" role="button" id="get-nextcloud-button">BUY IT <span class="icon-arrow"></span></a>
 			</div>
 		</div>
 		<div class="col-md-4 table-column">
 			<div class="table-header premium">
 				<h1>Premium</h1>
-				<h2>From 3250E/year</h2>
+				<h2>From 3250€/year</h2>
 			</div>
 			<div class="table-content">
 				<h1>Supported Capabilities</h1>
 				<div class="line"></div>
 				<div class="list">
-				<ul>
 					<li class="check">File sync and share</li>
 					<li class="check">Mobile and desktop clients</li>
 					<li class="check">Enterprise capabilities</li>
-					<li class="nocheck">Calendar and Contacts</li>
-					<li class="nocheck">Secure WebRTC Conferencing</li>
-					<li class="nocheck">SIP Gateway</li>
-					<li class="nocheck">Collabora Online Office</li>
-				</ul>
+					<li class="check">fully Open Source</li>
+					<li class="check">Calendar and Contacts</li>
+					<li class="check">Secure WebRTC Conferencing</li>
+					<li class="check">SIP Gateway</li>
+					<li class="check">Collabora Online Office</li>
 				</div>
 				<h1>Support</h1>
 				<div class="line"></div>
-				<div> class="list">
-				<ul>
-					<li class="check">File sync and share</li>
-					<li class="check">Mobile and desktop clients</li>
-					<li class="check">Enterprise capabilities</li>
-					<li class="check">File sync and share</li>
-					<li class="check">Mobile and desktop clients</li>
-					<li class="check">Enterprise capabilities</li>
-					<li class="check">File sync and share</li>
-				</ul>
-				</div>		
+				<div class="list">
+					<li class="check">Production Support SLA: 24h or as agreed</li>
+					<li class="check">Portal support</li>
+					<li class="check">ownCloud™ migration support</li>
+					<li class="check">Email support</li>
+					<li class="check">Phone support</li>
+					<li class="check">Custom consulting</li>
+					<li class="check">Clustering support</li>
+					<li class="check">Maintenance life cycle: 5+5 year</li>
+				</div>
+				<a class="btn btn-primary btn-lg btn-absolute-icon" href="/enterprise/buy" role="button" id="get-nextcloud-button">BUY IT <span class="icon-arrow"></span></a>
 			</div>
 		</div>
 	</div>
 </div>
 
+<div class="container discounts">
+	<div class="row">
+		<h1 class="header">Discounts</h1>
+		<div class="col-md-5 col-md-offset-1 box">
+		<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/enterprise/publicsector.svg">
+		<h1>Public Sector</h1>
+		<p>We provide interesting discount options to organizations in the public sector.</p>
+		<p><a href="/enterprise/buy">Contact us</a></p>
+		</div>
+		<div class="col-md-5 col-md-offset-1 box">
+		<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/enterprise/education.svg">
+		<h1>Education</h1>
+		<p>Organizations in the educational sector receive a significant discount on all subscriptions</p>
+		<p><a href="/enterprise/buy">Contact us</a></p>	</div>
+	</div>
+</div>
 
-
-
-
-
-
+<div class="faq container-fluid">
+			<div class="text-center">
+				<h1>Frequently asked questions</h1>
+			</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<div class="downarrow"><a href="#why_pay" data-toggle="collapse"><h3>Why would I pay for open source software?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="why_pay" class="collapse">
+					<p>Nextcloud is great out of the box. But installing and running a server in a secure and reliable manner and for hundreds or thousands of users, important for a mission critical environment, is not always easy. You probably want access to an expert if something does not function as expected. We provide not only that but also pro-active support with installation and security issues, we reach out when we do feature planning to ensure your needs are served and we support Nextcloud long after you would otherwise be forced to upgrade for security, performance and stability reasons.</p>
+					<p>By offering more in-depth knowledge than anybody else, we give you the confidence you need to be able to rely on Nextcloud.</p>
+				</div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#languages" data-toggle="collapse"><h3>In what languages do I get support?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="languages" class="collapse">
+					<p>We provide support in German and English and other languages through our partners, <a href="#contact">contact us</a> for details.</p>
+				</div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#howbuy" data-toggle="collapse"><h3>How do I buy Nextcloud subscriptions?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="howbuy" class="collapse">
+					<p>Just contact us or one of our partners. Our partners will help you set up Nextcloud and they act as your first point of contact.</p>
+				</div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#partner" data-toggle="collapse"><h3>How do I become a Nextcloud partner?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="partner" class="collapse">
+					<p>We are always looking for competent partners who share our passion for making customers happy! You can join our ecosystem, <a href="/contact">contact us for more information</a>.</p>
+				</div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#sourcerelease" data-toggle="collapse"><h3>Do I have to release the source code of systems Nextcloud connects to?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="sourcerelease" class="collapse">
+					<p>No. Nextcloud will communicate with your back-end systems over external APIs. Those systems will not become a derivative work of Nextcloud just because they communitcate with it. Therefore you can keep the source code of your back-end systems private and don't have to share it with anyone.</p>
+				</div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#logos" data-toggle="collapse"><h3>Are my logos or the company name affected by the AGPL?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="logos" class="collapse">
+					<p>No. Logos, company names, etc. are protected by trademarks. The GNU AGPLv3 is a copyright license and don't affect any trademarks. If you put your logo on your Nextcloud it is considered merely as data used by Nextcloud and has no effect on licenses or trademarks.</p></div>
+				<div class="line">
+				</div>
+				<div class="downarrow"><a href="#pubsource" data-toggle="collapse"><h3>Do I need to publish the source code if I use Nextcloud?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="pubsource" class="collapse">
+					<p>No. As long as you didn't make any modifications to Nextcloud, make modifications to existing apps or write your own apps, you don't have to do anything. If you did make modifications, then you have to make that code available under the GNU AGPLv3 or a compatible license to the users of the system. The easiest way to do so is to provide a download link. In any case, Nextcloud GmbH is happy to assist you if you need more information on how to distribute your code.</p>
+				</div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#sellapp" data-toggle="collapse"><h3>Can I sell my Nextcloud App?<span class="icon-arrow-down"></span></h3></a></div>
+				<div id="sellapp" class="collapse">
+					<p>Yes. Nextcloud is licensed under the GNU AGPLv3. This license only defines the rights you have to give the users of the software, but not how they get the software. You can decide freely if you want to sell the software in an app store or through any other channel, or if you want to make the app publicly available. The only requiremnt is, that you license your app under the AGPLv3 or any compatible license.</p>
+				</div>
+				<div class="line"></div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 
