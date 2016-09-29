b5b81607abeefbce98570ee2e220b795561d9661
Jos Poortvliet <jospoortvliet@gmail.com>
2016-09-29 12:01:58 +0200

    DeiC moves to Nextcloud

---
+++ page-pr20160928.php
@@ -0,0 +1,30 @@
+<h1>Danish research and education network moves from ownCloud to Nextcloud</h1>
+<p><strong>Lyngby, Denmark and Stuttgart, Germany, September 28</strong> DeiC and Nextcloud announce their collaboration on a large scale migration of their several years old ownCloud deployment to Nextcloud 10. DeIC, Danish e-Infrastructure Cooperation, supports the development of Denmark as an eScience nation through the delivery of e-infrastructure, guidance, and initiatives at national level. Through DeIC, the research institutes and universities of Denmark will use Nextcloud to facilitate sharing and collaboration between staff and students.</p>
+
+<p><em>"We require a truly scalable and 100% open source enterprise solution from a reliable partner"</em>, said Dave Chew, Chief Engineer, WDLabs. <em>"We’ve combined our latest technologies to create an integrated hardware/software platform for the Nextcloud community focused on ease of use, extensibility and affordability."</em></p>
+
+<p><em>"We have always believed that collaboration brings out the best in communities and companies alike,"</em> said Martin Bech, Head of NREN at DeIC. <em>"so we decided to migrate our data.deic.dk service from ownCloud to Nextcloud 10"</em></p>
+
+<p><em>"We are very pleased with the addition of DeIC to our customer base in the educational market"</em> said Frank Karlitschek, Founder and Managing Director at Nextcloud. <em>"Their trust in our service and support is a strong validation of our more sustainable and long-term focused business model."</em></p>
+
+<p>Key WD components including the WD PiDrive and SanDisk SD card deliver a device with the flexibility and openness to enable learning and experimenting, with easy installation and allowing replacement of components, adding functionality and capabilities like more storage space, networking interfaces and other addons.</p>
+
+<p>As provider for educational and research institutions, DeIC requires a fully open source solution from a business partner with proven security and scalability expertise and a long term support offering. Unique features like a high quality SAML implementation and monitoring tooling were critical in choosing Nextcloud as supplier.</p>
+
+<p>About Nextcloud</br />
+Nextcloud offers a unique-in-the-industry fully open source solution for on-premise data handling and communication with a uncompromising focus on security and privacy. Nextcloud brings together universal access to data with next-generation secure communication and collaboration capabilities under direct control of IT and integrated with existing compliant infrastructure. Nextcloud's open, modular architecture, emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com/about">nextcloud.com</a> or follow <a href="https://twitter.com/nextclouders">@Nextclouders on Twitter</a>.</p>
+
+<p>About DeIC</br />
+DeIC – Danish e-Infrastructure Cooperation has the stated aim of supporting the development of Denmark as an eScience nation through the delivery of e-infrastructure (computing, data warehousing, network connections and auxiliary services), guidance, and initiatives at national level. DeIC is a virtual unit under the Danish Ministry of Higher Education and Science and the result of an agreement concluded between the eight Danish universities and the Danish Agency for Science, Technology and Innovation. For details, see <a href="https://www.deic.dk">www.deic.dk</a>.</p>
+
+<p><strong>Links:</strong><br />
+About Nextcloud: <a href="https://nextcloud.com/about">nextcloud.com/about</a><br />
+About DeiC: <a href="https://www.deic.dk/about-deic?language=en">www.deic.dk/about-deic?language=en</a><br />
+<br />
+</p>
+<p><strong>Press contacts</strong><br />
+Nextcloud GmbH<br />
+Jos Poortvliet<br />
+Kronenstraße 22A<br />
+D-70173 Stuttgart, Germany<br />
+e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
