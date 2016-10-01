e8d43f601dbeeb2ccec4234e6e1e59ef0c53a6bd
Jos Poortvliet <jospoortvliet@gmail.com>
2016-10-01 14:53:14 +0200

    viewport change from Jan

--- templates/head.php
+++ templates/head.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
 	<meta itemprop="image" content="https://nextcloud.com/wp-content/themes/nextcloudorgnew/assets/img/home/ncsplash.png">
