7701f63f690e69fd3c6b639794d2a12ec43323a6
Jan-Christoph Borchardt <hey@jancborchardt.net>
2016-10-09 00:47:46 +0200

    use dash instead of pipe in title, as we do

--- templates/head.php
+++ templates/head.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php wp_title('|', true, 'right'); ?></title>
+	<title><?php wp_title('-', true, 'right'); ?></title>
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
