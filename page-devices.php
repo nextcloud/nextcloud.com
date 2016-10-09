82ad2f8c7e789eabc8d5c4c019702eec81f95050
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-10-09 00:07:39 -0400

    Removing extra copy-paste

--- page-devices.php
+++ page-devices.php
@@ -54,22 +54,3 @@
 		<div class="text-center morebuttondiv"> <a href="http://syncloud.org" class="btn btn-primary morebutton" target="_blank">Learn more! <i class="icon-arrow-circle-o-right icon"></i></a></div>
 	</div>
 </div>
-<div class="row">
-	<div class="col-md-6">
-		<img class="device-img img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/pidrive/box.jpg" alt="" />
-		<h1 class="text-center">The Nextcloud Box</h1>
-		<p>The Nextcloud box comes preinstalled with Nextcloud, running on <a href="https://developer.ubuntu.com/en/snappy/" target="_blank">Snappy Ubuntu Core</a> as OS. The hardware:</p>
-		<ul>
-			<li>1 TB USB3 hard drive from WDLabs</li>
-			<li>Nextcloud case with room for the drive and a compute board</li>
-			<li>microUSB charger, cables and adapters, a screw driver and screws</li>
-		</ul>
-		<p>The Box is compatible with the Raspberry Pi 2 <strong>(not included)</strong>. Support for other boards is coming.</p>
-		<p>The Nextcloud Box starts at EUR 70 / USD 79.99</p>
-		<div class="text-center morebuttondiv">
-				<a href="/box" class="btn btn-primary morebutton" target="_blank">Learn more! <i class="icon-arrow-circle-o-right icon"></i></a>
-		</div>
-	</div>
-	<div class="col-md-6">
-	</div>
-</div>
