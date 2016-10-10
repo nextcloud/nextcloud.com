0b3325e4c16f272df2aa246ddcaedd19ef4d80c9
Lukas Reschke <lukas@statuscode.ch>
2016-10-10 20:39:14 +0200

    Update advisories

--- advisories/server-list-part.php
+++ advisories/server-list-part.php
@@ -1,5 +1,6 @@
 <p>Version 10.0.1</p>
 <a href="/security/advisory/?id=nc-sa-2016-006">SMB User Authentication Bypass</a><br>
+<a href="/security/advisory/?id=nc-sa-2016-008">Stored XSS in CardDAV image export</a><br>
 <a href="/security/advisory/?id=nc-sa-2016-009">Stored XSS in Gallery application</a><br>
 <a href="/security/advisory/?id=nc-sa-2016-010">Content-Spoofing in "files" app</a><br>
 <a href="/security/advisory/?id=nc-sa-2016-011">Content-Spoofing in "dav" app</a><br>
