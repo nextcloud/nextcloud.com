21137ce172315b11a84a266f2592aab1516b18c0
Paulo Moura <paulomoura@Paulos-iMac.local>
2016-10-17 17:08:51 +0100

    [WIP] Initial Structure

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -109,3 +109,150 @@ $(document).ready(function() {
 	// Fade In animation 
 	$(".navbar-header").velocity('transition.fadeIn', 1000 );
 });
+
+
+
+
+// New Header
+
+jQuery(document).ready(function($){
+    function morphDropdown( element ) {
+        this.element = element;
+        this.mainNavigation = this.element.find('.main-nav');
+        this.mainNavigationItems = this.mainNavigation.find('.has-dropdown');
+        this.dropdownList = this.element.find('.dropdown-list');
+        this.dropdownWrappers = this.dropdownList.find('.dropdown');
+        this.dropdownItems = this.dropdownList.find('.content');
+        this.dropdownBg = this.dropdownList.find('.bg-layer');
+        this.mq = this.checkMq();
+        this.bindEvents();
+    }
+
+    morphDropdown.prototype.checkMq = function() {
+        //check screen size
+        var self = this;
+        return window.getComputedStyle(self.element.get(0), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "").split(', ');
+    };
+
+    morphDropdown.prototype.bindEvents = function() {
+        var self = this;
+        //hover over an item in the main navigation
+        this.mainNavigationItems.mouseenter(function(event){
+            //hover over one of the nav items -> show dropdown
+            self.showDropdown($(this));
+        }).mouseleave(function(){
+            setTimeout(function(){
+                //if not hovering over a nav item or a dropdown -> hide dropdown
+                if( self.mainNavigation.find('.has-dropdown:hover').length == 0 && self.element.find('.dropdown-list:hover').length == 0 ) self.hideDropdown();
+            }, 50);
+        });
+        
+        //hover over the dropdown
+        this.dropdownList.mouseleave(function(){
+            setTimeout(function(){
+                //if not hovering over a dropdown or a nav item -> hide dropdown
+                (self.mainNavigation.find('.has-dropdown:hover').length == 0 && self.element.find('.dropdown-list:hover').length == 0 ) && self.hideDropdown();
+            }, 50);
+        });
+
+        //click on an item in the main navigation -> open a dropdown on a touch device
+        this.mainNavigationItems.on('touchstart', function(event){
+            var selectedDropdown = self.dropdownList.find('#'+$(this).data('content'));
+            if( !self.element.hasClass('is-dropdown-visible') || !selectedDropdown.hasClass('active') ) {
+                event.preventDefault();
+                self.showDropdown($(this));
+            }
+        });
+
+        //on small screens, open navigation clicking on the menu icon
+        this.element.on('click', '.nav-trigger', function(event){
+            event.preventDefault();
+            self.element.toggleClass('nav-open');
+        });
+    };
+
+    morphDropdown.prototype.showDropdown = function(item) {
+        this.mq = this.checkMq();
+        if( this.mq == 'desktop') {
+            var self = this;
+            var selectedDropdown = this.dropdownList.find('#'+item.data('content')),
+                selectedDropdownHeight = selectedDropdown.innerHeight(),
+                selectedDropdownWidth = selectedDropdown.children('.content').innerWidth(),
+                selectedDropdownLeft = item.offset().left + item.innerWidth()/2 - selectedDropdownWidth/2;
+
+            //update dropdown position and size
+            this.updateDropdown(selectedDropdown, parseInt(selectedDropdownHeight), selectedDropdownWidth, parseInt(selectedDropdownLeft));
+            //add active class to the proper dropdown item
+            this.element.find('.active').removeClass('active');
+            selectedDropdown.addClass('active').removeClass('move-left move-right').prevAll().addClass('move-left').end().nextAll().addClass('move-right');
+            item.addClass('active');
+            //show the dropdown wrapper if not visible yet
+            if( !this.element.hasClass('is-dropdown-visible') ) {
+                setTimeout(function(){
+                    self.element.addClass('is-dropdown-visible');
+                }, 10);
+            }
+        }
+    };
+
+    morphDropdown.prototype.updateDropdown = function(dropdownItem, height, width, left) {
+        this.dropdownList.css({
+            '-moz-transform': 'translateX(' + left + 'px)',
+            '-webkit-transform': 'translateX(' + left + 'px)',
+            '-ms-transform': 'translateX(' + left + 'px)',
+            '-o-transform': 'translateX(' + left + 'px)',
+            'transform': 'translateX(' + left + 'px)',
+            'width': width+'px',
+            'height': height+'px'
+        });
+
+        this.dropdownBg.css({
+            '-moz-transform': 'scaleX(' + width + ') scaleY(' + height + ')',
+            '-webkit-transform': 'scaleX(' + width + ') scaleY(' + height + ')',
+            '-ms-transform': 'scaleX(' + width + ') scaleY(' + height + ')',
+            '-o-transform': 'scaleX(' + width + ') scaleY(' + height + ')',
+            'transform': 'scaleX(' + width + ') scaleY(' + height + ')'
+        });
+    };
+
+    morphDropdown.prototype.hideDropdown = function() {
+        this.mq = this.checkMq();
+        if( this.mq == 'desktop') {
+            this.element.removeClass('is-dropdown-visible').find('.active').removeClass('active').end().find('.move-left').removeClass('move-left').end().find('.move-right').removeClass('move-right');
+        }
+    };
+
+    morphDropdown.prototype.resetDropdown = function() {
+        this.mq = this.checkMq();
+        if( this.mq == 'mobile') {
+            this.dropdownList.removeAttr('style');
+        }
+    };
+
+    var morphDropdowns = [];
+    if( $('.cd-morph-dropdown').length > 0 ) {
+        $('.cd-morph-dropdown').each(function(){
+            //create a morphDropdown object for each .cd-morph-dropdown
+            morphDropdowns.push(new morphDropdown($(this)));
+        });
+
+        var resizing = false;
+
+        //on resize, reset dropdown style property
+        updateDropdownPosition();
+        $(window).on('resize', function(){
+            if( !resizing ) {
+                resizing =  true;
+                (!window.requestAnimationFrame) ? setTimeout(updateDropdownPosition, 300) : window.requestAnimationFrame(updateDropdownPosition);
+            }
+        });
+
+        function updateDropdownPosition() {
+            morphDropdowns.forEach(function(element){
+                element.resetDropdown();
+            });
+
+            resizing = false;
+        };
+    }
+});
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -8,7 +8,7 @@
 		'enterprise' => 'Enterprise',
 	];
 ?>
-<section class="Header-Navigation">
+<!-- <section class="Header-Navigation">
 	<nav class="navbar navbar-fixed-top" id="navbar">
 		<div class="container-fluid">
 			<div class="navbar-header">
@@ -40,3 +40,107 @@
 	</nav>
 </div>
 </section>
+ -->
+
+ <header class="cd-morph-dropdown">
+	<a class="brand" href="<?php echo home_url() ?>">
+		<div class="logo"></div>
+	</a>
+		<a href="#0" class="nav-trigger">Open Nav<span aria-hidden="true"></span></a>
+		
+		<nav class="main-nav">
+			<ul>
+				<li class="has-dropdown gallery" data-content="about">
+					<a href="#0">About us</a>
+				</li>
+
+				<li class="has-dropdown links" data-content="Get Involved">
+					<a href="#0">Get Involved</a>
+				</li>
+
+				<li class="has-dropdown button" data-content="Features">
+					<a href="#0">Features</a>
+				</li>
+
+				<li class="has-dropdown button" data-content="Enterprise">
+					<a href="#0">Enterprise</a>
+				</li>
+			</ul>
+		</nav>
+		
+		<div class="morph-dropdown-wrapper">
+			<div class="dropdown-list" style="transform: translateX(757px); width: 390px; height: 349px;">
+				<ul>
+					<li id="about" class="dropdown gallery">
+						<a href="#0" class="label">About</a>
+						
+						<div class="content">
+							<ul>
+								<li><a href="#0">Who we are</a></li>
+								<li><a href="#0">Community</a></li>
+								<li><a href="#0">Team</a></li>
+								<li><a href="#0">Jobs</a></li>
+								<li><a href="#0">Code of conduct</a></li>
+						</div>
+					</li>
+
+					<li id="Get Involved" class="dropdown links">
+						<a href="#0" class="label">Get Involved</a>
+
+						<div class="content">
+							<ul>
+								<li>
+									<h2>Services</h2>
+
+									<ul class="links-list">
+										<li><a href="#0">Logo Design</a></li>
+										<li><a href="#0">Branding</a></li>
+										<li><a href="#0">Web Design</a></li>
+										<li><a href="#0">iOS</a></li>
+										<li><a href="#0">Android</a></li>
+										<li><a href="#0">HTML/CSS/JS</a></li>
+										<li><a href="#0">Packaging</a></li>
+										<li><a href="#0">Mobile</a></li>
+										<li><a href="#0">UI/UX</a></li>
+										<li><a href="#0">Prototyping</a></li>
+									</ul>
+								</li>
+
+								<li>
+									<h2>Projects</h2>
+
+									<ul class="links-list">
+										<li><a href="#0">Logo Design</a></li>
+										<li><a href="#0">Branding</a></li>
+										<li><a href="#0">Web Design</a></li>
+										<li><a href="#0">iOS</a></li>
+										<li><a href="#0">Android</a></li>
+										<li><a href="#0">HTML/CSS/JS</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</li>
+
+					<li id="Features" class="dropdown button">
+						<a href="#0" class="label">Features</a>
+						
+						<div class="content">	
+							<ul class="links-list">
+								<li><a href="#0">Link #1</a></li>
+								<li><a href="#0">Link #2</a></li>
+								<li><a href="#0">Link #3</a></li>
+								<li><a href="#0">Link #4</a></li>
+								<li><a href="#0">Link #5</a></li>
+								<li><a href="#0">Link #6</a></li>
+							</ul>
+
+							<a href="#0" class="btn">Get in Touch</a>
+						</div>
+					</li>
+				</ul>
+
+				<div class="bg-layer" aria-hidden="true" style="transform: scaleX(390) scaleY(349);"></div>
+			</div> <!-- dropdown-list -->
+		</div> <!-- morph-dropdown-wrapper -->
+	</header>
