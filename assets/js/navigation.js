22564020bf972e0ecd59f3b3df9bea4b54de887b
Paulo Moura <paulomoura@Paulos-iMac.local>
2016-10-17 17:08:51 +0100

    Added dropdowns and Javascript logic

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,111 +1,150 @@
-/**
-* Listen to scroll to change header opacity class
-*/
 $(window).load(function() {
-   
-    function checkScroll() {
-    var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-      if (bodyScrollTop !== 0) {
-            $('.navbar').addClass('scrolled');
-        } else {
-            $('.navbar').removeClass('scrolled');
-        }
-    }
-
-    if ($('.navbar').length > 0) {
-        $(window).on('scroll load resize', function () {
-            checkScroll();
+    'use strict';
+    var sections = $('.nav__sections')[0];
+    var links = $('.nav__section');
+    var bgWrapper = $('.nav__bg-wrapper')[0];
+    var bg = $('.nav__bg')[0];
+    console.log(sections);
+    var bgBCR = bg.getBoundingClientRect();
+    sections.addEventListener('mouseover', function () {
+        setTimeout(function () {
+            return bg.classList.add('is-animatable');
         });
-    }
-});
-
-/**
-* Mobile fullscreen trigger
-*/
-$(document).ready(function() {
-	$('#toggle').click(function() {
-	$(this).toggleClass('active');
-	$('#overlay').toggleClass('open');
-	$('.navbar').toggleClass('mobile-menu-open');
-	});
-});
-
-/**
-* Show Header when scroll in resolution lower then width 800px
-*/
-var didScroll;
-var lastScrollTop = 0;
-var delta = 100;
-var navbarHeight = $('#navbar').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('#navbar').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('#navbar').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
-}
-
-$(document).ready(function() {
-    var menuAnimation;
-    var menuOpened = false;
-
-    // Prevent scrolling if menu is opened
-    $('html').on('scroll touchmove mousewheel', function(e) {
-        if(menuOpened) {
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
-        }
     });
-
-    var animContainer = document.querySelectorAll('.navbar-header button')[0];
-    var params = {
-        container: animContainer,
-        renderer: 'svg',
-        loop: false,
-        autoplay: false,
-        autoloadSegments: true,
-        path: templateUrl + '/assets/img/menu/data.json'
-    };
-    menuAnimation = bodymovin.loadAnimation(params);
-    menuAnimation.stop();
-
-    $('.navbar-header button').click(function () {
-        if(menuOpened) {
-            menuAnimation.setDirection(-1);
-        } else {
-            menuAnimation.setDirection(0);
-        }
-        menuAnimation.play();
-        menuOpened = !menuOpened;
+    sections.addEventListener('mouseleave', function () {
+        bg.classList.remove('is-animatable');
     });
+    [].forEach.call(links, function (link) {
+        link.addEventListener('mouseover', function () {
+            bgWrapper.classList.add('is-visible');
+            var links = this.querySelector('.nav__links');
+            var linksBCR = links.getBoundingClientRect();
+            var scaleX = linksBCR.width / bgBCR.width;
+            var scaleY = linksBCR.height / bgBCR.height;
+            bg.style.transform = 'translate(' + linksBCR.left + 'px) scale(' + scaleX + ', ' + scaleY + ')';
+        });
+        link.addEventListener('mouseleave', function () {
+            bgWrapper.classList.remove('is-visible');
+        });
+    });
+});
+
+
+
+
+
+
+
+// Older version
+
+// /**
+// * Listen to scroll to change header opacity class
+// */
+// $(window).load(function() {
+   
+//     function checkScroll() {
+//     var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+//       if (bodyScrollTop !== 0) {
+//             $('.navbar').addClass('scrolled');
+//         } else {
+//             $('.navbar').removeClass('scrolled');
+//         }
+//     }
+
+//     if ($('.navbar').length > 0) {
+//         $(window).on('scroll load resize', function () {
+//             checkScroll();
+//         });
+//     }
+// });
+
+// /**
+// * Mobile fullscreen trigger
+// */
+// $(document).ready(function() {
+// 	$('#toggle').click(function() {
+// 	$(this).toggleClass('active');
+// 	$('#overlay').toggleClass('open');
+// 	$('.navbar').toggleClass('mobile-menu-open');
+// 	});
+// });
+
+// /**
+// * Show Header when scroll in resolution lower then width 800px
+// */
+// var didScroll;
+// var lastScrollTop = 0;
+// var delta = 100;
+// var navbarHeight = $('#navbar').outerHeight();
+
+// $(window).scroll(function(event){
+//     didScroll = true;
+// });
+
+// setInterval(function() {
+//     if (didScroll) {
+//         hasScrolled();
+//         didScroll = false;
+//     }
+// }, 250);
+
+// function hasScrolled() {
+//     var st = $(this).scrollTop();
+    
+//     // Make sure they scroll more than delta
+//     if(Math.abs(lastScrollTop - st) <= delta)
+//         return;
+    
+//     // If they scrolled down and are past the navbar, add class .nav-up.
+//     // This is necessary so you never see what is "behind" the navbar.
+//     if (st > lastScrollTop && st > navbarHeight){
+//         // Scroll Down
+//         $('#navbar').removeClass('nav-down').addClass('nav-up');
+//     } else {
+//         // Scroll Up
+//         if(st + $(window).height() < $(document).height()) {
+//             $('#navbar').removeClass('nav-up').addClass('nav-down');
+//         }
+//     }
+    
+//     lastScrollTop = st;
+// }
+
+// $(document).ready(function() {
+//     var menuAnimation;
+//     var menuOpened = false;
+
+//     // Prevent scrolling if menu is opened
+//     $('html').on('scroll touchmove mousewheel', function(e) {
+//         if(menuOpened) {
+//             e.preventDefault();
+//             e.stopPropagation();
+//             return false;
+//         }
+//     });
+
+//     var animContainer = document.querySelectorAll('.navbar-header button')[0];
+//     var params = {
+//         container: animContainer,
+//         renderer: 'svg',
+//         loop: false,
+//         autoplay: false,
+//         autoloadSegments: true,
+//         path: templateUrl + '/assets/img/menu/data.json'
+//     };
+//     menuAnimation = bodymovin.loadAnimation(params);
+//     menuAnimation.stop();
+
+//     $('.navbar-header button').click(function () {
+//         if(menuOpened) {
+//             menuAnimation.setDirection(-1);
+//         } else {
+//             menuAnimation.setDirection(0);
+//         }
+//         menuAnimation.play();
+//         menuOpened = !menuOpened;
+//     });
 	
-	// Fade In animation 
-	$(".navbar-header").velocity('transition.fadeIn', 1000 );
-});
+// 	// Fade In animation 
+// 	$(".navbar-header").velocity('transition.fadeIn', 1000 );
+// });
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,17 +1,182 @@
-<?php
 
-	$l = new L10N('header-navbar');
+<nav class="nav">
+	<div class="container">
+		<div class="nav__bg-wrapper">
+			<div class="nav__bg"></div>
+		</div>
 
+		<div class="brand">
+			<div class="logo"></div>
+		</div>
+
+		<ul class="nav__sections">
+			<li class="nav__section"><a href="#0" class="nav__label">About us</a>
+				<ul class="nav__links nav__links--large">
+					<li class="nav__item"><a href="#0">Who we are</a></li>
+					<li class="nav__item"><a href="#0">Community</a></li>
+					<li class="nav__item"><a href="#0">Team</a></li>
+					<li class="nav__item"><a href="#0">Jobs</a></li>
+					<li class="nav__item"><a href="#0">Code of Conduct</a></li>
+				</ul>
+			</li>
+
+			<li class="nav__section"><a href="#0" class="nav__label">News</a>
+				<ul class="nav__links nav__links--small">
+					<li class="nav__item"><a href="#0">Blog</a></li>
+					<li class="nav__item"><a href="#0">Newsletter</a></li>
+				</ul>
+			</li>
+
+			<li class="nav__section"><a href="#0" class="nav__label">Features</a>
+				<ul class="nav__links nav__links--medium">
+					<li class="nav__item"><a href="#0">Sharing</a></li>
+					<li class="nav__item"><a href="#0">Workflow</a></li>
+					<li class="nav__item"><a href="#0">Secure</a></li>
+					<li class="nav__item"><a href="#0">Collabora online</a></li>
+					<li class="nav__item"><a href="#0">Webrtc</a></li>
+				</ul>
+			</li>
+
+			<li class="nav__section"><a href="#0" class="nav__label">Enterprise</a>
+				<ul class="nav__links nav__links--medium">
+					<li class="nav__item"><a href="#0">Enterprise</a></li>
+					<li class="nav__item"><a href="#0">Subscripitions</a></li>
+					<li class="nav__item"><a href="#0">Pricing</a></li>
+				</ul>
+			</li>
+
+			<li class="nav__section"><a href="#0" class="nav__label">Support</a>
+				<ul class="nav__links nav__links--medium">
+					<li class="nav__item"><a href="#0">Support options</a></li>
+					<li class="nav__item"><a href="#0">User Documentation</a></li>
+					<li class="nav__item"><a href="#0">Administrator</a></li>
+					<li class="nav__item"><a href="#0">Developer</a></li>
+					<li class="nav__item"><a href="#0">Security</a></li>
+				</ul>
+			</li>
+		</ul>
+
+		<div class="right-buttons">
+			<ul>
+				<li><a href="https://demo.nextcloud.com/">Demo</a></li>
+	            <li><a href="<?php echo home_url('install') ?>">Download</a>
+            </ul>
+		</div>
+	</div>
+</nav>
+
+
+
+
+
+
+<!-- <header class="global-nav">
+	<div class="container">
+		<div class="nav-wrapper">
+		
+		<div class="brand">
+			
+			<div class="logo"></div>
+		</div>
+			
+			<div class="center-nav">
+				<ul>
+					<li class="has-dropdown" data-content="about">
+						<a href="#0">About us</a>
+					</li>
+		 
+					<li class="has-dropdown" data-content="involved">
+						<a href="#0">Get involved</a>
+					</li>
+		 
+					<li class="has-dropdown" data-content="features">
+						<a href="#0">Features</a>
+					</li>
+
+					<li class="has-dropdown" data-content="enterprise">
+						<a href="#0">Enterprise</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="right-nav">
+				<ul>
+
+					<li>
+					<a href="#0">Demo</a>
+					</li>
+
+					<li>
+					<a href="#0">Download</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="morph-dropdown-wrapper">
+			<div class="dropdown-list">
+			<ul>
+					<li id="contact" class="dropdown button">
+						<a href="#0" class="label">Contact</a>
+						
+						<div class="content">	
+							<ul class="links-list">
+								<li><a href="#0">Who we are</a></li>
+								<li><a href="#0">Community</a></li>
+								<li><a href="#0">Team</a></li>
+								<li><a href="#0">Jobs</a></li>
+								<li><a href="#0">Code of Conduct</a></li>
+								<li><a href="#0">Link #6</a></li>
+							</ul>
+
+							<a href="#0" class="btn">Get in Touch</a>
+						</div>
+					</li>
+
+					</li>
+	 
+					<li id="features">
+			</li>
+
+					<li id="enterprise">
+					</li>
+				</ul> 
+	 
+				<div class="bg-layer" aria-hidden="true"></div>
+			</div> 
+		</div> 
+	</div>
+</header> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Older -->
+
+<!-- <?php
 	$navigationItems = [
-		'news' => $l->t('News'),
-		'features' => $l->t('Features'),
-		'about' => $l->t('About us'),
-		'contribute' => $l->t('Get involved'),
-		'support' => $l->t('Support'),
-		'enterprise' => $l->t('Enterprise'),
+		'news' => 'News',
+		'features' => 'Features',
+		'about' => 'About us',
+		'contribute' => 'Get involved',
+		'support' => 'Support',
+		'enterprise' => 'Enterprise',
 	];
-?>
-<section class="Header-Navigation">
+?> -->
+<!-- <section class="Header-Navigation">
 	<nav class="navbar navbar-fixed-top" id="navbar">
 		<div class="container-fluid">
 			<div class="navbar-header">
@@ -33,8 +198,8 @@
                                 ?>
                                 <li <?php if($active): ?>class="active"<?php endif;?>><a href="<?php echo home_url($key) ?>"><?php echo $text ?></a></li>
                                 <?php endforeach; ?>
-                                <li><a href="https://demo.nextcloud.com/"><?php echo $l->t('Demo') ?></a></li>
-                                <li class="btn-primary"><a href="<?php echo home_url('install') ?>"><?php echo $l->t('Download') ?></a>
+                                <li><a href="https://demo.nextcloud.com/">Demo</a></li>
+                                <li class="btn-primary"><a href="<?php echo home_url('install') ?>">Download</a>
                         </li>
                     </ul>
                   </nav>
@@ -43,3 +208,6 @@
 	</nav>
 </div>
 </section>
+ -->
+
+ 
