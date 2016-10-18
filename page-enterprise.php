ac883af695829e6c2b99f84179dd1675d9164697
Jos Poortvliet <jospoortvliet@gmail.com>
2016-10-18 21:03:10 +0200

    fix no FAQ lines on firefox

--- page-enterprise.php
+++ page-enterprise.php
@@ -171,15 +171,15 @@
 				<h1 class="revealOnScroll">Frequently asked questions</h1>
 			</div>
 	<div class="container">
-		<div class="row">
+		<div class="row revealonscroll">
 			<div class="col-md-12 col-sm-12 ">
-				<div class="downarrow revealOnScroll"><a href="#why_pay" data-toggle="collapse"><h3>Why would I pay for open source software?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="downarrow"><a href="#why_pay" data-toggle="collapse"><h3>Why would I pay for open source software?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="why_pay" class="collapse ">
 					<p>Nextcloud is great out of the box. But installing and running a server in a secure and reliable manner and for hundreds or thousands of users, important for a mission critical environment, is not always easy. You probably want access to an expert if something does not function as expected. We provide not only that but also pro-active support with installation and security issues, we reach out when we do feature planning to ensure your needs are served and we support Nextcloud long after you would otherwise be forced to upgrade for security, performance and stability reasons.</p>
 					<p>By offering more in-depth knowledge than anybody else, we give you the confidence you need to be able to rely on Nextcloud.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#what_I_get" data-toggle="collapse"><h3>What does the subscription give me?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#what_I_get" data-toggle="collapse"><h3>What does the subscription give me?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="what_I_get" class="collapse">
 					<p>Your Nextcloud subscription enables you to successfully deploy and manage your servers. Our open approach gives you direct access to Nextcloud engineers, the latest knowledge and best practices. We provide technical expertise, guidance and collaboration with phone and chat contact. Pro-active security support helps you identify and address vulnerabilities and harden your servers to protect the safety and integrity of your data.</p>
 					<p>With the Nextcloud Enterprise subscription you get:
@@ -197,52 +197,52 @@
 					</ul></p>
 					<p>Nextcloud consulting goes beyond technical expertise, offering strategic advice for organizations. We analyze your challenges and help you implement a comprehensive, cost-effective and compliance-aware solution which reduces complexity and risks while delivering the highest security and privacy protection.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#languages" data-toggle="collapse"><h3>In what languages do I get support?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#languages" data-toggle="collapse"><h3>In what languages do I get support?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="languages" class="collapse">
 					<p>We provide support in German and English and other languages through our partners, <a href="#contact">contact us</a> for details.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#migration" data-toggle="collapse"><h3>How difficult is migration from ownCloud?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#migration" data-toggle="collapse"><h3>How difficult is migration from ownCloud?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="migration" class="collapse">
 					<p>Migration from ownCloud to Nextcloud is easy. Our support team will help you move to Nextcloud and optimally benefit from the added scalability, features, stability, performance and security. See <a href="/migration">our migration page</a> for more details on migration.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#howbuy" data-toggle="collapse"><h3>How do I buy Nextcloud subscriptions?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#howbuy" data-toggle="collapse"><h3>How do I buy Nextcloud subscriptions?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="howbuy" class="collapse">
 					<p>Just <a href="/buy">contact us</a> or one of our partners. Our partners will help you set up Nextcloud and they act as your first point of contact.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#partner" data-toggle="collapse"><h3>How do I become a Nextcloud partner?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#partner" data-toggle="collapse"><h3>How do I become a Nextcloud partner?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="partner" class="collapse">
 					<p>We are always looking for competent partners who share our passion for making customers happy! You can join our ecosystem, <a href="/contact">contact us for more information</a>.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#gpl" data-toggle="collapse"><h3>What does the Open Source License mean?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#gpl" data-toggle="collapse"><h3>What does the Open Source License mean?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="gpl" class="collapse">
 					<p>Nextcloud is licensed under the GNU AGPLv3, one of the most widely used Open Source license. Written with input from lawyers and industry leaders from all over the world, it is a clear, balanced and well understood license, providing users a level of certainty no license from any single company can give. Today it is a widely used and broadly accepted license across various stakeholders, including developers, users, organizations and large enterprises. The AGPLv3 guarantees that all users of the software can use, study, share and improve the software without any legal risks. Further it's patent clause ensures all users have a non-exclusive, non-transferable, worldwide, royalty-free license on any patents owned by code contributors now or in the future. On top of this, Nextcloud GmbH guarantees that every customer can use Nextcloud and exercise all rights given by the GNU AGPLv3 without violating any patents or copyright.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#sourcerelease" data-toggle="collapse"><h3>Do I have to release the source code of systems Nextcloud connects to?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#sourcerelease" data-toggle="collapse"><h3>Do I have to release the source code of systems Nextcloud connects to?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="sourcerelease" class="collapse">
 					<p>No. Nextcloud will communicate with your back-end systems over external APIs. Those systems will not become a derivative work of Nextcloud just because they communitcate with it. Therefore you can keep the source code of your back-end systems private and don't have to share it with anyone.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#logos" data-toggle="collapse"><h3>Are my logos or the company name affected by the AGPL?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#logos" data-toggle="collapse"><h3>Are my logos or the company name affected by the AGPL?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="logos" class="collapse">
 					<p>No. Logos, company names, etc. are protected by trademarks. The GNU AGPLv3 is a copyright license and doesn't affect any trademarks. If you put your logo on your Nextcloud it is considered merely as data used by Nextcloud and has no effect on licenses or trademarks.</p></div>
-				<div class="line revealOnScroll">
+				<div class="line">
 				</div>
-				<div class="downarrow revealOnScroll"><a href="#pubsource" data-toggle="collapse"><h3>Do I need to publish the source code if I use Nextcloud?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="downarrow"><a href="#pubsource" data-toggle="collapse"><h3>Do I need to publish the source code if I use Nextcloud?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="pubsource" class="collapse">
 					<p>No. As long as you didn't make any modifications to the Nextcloud code, existing apps or write your own apps, you don't have to do anything. Note that configuration does not count as modification! If you did make code modifications, then you have to make that code available under the GNU AGPLv3 or a compatible license to the users of the system. The easiest way to do so is to provide a download link. Nextcloud GmbH assists customers with license compliance.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
-				<div class="downarrow revealOnScroll"><a href="#sellapp" data-toggle="collapse"><h3>Can I sell my Nextcloud App?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="line"></div>
+				<div class="downarrow"><a href="#sellapp" data-toggle="collapse"><h3>Can I sell my Nextcloud App?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="sellapp" class="collapse">
 					<p>Yes. Nextcloud is licensed under the GNU AGPLv3. This license only defines the rights you have to give the users of the software, but not how they get the software. You can decide freely if you want to sell the software in an app store or through any other channel, or if you want to make the app publicly available. The only requirement is that you license your app under the AGPLv3 or a compatible license.</p>
 				</div>
-				<div class="line revealOnScroll"></div>
+				<div class="line"></div>
 			</div>
 		</div>
 	</div>
