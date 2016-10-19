745e36269adfe4320b658f1ac5ffbdd7bf739fde
Jos Poortvliet <jospoortvliet@gmail.com>
2016-10-19 13:55:50 +0200

    scroll to

--- page-enterprise.php
+++ page-enterprise.php
@@ -4,7 +4,7 @@
 <div class="container-fluid background"> 
 	<div class="container">
 		<div class="col-md-6 topheader">
-			<h1>Enterprise Support Subscriptions</h1>
+			<h1>Enterprise support subscriptions</h1>
 			<h2>An enterprise subscription provides you the confidence of knowing that your Nextcloud deployment is backed by the best available technical expertise and with the features you need for your organization</h2>
 		</div>
 	</div>
@@ -12,14 +12,14 @@
 
 <div class="container pricing">
 	<div class="row">
-		<h1 class="header revealOnScroll">Plans and Pricing</h1>
+		<h1 class="header revealOnScroll">Plans and pricing</h1>
 		<div class="col-md-4 table-column revealOnScroll">
 			<div class="table-header basic revealOnScroll">
 				<h1 class="header revealOnScroll">Basic</h1>
 				<h2 class="header revealOnScroll">From 1500€/year</h2>
 			</div>
 			<div class="table-content">
-				<h1 class="header revealOnScroll">Supported Capabilities</h1>
+				<h1 class="header revealOnScroll">Capabilities</h1>
 				<div class="line revealOnScroll"></div>
 				<div class="list revealOnScroll">
 					<li class="check overinfo"><div class="infotext">Sync your files with our desktop and mobile clients, share them with other users on your server, with public links or across servers.</div>File sync and share</li>
@@ -35,8 +35,8 @@
 				<div class="line revealOnScroll"></div>
 				<div class="list revealOnScroll">
 					<li class="check overinfo"><div class="infotext">Questions and issues are generally dealt with in this time, depending on the severity of the issue.</div>Production SLA: 3 business days</li>
-					<li class="check overinfo"><div class="infotext">Our Nextcloud Knowledge Portal helps you find answers to your questions or contact our professional support team.</div>Portal support</li>
-					<li class="check overinfo"><div class="infotext">We help you migrate from ownCloud to Nextcloud.</div>ownCloud migration support</li>
+					<li class="check overinfo"><div class="infotext">Our Nextcloud Knowledge Portal helps you find answers to your questions or contact our professional support team.</div>Portal and email support</li>
+					<li class="check overinfo"><div class="infotext">We help you migrate from ownCloud to Nextcloud.</div><a href="/migration">ownCloud migration support</a></li>
 					<li class="check overinfo"><div class="infotext">You can contact our capable and responsive support team by email and expect a quick response time.</div>Email support</li>
 					<li class="nocheck overinfo"><div class="infotext">You can contact our support engineers by phone in these times.</div>Phone support</li>
 					<li class="nocheck overinfo"><div class="infotext">If you need access to experienced engineers to built custom solutions for your unique needs.</div>Custom consulting</li>
@@ -69,17 +69,17 @@
 					<li class="check overinfo"><div class="infotext">Android, iOS and desktop apps, branding possible.</div>Mobile and desktop clients</li>
 					<li class="check overinfo"><div class="infotext">Powerful enterprise features like SAML authentication, Windows Network Drive support, password policies, audit-ready logging, workflow handling and more.</div>Enterprise capabilities</li>
 					<li class="check overinfo"><div class="infotext">the AGPL license provides legal certainty for all customers and users.</div>Fully Open Source</li>
-					<li class="check overinfo"><div class="infotext">Support for the Calendar and Contacts apps.</div>Calendar and Contacts</li>
-					<li class="optional overinfo"><div class="infotext">Support for our secure and private peer to peer audio and video conferencing.</div>Secure WebRTC Conferencing</li>
+					<li class="optional overinfo"><div class="infotext">Support for the Calendar and Contacts apps.</div>Calendar and Contacts</li>
+					<li class="optional overinfo"><div class="infotext">Support for our secure and private peer to peer audio and video conferencing.</div><a href="/webrtc" target="_blank">Secure WebRTC Conferencing</a></li>
 					<li class="optional overinfo"><div class="infotext">Allows web conferencing calls to connect with phone lines.</div>SIP gateway</li>
-					<li class="optional overinfo"><div class="infotext">Real time collaborative editing of often used office fileformats like DOCX, PPTX and more.</div>Collabora Online Office</li>
+					<li class="optional overinfo"><div class="infotext">Real time collaborative editing of often used office fileformats like DOCX, PPTX and more.</div><a href="/collaboraonline" target="_blank">Collabora Online Office</a></li>
 				</div>
 				<h1 class="revealOnScroll">Support</h1>
 				<div class="line revealOnScroll"></div>
 				<div class="list revealOnScroll">
 					<li class="check overinfo"><div class="infotext">Questions and issues are generally dealt with in this time, depending on the severity of the issue.</div>Production SLA: 2 business days</li>
-					<li class="check overinfo"><div class="infotext">Our Nextcloud Knowledge Portal helps you find answers to your questions or contact our professional support team.</div>Portal support</li>
-					<li class="check overinfo"><div class="infotext">We help you migrate from ownCloud to Nextcloud.</div>ownCloud migration support</li>
+					<li class="check overinfo"><div class="infotext">Our Nextcloud Knowledge Portal helps you find answers to your questions or contact our professional support team.</div>Portal and email support</li>
+					<li class="check overinfo"><div class="infotext">We help you migrate from ownCloud to Nextcloud.</div><a href="/migration">ownCloud migration support</a></li>
 					<li class="check overinfo"><div class="infotext">You can contact our capable and responsive support team by email and expect a quick response time.</div>Email support: business hours</li>
 					<li class="check overinfo"><div class="infotext">You can contact our support engineers by phone in these times.</div>Phone support: business hours</li>
 					<li class="check overinfo"><div class="infotext">If you need access to experienced engineers to built custom solutions for your unique needs.</div>Custom consulting</li>
@@ -105,7 +105,7 @@
 				<h2 class="revealOnScroll">From 3250€/year</h2>
 			</div>
 			<div class="table-content">
-				<h1 class="revealOnScroll">Supported Capabilities</h1>
+				<h1 class="revealOnScroll">Capabilities</h1>
 				<div class="line revealOnScroll"></div>
 				<div class="list revealOnScroll">
 					<li class="check overinfo"><div class="infotext">Sync your files with our desktop and mobile clients, share them with other users on your server, with public links or across servers.</div>File sync and share</li>
@@ -113,22 +113,22 @@
 					<li class="check overinfo"><div class="infotext">Powerful enterprise features like SAML authentication, Windows Network Drive support, password policies, audit-ready logging, workflow handling and more.</div>Enterprise capabilities</li>
 					<li class="check overinfo"><div class="infotext">the AGPL license provides legal certainty for all customers and users.</div>Fully Open Source</li>
 					<li class="check overinfo"><div class="infotext">Support for the Calendar and Contacts apps.</div>Calendar and Contacts</li>
-					<li class="optional overinfo"><div class="infotext">Optional: Support for our secure and private peer to peer audio and video conferencing.</div>Secure WebRTC Conferencing</li>
+					<li class="optional overinfo"><div class="infotext">Optional: Support for our secure and private peer to peer audio and video conferencing.</div><a href="/webrtc" target="_blank">Secure WebRTC Conferencing</a></li>
 					<li class="optional overinfo"><div class="infotext">Optional: Allows web conferencing calls to connect with phone lines.</div>SIP gateway</li>
-					<li class="optional overinfo"><div class="infotext">Optional: Real time collaborative editing of often used office fileformats like DOCX, PPTX and more.</div>Collabora Online Office</li>
+					<li class="optional overinfo"><div class="infotext">Optional: Real time collaborative editing of often used office fileformats like DOCX, PPTX and more.</div><a href="/collaboraonline" target="_blank">Collabora Online Office</a></li>
 				</div>
 				<h1 class="revealOnScroll">Support</h1>
 				<div class="line revealOnScroll"></div>
 				<div class="list revealOnScroll">
 					<li class="check overinfo"><div class="infotext">Questions and issues are generally dealt with in this time, depending on the severity of the issue.</div>Production SLA: 24h or as agreed</li>
-					<li class="check overinfo"><div class="infotext">Our Nextcloud Knowledge Portal helps you find answers to your questions or contact our professional support team.</div>Portal support</li>
-					<li class="check overinfo"><div class="infotext">We help you migrate from ownCloud to Nextcloud.</div>ownCloud migration support</li>
+					<li class="check overinfo"><div class="infotext">Our Nextcloud Knowledge Portal helps you find answers to your questions or contact our professional support team.</div>Portal and email support</li>
+					<li class="check overinfo"><div class="infotext">We help you migrate from ownCloud to Nextcloud.</div><a href="/migration">ownCloud migration support</a></li>
 					<li class="check overinfo"><div class="infotext">You can contact our capable and responsive support team by email and expect a quick response time.</div>Email support: up to 24/7</li>
 					<li class="check overinfo"><div class="infotext">You can contact our support engineers by phone in these times.</div>Phone support: up to 24/7</li>
 					<li class="check overinfo"><div class="infotext">If you need access to experienced engineers to built custom solutions for your unique needs.</div>Custom consulting</li>
 					<li class="check overinfo"><div class="infotext">Our engineers walk you through the installation process where needed.</div>Remote installation support</li>
 					<li class="check overinfo"><div class="infotext">Fully branded clients.</div>Branded clients</li>
-					<li class="check overinfo"><div class="infotext">Once you need to scale Nextcloud to large numbers of users, a full clustering solution is required and we'll help you set it up and we'll help you set it up.</div>Clustering support</li>
+					<li class="check overinfo"><div class="infotext">Once you need to scale Nextcloud to large numbers of users, a full clustering solution is required and we'll help you set it up.</div>Clustering support</li>
 					<li class="check overinfo"><div class="infotext">If you need the certainty that you can remain on your stable, proven Nextcloud release for a long time.</div>Maintenance life cycle: 5+5 years</li>
 				</div>
 				<h1 class="revealOnScroll">Pricing</h1>
@@ -165,7 +165,7 @@
 		<p><a href="/enterprise/buy">Contact us</a></p>	</div>
 	</div>
 </div>
-
+<a name="faq">
 <div class="faq container-fluid">
 			<div class="text-center">
 				<h1 class="revealOnScroll">Frequently asked questions</h1>
@@ -218,6 +218,7 @@
 					<p>We are always looking for competent partners who share our passion for making customers happy! You can join our ecosystem, <a href="/contact">contact us for more information</a>.</p>
 				</div>
 				<div class="line"></div>
+				<a name="gpl">
 				<div class="downarrow"><a href="#gpl" data-toggle="collapse"><h3>What does the Open Source License mean?<span class="icon-arrow-down"></span></h3></a></div>
 				<div id="gpl" class="collapse">
 					<p>Nextcloud is licensed under the GNU AGPLv3, one of the most widely used Open Source license. Written with input from lawyers and industry leaders from all over the world, it is a clear, balanced and well understood license, providing users a level of certainty no license from any single company can give. Today it is a widely used and broadly accepted license across various stakeholders, including developers, users, organizations and large enterprises. The AGPLv3 guarantees that all users of the software can use, study, share and improve the software without any legal risks. Further it's patent clause ensures all users have a non-exclusive, non-transferable, worldwide, royalty-free license on any patents owned by code contributors now or in the future. On top of this, Nextcloud GmbH guarantees that every customer can use Nextcloud and exercise all rights given by the GNU AGPLv3 without violating any patents or copyright.</p>
@@ -233,7 +234,7 @@
 					<p>No. Logos, company names, etc. are protected by trademarks. The GNU AGPLv3 is a copyright license and doesn't affect any trademarks. If you put your logo on your Nextcloud it is considered merely as data used by Nextcloud and has no effect on licenses or trademarks.</p></div>
 				<div class="line">
 				</div>
-				<div class="downarrow"><a href="#pubsource" data-toggle="collapse"><h3>Do I need to publish the source code if I use Nextcloud?<span class="icon-arrow-down"></span></h3></a></div>
+				<div class="downarrow"><a href="#pubsource" data-toggle="collapse"><h3>Do I need to publish the source code if I use Nextcloud?<span class="icon-arrow-down"></span></h3></a><a name="pubsource"></div>
 				<div id="pubsource" class="collapse">
 					<p>No. As long as you didn't make any modifications to the Nextcloud code, existing apps or write your own apps, you don't have to do anything. Note that configuration does not count as modification! If you did make code modifications, then you have to make that code available under the GNU AGPLv3 or a compatible license to the users of the system. The easiest way to do so is to provide a download link. Nextcloud GmbH assists customers with license compliance.</p>
 				</div>
@@ -248,3 +249,16 @@
 	</div>
 </div>
 
+<script type='text/javascript'>
+// $(document).ready(function(){
+//   if(window.location.hash != "") {
+//       $('a[href="' + window.location.hash + '"]').click();
+//   };
+// });
+    window.onhashchange = gotofaq();  // only works on page load :( :(
+
+function gotofaq()
+	{
+    $('a[href="' + window.location.hash + '"]').click();
+};
+</script>
