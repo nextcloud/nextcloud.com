49118079080fe118a2eba414d530cff97e726085
Paulo Moura <paulomoura@Paulos-iMac.local>
2016-10-21 17:19:12 +0100

    [WIP] Done until the section get the box

--- page-box.php
+++ page-box.php
@@ -21,7 +21,30 @@ you to sync between your devices and share with others.</p>
 </section>
 
 <section class="section-tecnology">
-	<div class="line"></div>
-	
+	<div class="line revealOnScroll"></div>
+	<div class="container">
+		<div class="box-open col-md-7 revealOnScroll"></div>
+		<div class="wrapper-tex col-md-5 revealOnScroll">
+			<h2>Technology</h1>
+			<h6>The Nextcloud box consists of the following parts:</h6>
+			<p>1 TB USB3 hard drive from WDLabs</p>
+			<p>Nextcloud case with room for the drive and a compute board</p>
+			<p>microUSB charger, cables and adapters, a screw driver and screws</p>
+			<p>microSD card with Snappy Ubuntu Core as OS Apache, MySQL and the latest Nextcloud 10 pre-installed and ready to go</p>
+			<p>The Box is compatible with the Raspberry Pi 2 and 3, and also with oDroid C2 (not included).</p>
+		</div>
+	</div>
+</section>
 
+<section class="section-getbox">
+	<div class="container">
+	<div class="wrapper-tex col-md-4">
+	<h2>Get the box</h2>
+	<p>Currently you can only purchase the Nextcloud Box in Europe and the USA. We're working on bringing the box to other regions!</p>
+	<button type="button" data-toggle="dropdown" class="btn btn-primary buybutton dropdown-toggle" aria-expanded="false">Select your country &nbsp;<span class="caret"></span></button>
+	</div>
+	<div class="box-perspective col-md-8 pull-right"></div>
+
+	
+	</div>
 </section>
