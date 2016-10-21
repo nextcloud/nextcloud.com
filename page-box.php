4fd844a0430c357d45bd2e082aacad7d283f3c36
Paulo Moura <paulomoura@Paulos-iMac.local>
2016-10-21 13:55:39 +0100

    [WIP] Done structure and scss until Bring your data home

--- page-box.php
+++ page-box.php
@@ -2,8 +2,25 @@
 <link href="<?php echo get_template_directory_uri(); ?>/assets/css/box.css" rel="stylesheet">
 
 
-<section class="top-jumbotron">
+<section class="section-top-jumbotron">
 	<div class="container">
-		
+		<h2 class="text-center">Introducing the Nextcloud Box</h2>
+		<div class="box"></div>
 	</div>
+</section>
+
+<section class="section-data-home">
+	<div class="container">
+		<h2 class="text-center">Bring your data to home</h2>
+		<p class="text-center">The Nextcloud Box is your private cloud. It stores your data and enables 
+you to sync between your devices and share with others.</p>
+		<p><a class="btn-primary revealOnScroll" href="<?php echo home_url('features') ?>" role="button"><?php
+ echo $l->t('Know Nextcloud</a>');?></p>
+	</div>
+</section>
+
+<section class="section-tecnology">
+	<div class="line"></div>
+	
+
 </section>
