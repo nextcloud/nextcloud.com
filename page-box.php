d9316778e968cb530476b21884e498b418b75898
Paulo Moura <paulomoura@Paulos-iMac.local>
2016-10-21 14:04:52 +0100

    [WIP] Added first animations

--- page-box.php
+++ page-box.php
@@ -1,18 +1,19 @@
 
 <link href="<?php echo get_template_directory_uri(); ?>/assets/css/box.css" rel="stylesheet">
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/box.js"></script>
 
 
 <section class="section-top-jumbotron">
 	<div class="container">
-		<h2 class="text-center">Introducing the Nextcloud Box</h2>
-		<div class="box"></div>
+		<h2 class="text-center introText">Introducing the Nextcloud Box</h2>
+		<div class="box introImage"></div>
 	</div>
 </section>
 
 <section class="section-data-home">
 	<div class="container">
-		<h2 class="text-center">Bring your data to home</h2>
-		<p class="text-center">The Nextcloud Box is your private cloud. It stores your data and enables 
+		<h2 class="text-center revealOnScroll">Bring your data to home</h2>
+		<p class="text-center revealOnScroll">The Nextcloud Box is your private cloud. It stores your data and enables 
 you to sync between your devices and share with others.</p>
 		<p><a class="btn-primary revealOnScroll" href="<?php echo home_url('features') ?>" role="button"><?php
  echo $l->t('Know Nextcloud</a>');?></p>
