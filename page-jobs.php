94199b505c056a195ab60e9a2b6e4534908fff31
Jos Poortvliet <jospoortvliet@gmail.com>
2016-10-27 18:44:51 +0200

    remove translations

--- page-jobs.php
+++ page-jobs.php
@@ -1,67 +1,67 @@
 <!-- <?php get_template_part('templates/parts/title'); ?> -->
 <div class="page-header">
-	<h1><?php echo $l->t('We are hiring!');?></h1>
+	<h1>We are hiring!</h1>
 </div>
-<p><?php echo $l->t('Nextcloud is looking for people who want to join our team.');?></p>
-<h2><?php echo $l->t('Company description');?></h2>
-<p><?php echo $l->t('We are a modern, distributed, open source company. It is our mission to provide our users and customers with the means for data storage, access and communication under their control; protecting their privacy and security.');?></p>
-<p><?php echo $l->t('We pursue a fully-open strategy, keeping internal communication to a minimum while aiming to work, discuss and develop in public. We are a thriving, quickly growing company with a solid customer base.');?></p>
-<h2><?php echo $l->t('Job openings');?></h2>
-<h3 class="jobs"><?php echo $l->t('Sales Engineer / Manager DACH/Europe');?></h3>
-<p><?php echo $l->t('Nextcloud is getting an overwhelming response in the market and thus we are looking for another (junior) sales engineer and manager who can help sell our product and services in multiple verticals. In this role you will work as a member of our sales team to identify, nurture and close opportunities with new and existing customers, presenting our product to them and helping them with Proof of Concepts. We would appreciate somebody with an open source background!');?></p>
-<p><?php echo $l->t('This position would ideally be based in Stuttgart, Hamburg, Frankfurt or Munich but we are open to remote work.');?></p>
-<p><?php echo $l->t('Responsibilities:');?></p>
+<p>Nextcloud is looking for people who want to join our team.</p>
+<h2>Company description</h2>
+<p>We are a modern, distributed, open source company. It is our mission to provide our users and customers with the means for data storage, access and communication under their control; protecting their privacy and security.</p>
+<p>We pursue a fully-open strategy, keeping internal communication to a minimum while aiming to work, discuss and develop in public. We are a thriving, quickly growing company with a solid customer base.</p>
+<h2>Job openings</h2>
+<h3 class="jobs">Sales Engineer / Manager DACH/Europe</h3>
+<p>Nextcloud is getting an overwhelming response in the market and thus we are looking for another (junior) sales engineer and manager who can help sell our product and services in multiple verticals. In this role you will work as a member of our sales team to identify, nurture and close opportunities with new and existing customers, presenting our product to them and helping them with Proof of Concepts. We would appreciate somebody with an open source background!</p>
+<p>This position would ideally be based in Stuttgart, Hamburg, Frankfurt or Munich but we are open to remote work.</p>
+<p>Responsibilities:</p>
 <ul>
-	<li><?php echo $l->t('Present and sell Nextcloud products');?></li>
-	<li><?php echo $l->t('Meet sales goals and represent and develop our brand, product and value proposition');?></li>
-	<li><?php echo $l->t('Identify and build new relationships, and strengthen existing relationships with our customers');?></li>
-	<li><?php echo $l->t('Provide credibility to customers and analyze their business and technical requirements');?></li>
-	<li><?php echo $l->t('Work with customers on Proof of Concept deployments');?></li>
-	<li><?php echo $l->t('Present the product and answer technical questions.');?></li>
+	<li>Present and sell Nextcloud products</li>
+	<li>Meet sales goals and represent and develop our brand, product and value proposition</li>
+	<li>Identify and build new relationships, and strengthen existing relationships with our customers</li>
+	<li>Provide credibility to customers and analyze their business and technical requirements</li>
+	<li>Work with customers on Proof of Concept deployments</li>
+	<li>Present the product and answer technical questions.</li>
 </ul>
-<p><?php echo $l->t('Required skills:');?></p>
+<p>Required skills:</p>
 <ul>
-	<li><?php echo $l->t('Sales experience and a track record of success in the IT industry');?></li>
-	<li><?php echo $l->t('Bachelor\'s degree, ideally in marketing, business or a technical discipline');?></li>
-	<li><?php echo $l->t('A focus on solutions sales in a competitive environment');?></li>
-	<li><?php echo $l->t('experience with cloud, storage, groupware, communication and/or collaboration technologies is a plus');?></li>
-	<li><?php echo $l->t('Insight in benefits of a open source support and service business model');?></li>
-	<li><?php echo $l->t('Writing and speaking German language');?></li>
+	<li>Sales experience and a track record of success in the IT industry</li>
+	<li>Bachelor\'s degree, ideally in marketing, business or a technical discipline</li>
+	<li>A focus on solutions sales in a competitive environment</li>
+	<li>experience with cloud, storage, groupware, communication and/or collaboration technologies is a plus</li>
+	<li>Insight in benefits of a open source support and service business model</li>
+	<li>Writing and speaking German language</li>
 </ul>
-<p><?php echo $l->t('What we offer:');?></p>
+<p>What we offer:</p>
 <ul>
-	<li><?php echo $l->t('A distributed, open team with a passion for working on and within the open source community');?></li>
-	<li><?php echo $l->t('Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community');?></li>
-	<li><?php echo $l->t('A competitive salary');?></li>
-	<li><?php echo $l->t('Flexibility for a healthy work-life balance');?></li>
-	<li><?php echo $l->t('Time for personal growth and education');?></li>
+	<li>A distributed, open team with a passion for working on and within the open source community</li>
+	<li>Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community</li>
+	<li>A competitive salary</li>
+	<li>Flexibility for a healthy work-life balance</li>
+	<li>Time for personal growth and education</li>
 </ul>
-<p><?php echo $l->t('We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.');?></p>
-<h3 class="jobs"><?php echo $l->t('Linux Kernel and Embedded Engineer');?></h3>
-<p><?php echo $l->t('To satisfy the demand for our Spreedbox, we are seeking a Linux kernel and embedded development expert to join our team in Stuttgart (but we are open to remote work!). In this role you are responsible for the technical foundation of this product.');?></p>
-<p><?php echo $l->t('Responsibilities:');?></p>
+<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
+<h3 class="jobs">Linux Kernel and Embedded Engineer</h3>
+<p>To satisfy the demand for our Spreedbox, we are seeking a Linux kernel and embedded development expert to join our team in Stuttgart (but we are open to remote work!). In this role you are responsible for the technical foundation of this product.</p>
+<p>Responsibilities:</p>
 <ul>
-	<li><?php echo $l->t('Software development for Linux and C/C++');?></li>
-	<li><?php echo $l->t('Linux kernel driver development');?></li>
-	<li><?php echo $l->t('Documentation');?></li>
-	<li><?php echo $l->t('Build system maintenance');?></li>
-	<li><?php echo $l->t('Image building');?></li>
+	<li>Software development for Linux and C/C++</li>
+	<li>Linux kernel driver development</li>
+	<li>Documentation</li>
+	<li>Build system maintenance</li>
+	<li>Image building</li>
 </ul>
-<p><?php echo $l->t('Required skills:');?></p>
+<p>Required skills:</p>
 <ul>
-	<li><?php echo $l->t('Experience with Linux and kernel driver development');?></li>
-	<li><?php echo $l->t('C/C++ and low level userland development experience');?></li>
-	<li><?php echo $l->t('German and English language skills');?></li>
+	<li>Experience with Linux and kernel driver development</li>
+	<li>C/C++ and low level userland development experience</li>
+	<li>German and English language skills</li>
 
 </ul>
-<p><?php echo $l->t('What we offer:');?></p>
+<p>What we offer:</p>
 <ul>
-	<li><?php echo $l->t('An open team with a passion for working on and within the open source community');?></li>
-	<li><?php echo $l->t('Working on a product pushing the boundaries of technology and with a global user and contributor community');?></li>
-	<li><?php echo $l->t('A competitive salary');?></li>
-	<li><?php echo $l->t('Flexibility for a healthy work-life balance');?></li>
-	<li><?php echo $l->t('Time for personal growth and education');?></li>
+	<li>An open team with a passion for working on and within the open source community</li>
+	<li>Working on a product pushing the boundaries of technology and with a global user and contributor community</li>
+	<li>A competitive salary</li>
+	<li>Flexibility for a healthy work-life balance</li>
+	<li>Time for personal growth and education</li>
 </ul>
-<p><?php echo $l->t('We are looking for all people open to learning and growing into this role! If you are interested, shoot us an email at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a>.');?></p>
-<h3><?php echo $l->t('And more');?></h3>
-<p><?php echo $l->t('We are always looking for talented new people in all areas of our business--from engineering and support to sales and marketing. Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> and tell us how you can help!');?></p>
+<p>We are looking for all people open to learning and growing into this role! If you are interested, shoot us an email at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a>.</p>
+<h3>And more</h3>
+<p>We are always looking for talented new people in all areas of our business--from engineering and support to sales and marketing. Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> and tell us how you can help!</p>
