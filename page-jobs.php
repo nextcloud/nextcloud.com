796f38d6232ee3cdf1e33d87e3dddb63cff0e1f3
Jos Poortvliet <jospoortvliet@gmail.com>
2016-10-27 19:10:18 +0200

    2 jobs - account manager and sales engineer

--- page-jobs.php
+++ page-jobs.php
@@ -2,31 +2,50 @@
 <div class="page-header">
 	<h1>We are hiring!</h1>
 </div>
-<p>Nextcloud is looking for people who want to join our team.</p>
+<p>Nextcloud is looking for people who want to <a href="<?php echo home_url('team') ?>">join our team.</a></p>
 <h2>Company description</h2>
 <p>We are a modern, distributed, open source company. It is our mission to provide our users and customers with the means for data storage, access and communication under their control; protecting their privacy and security.</p>
-<p>We pursue a fully-open strategy, keeping internal communication to a minimum while aiming to work, discuss and develop in public. We are a thriving, quickly growing company with a solid customer base.</p>
+<p>We pursue a fully-open strategy, keeping internal communication to a minimum while aiming to work, discuss and develop in public. We are a thriving, quickly growing company with a solid customer base. An open source background or experience contributing to Nextcloud is a big plus for job applicants!</p>
+<a name="1"></a>
 <h2>Job openings</h2>
-<h3 class="jobs">Sales Engineer / Manager DACH/Europe</h3>
-<p>Nextcloud is getting an overwhelming response in the market and thus we are looking for another (junior) sales engineer and manager who can help sell our product and services in multiple verticals. In this role you will work as a member of our sales team to identify, nurture and close opportunities with new and existing customers, presenting our product to them and helping them with Proof of Concepts. We would appreciate somebody with an open source background!</p>
-<p>This position would ideally be based in Stuttgart, Hamburg, Frankfurt or Munich but we are open to remote work.</p>
+<p>We have the following job openings:</p>
+<p><a href="#1">Account Manager</a> | <a href="#2">Sales Engineer/Consultant</a> | <a href="#3">Linux Kernel and Embedded Engineer</a></p>
+<h3 class="jobs">Account Manager<!-- DACH/Europe--></h3>
+<p>We are looking for a passionate Account Manager who will partner with and ensure the long-term success of our customers.</p>
+<p>Your account management responsibilities include developing long-term relationships with your portfolio of assigned customers, connecting with key business executives and stakeholders. You will liaise between customers and cross-functional internal teams to ensure the timely and successful delivery of our solutions according to customer needs.</p>
+<!-- <p>This position would ideally be based in Stuttgart, Hamburg, Frankfurt or Munich but we are open to remote work.</p> -->
 <p>Responsibilities:</p>
 <ul>
-	<li>Present and sell Nextcloud products</li>
-	<li>Meet sales goals and represent and develop our brand, product and value proposition</li>
-	<li>Identify and build new relationships, and strengthen existing relationships with our customers</li>
-	<li>Provide credibility to customers and analyze their business and technical requirements</li>
-	<li>Work with customers on Proof of Concept deployments</li>
-	<li>Present the product and answer technical questions.</li>
+	<li>Operate as the lead point of contact for any and all matters
+    specific to your customers</li>
+	<li>Collaborate with our Partners in delivering the best solution to our
+    customers</li>
+	<li>Build and maintain strong, long-lasting customer relationships</li>
+	<li>Ensure the timely and successful delivery of our solutions according
+    to customer needs and objectives</li>
+	<li>Communicate clearly the progress of monthly/quarterly initiatives
+    internally</li>
+	<li>Forecast and track key account metrics</li>
+	<li>negotiating tender and contract terms and conditions to meet both
+    client and company needs</li>
+	<li>Identify and grow opportunities within territory and collaborate
+    with sales teams to ensure growth attainment</li>
+	<li>Assist with high severity requests or issue escalations as needed</li>
 </ul>
 <p>Required skills:</p>
 <ul>
-	<li>Sales experience and a track record of success in the IT industry</li>
-	<li>Bachelor\'s degree, ideally in marketing, business or a technical discipline</li>
-	<li>A focus on solutions sales in a competitive environment</li>
-	<li>experience with cloud, storage, groupware, communication and/or collaboration technologies is a plus</li>
-	<li>Insight in benefits of a open source support and service business model</li>
-	<li>Writing and speaking German language</li>
+	<li>Proven account management or other relevant experience</li>
+	<li>Demonstrated ability to communicate, present and influence credibly
+    and effectively at all levels of the organization, including
+    executive and C-level</li>
+	<li>Experience in delivering client-focused solutions based on customer
+    needs</li>
+	<li>Proven ability to manage multiple projects at a time while paying
+    strict attention to prioritize</li>
+	<li>Excellent listening, negotiation and presentation skills</li>
+	<li>Excellent verbal and written communications skills</li>
+	<li>Problem solving skills</li>
+<!-- 	<li>Writing and speaking German language</li> -->
 </ul>
 <p>What we offer:</p>
 <ul>
@@ -34,6 +53,51 @@
 	<li>Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community</li>
 	<li>A competitive salary</li>
 	<li>Flexibility for a healthy work-life balance</li>
+<a name="2"></a>
+	<li>Time for personal growth and education</li>
+</ul>
+<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
+<h3 class="jobs">Sales Engineer/Consultant<!-- DACH/Europe--></h3>
+<p>Technical sales engineers use their technical knowledge along with sales skills to provide advice and support on a range of products, for which a certain level of expertise is needed.</p>
+<p>You work close together with sales and engineering and assist colleagues with bids and tenders for new clients from a technical perspective, working on proof of concept deployments and answering questions.</p>
+<p>You like to work independently and self responsible on dedicated projects but like to also to exchange your knowledge with your colleagues.</p>
+<!-- <p>This position would ideally be based in Stuttgart, Hamburg, Frankfurt or Munich but we are open to remote work.</p> -->
+<p>Responsibilities:</p>
+<ul>
+	<li>persuading clients that a product or service best satisfies their needs in terms of quality, price and delivery</li>
+	<li>working closely together with sales on customer projects</li>
+	<li>calculating client quotations</li>
+	<li>providing pre-sales technical assistance and product education</li>
+	<li>working on after-sales support services and providing technical backup as required</li>
+	<li>arranging and carrying out product training</li>
+	<li>support POC and Pilots at the customers together with support and engineering</li>
+	<li>supporting sales and marketing activities by attending trade shows, conferences and other marketing events</li>
+	<li>making technical presentations and demonstrating how a product meets client needs</li>
+	<li>liaising with other members of the sales team and other technical experts</li>
+	<li>helping in the design of custom-made products</li>
+	<li>providing training and producing support material for other members of the sales team</li>
+</ul>
+<p>Required skills:</p>
+<ul>
+	<li>Solid technical background for infrastructure set-ups in medium and large organizations</li>
+	<li>Sales skills</li>
+	<li>Sound judgement and good business sense</li>
+	<li>Organizational skills</li>
+	<li>Teamworking capabilities</li>
+	<li>Analytical and problem-solving skills</li>
+	<li>Independence and self-reliance.</li>
+	<li>Problem solving</li>
+	<li>Product knowledge around open source technology</li>
+	<li>General programming skills in PHP, HTML, CSS</li>
+<!-- 	<li>Writing and speaking German language</li> -->
+</ul>
+<p>What we offer:</p>
+<ul>
+	<li>A distributed, open team with a passion for working on and within the open source community</li>
+	<li>Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community</li>
+	<li>A competitive salary</li>
+	<li>Flexibility for a healthy work-life balance</li>
+<a name="3"></a>
 	<li>Time for personal growth and education</li>
 </ul>
 <p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
