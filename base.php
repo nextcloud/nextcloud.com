f891880e94dfa3c17790a95524b9ffb8be956183
Lukas Reschke <lukas@statuscode.ch>
2016-11-04 17:48:47 +0100

    Better syncing

--- base.php
+++ base.php
@@ -6,145 +6,216 @@ require_once __DIR__ . '/l10n.php';
 // Base file to handle each page request
 require get_template_directory().'/config.php';
 require get_template_directory().'/strings.php';
+$html = '';
+
 // If news then include nothing
 if(is_page('oc-news') || is_page('blogfeed')) {
+	ob_clean();
+	ob_start();
 	include roots_template_path();
-} else { 
-
-/**
- * Pages that still use the old layout and haven't been migrated
- */
-$oldPages = [
-	'404',
-	'5years',
-	'about',
-	'advisories',
-	'advisory',
-	'agreement',
-	'android',
-	'appform',
-	'appform-submit',
-	'apply',
-	'apply2',
-	'changelog',
-	'code-of-conduct',
-	'collaboraonline',
-	'community',
-	'conf',
-	'conference-program',
-	'confinfo',
-	'confsubscribe',
-	'connect',
-	'consulting',
-	'contact',
-	'contactform',
-	'contactsubmit',
-	'contribute',
-	'contributors',
-	'design',
-	'desktop',
-	'devices',
-	'events',
-	'faq',
-	'features',
-	'federation',
-	'hackathon',
-	'history',
-	'impressum',
-	'install',
-	'install-backup',
-	'meetups',
-	'news',
-	'newsletter',
-	'nine',
-	'pidrive',
-	'policy',
-	'pr20160602',
-	'pr20160614',
-	'pr20160705',
-	'pr20160719',
-	'pr20160721',
-	'pr20160725',
-	'pr20160812',
-	'pr20160825',
-	'pr20160901',
-	'pr20160916',
-	'pr20160929',
-	'pr20161027',
-	'press',
-	'privacy',
-	'promote',
-	'providers',
-	'providersubmit',
-	'release-channels',
-	'salessubmit',
-	'securesharesubmit',
-	'securesharing',
-	'security',
-	'sharing',
-	'speaking',
-	'spreedbox',
-	'support',
-	'team',
-	'thanks',
-	'thankyou',
-	'theming',
-	'threat-model',
-	'trademarks',
-	'translation',
-	'user',
-	'webrtc',
-	'workflow'
-
-];
-function is_blog () {
-	global  $post;
-	$posttype = get_post_type($post );
-	return ( ((is_archive()) || (is_author()) || (is_category()) || (is_home()) || (is_single()) || (is_tag())) && ( $posttype == 'post')  ) ? true : false ;
-}
-
-$currentPage = strtolower(get_post()->post_name);
-$oldPage = true;
-if(in_array($currentPage, $oldPages) || is_blog()) {
-	// It's an old page, use the old template
-	get_template_part('templates/head-old');
+	$html .= ob_get_contents();
+	ob_end_clean();
 } else {
-	// It's a new page, use the new template
-	get_template_part('templates/head');
-	$oldPage = false;
+
+	/**
+	 * Pages that still use the old layout and haven't been migrated
+	 */
+	$oldPages = [
+		'404',
+		'5years',
+		'about',
+		'advisories',
+		'advisory',
+		'agreement',
+		'android',
+		'appform',
+		'appform-submit',
+		'apply',
+		'apply2',
+		'changelog',
+		'code-of-conduct',
+		'collaboraonline',
+		'community',
+		'conf',
+		'conference-program',
+		'confinfo',
+		'confsubscribe',
+		'connect',
+		'consulting',
+		'contact',
+		'contactform',
+		'contactsubmit',
+		'contribute',
+		'contributors',
+		'design',
+		'desktop',
+		'devices',
+		'events',
+		'faq',
+		'features',
+		'federation',
+		'hackathon',
+		'history',
+		'impressum',
+		'install',
+		'install-backup',
+		'meetups',
+		'news',
+		'newsletter',
+		'nine',
+		'pidrive',
+		'policy',
+		'pr20160602',
+		'pr20160614',
+		'pr20160705',
+		'pr20160719',
+		'pr20160721',
+		'pr20160725',
+		'pr20160812',
+		'pr20160825',
+		'pr20160901',
+		'pr20160916',
+		'pr20160929',
+		'pr20161027',
+		'press',
+		'privacy',
+		'promote',
+		'providers',
+		'providersubmit',
+		'release-channels',
+		'salessubmit',
+		'securesharesubmit',
+		'securesharing',
+		'security',
+		'sharing',
+		'speaking',
+		'spreedbox',
+		'support',
+		'team',
+		'thanks',
+		'thankyou',
+		'theming',
+		'threat-model',
+		'trademarks',
+		'translation',
+		'user',
+		'webrtc',
+		'workflow'
+
+	];
+	function is_blog()
+	{
+		global $post;
+		$posttype = get_post_type($post);
+		return (((is_archive()) || (is_author()) || (is_category()) || (is_home()) || (is_single()) || (is_tag())) && ($posttype == 'post')) ? true : false;
+	}
+
+	$currentPage = strtolower(get_post()->post_name);
+	$oldPage = true;
+	if (in_array($currentPage, $oldPages) || is_blog()) {
+		// It's an old page, use the old template
+		ob_clean();
+		ob_start();
+		get_template_part('templates/head-old');
+		$html .= ob_get_contents();
+		ob_end_clean();
+	} else {
+		// It's a new page, use the new template
+		ob_clean();
+		ob_start();
+		get_template_part('templates/head');
+		$html .= ob_get_contents();
+		ob_end_clean();
+		$oldPage = false;
+	}
+
+	$html .= '<body ' . body_class() . '>';
+
+	ob_clean();
+	ob_start();
+	get_template_part('templates/header-top-navbar');
+	$html .= ob_get_contents();
+	ob_end_clean();
+
+	// The basic translation for the files
+	$l = new L10N(get_post()->post_name);
+
+	if ($oldPage === true) {
+		$html .= '<div class="wrap container" role="document"><div class="content row">';
+		$html .= '<main class="main ' . roots_main_class() . '; ?>" role="main">';
+		ob_clean();
+		ob_start();
+		include roots_template_path();
+		$html .= ob_get_contents();
+		ob_end_clean();
+		$html .= '</main></div></div>';
+	} else {
+		ob_clean();
+		ob_start();
+		include roots_template_path();
+		$html .= ob_get_contents();
+		ob_end_clean();
+	}
+
+	ob_clean();
+	ob_start();
+	require get_template_directory() . '/templates/footer.php';
+	$html .= ob_get_contents();
+	ob_end_clean();
+
+	if (PIWIKTRACKING) {
+		ob_clean();
+		ob_start();
+		get_template_part('templates/piwik-tracking');
+		$html .= ob_get_contents();
+		ob_end_clean();
+	}
+
+	$html .= '</body>';
+	$html .= '</html>';
 }
 
-?>
-  <body <?php body_class(); ?>>
+// Check if the page is completely translated, if so use the translated one. If not default to the cached value.
+$storeToCache = false;
+$echoed = false;
+$hl = '';
+if(isset($_GET['hl'])) {
+	$hl = strtolower((string)$_GET['hl']);
+	if (ctype_alnum($hl) && strlen($hl) === 2) {
+		$l10nFiles = [
+			get_post()->post_name,
+			'header-navbar',
+			'footer',
+		];
+		$storeToCache = true;
+		foreach($l10nFiles as $file) {
+			$translatedFile = json_decode(file_get_contents(__DIR__ . '/l10n/' . $hl . '/' . $file . '.json'), true);
+			$originalFile = json_decode(file_get_contents(__DIR__ . '/l10n/base/' . $file . '.json'), true);
 
-    <!--[if lt IE 8]>
-      <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
-      </div>
-    <![endif]-->
+			if(!is_array($translatedFile) || !is_array($originalFile)) {
+				$storeToCache = false;
+			}
 
-    <?php
-		get_template_part('templates/header-top-navbar');
+			if (array_keys($translatedFile) !== array_keys($originalFile)) {
+				$storeToCache = false;
+			}
+		}
 
-		// The basic translation for the files
-		$l = new L10N(get_post()->post_name);
-	?>
-	<?php if($oldPage === true): ?>
-		<div class="wrap container" role="document">
-		  <div class="content row">
-		<main class="main <?php echo roots_main_class(); ?>" role="main">
-		  <?php include roots_template_path(); ?>
-		</main><!-- /.main -->
-		  </div><!-- /.content -->
-		</div><!-- /.wrap -->
-	<?php else: ?>
-		<?php include roots_template_path(); ?>
-	<?php endif; ?>
+		$cacheFile = __DIR__ . '/l10n/cache/' .  md5($_SERVER['REQUEST_URI']);
+		if(file_exists($cacheFile) && $storeToCache === false) {
+			echo file_get_contents($cacheFile);
+			$echoed = true;
+		}
+		if($storeToCache === true) {
+			file_put_contents($cacheFile, $html);
+			if(file_exists($cacheFile)) {
+				echo file_get_contents($cacheFile);
+				$echoed = true;
+			}
+		}
+	}
+}
 
-	<?php require get_template_directory().'/templates/footer.php'; ?>
-
-    <?php if(PIWIKTRACKING) { get_template_part('templates/piwik-tracking'); } ?>
-
-  </body>
-</html>
-<?php } ?>
+if($echoed === false) {
+	echo $html;
+}
