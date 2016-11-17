ca026ec28a43ad9e5cde15224d47861b0d7560b6
Jos Poortvliet <jospoortvliet@gmail.com>
2016-11-17 11:33:40 +0100

    move Windows app a bit up

--- install-instructions.php
+++ install-instructions.php
@@ -159,13 +159,13 @@
 								</div>
 							</div>
 							<p><a href="https://github.com/nextcloud/android"><?php echo $l->t('Find Android Sources here</a>, <a href="https://download.nextcloud.com/android">direct APK download here</a> and <a href="https://github.com/nextcloud/windows-uwp">Windows Mobile sources here</a>.');?></p>
-							<p><?php echo $l->t('Nextcloud is using WebDAV, so you can also try out any other client you want!');?></p>
-							<p><?php echo $l->t('<strong>Note:</strong> The Windows app is still in alpha');?></p>
+							<p><?php echo $l->t('<strong>Note:</strong> The Windows app is still in testing, use at your own risk and <a href="https://github.com/nextcloud/windows-uwp/issues">let us know about your experience</a>:');?></p>
 								<div class="col-xs-12 mobileclientbuttons">
 									<a target="_blank" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_WIN; ?>">
 									<img src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/windowsstore.png">
 									</a>
 								</div>
+							<p><?php echo $l->t('Nextcloud is using WebDAV, so you can also try out any other client you want!');?></p>
 								<p><?php echo $l->t('Compatible third party clients include:');?></p>
 								<ul>
 									<li><a href="https://davdroid.bitfire.at/">DAVDroid</a> <?php echo $l->t('supports Nextcloud Calendar, Tasks and Contacts, syncing your agenda, tasks and addressbook (android only, open source)');?></li>
