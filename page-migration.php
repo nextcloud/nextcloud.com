f0d652fc3cb9813cb00dd67d81abdd6932e87950
Jos Poortvliet <jospoortvliet@gmail.com>
2016-11-17 11:44:42 +0100

    fix URL's

--- page-migration.php
+++ page-migration.php
@@ -19,7 +19,7 @@
 	</ul>
 	<p><?php echo $l->t('Find more details in');?> <a href="<?php echo $DOCUMENTATION_ADMIN; ?>maintenance/manual_upgrade.html" target="_blank" rel="tooltip" title="<?php echo $l->t('Nextcloud administrator Documentation">our documentation.</a>');?></p>
 
-	<p><?php echo $l->t('Note that, as ownCloud does not support skipping releases, upgrades have to go through all releases between the existing ownCloud release and the desired Nextcloud release. We will support upgrades from ownCloud 8.2 and onwards. If you are on a older release, <a href="mailto:support@nextcloud.com">contact our support team</a> or <a href="/contact">ask sales for an offer.</a>');?></p>
+	<p><?php echo $l->t('Note that, as ownCloud does not support skipping releases, upgrades have to go through all releases between the existing ownCloud release and the desired Nextcloud release. We will support upgrades from ownCloud 8.2 and onwards. If you are on a older release, <a href="mailto:support@nextcloud.com">contact our support team</a> or');?> <a href="<?php echo home_url('contact') ?>"><?php echo $l->t('ask sales for an offer.</a>');?></p>
 	<h2><?php echo $l->t('Upgrade path');?></h2>
 	<p><?php echo $l->t('Depending on your current ownCloud™ release, you have to follow a different upgrade path. These are some examples. The minor release (.x below) should always be the latest bugfix release.');?></p>
 	<ul>
@@ -27,7 +27,7 @@
 	<li><?php echo $l->t('ownCloud 8.2.x -> Nextcloud 9.0.x -> Nextcloud 10.0.x');?></li>
 	</ul>
 	<h2><?php echo $l->t('License Compliance');?></h2>
-	<p><?php echo $l->t('Nextcloud is licensed exclusively under the AGPL or less restrictive open source licenses, removing any risk of mixing incompatible licenses or other legal issues. If you have not made any changes to the code of ownCloud Server™ and did not develop custom ownCloud applications, you will not need to do anything to be in compliance with the open source licenses of Nextcloud. Note that theme changes or branding are unaffected by AGPL licensing. Find more information in our FAQ. Our sales team will be happy to <a href="/contact">answer any questions you have.</a>');?></p>
+	<p><?php echo $l->t('Nextcloud is licensed exclusively under the AGPL or less restrictive open source licenses, removing any risk of mixing incompatible licenses or other legal issues. If you have not made any changes to the code of ownCloud Server™ and did not develop custom ownCloud applications, you will not need to do anything to be in compliance with the open source licenses of Nextcloud. Note that theme changes or branding are unaffected by AGPL licensing. Find more information in our FAQ. Our sales team will be happy to <a href="');?><?php echo home_url('contact') ?>"><?php echo $l->t('answer any questions you have.</a>');?></p>
 
 	<h2><?php echo $l->t('Take over of support contract');?></h2>
 	<p><?php echo $l->t('Nextcloud will honor any any valid contract with ownCloud which has started before the 2nd of June for free if a new agreement is signed for a period of at least 12 months.');?></p>
