b06ad9e1965d8a12104d1d4e9c9dc272c58d51eb
Jos Poortvliet <jospoortvliet@gmail.com>
2016-12-01 13:54:04 +0100

    fix wrong link

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -43,7 +43,7 @@ $l = new L10N('header-navbar');
 
 				<li class="nav__section"><a class="nav__label"><?php echo $l->t('Enterprise');?></a>
 					<ul class="nav__links">
-						<li class="nav__item"><a href="<?php echo home_url('pricing') ?>"><?php echo $l->t('Pricing');?></a></li>
+						<li class="nav__item"><a href="<?php echo home_url('enterprise') ?>"><?php echo $l->t('Pricing');?></a></li>
 						<li class="nav__item"><a href="<?php echo home_url('buy') ?>"><?php echo $l->t('Get an offer');?></a></li>
 					</ul>
 				</li>
