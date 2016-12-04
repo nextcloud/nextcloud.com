63ecbc7219385b67b73d5cb7d2369592f1c750ea
Espina2 <expina2@hotmail.com>
2016-12-04 17:17:09 +0000

    added dropdown button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,35 @@
 
-// Main Fadein animation function
 $(document).ready(function() {
+    "use strict";
+    var defaultComponents = {
+        init: function() {
 
-$(".button--dropdown").on("click", function(event) {
-	$(".dropdown-content").addClass("visible");
-	event.preventDefault();
+			this.variables.buttonDropdownSelector.on("click", _.bind(this.buttonDropdown, this))
+		},
+
+		variables : {
+			buttonDropdownSelector: $(".button--dropdown"),
+			buttonDropdownContent: $(".dropdown-content"),
+			visibleClass : "visible",
+			activeClass: "active"
+		},
+
+		buttonDropdown: function (event) {
+			this.variables.buttonDropdownSelector.toggleClass(this.variables.activeClass);
+			this.variables.buttonDropdownContent.toggleClass(this.variables.visibleClass);
+			event.preventDefault();
+		}
+
+		}
+    defaultComponents.init();
 });
+
+
+
+
+
+
+
 	$(window).on('scroll.fadeOnce', function(event) {	
 		var scrollTop = $(this).scrollTop();
 		$('.revealOnScroll:not(.fade-in)').each(function(index, element) {
@@ -15,4 +39,4 @@ $(".button--dropdown").on("click", function(event) {
 			}
 		 });
 	});	
-});	
+	
