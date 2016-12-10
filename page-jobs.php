0bf9fdc858bd369bc341186b3cf9738741db599f
Espina2 <expina2@hotmail.com>
2016-12-10 18:46:20 +0000

    fix issue #293

--- page-jobs.php
+++ page-jobs.php
@@ -20,139 +20,109 @@
 
 <section class="section-jobs-opening">
 	<div class="container">
-
 		<h2 class="text-center revealOnScroll">Job openings</h2>
-
 		<div class="wrapper-text">
-
-	<!-- 	<p>We have the following job openings:</p> -->
-
-		<a data-target="#accountmgr" data-toggle="collapse"><h3 class="jobs">Account Manager<span class="icon-arrow"></span></h3></a>
-
-		<div id="accountmgr" class="collapse">
-
-			<p>We are looking for a passionate Account Manager who will partner with and ensure the long-term success of our customers.</p>
-
-			<p>Your account management responsibilities include developing long-term relationships with your portfolio of assigned customers, connecting with key business executives and stakeholders. You will liaise between customers and cross-functional internal teams to ensure the timely and successful delivery of our solutions according to customer needs.</p>
-
-			<!-- <p>This position would ideally be based in Stuttgart, Hamburg, Frankfurt or Munich but we are open to remote work.</p> -->
-			<p>Responsibilities:</p>
-
-			<ul>
-				<li>Operate as the lead point of contact for any and all matters
-				specific to your customers</li>
-
-				<li>Collaborate with our Partners in delivering the best solution to our
-				customers</li>
-
-				<li>Build and maintain strong, long-lasting customer relationships</li>
-
-				<li>Ensure the timely and successful delivery of our solutions according
-				to customer needs and objectives</li>
-
-				<li>Communicate clearly the progress of monthly/quarterly initiatives
-				internally</li>
-
-				<li>Forecast and track key account metrics</li>
-
-				<li>negotiating tender and contract terms and conditions to meet both
-				client and company needs</li>
-
-				<li>Identify and grow opportunities within territory and collaborate
-				with sales teams to ensure growth attainment</li>
-
-				<li>Assist with high severity requests or issue escalations as needed</li>
-			</ul>
-
-			<p>Required skills:</p>
-
-			<ul>
-				<li>Proven account management or other relevant experience</li>
-
-				<li>Demonstrated ability to communicate, present and influence credibly
-				and effectively at all levels of the organization, including
-				executive and C-level</li>
-
-				<li>Experience in delivering client-focused solutions based on customer
-				needs</li>
-
-				<li>Proven ability to manage multiple projects at a time while paying
-				strict attention to prioritize</li>
-
-				<li>Excellent listening, negotiation and presentation skills</li>
-
-				<li>Excellent verbal and written communications skills</li>
-
-				<li>Problem solving skills</li>
-
-				<li>Writing and speaking German language</li>
-			</ul>
-
-			<p>What we offer:</p>
 			
-			<ul>
-				<li>A distributed, open team with a passion for working on and within the open source community</li>
+			<a href="#accountmgr" data-toggle="collapse"><h3 class="jobs">Account Manager<span class="icon-arrow"></span></h3></a>
+			<div id="accountmgr" class="collapse">
+				<p>We are looking for a passionate Account Manager who will partner with and ensure the long-term success of our customers.</p>
+				<p>Your account management responsibilities include developing long-term relationships with your portfolio of assigned customers, connecting with key business executives and stakeholders. You will liaise between customers and cross-functional internal teams to ensure the timely and successful delivery of our solutions according to customer needs.</p>
+				<p>Responsibilities:</p>
 
-				<li>Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community</li>
+				<ul>
+					<li>Operate as the lead point of contact for any and all matters
+					specific to your customers</li>
+					<li>Collaborate with our Partners in delivering the best solution to our
+					customers</li>
+					<li>Build and maintain strong, long-lasting customer relationships</li>
+					<li>Ensure the timely and successful delivery of our solutions according
+					to customer needs and objectives</li>
+					<li>Communicate clearly the progress of monthly/quarterly initiatives
+					internally</li>
+					<li>Forecast and track key account metrics</li>
+					<li>negotiating tender and contract terms and conditions to meet both
+					client and company needs</li>
+					<li>Identify and grow opportunities within territory and collaborate
+					with sales teams to ensure growth attainment</li>
+					<li>Assist with high severity requests or issue escalations as needed</li>
+				</ul>
 
-				<li>A competitive salary</li>
+				<p>Required skills:</p>
 
-				<li>Flexibility for a healthy work-life balance</li>
+				<ul>
+					<li>Proven account management or other relevant experience</li>
+					<li>Demonstrated ability to communicate, present and influence credibly
+					and effectively at all levels of the organization, including
+					executive and C-level</li>
+					<li>Experience in delivering client-focused solutions based on customer
+					needs</li>
+					<li>Proven ability to manage multiple projects at a time while paying
+					strict attention to prioritize</li>
+					<li>Excellent listening, negotiation and presentation skills</li>
+					<li>Excellent verbal and written communications skills</li>
+					<li>Problem solving skills</li>
+					<li>Writing and speaking German language</li>
+				</ul>
 
-				<li>Time for personal growth and education</li>
-			</ul>
+				<p>What we offer:</p>
+				
+				<ul>
+					<li>A distributed, open team with a passion for working on and within the open source community</li>
+					<li>Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community</li>
+					<li>A competitive salary</li>
+					<li>Flexibility for a healthy work-life balance</li>
+					<li>Time for personal growth and education</li>
+				</ul>
 
-			<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
-		</div>
+				<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
+			</div>
 
-		<a data-target="#saleseng" data-toggle="collapse"><h3 class="jobs">Sales Engineer/ Consultant<span class="icon-arrow"></span></h3></a>
-		
-		<div id="saleseng" class="collapse">
-			<p>Technical sales engineers use their technical knowledge along with sales skills to provide advice and support on a range of products, for which a certain level of expertise is needed.</p>
-			<p>You work close together with sales and engineering and assist colleagues with bids and tenders for new clients from a technical perspective, working on proof of concept deployments and answering questions.</p>
-			<p>You like to work independently and self responsible on dedicated projects but like to also to exchange your knowledge with your colleagues.</p>
-			<!-- <p>This position would ideally be based in Stuttgart, Hamburg, Frankfurt or Munich but we are open to remote work.</p> -->
-			<p>Responsibilities:</p>
-			<ul>
-				<li>persuading clients that a product or service best satisfies their needs in terms of quality, price and delivery</li>
-				<li>working closely together with sales on customer projects</li>
-				<li>calculating client quotations</li>
-				<li>providing pre-sales technical assistance and product education</li>
-				<li>working on after-sales support services and providing technical backup as required</li>
-				<li>arranging and carrying out product training</li>
-				<li>support POC and Pilots at the customers together with support and engineering</li>
-				<li>supporting sales and marketing activities by attending trade shows, conferences and other marketing events</li>
-				<li>making technical presentations and demonstrating how a product meets client needs</li>
-				<li>liaising with other members of the sales team and other technical experts</li>
-				<li>helping in the design of custom-made products</li>
-				<li>providing training and producing support material for other members of the sales team</li>
-			</ul>
-			<p>Required skills:</p>
-			<ul>
-				<li>Solid technical background for infrastructure set-ups in medium and large organizations</li>
-				<li>Sales skills</li>
-				<li>Sound judgement and good business sense</li>
-				<li>Organizational skills</li>
-				<li>Teamworking capabilities</li>
-				<li>Analytical and problem-solving skills</li>
-				<li>Independence and self-reliance.</li>
-				<li>Problem solving</li>
-				<li>Product knowledge around open source technology</li>
-				<li>General programming skills in PHP, HTML, CSS</li>
-				<li>Writing and speaking German language</li>
-			</ul>
-			<p>What we offer:</p>
-			<ul>
-				<li>A distributed, open team with a passion for working on and within the open source community</li>
-				<li>Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community</li>
-				<li>A competitive salary</li>
-				<li>Flexibility for a healthy work-life balance</li>
-				<li>Time for personal growth and education</li>
-			</ul>
-			<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
-		</div>
+			<a href="#saleseng" data-toggle="collapse"><h3 class="jobs">Sales Engineer/ Consultant<span class="icon-arrow"></span></h3></a>
+			<div id="saleseng" class="collapse">
+				<p>Technical sales engineers use their technical knowledge along with sales skills to provide advice and support on a range of products, for which a certain level of expertise is needed.</p>
+				<p>You work close together with sales and engineering and assist colleagues with bids and tenders for new clients from a technical perspective, working on proof of concept deployments and answering questions.</p>
+				<p>You like to work independently and self responsible on dedicated projects but like to also to exchange your knowledge with your colleagues.</p>
+				<p>Responsibilities:</p>
+				<ul>
+					<li>persuading clients that a product or service best satisfies their needs in terms of quality, price and delivery</li>
+					<li>working closely together with sales on customer projects</li>
+					<li>calculating client quotations</li>
+					<li>providing pre-sales technical assistance and product education</li>
+					<li>working on after-sales support services and providing technical backup as required</li>
+					<li>arranging and carrying out product training</li>
+					<li>support POC and Pilots at the customers together with support and engineering</li>
+					<li>supporting sales and marketing activities by attending trade shows, conferences and other marketing events</li>
+					<li>making technical presentations and demonstrating how a product meets client needs</li>
+					<li>liaising with other members of the sales team and other technical experts</li>
+					<li>helping in the design of custom-made products</li>
+					<li>providing training and producing support material for other members of the sales team</li>
+				</ul>
+				<p>Required skills:</p>
+				<ul>
+					<li>Solid technical background for infrastructure set-ups in medium and large organizations</li>
+					<li>Sales skills</li>
+					<li>Sound judgement and good business sense</li>
+					<li>Organizational skills</li>
+					<li>Teamworking capabilities</li>
+					<li>Analytical and problem-solving skills</li>
+					<li>Independence and self-reliance.</li>
+					<li>Problem solving</li>
+					<li>Product knowledge around open source technology</li>
+					<li>General programming skills in PHP, HTML, CSS</li>
+					<li>Writing and speaking German language</li>
+				</ul>
+				<p>What we offer:</p>
+				<ul>
+					<li>A distributed, open team with a passion for working on and within the open source community</li>
+					<li>Promoting and selling a product which pushes the boundaries of technology and with a global user and contributor community</li>
+					<li>A competitive salary</li>
+					<li>Flexibility for a healthy work-life balance</li>
+					<li>Time for personal growth and education</li>
+				</ul>
+				<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
+			</div>
 
-		<a data-target="#linuxeng" data-toggle="collapse"><h3 class="jobs">Linux Kernel and Embedded Engineer<span class="icon-arrow"></span></h3></a>
+		<a href="#linuxeng" data-toggle="collapse"><h3 class="jobs">Linux Kernel and Embedded Engineer<span class="icon-arrow"></span></h3></a>
 		<div id="linuxeng" class="collapse">
 			<p>To satisfy the demand for our Spreedbox, we are seeking a Linux kernel and embedded development expert to join our team in Stuttgart (but we are open to remote work!). In this role you are responsible for the technical foundation of this product.</p>
 			<p>Responsibilities:</p>
@@ -180,7 +150,7 @@
 			</ul>
 			<p>We are looking for all people open to learning and growing into this role! If you are interested, shoot us an email at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a>.</p>
 		</div>
-		<a data-target="#qtdev" data-toggle="collapse"><h3 class="jobs">Qt Developer<span class="icon-arrow"></span></h3></a>
+		<a href="#qtdev" data-toggle="collapse"><h3 class="jobs">Qt Developer<span class="icon-arrow"></span></h3></a>
 		<div id="qtdev" class="collapse">
 			<p>We're looking for developers with experience in C++/Qt development to join our client team!</p>
 			<p>You would work with the engineering team to bring new features and improvements to our client for Windows, Linux and Mac. You ideally have open source experience, know your way around git and Github and you live in the CET timezone.</p>
@@ -207,7 +177,8 @@
 			</ul>
 			<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
 		</div>
-		<a data-target="#jsdev" data-toggle="collapse"><h3 class="jobs">Frontend Developer<span class="icon-arrow"></span></h3></a>
+
+		<a href="#jsdev" data-toggle="collapse"><h3 class="jobs">Frontend Developer<span class="icon-arrow"></span></h3></a>
 		<div id="jsdev" class="collapse">
 			<p>We're looking for developers with experience in Javascript front-end development to join our server team!</p>
 			<p>You would work with the engineering team to bring new features and improvements to the server user interface like our File management app or one of the many other applications like Calendar and Contacts, Announcements and more. You ideally have open source experience, know your way around git and Github and you live in the CET timezone.</p>
@@ -235,7 +206,7 @@
 			</ul>
 			<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
 		</div>
-		<a data-target="#phpdev" data-toggle="collapse"><h3 class="jobs">PHP back-end developer<span class="icon-arrow"></span></h3></a>
+		<a href="#phpdev" data-toggle="collapse"><h3 class="jobs">PHP back-end developer<span class="icon-arrow"></span></h3></a>
 		<div id="phpdev" class="collapse">
 			<p>We're looking for developers with experience in PHP back-end development to join our server team!</p>
 			<p>You would work with the engineering team to bring new features and improvements to the server back-end like new authentication or storage mechanisms, audio/video chat and more. You ideally have open source experience, know your way around git and Github and you live in the CET timezone.</p>
@@ -263,7 +234,7 @@
 			</ul>
 			<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
 		</div>
-		<a data-target="#androiddev" data-toggle="collapse"><h3 class="jobs">Android App developer<span class="icon-arrow"></span></h3></a>
+		<a href="#androiddev" data-toggle="collapse"><h3 class="jobs">Android App developer<span class="icon-arrow"></span></h3></a>
 		<div id="androiddev" class="collapse">
 			<p>We're looking for developers with experience in Android app development to join our client team!</p>
 			<p>You would work with the engineering team to bring new features and improvements to the Android app like new authentication mechanisms, audio/video chat integration and more. You ideally have open source experience, know your way around git and Github and you live in the CET timezone.</p>
@@ -291,7 +262,7 @@
 			</ul>
 			<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
 		</div>
-		<a data-target="#iosdev" data-toggle="collapse"><h3 class="jobs">iOS App developer<span class="icon-arrow"></span></h3></a>
+		<a href="#iosdev" data-toggle="collapse"><h3 class="jobs">iOS App developer<span class="icon-arrow"></span></h3></a>
 		<div id="iosdev" class="collapse">
 			<p>We're looking for developers with experience in iOS app development to join our client team!</p>
 			<p>You would work with the engineering team to bring new features and improvements to the iOS app like new authentication mechanisms, audio/video chat integration and more. You ideally have open source experience, know your way around git and Github and you live in the CET timezone.</p>
@@ -319,7 +290,7 @@
 			</ul>
 			<p>We are looking for somebody with ambition and drive, who would like to grow and learn. That means if you do not yet fulfill the requirements but want to learn, apply! Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> with your resume if you are interested in this role.</p>
 		</div>
-		<h3>And more</h3>
+		<h3 class="jobs">And more</h3>
 		<div>
 			<p>We are always looking for talented new people in all areas of our business--from engineering and support to sales and marketing. Contact us at <a href="mailto:jobs@nextcloud.com">jobs@nextcloud.com</a> and tell us how you can help!</p>
 		</div>
