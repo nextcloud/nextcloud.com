d1ac19511f73184cb83c8f7bde4cbb176cb8556e
Lukas Reschke <lukas@statuscode.ch>
2017-01-02 20:53:29 +0100

    Remove redundant tracker

--- templates/piwik-tracking.php
+++ templates/piwik-tracking.php
@@ -7,11 +7,10 @@
     var u="//stats.nextcloud.com/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 1]);
-    _paq.push(['addTracker', 'https://stats.nextcloud.opensourcesoftware.rocks/piwik.php', 4]);
 
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//stats.nextcloud.com/piwik.php?idsite=1" style="border:0;" alt="" /></p><p><img src="//stats.nextcloud.opensourcesoftware.rocks/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="//stats.nextcloud.com/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
