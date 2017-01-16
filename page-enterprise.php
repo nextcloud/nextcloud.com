53152a415be3576a717f06fe2fbd093235eedd4d
Jos Poortvliet <jospoortvliet@gmail.com>
2017-01-16 12:09:40 +0100

    fix translations

--- page-enterprise.php
+++ page-enterprise.php
@@ -6,18 +6,18 @@
 				<h1><?php echo $l->t('Get support from the source');?></h1>
 				<h2><?php echo $l->t('Going beyond the traditional licensing model, Nextcloud frees you from vendor lock-in and provides value through supporting your business\' needs.');?></h2>
 			</div>
-			<div class="col-md-6 topbanner"> <img src="../wp-content/themes/next/assets/img/enterprise/hero-image.png" alt=""></img></div>
+			<div class="col-md-6 topbanner"> <img src="<?php bloginfo('template_directory'); ?>/assets/img/enterprise/hero-image.png" alt=""></img></div>
 			</div>
 		</div>
 	</div>
 	<div class="container-fluid menu" id="menuAnchor">
 		<div class="container buttons">
-			<a href="#expertise">expertise</a>
-			<a href="#security">security</a>
-			<a href="#lifecycle">lifecycle</a>
-			<a href="#capabilities">capabilities</a>
-			<a class="btn btn-primary" href="/pricing">pricing plans</a>
-			<a class="btn btn-primary" href="/buy">get a quote</a>
+			<a href="#expertise"><?php echo $l->t('expertise');?></a>
+			<a href="#security"><?php echo $l->t('security');?></a>
+			<a href="#lifecycle"><?php echo $l->t('lifecycle');?></a>
+			<a href="#capabilities"><?php echo $l->t('capabilities');?></a>
+			<a class="btn btn-primary" href="/pricing"><?php echo $l->t('pricing plans');?></a>
+			<a class="btn btn-primary" href="/buy"><?php echo $l->t('get a quote');?></a>
 		</div>
 	</div>
 </section>
@@ -36,8 +36,8 @@
 		<p class="section--intro text-center revealOnScroll">Matter experts</p>
 		<h3 class="section--heading-1 text-center revealOnScroll">Dedicated to your success</h3>
 		<div class="expertise__ilustration"></div>
-		<p class="section--intro text-center revealOnScroll">We are dedicated to your success as Nextcloud customer. An analysis of your needs, a pilot customized for your architecture and remote installation support ensures a smooth introduction of Nextcloud in your organization and we will help you scale up to meet growing demand in your organization.
-		Nextcloud GmbH employs <a href="https://nextcloud.com/blog/nextcloud-the-most-active-open-source-file-sync-and-share-project/">9 of the top-ten developers in the Nextcloud Server codebase</a>, making our engineering team by far the most qualified to help you get the most out of your Enterprise File Sync and Share solution.</p>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('We are dedicated to your success as Nextcloud customer. An analysis of your needs, a pilot customized for your architecture and remote installation support ensures a smooth introduction of Nextcloud in your organization and we will help you scale up to meet growing demand in your organization.');?></p>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('Nextcloud GmbH employs <a href="https://nextcloud.com/blog/nextcloud-the-most-active-open-source-file-sync-and-share-project/">9 of the top-ten developers in the Nextcloud Server codebase</a>, making our engineering team by far the most qualified to help you get the most out of your Enterprise File Sync and Share solution.');?></p>
 		<p class="section--intro text-center revealOnScroll"><a class="button button--blue button--arrow button--large" href="/team">Our team</a></p>
 	</div>
 </section>
@@ -45,11 +45,11 @@
 <a name="security"></a>
 <section class="section--security">
 	<div class="container">
-		<p class="section--intro text-center revealOnScroll">Top notch security</p>
-		<h3 class="section--heading-1 text-center revealOnScroll">To keep your data safe</h3>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('Top notch security');?></p>
+		<h3 class="section--heading-1 text-center revealOnScroll"><?php echo $l->t('To keep your data safe');?></h3>
 		<div class="security__ilustration"></div>
-		<p class="section--intro text-center revealOnScroll">Nextcloud develops its software with a rigorous focus on security through the entire life cycle of the product. Our active and passive security measures are backed by the some of the highest security bug bounties in the open source industry.
-		As customer you get direct access to our security expertise, with hardening advice and ahead-of-release security issue mitigation and fixes.</p>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('Nextcloud develops its software with a rigorous focus on security through the entire life cycle of the product. Our active and passive security measures are backed by the some of the highest security bug bounties in the open source industry.');?></p>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('As customer you get direct access to our security expertise, with hardening advice and ahead-of-release security issue mitigation and fixes.');?></p>
 		<p class="section--intro text-center revealOnScroll"><a class="button button--blue button--arrow button--large" href="/secure">Learn about security</a></p>
 	</div>
 </section>
@@ -57,11 +57,11 @@
 <a name="lifecycle"></a>
 <section class="section--lifecycle">
 	<div class="container">
-		<p class="section--intro text-center revealOnScroll">Multi-year lifecycle</p>
-		<h3 class="section--heading-1 text-center revealOnScroll">For uninterupted service</h3>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('Multi-year lifecycle');?></p>
+		<h3 class="section--heading-1 text-center revealOnScroll"><?php echo $l->t('For uninterupted service');?></h3>
 		<div class="lifecycle__ilustration"></div>
-		<p class="section--intro text-center revealOnScroll">A Nextcloud support subscription gives you access to enterprise-ready software, updates, and information and support services that span your entire OS and application infrastructure life cycle and architecture. You can count on receiving the latest product versions with the stability and security you need.
-		Working with data is essential for your business and being forced on the upgrade threadmill is disruptive to your organization. With access to up to 15 years security and stability updates to Nextcloud, we match the lifecycle of your operating system so you can schedule upgrades when it works for you and secure the investments you did in your existing platform.</p>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('A Nextcloud support subscription gives you access to enterprise-ready software, updates, and information and support services that span your entire OS and application infrastructure life cycle and architecture. You can count on receiving the latest product versions with the stability and security you need.');?></p>
+		<p class="section--intro text-center revealOnScroll"><?php echo $l->t('Working with data is essential for your business and being forced on the upgrade threadmill is disruptive to your organization. With access to up to 15 years security and stability updates to Nextcloud, we match the lifecycle of your operating system so you can schedule upgrades when it works for you and secure the investments you did in your existing platform.');?></p>
 		<p class="section--intro text-center revealOnScroll"><a class="button button--blue button--arrow button--large" href="/pricing">See pricing</a></p>
 	</div>
 </section>
@@ -88,28 +88,28 @@
         <div class="texts-wrapper col-md-6">
 	        <div class="right-text-grey" data-slide="1">
 				<h5 class="textTrigger" id="feature-1"><?php echo $l->t('Deep integration in your infrastructure');?></h5>
-				<p>Nextcloud offers LDAP/Active Directory, SAML and Kerboros authentication. It accesses data on NFS, (s)FTP, WebDAV, Windows Network Drive, Object Stores like SWIFT and many more.</p>
+				<p><?php echo $l->t('Nextcloud offers LDAP/Active Directory, SAML and Kerboros authentication. It accesses data on NFS, (s)FTP, WebDAV, Windows Network Drive, Object Stores like SWIFT and many more.');?></p>
 					<a class="button button--blue button--arrow button--large" href="<?php echo get_template_directory_uri(); ?>/assets/files/architecture-whitepaper.pdf" role="button"><?php echo $l->t('Architecture Whitepaper');?></a>
 				</p>
 	        </div>
 
 	         <div class="right-text-grey" data-slide="2">
 	            <h5 class="textTrigger" id="feature-2"><?php echo $l->t('Security features keep your data safe');?></h5>
-	           <p>Nextcloud supports two-factor authentication and Server Side Encryption, has brute force protection and features industry-standard security measures like CSP, SSC and password and session management.></p>
+	           <p><?php echo $l->t('Nextcloud supports two-factor authentication and Server Side Encryption, has brute force protection and features industry-standard security measures like CSP, SSC and password and session management.');?></p>
                   <a class="button button--blue button--arrow button--large" href="/secure" role="button"><?php echo $l->t('Learn more about security');?></a>
                </p>
 	        </div>
 
 	        <div class="right-text-grey" data-slide="3">
 	            <h5 class="textTrigger" id="feature-3"><?php echo $l->t('IT stays in control');?></h5>
-	            <p>Our File Access Control app enables administrators to defining data access rules or triggering actions based on location, group membership, file type, tags and other properties.</p>
+	            <p><?php echo $l->t('Our File Access Control app enables administrators to defining data access rules or triggering actions based on location, group membership, file type, tags and other properties.');?></p>
                    <a class="button button--blue button--arrow button--large" href="/workflow/" role="button"><?php echo $l->t('Learn more about Workflow');?></a>
                 </p>
 	        </div>
 
 	        <div class="right-text-grey" data-slide="4">
 	            <h5 class="textTrigger" id="feature-4"><?php echo $l->t('Easy access for users');?></h5>
-	            <p>Users can access and share their data on Nextcloud through sync clients for Windows, Linux and Mac as well as Android and IOS, providing a smooth experience no matter where data is located.</p>
+	            <p><?php echo $l->t('Users can access and share their data on Nextcloud through sync clients for Windows, Linux and Mac as well as Android and IOS, providing a smooth experience no matter where data is located.');?></p>
                    <a class="button button--blue button--arrow button--large" href="/features" role="button"><?php echo $l->t('Learn more about features');?></a>
                 </p>
 	        </div>
