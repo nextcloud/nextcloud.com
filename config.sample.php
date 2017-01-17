889a055063150429229ba22d08ce4b9c6eda6550
Paulo moura <expina2@hotmail.com>
2017-01-17 10:27:49 +0000

    Add files via upload
    Added config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+// Enables CONTRIBOOK (required mysql database)
+define(CONTRIBOOK, 0);
+
+// Defines the author id of the official user posting announcements (used to produce rss feed)
+define(OFFICIALAUTHORID, 1);
+
+// Enables or disables the piwik tracking code
+define(PIWIKTRACKING, 0);
+
+// Recaptcha
+DEFINE(RECAPTCHA_SITEKEY, '');
+DEFINE(RECAPTCHA_SECRET, '');
+?>
