1fa51ed5383826774cb82bdceee7b0c9e9a49605
Espina2 <expina2@hotmail.com>
2017-01-26 14:46:09 +0000

    added until section external storage

--- page-features.php
+++ page-features.php
@@ -19,13 +19,15 @@
 <div class="second-menu">
 	<div class="container-fluid menu" id="menuAnchor">
 		<div class="container buttons">
-			<a href="#files"><?php echo $l->t('File access & sharing');?></a>
-			<a href="#control"><?php echo $l->t('Mobile & desktop clients');?></a>
-			<a href="#lifecycle"><?php echo $l->t('External storage');?></a>
+			<a href="#files"><?php echo $l->t('File access');?></a>
+			<a href="#security"><?php echo $l->t('Security');?></a>
+			<a href="#workflow"><?php echo $l->t('Workflow');?></a>
+			<a href="#clients"><?php echo $l->t('Clients');?></a>
+			<a href="#storage"><?php echo $l->t('External storage');?></a>
 			<a href="#capabilities"><?php echo $l->t('Calendar & Contacts');?></a>
-			<a href="#capabilities"><?php echo $l->t('Secure audio & video calls');?></a>
-			<a href="#capabilities"><?php echo $l->t('Collabora Online Office');?></a>
-			<a href="#capabilities"><?php echo $l->t('Nextcloud in action');?></a>
+			<a href="#capabilities"><?php echo $l->t('Video calls');?></a>
+			<a href="#capabilities"><?php echo $l->t('Collabora');?></a>
+			<a href="#capabilities"><?php echo $l->t('In action');?></a>
 		</div>
 	</div>
 </div>
@@ -51,10 +53,10 @@
 	</div>	
 </section>
 
-<section class="section--control">
-	<a name="control" id="control"></a>
+<section class="section--security">
+	<a name="security" id="security"></a>
 	<div class="container">
-		<div class="col-md-5">
+		<div class="col-md-6">
 			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/TOTP.png"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/TOTP-wee.png" alt="in action" /></a>
 		</div>
 
@@ -64,11 +66,51 @@
 			<p class="section--paragraph"><?php echo $l->t('we follow industry best practices around security (aligned to ISO27001c14).');?></p>
 			<p class="section--paragraph"><?php echo $l->t('we offer some of the <a class="hyperlink" href="https://nextcloud.com/introducing-the-nextcloud-bug-bounty-program/" target="_blank">highest security bug bounties in the open source industry.</a>');?></p>
 			<p class="section--paragraph"><?php echo $l->t('we are confident that Nextcloud offers the best security in the open source file sync and share industry.');?></p>
+			<a href="/sharing" class="button button--blue button--arrow button--large"><?php echo $l->t('Learn more');?></a>
+		</div>
+	</div>
+</section>
+
+<section class="section--workflow">
+	<a name="workflow" id="workflow"></a>
+	<div class="container">
+		<div class="col-md-6 image--floated">
+			<a><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/monitoring-wee.png" alt="in action"/></a>
+		</div>
+			<p class="section--paragraph__tittle"><?php echo $l->t('Workflow management');?></p>
+			<p class="section--paragraph"><?php echo $l->t('With Nextcloud, system administrators can control and direct the flow of data between users on or between servers. Rule based file tagging and responding to these tags as well as other triggers like physical location, user group, file properties and request type enables administrators to specifically deny access to, convert, delete or retain data following business or legal requirements.
+				Nextcloud puts you in control of your data and keeps it safe.');?></p>
 			<a href="/sharing" class="button button--blue button--arrow button--large"><?php echo $l->t('Workflow handling');?></a>
 		</div>
 	</div>
 </section>
 
+<section class="section--clients">
+	<a name="clients" id="clients"></a>
+	<div class="container">
+		<div class="col-md-6">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/TOTP.png"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/TOTP-wee.png" alt="in action" /></a>
+		</div>
+
+		<div class="col-md-6 feature--block">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Mobile and desktop clients');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Nextcloud clients for Android, iOS and desktop systems allow you to sync and share files, in a fully secure way through an encrypted connection. The mobile clients feature automatic upload of pictures and videos you take and can synchronize select files and folders. The clients can handle multiple accounts, show all activity happening on your server and notify you of new events such as the availability of new shares.');?></p>
+		</div>
+	</div>
+</section>
+
+<section class="section--storage">
+	<a name="storage" id="storage"></a>
+	<div class="container">
+		<div class="col-md-6 image--floated">
+			<a><img class="img-responsive featureimg" src="<?php bloginfo('template_directory'); ?>/assets/img/features/externalstorage.png"/></a>
+		</div>
+			<p class="section--paragraph__tittle"><?php echo $l->t('External storage');?></p>
+			<p class="section--paragraph"><?php echo $l->t('The external storage feature of Nextcloud gives you access to your data wherever it is. Nextcloud can access files stored with a wide variety of popular cloud service providers such as Amazon, Google and Dropbox, but you can also access them using standard protocols such as NFS, (S)FTP, WebDAV and more. <br> <br>
+				The Encryption App can encrypt data at rest for both local and remote storage, protecting data stored on networks outside of your infrastructure. Keys can be handled by an external key management server or stored locally, on the server. Nextcloud will keep your data where it is and retrieve it over a secure communication channel whenever you need to access it.');?></p>
+		</div>
+	</div>
+</section>
 
 
 
