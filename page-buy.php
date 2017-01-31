eac59a248124291cd55a5dbf3614569357f168c4
Jos Poortvliet <jospoortvliet@gmail.com>
2017-01-31 14:23:43 +0100

    update to quote form

--- page-buy.php
+++ page-buy.php
@@ -27,21 +27,61 @@
 </section>
 
 <div class="container">
+	<h2><?php echo $l->t('Nextcloud helps you be successful.');?></h2>
+	<p><?php echo $l->t('You run your own Nextcloud server, keeping your data in-house and under control. We make sure it works.');?></p>
 	<div class="contact">
 		<h3><?php echo $l->t('Contact us for more information and a quote fitting your use case.');?></h3>
+		<p><?php echo $l->t('Your answers to the following questions will help us better understand
+your project.');?></p>
 		<form name="sales" method="post" action="../salessubmit/">
-				<p><?php echo $l->t('<label for="yourname">Your name<br>
-				<input  type="text" name="yourname" maxlength="60" size="60"></label>');?></p>
-				<p><?php echo $l->t('<label for="email">Email<br>
-				<input  type="text" name="email" maxlength="80" size="60"></label>');?></p>
-				<p><?php echo $l->t('<label for="organization">Organization<br>
-				<input  type="text" name="organization" maxlength="100" size="60" placeholder="Name of your organization"></label>');?></p>
-				<p><?php echo $l->t('<label for="users">Number of users that need Nextcloud<br>
-				<input  type="text" name="users" maxlength="80" size="60" placeholder="Our pricing starts at 50 users."></label>');?></p>
-				<p><?php echo $l->t('<label for="phone">Phone number<br>
-				<input  type="text" name="phone" maxlength="40" size="60" placeholder="Please include country code"></label>');?></p>
-				<p><?php echo $l->t('<label for="comments">Your message<br />
-				<textarea  name="comments" maxlength="2000" cols="80" rows="8" placeholder="Let us know how we can help you!"></textarea></label>');?></p>
+				<p><label for="yourname"><?php echo $l->t('Your name');?><br>
+				<input  type="text" name="yourname" maxlength="60" size="60"></label></p>
+				<p><label for="email">Email<br>
+				<input  type="text" name="email" maxlength="80" size="60"></label></p>
+				<p><label for="organization"><?php echo $l->t('Organization<br>
+				<input  type="text" name="organization" maxlength="100" size="60" placeholder="Name of your organization">');?></label></p>
+				<p><label for="phone"><?php echo $l->t('Phone number<br>
+				<input  type="text" name="phone" maxlength="40" size="60" placeholder="Please include country code">');?></label></p>
+				<p><label for="users"><?php echo $l->t('Number of users that need Nextcloud<br>
+				<input  type="text" name="users" maxlength="80" size="60" placeholder="Our pricing starts at 50 users.">');?></label></p>
+				<p><label for="users-12months"><?php echo $l->t('Number of users over next 12 months (estimation)<br>
+				<input  type="text" name="users-12months" maxlength="80" size="60" placeholder="Will the number of users grow?">');?></label></p>
+				<p><label for="SLA"><?php echo $l->t('What kind of response time do you require?<br>
+				<textarea name="SLA" maxlength="120" cols="70" rows="2" placeholder="24/7, business hours, within 3 days, etc">');?></textarea></label></p>
+				<p><label for="need-phone-support"><?php echo $l->t('Do you require phone support?');?><br>
+				<select name="need-phone-support">
+					<option value="unclear"><?php echo $l->t('Not sure');?></option>
+					<option value="yes"><?php echo $l->t('Yes');?></option>
+					<option value="no"><?php echo $l->t('No');?></option>
+				</select></label></p>
+				<p><label for="need-support-manager"><?php echo $l->t('Would you need a dedicated Support Account Manager?');?><br>
+				<select name="need-support-manager">
+					<option value="unclear"><?php echo $l->t('Not sure');?></option>
+					<option value="yes"><?php echo $l->t('Yes');?></option>
+					<option value="no"><?php echo $l->t('No');?></option>
+				</select></label></p>
+				<p><label for="need-setup-help"><?php echo $l->t('Would you require assistance to set up the service or to design/review the architecture?');?><br>
+				<select name="need-setup-help">
+					<option value="unclear"><?php echo $l->t('Not sure');?></option>
+					<option value="yes"><?php echo $l->t('Yes');?></option>
+					<option value="no"><?php echo $l->t('No');?></option>
+				</select></label></p>
+				<p><label for="clustering"><?php echo $l->t('Are you using any sort of clustering for the application, database or storage?');?><br>
+				<select name="clustering">
+					<option value="unclear"><?php echo $l->t('Not sure');?></option>
+					<option value="yes"><?php echo $l->t('Yes');?></option>
+					<option value="no"><?php echo $l->t('No');?></option>
+				</select></label></p>
+				<p><label for="collabora"><?php echo $l->t('Would you need to be able to edit office documents online?');?><br>
+				<select name="collabora">
+					<option value="unclear"><?php echo $l->t('Not sure');?></option>
+					<option value="yes"><?php echo $l->t('Yes');?></option>
+					<option value="no"><?php echo $l->t('No');?></option>
+				</select></label></p>
+				<p><label for="LTS"><?php echo $l->t('What maintenance lifecycle is your Nextcloud server on?<br>
+				<textarea name="LTS" maxlength="120" cols="70" rows="2" placeholder="Upgrade every 4 months; once a year; or do you need 5 or 10 year support for your platform?');?>"></textarea></label></p>
+				<p><label for="comments"><?php echo $l->t('Your message<br />
+				<textarea  name="comments" maxlength="2000" cols="80" rows="8" placeholder="Let us know how we can help you!">');?></textarea></label></p>
 				<td colspan="2" style="text-align:center">
 				<div class="g-recaptcha" data-sitekey="<?php echo RECAPTCHA_SITEKEY; ?>"></div>
 				<input type="submit" value=" Submit inquiry ">
