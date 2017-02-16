75e28610fb64e30a5f0061b49b3101431f57fd2d
Jos Poortvliet <jospoortvliet@gmail.com>
2017-02-16 20:27:56 +0100

    keep trying

--- page-contributors.php
+++ page-contributors.php
@@ -38,7 +38,7 @@
 				$data = CONTRIBOOK_USER::getuser($user);
 				$image_src = CONTRIBOOK_PHOTO_URL.blogger_200.png;
 				if(in_array($data['userid'], $forbiddenUsers, true)) { continue; }
-				echo('<div class="col-sm-4 col-md-3 col-lg-2"><div class="contribshowhide">');
+				echo('<div class="col-sm-4 col-md-3 col-lg-2 contributor-area"><div class="contribshowhide">');
 				if($data['picture_200'] !== ''){
 					$image_src = CONTRIBOOK_PHOTO_URL.$data['picture_200'];
 		// 			echo('<span class="bloguserpicture"><img src="'.CONTRIBOOK_PHOTO_URL.$_['picture_200'].'" border="1" /></span>');
