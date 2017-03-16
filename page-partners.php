7f026aa65ec81f5840eab5dfc016c2e8867ab879
Jos Poortvliet <jospoortvliet@gmail.com>
2017-03-16 17:42:14 +0100

    partners page  - WIP

---
+++ page-partners.php
@@ -0,0 +1,198 @@
+<head>
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/partners.css" rel="stylesheet">
+<script>
+	require(["require.config"], function() {
+		require(["bootstrap"])
+	});
+</script>
+</head>
+<section class="hero-section">
+	<div class="background partners-background">
+		<div class="container">
+			<div class="col-md-6 topheader">
+				<h1><?php echo $l->t('Nextcloud Partners');?></h1>
+				<h2><?php echo $l->t('We team up with the best to provide you what you need.');?></h2>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section--intro">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+		<p class="section--paragraph text-center">Nextcloud works with a variety of partners in our ecosystem, building on, providing services around and complementing our product.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section--providers">
+	<div class="wrap container partnergroup">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<h1 class="text-center">Solution providers</h1>
+				<p class="section--paragraph text-center">If you want to ensure your Nextcloud contributes to your business success, we recommend working locally with one of our partners which have the full backing of Nextcloud GmbH. These partners can help you do a proof of concept and purchase, deploy, optimize and maintain your Nextcloud.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="https://www.arcadis.com" target="_blank" title="Arcadis consulting">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/arcadis.png" title="Arcadis consulting" />
+				</a>
+				<p class="partnertext">Arcadis is part of the Spreedbox network in Australia, delivering the Nextcloud based Spreedboxes with services and support. Learn more on <a href="https://spreedbox.me">spreedbox.me</a></p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://dass-it.de" target="_blank" title="Dass IT">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/dassit.png" title="Dass IT" />
+				</a>
+				<p  class="partnertext">dass IT GmbH in Cologne , founded in 2004 by former senior consultants of Suse Linux, is primarily a system integrator, offering support and consulting around Linux and Open Source Software. As a long-standing owncloud partner dass IT can help you migrate from ownCloud to Nextcloud or to build a private cloud from scratch.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="http://www.daseq.de/produkte/nextcloud/" target="_blank" title="DASEQ">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/daseq.png" title="DASEQ" />
+				</a>
+				<p  class="partnertext">DASEQ is delivering Nextcloud as integral part of their OpenSource solution framework which consists of open source technology, services and training as well as security solutions.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="https://www.etes.de/" target="_blank" title="etes">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/etes.png" title="etes" />
+				</a>
+				<p  class="partnertext">ETES GmbH is an IT company from Stuttgart with a focus on Linux and Open Source, offering numerous IT services from advice to implementation, maintenance and support. The servers are hosted in Germany which guarantees industry standards concerning security (aligned to ISO 27001).</p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://www.hkn.de/nextcloud-server.php" target="_blank" title="HKN">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/hkn.png" title="HKN logo" />
+				</a>
+				<p  class="partnertext">HKN has been making the internet easier for its customers since 1996. We are a specialist for fully managed Servers hosted in Germany with German privacy protection.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://360ict.nl/" target="_blank" title="360ICT">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/360ict.png" title="360ICT logo" />
+				</a>
+				<p  class="partnertext">In a world where business depends on ICT, 360 ICT delivers garantees in the form of hard SLA's while remaining as approachable as a local ICT company for small and medium businesses. Our strength: solid, solution-focused and personal.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="https://www.it25.de/" target="_blank" title="IT25">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/it25.png" title="it25" />
+				</a>
+				<p  class="partnertext">Since 2016 IT25 GmbH has specialized in email and groupware solutions, with a focus on the relevant products and tools in the market and a strong know-how in these core products.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="http://www.netcloud.ch/" target="_blank" title="Netcloud AG">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/netcloud.png" title="netcloud logo" />
+				</a>
+				<p  class="partnertext">Netcloud AG is a Winterthur (Zurich)-based company with 130 employees. Core competencies include consulting, designing, implementing and operating IT infrastructure solutions around Cisco technology, borderless networks, data-center and unified communications solutions.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="http://www.regioit.de/" target="_blank" title="Regioit">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/regioit.png" title="regioit logo" />
+				</a>
+				<p  class="partnertext">regio iT is a service provider with over 300 employees delivering solutions for communities, schools, non-profits as well as customers in the energy production and distribution space.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="https://www.sendin.nl/nextcloud/" target="_blank" title="Sendin">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/sendin.png" title="sendin logo" />
+				</a>
+				<p  class="partnertext">Sendin is a Dutch company specializing in the storage and shipping of files, building simple solutions so users need no manual. Our clients use our solutions to make their files and documents accessible in the cloud but also for paperless meetings and more.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://www.stylez.co.jp/" target="_blank" title="Stylez">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/stylez.png" title="stylez logo" />
+				</a>
+				<p  class="partnertext">Since 2013, Stylez has grown by focusing on integration and Business Process Outsourcing. Today Stylez expands its scope into cloud and open source services, keeping up their focus on customers and quality.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://www.teebase.eu" target="_blank" title="Teebase Ltd">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/teebase.png" title="Teebase logo" />
+				</a>
+				<p  class="partnertext">TeeBase UK Ltd offers an arsenal of devices and services for digital security to protect your data and intellectual property even when using the Internet. Be free from hackers, malware and ransomware that destroy shareholder value and ruin careers</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-4">
+				<a href="https://www.univention.de/" target="_blank" title="Univention">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/univention.png" title="Univention" />
+				</a>
+				<p  class="partnertext">Univention is a leading provider of Open Source products for running and managing whole IT infrastructures and applications. Our core product is Univention Corporate Server (UCS), a flexible, effective alternative to Microsoft solutions with AD functions, a strong identity management and a wide range of applications in our App Center.</p>
+			</div>
+<!--			<div class="col-md-4">
+				<a href="https://www.omnis-systems.com/en/" target="_blank" title="Omnis Systems Ltd">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/omnis.png" title="Omnis Systems logo" />
+				</a>
+				<p  class="partnertext">Omnis Systems Ltd is probably the only a Value Added Distributor in Europe specialised in commercially backed Open Source and Linux based solutions.</p>
+			</div>-->
+<!--			<div class="col-md-4">
+				<a href="https://www.cyber-guardians.net" target="_blank" title="Cyber Guardians">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/cyberguardians.png" title="Cyber Guardians logo" />
+				</a>
+				<p  class="partnertext"></p>
+			</div>-->
+			
+			
+		</div>
+	</div>
+</section>
+<section class="section--partners">
+	<div class="wrap container partnergroup">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<h1 class="text-center">Technology Partners</h1>
+				<p class="section--paragraph text-center">Our technology partners work with us to develop to improve Nextcloud and offer technology solutions supporting or inter-operating with our product.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/assanti.png" title="Assanti" />
+				<p>Assanti is a technology company with a focus on the insurance industry based in the Netherlands. Assanti delivers secure share and storage solutions for insurance brokers built on Nextcloud and develops the Nextcloud Secure Sharing Outlook Addin.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://www.canonical.com/" target="_blank" title="Canonical">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/canonical.png" title="Canonical" />
+				</a>
+				<p  class="partnertext">Canonical is the company behind Ubuntu, the leading Operating System for cloud and the Internet of Things. Most public cloud workloads are running on Ubuntu, and most new smart gateways, self-driving cars and advanced humanoid robots also run Ubuntu. Nextcloud and Canonical collaborate to build the <a href="/box">Nextcloud Box</a>, a device that makes it easy for anyone to self host their data.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://www.collaboraoffice.com/" target="_blank" title="Collabora Office website">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/collabora.png" title="Collabora" />
+				</a>
+				<p  class="partnertext">Collabora Productivity is the world’s leading LibreOffice services company and one of the biggest contributors to the LibreOffice for desktop codebase. It is the driving force behind putting LibreOffice in the Cloud with Collabora Online.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="http://www.openinventionnetwork.com" target="_blank" title="OIN">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/oin.png" title="OIN" />
+				</a>
+				<p  class="partnertext">The Open Invention Network is a shared defensive patent pool with the mission to protect Linux.  Launched in 2005, OIN has strong industry support with backing from Google, IBM, NEC, Philips, Red Hat, Sony, SUSE, and Toyota.  Any company, project or developer that is working on Linux, GNU, Android or any other Linux-related software is welcome to join OIN, free of charge or royalties.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://osb-alliance.com" target="_blank" title="Open Source Business Alliance">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/osballiance.png" title="The Open Source Business Alliance" />
+				</a>
+				<p  class="partnertext">The German language focused OSB Alliance is a collaboration of IT providers who want to push Open Source Software forward. As member Nextcloud GmbH helps push interoperability and open standards forward in the open cloud.</p>
+			</div>
+			<div class="col-md-4">
+				<a href="http://www.twsweb.it/" target="_blank" title="TWS">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/tws.png" title="TWS" />
+				</a>
+				<p  class="partnertext">TWS is the official provider of Nexcloud iOS Client. The company has its headquarters in Italy and it mainly deals with design and development of web-based application software.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="http://wdlabs.wd.com/products/wd-pidrive-1tb-kit/" target="_blank" title="Western Digital Labs">
+				<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/partners/wdlabs.png" title="Western Digital Labs" />
+				</a>
+				<p  class="partnertext"><a href="http://wdlabs.wd.com/">WDLabs™</a> is a business growth incubator of storage solutions leader <a href="http://www.wdc.com/en/">Western Digital Corporation</a> (NASDAQ:WDC). We work with Western Digital Labs on the <a href="/box">Nextcloud Box project</a> to create a device that makes it easier for people to self-host their data.</p>
+			</div>
+		</div>
+		<br /><br />
+		<p><small>All product names and trademarks are the property of their respective owners, which might in no way be associated or affiliated with Nextcloud.</small></p>
+	</div>
+	
