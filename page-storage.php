05cd2bd873ca098f6e7fc98bbcccbe58c9c4aecc
Jos Poortvliet <jospoortvliet@gmail.com>
2017-04-03 21:05:04 +0200

    storage page draft done

--- page-storage.php
+++ page-storage.php
@@ -20,33 +20,37 @@
 <section class="section--storage">
 <div class="container">
 	<div class="row">
-		<div class="col-md-6 revealOnScroll">
-			<p class="section--paragraph__tittle"><?php echo $l->t('Access your data anywhere');?></p>
-			<p class="section--paragraph"><?php echo $l->t('The unique External Storage feature of Nextcloud provides your employees access to company data wherever it is, unlocking corporate storage silos through a single, easy to use interface.');?></p> 
-			 <p class="section--paragraph"><?php echo $l->t('Avoid costly migrations and retain your existing, compliant processes while enabling your employees to collaborate and work efficiently. Nextcloud ensures your IT stays in control of the data by providing <a class="hyperlink" href="/workflow">file access control</a> and <a class="hyperlink" href="/monitoring">logging capabilities.</a>');?></p>
-		</div>
-		<div class="col-md-6 revealOnScroll">
+		<div class="col-md-6 image--floated revealOnScroll">
 			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/externalstorage.png"><img class="img-responsive featureimg" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw.png" /></a>
 		</div>
-	</div>
-	<div class="row">
 		<div class="col-md-6 revealOnScroll">
-			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/tagging-retention.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/tagging-retention-wee.png" class="img-responsive featureimg" /></a>
-		</div>
-		<div class="col-md-6 revealOnScroll">
-			<p class="section--paragraph__tittle"><?php echo $l->t('Define automated Workflows and retention');?></p>
-			<p class="section--paragraph"><?php echo $l->t('The Workflow engine expands these capabilities, enabling administrators to start any kind of actions based on these triggers, for example converting document file types to PDF upon upload by members of a specified group or emailing files put in a specified folder with a given tag to a given mail address.');?></p>
-			<p class="section--paragraph"><?php echo $l->t('File retention and deletion can also be controlled based on tags set manually or automatically, ensuring legal or practical requirements for the longevity of data lifespans can be enforced.');?></p>
+			<p class="section--paragraph__tittle"><?php echo $l->t('Unlocking corporate data silos');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Avoid costly and disruptive migrations and retain your existing, compliant workflows and processes while enabling your employees to collaborate and work efficiently.');?></p> 
+			 <p class="section--paragraph"><?php echo $l->t('Nextcloud ensures your IT stays in control of the data by providing powerful <a class="hyperlink" href="/workflow">File Access Control</a> and <a class="hyperlink" href="/monitoring">logging</a> capabilities.');?></p>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-6 revealOnScroll">
-			<p class="section--paragraph__tittle"><?php echo $l->t('File access control at home');?></p>
-			<p class="section--paragraph"><?php echo $l->t('Home users will find that the File Access Control app and other workflow tools can be used to prevent accidental sharing of sensitive data, adding an additional layer of protection to Nextcloud.');?></p>
-			<p class="section--paragraph"><?php echo $l->t('The example on the right ensures that files with the "protected" tag are not accessible outside the home network nor synced by the clients. <a href="https://nextcloud.com/?p=869" target="_blank">Learn more in our blog</a>.');?></p>
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw2.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw2-wee.png" class="img-responsive featureimg" /></a>
 		</div>
 		<div class="col-md-6 revealOnScroll">
-			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/FileAccessControl-at-home.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/FileAccessControl-at-home.png" class="img-responsive featureimg" /></a>
+			<p class="section--paragraph__tittle"><?php echo $l->t('Public, Private and Hybrid unified');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Seemlessly access existing storage silos like FTP, Windows Network Drives, Object Storage and Samba shares through Nextcloud. Protect and optimize data usage in line with compliance and company policy by controling sharing and performance for each external storage system.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Fine tune the balance between cost, availability, performance and security by deploying multiple data storage systems in the public cloud or hosted with a trusted provider or on-premise.');?></p>
+		</div>
+	<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph"><?php echo $l->t('Some external storage technologies supported out of the box include:');?></p>
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-3.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-3.png" class="img-responsive featureimg" /></a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 image--floated revealOnScroll">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/encryption.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/encryption-nw-wee.png" class="img-responsive featureimg" /></a>
+		</div>
+		<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Secure server side encryption');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Server side encryption can be employed on select external or primary storage to securely encrypt the data residing on them. Nextcloud supports external key management, a master recovery password and changing passwords by users.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Learn more about Nextcloud server side encryption ');?> <a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_files/encryption_configuration.html" target="_blank"><?php echo $l->t('in the documentation.');?></a>.</p>
 		</div>
 	</div>
 </div>
