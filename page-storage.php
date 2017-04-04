5a52724583c90771d6b73fb9103e5d2642a2086a
Jos Poortvliet <jospoortvliet@gmail.com>
2017-04-04 18:41:40 +0200

    feedback Nina and Paulo

--- page-storage.php
+++ page-storage.php
@@ -10,8 +10,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 topheader">
-				<h1><?php echo $l->t('Access your data wherever it is');?></h1>
-				<h2><?php echo $l->t('Avoid migration and access your files through Nextcloud');?></p>
+				<h1><?php echo $l->t('Secure access to your data anywhere');?></h1>
+				<h2><?php echo $l->t('Avoid migration and access your files wherever they are stored');?></p>
 			</div>
 		</div>
 	</div>
@@ -21,36 +21,41 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-6 image--floated revealOnScroll">
-			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/externalstorage.png"><img class="img-responsive featureimg" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw.png" /></a>
+			<img src="<?php bloginfo('template_directory'); ?>/assets/img/features/mobileDesktop.png" class="img-responsive featureimg" />
 		</div>
 		<div class="col-md-6 revealOnScroll">
-			<p class="section--paragraph__tittle"><?php echo $l->t('Unlocking corporate data silos');?></p>
-			<p class="section--paragraph"><?php echo $l->t('Avoid costly and disruptive migrations and retain your existing, compliant workflows and processes while enabling your employees to collaborate and work efficiently. Nextcloud brings together data from your existing storage systems in one seamless user interface.');?></p> 
+			<p class="section--paragraph__tittle"><?php echo $l->t('Unlock your data');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Avoid costly and disruptive migrations and retain your existing, compliant workflows and processes while enabling your employees to collaborate and work efficiently. Nextcloud unlocks corporate data silos, bringing together data from your existing storage systems in one seamless user interface.');?></p> 
 			 <p class="section--paragraph"><?php echo $l->t('Your IT stays in control of the data thanks to powerful <a class="hyperlink" href="/workflow">File Access Control</a> and <a class="hyperlink" href="/monitoring">logging</a> capabilities.');?></p>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-6 revealOnScroll">
-			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw2.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw2-wee.png" class="img-responsive featureimg" /></a>
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/externalstorage.png"><img class="img-responsive featureimg" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw.png" /></a>
 		</div>
 		<div class="col-md-6 revealOnScroll">
 			<p class="section--paragraph__tittle"><?php echo $l->t('Public, Private and Hybrid unified');?></p>
-			<p class="section--paragraph"><?php echo $l->t('Seemlessly access existing storage silos like FTP, Windows Network Drives, Object Storage and Samba shares through Nextcloud. Protect and optimize data usage in line with compliance and company policy by controling sharing and performance for each external storage system.');?></p>
-			<p class="section--paragraph"><?php echo $l->t('Fine tune the balance between cost, availability, performance and security by deploying multiple data storage systems in the public cloud or hosted with a trusted provider or on-premise.');?></p>
-		</div>
-	<div class="col-md-6 revealOnScroll">
-			<p class="section--paragraph"><?php echo $l->t('Some external storage technologies supported out of the box include:');?></p>
-			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-3.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-3.png" class="img-responsive featureimg" /></a>
+			<p class="section--paragraph"><?php echo $l->t('Access existing storage silos like FTP, Windows Network Drives, Object Storage and Samba shares seamlessly through Nextcloud. Protect and optimize data usage in line with compliance and company policy by controling sharing and performance for each external storage system.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Use Nextcloud to fine tune the balance between cost, availability, performance and security. Deploy multiple data storage systems in the public cloud or hosted with a trusted provider or on-premise.');?></p>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-6 image--floated revealOnScroll">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw2.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/external-storage-nw2-wee.png" class="img-responsive featureimg" /></a>
+		</div>
+	<div class="col-md-6 revealOnScroll">
+		<p class="section--paragraph"><?php echo $l->t('Some of the storage technologies supported in Nextcloud include NFS and local storage as well as scalable, multi-bucket capable object storage like S3 and SWIFT compatible and CIFS/Windows Shares. Nextcloud can also seamlessly access files on (s)FTP, Dropbox or Google Drive systems and more.');?></p>
+		<p class="section--paragraph"><?php echo $l->t('<a class="hyperlink" href="/contact/">Contact us</a> if you want to know about support for your enterprise storage technology.');?></p>
+	</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 revealOnScroll">
 			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/encryption.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/encryption-nw-wee.png" class="img-responsive featureimg" /></a>
 		</div>
 		<div class="col-md-6 revealOnScroll">
 			<p class="section--paragraph__tittle"><?php echo $l->t('Secure server side encryption');?></p>
-			<p class="section--paragraph"><?php echo $l->t('Server side encryption can be employed on select external or primary storage to securely encrypt the data residing on them. Nextcloud supports external key management, a master recovery password and changing passwords by users.');?></p>
-			<p class="section--paragraph"><?php echo $l->t('Learn more about Nextcloud server side encryption ');?> <a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_files/encryption_configuration.html" target="_blank"><?php echo $l->t('in the documentation.');?></a>.</p>
+			<p class="section--paragraph"><?php echo $l->t('Server side encryption can optionally be enabled on select external or primary storage to securely encrypt the data residing on them. Nextcloud supports external key management, a master recovery password and changing passwords by users.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Learn more about Nextcloud server side encryption ');?> <a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_files/encryption_configuration.html" target="_blank"><?php echo $l->t('in the documentation.');?></a></p>
 		</div>
 	</div>
 </div>
