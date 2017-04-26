df79a6812fa3bb89c3ebf9693149d18e9cd906bc
Lance M <mightyfree@users.noreply.github.com>
2017-04-26 19:35:14 -0400

    Update page-webrtc.php
    Corrected missing 'Built' to 'Build' line 125.

--- page-webrtc.php
+++ page-webrtc.php
@@ -122,7 +122,7 @@
 	<li><?php echo $l->t('<code>./autogen.sh</code><br/>
  	Generate configure and Makefile');?></li>
 	<li><?php echo $l->t('<code>./configure</code><br/>
- 	Detect all dependencies and get ready to 
+ 	Detect all dependencies and get ready to Build 
 	');?></li>
  	<li><?php echo $l->t('<code>make</code><br/>
  	Build Spreed WebRTC');?></li>
