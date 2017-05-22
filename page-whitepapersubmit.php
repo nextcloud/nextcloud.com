b9b87870c941d4995a4d194bd324291d0d325ba9
Jos Poortvliet <jospoortvliet@gmail.com>
2017-05-22 12:19:46 +0200

    take sales out of CC

--- page-whitepapersubmit.php
+++ page-whitepapersubmit.php
@@ -56,8 +56,7 @@ if(isset($_POST['email'])) {
 
 // create email headers
     $headers = 'From: '.$email_from."\r\n".
-    'Reply-To: '.$email_from."\r\n" .
-    'Cc: '.$email_from;
+    'Reply-To: '.$email_from;
 // Send the email to the list
     @mail($email_to, $email_subject, $email_message, $headers);
 // Second email to subscribe to the mailing list
