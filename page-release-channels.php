15672dc923bbe11b89ddc9239923ee9bd1ac8b41
Jos Poortvliet <jospoortvliet@gmail.com>
2017-06-14 14:47:06 +0200

    remove table colors

--- page-release-channels.php
+++ page-release-channels.php
@@ -65,33 +65,33 @@
 <div class="container">
 <table class="table table-striped">
     <tr>
-      <td class="warning" style="min-width:250px; max-widt:250px"><i class="icon-sitemap"></i>&nbsp;&nbsp;Release channel</td>
-      <td class="info" style="min-width:300px"><i class="icon-briefcase"></i>&nbsp;&nbsp;Use case</td>
-      <td class="info" style="min-width:300px"><i class="icon-archive"></i>&nbsp;&nbsp;Get it here</td>
+      <td class="" style="min-width:250px; max-widt:250px"><i class="icon-sitemap"></i>&nbsp;&nbsp;Release channel</td>
+      <td class="" style="min-width:300px"><i class="icon-briefcase"></i>&nbsp;&nbsp;Use case</td>
+      <td class="" style="min-width:300px"><i class="icon-archive"></i>&nbsp;&nbsp;Get it here</td>
     </tr>
     <tr>
-      <td class="warning"><h3>Production</h3></td>
-      <td class="info">This channel delivers the latest fully tested release of Nextcloud.</br>
+      <td class=""><h3>Production</h3></td>
+      <td class="">This channel delivers the latest fully tested release of Nextcloud.</br>
 		       That does not imply that these releases are bug free. The issues are known and can be worked around. The downside of this channel is that the releases are behind in terms of features and performance.</td>
-            <td class="info">Older archives are available from our <a class="hyperlink" href="/changelog">changelog page.</a>
+            <td class="">Older archives are available from our <a class="hyperlink" href="/changelog">changelog page.</a>
             </td>
     </tr>
     <tr>
-      <td class="warning"><h3>Stable</h3></td>
-      <td class="info">This channel delivers the latest feature releases ready for most users at minimal risk.<br/>
+      <td class=""><h3>Stable</h3></td>
+      <td class="">This channel delivers the latest feature releases ready for most users at minimal risk.<br/>
       Note that Nextcloud does staged rollouts, making releases incrementally available to the user base over time.</td>
-            <td class="info">You can get the latest stable version from our installation page.<br /><a href="/install" class="btn btn-lg btn-default">Install</a></td>
+            <td class="">You can get the latest stable version from our installation page.<br /><a href="/install" class="btn btn-lg btn-default">Install</a></td>
     </tr>
     <tr>
-      <td class="warning"><h3>Beta</h3></td>
-      <td class="info">Betas and release candidates are the versions of choice for testers in preparation for an Nextcloud release. They provide a short-term-stable point to report bugs against. Grab these to make sure the upcoming release works well on your infrastructure.</td>
-      <td class="info">Automatically generated <a class="hyperlink" href="/install/#testing-development">snapshot tarballs and betas</a> track daily Nextcloud development. These are suitable to check if bugs are fixed but can break any time.
+      <td class=""><h3>Beta</h3></td>
+      <td class="">Betas and release candidates are the versions of choice for testers in preparation for an Nextcloud release. They provide a short-term-stable point to report bugs against. Grab these to make sure the upcoming release works well on your infrastructure.</td>
+      <td class="">Automatically generated <a class="hyperlink" href="/install/#testing-development">snapshot tarballs and betas</a> track daily Nextcloud development. These are suitable to check if bugs are fixed but can break any time.
       </td>
     </tr>
     <tr>
-      <td class="warning"><h3>Daily</h3></td>
-      <td class="info">Daily builds are meant to reproduce bugs as well as to enable testers to follow the development of the next major version.</td>
-      <td class="info"><a href="<?php echo $DOWNLOAD_SERVER_DAILY_TAR; ?>" class="btn btn-lg btn-default"><i class="icon-archive"></i> Archive File</a> (<a class="hyperlink" href="<?php echo $DOWNLOAD_SERVER_DAILY_TAR_MD5; ?>">MD5</a>)<br />
+      <td class=""><h3>Daily</h3></td>
+      <td class="">Daily builds are meant to reproduce bugs as well as to enable testers to follow the development of the next major version.</td>
+      <td class=""><a href="<?php echo $DOWNLOAD_SERVER_DAILY_TAR; ?>" class="btn btn-lg btn-default"><i class="icon-archive"></i> Archive File</a> (<a class="hyperlink" href="<?php echo $DOWNLOAD_SERVER_DAILY_TAR_MD5; ?>">MD5</a>)<br />
     </tr>
 </table>
 </section>
@@ -103,7 +103,7 @@
         <p>Note that releases don't show up in the updater app right away. We usually stagger releases out to watch the impact and hold off in case very serious problems pop up. In practice, most bugfix releases are available within a week, major releases to a proportion of the users on release day in the Stable channel; we'll increase that percentage usually to 100% after the first bugfix release. We start the incremental rollout to the Production channel after the first bugfix release (for example, 12.0.1). Packages for the Release channels might be available in distributions, this is up to the packagers.</p>
         <!--<p><strong>Security note:</strong></br>
         To receive information about updates and security issues, we recommend a subscription to our low-traffic <a href="http://mailman.nextcloud.org/mailman/listinfo/announcements">announcement mailing list</a>.</p>-->
-        <a name="warning"></a>
+        <a name=""></a>
         <p>To <em>upgrade in the safest way possible</em>, always update to the latest minor release before upgrading to a new version. As an extreme example, to upgrade from 9 all the way to 12, upgrade 9.0.x to 9.0.6, then upgrade to 10.0.4, 11.0.3, then 12.0.1.</p>
     </div>
 <!--    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-10">
