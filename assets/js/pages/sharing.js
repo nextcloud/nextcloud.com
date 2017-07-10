0c10a6d2d252673a10d347e70dc36a7116aa3579
Paulo Moura <itsme@paulomoura.com>
2017-07-10 22:20:20 +0100

    fix sharing page stuff

--- assets/js/pages/sharing.js
+++ assets/js/pages/sharing.js
@@ -1,9 +1,28 @@
-define(["jquery", "bootstrap"  ], 
-       function ($, bootstrap ) {
-           $(document).ready(function() {
-               if(window.location.hash) {
-					$('#IDtext').text('Your Federated Cloud ID is');
-					$('#userName').text(window.location.hash.substring(1));
-				}
-       });
+define(["jquery", "underscore", "enquire", "jqueryUi", "youtubeplugin", "bootstrap"],
+function ($, _, enquire, jqueryui, jqyoutube) {
+  $(document).ready(function() {
+    'use strict';
+    const sharing = {
+      init: function() {
+        jQuery("a.youtube").YouTubePopup({ hideTitleBar: true });
+
+        if(window.location.hash) {
+          $('#IDtext').text('Your Federated Cloud ID is');
+          $('#userName').text(window.location.hash.substring(1));
+          $('html, body').animate({
+              scrollTop: $("#federationScroll").offset().top
+          }, 2000);
+        }
+
+        enquire.register('screen and (max-width: 480px)', {
+          //match: _.bind(this.resultsBindMobile, this)
+        });
+
+        enquire.register('screen and (min-width: 481px)', {
+          //match: _.bind(this.enterpriseBindDesktop, this)
+        });
+      }
+    }
+    sharing.init();
+  });
 });
