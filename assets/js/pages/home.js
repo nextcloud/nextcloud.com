0c9a4f043b2a32a8dec18d28468a32229f1a0851
Paulo Moura <itsme@paulomoura.com>
2017-07-20 19:12:42 +0100

    remove vendor files from repo and starting using yarn for handling, use slim versions instead of the whole libraries

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,41 +1,50 @@
-// Top Container Animations
-define(["jquery", "waypoints", "velocity", "bodymovin", "velocityUI"],
-function (jquery, waypoints, Velocity, bodymovin, velocityUI) {
-    $(document).ready(function() {
-        $(".topbanner").velocity('transition.slideUpBigIn');
-        $(".toptext").velocity('transition.fadeIn', 2000);
-    });
+define(['jquery', 'lodash', 'waypoints', 'velocity', 'bodymovin', 'velocityUI'],
+	function ($, _, waypoints, Velocity, bodymovin) {
+		$(document).ready(function() {
+			'use strict';
+			var homePage = {
+				init: function() {
+					this.svgAnimationsHandler();
+				},
 
-    // Why nextcloud icon animations
-    $(window).load(function() {
-        /**
-         * Animations
-         */
-        var elements = [
-            'community',
-            'privacy',
-            'flexibility'
-        ];
-        elements.forEach(function(key) {
-            new Waypoint({
-                element: document.getElementById('why-nextcloud'),
-                offset: '60%',
-                handler: function() {
-                    anim.play();
-                }
-            });
-            var animContainer = document.querySelectorAll('#why-nextcloud div [data-name='+key+']')[0];
-            var params = {
-                container: animContainer,
-                renderer: 'svg',
-                loop: false,
-                autoplay: false,
-                autoloadSegments: false,
-                path: templateUrl + '/assets/img/home/'+key+'/data.json'// path to your data.json file you rendered from AE
+				svgAnimationsHandler: function() {
 
-            };
-            var anim;
-            anim = bodymovin.loadAnimation(params);
-        });
-    });
-});
+					var elements = [
+						'community',
+						'privacy',
+						'flexibility'
+					];
+
+					elements.forEach(_.bind(this.animateSvg, this));
+				},
+
+				animateSvg: function(key) {
+					new Waypoint({
+						element: document.getElementById('why-nextcloud'),
+						offset: '60%',
+						handler: function() {
+							anim.play();
+						}
+					});
+
+					var animContainer = document.querySelectorAll('#why-nextcloud div [data-name='+key+']')[0];
+					var params = {
+						container: animContainer,
+						renderer: 'svg',
+						loop: false,
+						autoplay: false,
+						autoloadSegments: false,
+						path: templateUrl + '/assets/img/home/'+key+'/data.json'// path to your data.json file you rendered from AE
+					};
+					var anim;
+					anim = bodymovin.loadAnimation(params);
+				}
+			};
+			homePage.init();
+		});
+	});
+
+
+// Replace with a global animation
+// $('.topbanner').velocity('transition.slideUpBigIn');
+// $('.toptext').velocity('transition.fadeIn', 2000);
