092922e9973771f99c6235eeb964b55d31dfefce
Paulo Moura <itsme@paulomoura.com>
2017-07-20 19:12:42 +0100

    remove vendor files from repo and starting using yarn for handling, use slim versions instead of the whole libraries

--- assets/js/modules/submenu.js
+++ assets/js/modules/submenu.js
@@ -1,47 +1,47 @@
-define(["jquery", "underscore", "enquire", "velocity", "velocityUI", "scrollMagic"],
-function ($, _, enquire, Velocity, velocityUI, ScrollMagic) {
-	$(document).ready(function() {
+define(['jquery', 'lodash', 'enquire', 'velocity', 'velocityUI', 'scrollMagic'],
+	function ($, _, enquire, Velocity, velocityUI, ScrollMagic) {
+		$(document).ready(function() {
 	    'use strict';
 	    var subMenuModule = {
 	        init: function() {
 
-            	$(this.variables.menuAnchorSelector).velocity("transition.fadeIn", 1000 );
+            	$(this.variables.menuAnchorSelector).velocity('transition.fadeIn', 1000 );
 
-				enquire.register('screen and (max-width: 480px)', {
+					enquire.register('screen and (max-width: 480px)', {
 					//match: _.bind(this.resultsBindMobile, this)
-				});
+					});
 
-				enquire.register('screen and (min-width: 481px)', {
-					match: _.bind(this.subMenuEvent, this)
-				});
-			},
+					enquire.register('screen and (min-width: 481px)', {
+						match: _.bind(this.subMenuEvent, this)
+					});
+				},
 
-			variables : {
-				topHeaderSelector: ".topheader",
-				heroSectionBackgroundSelector: ".background",
-				menuAnchorSelector: "#menuAnchor"
-			},
+				variables : {
+					topHeaderSelector: '.topheader',
+					heroSectionBackgroundSelector: '.background',
+					menuAnchorSelector: '#menuAnchor'
+				},
 
-			subMenuEvent: function() {
-				this.pinFiltersBar();
-				this.smothscrollToggleActive();
-			},
+				subMenuEvent: function() {
+					this.pinFiltersBar();
+					this.smothscrollToggleActive();
+				},
 
-			pinFiltersBar: function() {
-				var controller = new ScrollMagic.Controller();
-				var scene = new ScrollMagic.Scene({
-					triggerElement: this.variables.menuAnchorSelector,
-					triggerHook:0,
-					offset:-90
-				})
-					.setPin(this.variables.menuAnchorSelector)
-					.setClassToggle(".nav", "no-shadow")
-					.addTo(controller);
-			},
+				pinFiltersBar: function() {
+					var controller = new ScrollMagic.Controller();
+					var scene = new ScrollMagic.Scene({
+						triggerElement: this.variables.menuAnchorSelector,
+						triggerHook:0,
+						offset:-90
+					})
+						.setPin(this.variables.menuAnchorSelector)
+						.setClassToggle('.nav', 'no-shadow')
+						.addTo(controller);
+				},
 
-			smothscrollToggleActive: function() {
-				$('a[href^="#"]:not([data-toggle="collapse"])').click(function(event) {
-						var id = $(this).attr("href");
+				smothscrollToggleActive: function() {
+					$('a[href^="#"]:not([data-toggle="collapse"])').click(function(event) {
+						var id = $(this).attr('href');
 
 						var target = $(id).offset().top;
 						$('html, body').animate({scrollTop:target}, 500);
@@ -49,37 +49,37 @@ function ($, _, enquire, Velocity, velocityUI, ScrollMagic) {
 
 					});
 
-				function getTargetTop(elem){
-				 	var id = elem.attr("href");
+					function getTargetTop(elem){
+				 	var id = elem.attr('href');
 				 	var offset = 60;
 				 	return $(id).offset().top - offset;
 				 }
 
 
-				$(window).scroll(function(e){
-			 		isSelected($(window).scrollTop())
-				});
+					$(window).scroll(function(e){
+			 		isSelected($(window).scrollTop());
+					});
 
-				var sections = $('a[href^="#"]');
+					var sections = $('a[href^="#"]');
 
-				function isSelected(scrolledTo){
+					function isSelected(scrolledTo){
 
-					var threshold = 100;
-					var i;
+						var threshold = 100;
+						var i;
 
-					for (i = 0; i < sections.length; i++) {
-						var section = $(sections[i]);
-						var target = getTargetTop(section);
+						for (i = 0; i < sections.length; i++) {
+							var section = $(sections[i]);
+							var target = getTargetTop(section);
 
-						if (scrolledTo > target - threshold && scrolledTo < target + threshold) {
-							sections.removeClass("active");
-							section.addClass("active");
+							if (scrolledTo > target - threshold && scrolledTo < target + threshold) {
+								sections.removeClass('active');
+								section.addClass('active');
+							}
+
 						}
-
-					};
+					}
 				}
-			}
-	    }
+	    };
 	    subMenuModule.init();
+		});
 	});
-});
