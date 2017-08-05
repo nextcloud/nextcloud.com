ad1cd2f0859f3577c9ee0c0ce79da617501ab1c3
Jos Poortvliet <jospoortvliet@gmail.com>
2017-08-05 16:02:04 +0200

    add changelog

---
+++ page-pr20170807.php
@@ -0,0 +1,133 @@
+<div class="pr">
+<h1>Nextcloud and Partners Announce Education Edition With Moodle, Zenodo and other integration features</h1>
+
+<p><strong>August 2, Stuttgart, Germany.</strong> In collaboration with multiple education and research institutions and software providers Nextcloud GmbH announces the immediate availability of the Nextcloud Education Edition. Built on the successful Nextcloud 12, the Education Edition delivers capabilities highly sought after in the educational market like Moodle and Zenodo integration, ORCID authentication, a Dashboard app and more.</p>
+
+<p><blockquote>The need for secure, self hosted cloud collaboration solutions in the educational and research space is growing. The Education Edition positions Nextcloud as the most integration focused solution in this market.</blockquote>
+said Frank Karlitschek, Managing Director of Nextcloud GmbH.</p>
+
+<p>The Nextcloud Education Edition debuts support for file handling integration in learning platform Moodle with an app adapted from work done by Moodle community members at the University of Muenster. In collaboration with the Moodle community and Moodle Pty Ltd, the company behind Moodle, Nextcloud will continue to develop and enhance this integration.</p>
+
+<p><blockquote>I'm excited about the Moodle integration in Nextcloud as part of the Education Edition. We're looking forward to co-operate with Nextcloud on deepening the integration and offering a powerful education and research solution to our users.</blockquote>
+said Martin Dougiamas, Founder/CEO at Moodle Pty Ltd.</p>
+
+<p>Together with the Danish cooperative of universities, DeiC, Nextcloud developed the ability to do direct publishing to the popular Zenodo.org shared research publishing platform. DeiC also supported work by Nextcloud on the advanced group management app Circles and was part of an effort to integrate support for ORCID authentication, both of which are included in the Education Edition.</p>
+
+<p><blockquote>Integration with applications commonly used by universities is what sets Nextcloud apart from the competition. The Danish universities will deploy the Education Edition to benefit directly from these capabilities. It continues to be a delight to work with Nextcloud on the next generation file sync and share technology in the educational and research area!</blockquote>
+noted Frederik Orellana, lead cloud integration architect at DeiC.</p>
+
+<p>Together with German IT consulting organization regio iT, Nextcloud integrated the Dashboard app which provides users with a easy to use overview of new/changed files, mail, news or calendar events in one place. This Dashboard is already available to the 3000 users at 122 regional schools serviced by ucloud4schools, a regio iT product.</p>
+
+<p><blockquote>Nextcloud provides a powerful base to built additional functionality on. We will continue to extend our product, going beyond what an ordinary cloud can offer.</blockquote>
+said Stefan Döhler, project manager at regio iT.</p>
+
+<p>The Education Edition also comes with our native Single Sign On & SAML authentication integration, which is compatible with all web servers and can handle group memberships, flexible session management and app specific passwords. Nextcloud also includes built in support for Oauth2 and through Apache modules, Nextcloud works with Kerberos, OpenID Connect and more. LDAP integration is also standard enabled.</p>
+
+<p>Out of the box Nextcloud Education Edition puts administrators in control, bringing with it Brute Force Protection settings and Password Policy configuration as well as Quota Warning, Admin Notification and Announcement center integration to broadcast announcements from the IT team or university and warn users of running out of storage space.</p>
+
+<p><blockquote>We are looking forward to the capabilities in the Education Edition. Since our successful migration which brought us a 38% decrease in peak database load and a much more responsive user interface, we're planning to move forward with new versions to benefit from the innovations brought to the market by Nextcloud</blockquote>
+noted Thomas Hildman, head of IT operations at TU Berlin.</p>
+
+<p>Our innovative Global Scale Architecture, co-developed with partners including TU Berlin, DeiC and others, will become a core
+component of the Education Edition in the future.</p>
+
+<p>The Education Edition bundle is available with the Nextcloud 12.0.1 release and can be installed with one click from the Nextcloud app store. We are looking for feedback from universities, schools, research institutions and others on this initial version to direct our future development.</p>
+
+<p>Through a collaboration with Univention, the makers of Univention Corporate Server (UCS), Nextcloud provides an easy to use and ready to go virtual machine image with the Education Edition pre-configured for immediate download.</p>
+
+<p><blockquote>The Nextcloud Education Edition is a perfect fit for a wide range of organizations in research and education. Univention provides a powerful, reliable and easy to administer solution to deploy technology like Nextcloud in schools, universities and other organizations for building up an open, scalable and secure IT infrastructure. So we're happy to work with Nextcloud on bringing their product to this market.</blockquote>
+said Jan Christoph Ebersbach, responsible Product Manager for UCS@school at Univention.</p>
+
+<p>Prospective customers can make use of our special offer available for ​GÉANT members. The offer includes special educational pricing for Collabora Online Office.</p>
+
+<p><strong>About Nextcloud</strong><br />
+Nextcloud offers an industry-leading fully open source solution for on-premise data handling and communication with an uncompromising focus on security and privacy and unprecedented scalability. Nextcloud brings together universal access to data with next-generation secure communication and collaboration capabilities under direct control of IT and integrated with existing compliant infrastructure. Nextcloud's open, modular architecture, emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">https://nextcloud.com</a> or follow <a href="https://twitter.com/nextclouders">@Nextclouders on Twitter.</a></p>
+
+<p><strong>About Moodle</strong><br />
+First released in 2001 by Martin Dougiamas, Moodle has provided educators, workplaces and businesses with powerful tools to manage and promote learning online. Today, Moodle is the most widely used learning management system in the world, with over 80 million registered users in more than 230 countries. Supported by a global network of 85 certified Moodle Partnerships, the Moodle learning management system continues to offer collaborative and innovative online learning technology to a global audience. For more information, visit <a href="http://moodle.com">moodle.com</a></p>
+
+<p><strong>About DeIC</strong><br />
+DeIC – Danish e-Infrastructure Cooperation has the stated aim of supporting the development of Denmark as an eScience nation through the delivery of e-infrastructure (computing, data warehousing, network connections and auxiliary services), guidance, and initiatives at national level. DeIC is a virtual unit under the Danish Ministry of Higher Education and Science and the result of an agreement concluded between the eight Danish universities and the Danish Agency for Science, Technology and Innovation. For details, see <a href="https://www.deic.dk">www.deic.dk</a></p>
+
+<p><strong>About regio iT</strong></br>
+As an IT service provider, regio iT GmbH is a fair and reliable partner for municipalities and schools, energy utilities and waste disposal companies as well as non-profit organizations.<br />
+Based in Aachen and its subsidiary in Gütersloh, regio iT offers strategic and project-related IT consulting, integration, IT infrastructure and full service in four areas: IT service and operation, administration and finance, energy and waste disposal, education and development.<br />
+With over 380 employees, it currently supports more than 27,000 clients and more than 190 schools. The company generated revenues of EUR 58.2 million in 2016.<br />
+Learn more on <a href="https://www.regioit.de">www.regioit.de</a></p>
+
+<p><strong>About the TU Berlin</strong><br />
+TU Berlin looks back at a long and distinguished tradition of teaching and research. In 1799 its most important predecessor, the Building Academy, was founded. In 1946 the university was re-established under the name of Technische Universität Berlin. The seven Faculties of the university offer 100 courses of study from the fields of engineering and natural sciences, economics and business, planning sciences, humanities and the social sciences. Enrollment at TU Berlin is about 32,000 and about 315 professors and an academic staff of 2,600 instruct the students, making it one of the largest technical universities in Germany.
+Learn more at <a href="http://www.tu-berlin.de">www.tu-berlin.de</a></p>
+
+<p><strong>About Univention GmbH</strong><br />
+Univention GmbH (<a href="https://www.univention.de">www.univention.de</a>) is a leading European developer of Open Source products with its headquarters in Bremen, Germany.<br />
+The core product, Univention Corporate Server (UCS), can be used as a classic server solution, on the cloud, and in hybrid scenarios. UCS represents a cost-effective alternative to the server solutions available from Microsoft and includes extensive Active Directory functions as well as an App Center, which can be used to install and run more than 80 enterprise solutions in existing IT environments. UCS is also a technical platform for the open cloud offer of the Open Cloud Alliance (OCA) partners.
+In UCS@school Univention offers a solution for the centralized deployment of IT in schools, with which it is possible to implement a centralized, secure, and cutting-edge identity and access management system for IT in schools.<br />
+Univention has a global partner network and additional subsidiaries in Berlin and North America.</p>
+<br />
+
+<p><strong>List of apps included:</strong><br />
+<ul><li><a href="https://github.com/daita/moodle-repository_nextcloud">Moodle app</a>
+<ul><li><a href="https://raw.githubusercontent.com/daita/moodle-repository_nextcloud/master/pix/file_picker_login.png">screenshot 1</a> <a href="https://raw.githubusercontent.com/daita/moodle-repository_nextcloud/master/pix/file_picker_listing.png">screenshot 2</a></li></ul></li>
+<li><a href="https://apps.nextcloud.com/apps/zenodo">Zenodo publishing</a>
+<ul><li><a href="https://raw.githubusercontent.com/nextcloud/zenodo/master/screenshots/dialogpopup.png">screenshot</a></li></ul></li>
+<li><a href="https://apps.nextcloud.com/apps/dashboard">Dashboard</a>
+<ul><li><a href="https://raw.githubusercontent.com/nextcloud/dashboard/master/screenshots/v4.0.5.png">screenshot</a></li></ul></li>
+<li><a href="https://raw.githubusercontent.com/nextcloud/circles/master/screenshots/1.png">Circles group handling</a>
+<ul><li><a href="https://raw.githubusercontent.com/nextcloud/circles/master/screenshots/1.png">screenshot</a></li></ul></li>
+<li><a href="https://apps.nextcloud.com/apps/groupfolders">Group Folders</a>
+<ul><li><a href="https://raw.githubusercontent.com/nextcloud/groupfolders/master/screenshots/folders.png">screenshot</a></li></ul></li>
+<li><a href="https://apps.nextcloud.com/apps/announcementcenter">Announcement center</a>
+<ul><li><a href="https://github.com/nextcloud/announcementcenter/raw/new-screenshot/docs/AnnouncementCenterFrontpage.png">screenshot</a></li></ul></li>
+<li><a href="https://apps.nextcloud.com/apps/admin_notifications">Administrator notifications</a>
+<ul><li><a href="https://github.com/nextcloud/admin_notifications/raw/master/docs/Screenshot.png">screenshot</a></li></ul></li>
+<li><a href="https://apps.nextcloud.com/apps/quota_warning">Quota warning</a>
+<ul><li><a href="https://raw.githubusercontent.com/nextcloud/quota_warning/master/docs/screenshot.png">screenshot</a></li></ul></li>
+<li><a href="ORCID authentication">https://apps.nextcloud.com/apps/orcid</a>
+<ul><li><a href="https://raw.githubusercontent.com/nextcloud/orcid/master/screenshots/personalsettings.jpeg">screenshot</a></li></ul></li>
+<li><a href="https://apps.nextcloud.com/apps/user_saml">SSO & SAML authentication</a>
+<ul><li><a href="https://raw.githubusercontent.com/nextcloud/user_saml/master/screenshots/1.png">screenshot</a></li></ul></li>
+</ul>
+<br />
+</p>
+
+
+<p><strong>Links:</strong><br />
+<ul>
+<li><a href="https://nextcloud.com/blog/nextcloud-moodle-and-4-other-partners-announce-education-edition">Announcement blog</a></li>
+<li><a href="https://www.univention.com/products/univention-app-center/app-catalog/nextcloud/">UCS VM image download</a></li>
+<li><a href="https://nextcloud.com/education">Information on Nextcloud in Education</a></li>
+<li><a href="https://nextcloud.com/blog/nextcloud-announces-global-scale-architecture-as-part-of-nextcloud-12/">Nextcloud announces Global Scale architecture as part of Nextcloud 12</a></li>
+<li><a href="https://nextcloud.com/blog/tu-berlin-halves-database-load-by-migrating-22k-users-to-nextcloud/">TU Berlin and 9 German Education/Research institutions halve database load by migrating to Nextcloud</a></li>
+<li><a href="https://nextcloud.com/blog/the-danish-research-and-education-network-moves-from-owncloud-to-nextcloud/">The Danish research and education network moves to Nextcloud</a></li>
+<li><a href="https://nextcloud.com/blog/3000-users-at-ucloud4schools-migrated-to-nextcloud-11-by-regio-it/">3000 users at ucloud4schools migrated to Nextcloud 11 by regio iT</a></li>
+<li><a href="https://nextcloud.com/blog/nextcloud-partners-with-univention-to-deliver-solution-for-private-cloud-needs/">Nextcloud partners with Univention to deliver solution for private cloud needs</a></li>
+</ul>
+<br />
+</p>
+
+<p><strong>Press contacts</strong></p>
+<p><strong>Nextcloud GmbH</strong><br />
+Jos Poortvliet<br />
+Head of Marketing<br />
+<a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
+
+<p><strong>Moodle Pty Ltd</strong><br />
+Petra Vanessie<br />
+Head of Marketing<br />
+press(at)moodle(dot)com</p>
+
+<p><strong>regio iT</strong><br />
+Mirja Niewerth-Halis, M.A.<br />
++49 241 413 59 - 9698<br />
+<a href="mailto:mirja.niew<br />erth-halis@regioit.de">mirja.niewerth-halis@regioit.de</a></p>
+
+<p><strong>Univention GmbH</strong><br />
+Ms. Alice Horstmann<br />
+Mary-Somerville-Straße 1<br />
+28359 Bremen<br />
+<a href="mailto:horstmann@univention.de">horstmann@univention.de</a><br />
+Tel: +49 (0)421 22232-27<br />
+Fax: +49 (0)421 22232-99</p>
+
+</div>
