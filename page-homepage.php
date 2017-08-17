2bb230c78c883b932548a920ccae7d6d20867886
Jos Poortvliet <jospoortvliet@gmail.com>
2017-08-18 00:38:56 +0200

    3.1 mb svg to 480kb png
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-homepage.php
+++ page-homepage.php
@@ -8,7 +8,7 @@
 </script>
 </head>
 <div class="jumbotron">
-	<img class="jumbotron__pattern" src="<?php echo get_template_directory_uri(); ?>/assets/img/pattern.svg" alt="">
+	<img class="jumbotron__pattern" src="<?php echo get_template_directory_uri(); ?>/assets/img/pattern.png" alt="">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 toptext ">
