f98430860125a52a2f4a5f823063793f813afa97
Jos Poortvliet <jospoortvliet@gmail.com>
2017-08-19 17:01:45 +0200

    Update and add confinfo 2017
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-confinfo.php
+++ page-confinfo.php
@@ -1,44 +1,44 @@
 <div class="page-header">
-    <h1>Practical Conference Info 2016</h1>
+    <h1>Practical Conference Info 2017</h1>
     <p>Here you find everything you need to know while participating in the Nextcloud Conference!</p>
-    <p><strong>Registration on Friday and Mon-Thur is at the hacking area, room <code>3008</code>, Friday talks at room <code>3004</code>!</strong></p>
-    <p><strong>Registration on the weekend is at room <code>1058</code>, the keynote room.</strong></p>
+    <p><strong>Registration Tuesday-Thursday and Monday/Tuesday is at the hacking area, 5th floor of the Mathematics building. Friday, Saturday and Sunday we have our registration booth at the entree of the building.</strong></p>
 </div>
 
 <img src="https://raw.githubusercontent.com/nextcloud/promo/8dbb6978d22761589b012ec440d8ab11cc9bb46f/conference/conference-social-media-banner.png" style="width: 100%" alt="front door" class="alignright" />
 <div class="row conf-info">
     <div class="col-md-6">
     <h2>What and where</h2>
-    <p>Be sure to have <a href="https://conf.nextcloud.com/conference/NextcloudConference2016/register/new">registered in advance for the conference</a>! Pick up your badge and some food tickets at the entree of the "<em>Hauptgebaude</em>" building, Straße des 17. Juni 135 (<a href="http://www.openstreetmap.org/?mlat=52.512085&amp;mlon=13.32675#map=16/52.5132/13.3234">see on a map</a>).</p>
-    <p>"<em>Ernst-Reuter-Platz (U)</em>" and "<em>Tiergarten (S)</em>" are the closest public transport stops. Use <a href="http://fahrinfo.bvg.de/Fahrinfo/bin/query.bin/en?pk_campaign=BVG.de_Eingabe&from=&REQ0JourneyStopsS0ID=&REQ0JourneyStopsfromID=&REQ0JourneyStopsSA1=7&HWAI%3DJS!js=yes&HWAI%3DJS!ajax=yes&to=+10623+Berlin-Charlottenburg%2C+Str.+des+17.+Juni+136&REQ0JourneyStopsZ0ID=&REQ0JourneyStopstoID=A%3D2%40O%3D10623+Berlin-Charlottenburg%2C+Str.+des+17.+Juni+135%40X%3D13326247%40Y%3D52513247%40U%3D103%40L%3D7706964%40B%3D1%40p%3D1431509568%40&REQ0JourneyStopsZA1=7&date=16.09.2016&time=09%3A00&timesel=arrive&start=1">this</a> to plan your trip.</p>
-    <p><img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/frontdoornew.jpg" style="width: 100%" alt="front door" class="aligncenter" /></p>
-
-    <p>Note that carrying your badge is mandatory, especially after 18:00 and on the weekend!</p>
+    <p>Be sure to have <a href="">registered in advance for the conference</a>! Pick up your wristband and some food tickets at the registration. Our venue is the "<em>Mathematics building</em>" of the TU Berlin, <a href="http://www.openstreetmap.org/?mlat=52.51396&mlon=13.32583#map=19/52.51396/13.32583">Straße des 17. Juni 136</a>.</p>
+    <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=13.322677016258242%2C52.51264615039466%2C13.328247964382173%2C52.51446618928727&amp;layer=mapnik&amp;marker=52.5135561792638%2C13.325462490320206" style="border: 1px solid black"></iframe><br/><small><a href="http://www.openstreetmap.org/?mlat=52.51356&amp;mlon=13.32546#map=19/52.51356/13.32546">View Larger Map</a></small>
+    <p>"<em>Ernst-Reuter-Platz (U)</em>" and "<em>Tiergarten (S)</em>" are the closest public transport stops. Use <a href="http://fahrinfo.bvg.de/Fahrinfo/bin/query.bin/en?pk_campaign=BVG.de_Eingabe&from=&REQ0JourneyStopsS0ID=&REQ0JourneyStopsfromID=&REQ0JourneyStopsSA1=7&HWAI%3DJS!js=yes&HWAI%3DJS!ajax=yes&to=+10623+Berlin-Charlottenburg%2C+Str.+des+17.+Juni+136&REQ0JourneyStopsZ0ID=&REQ0JourneyStopstoID=A%3D2%40O%3D10623+Berlin-Charlottenburg%2C+Str.+des+17.+Juni+136%40X%3D13326247%40Y%3D52513247%40U%3D103%40L%3D7706964%40B%3D1%40p%3D1431509568%40&REQ0JourneyStopsZA1=7&date=16.09.2016&time=09%3A00&timesel=arrive&start=1">the BVG website</a> to plan your trip! We will put up signs outside from those places to the TU.</p>
+    <p><img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/frontdoor.jpg" style="width: 100%" alt="front door" class="aligncenter" /></p>
     </div>
     <div class="col-md-6">
     <h2>In the building</h2>
-    <h3>Conference days - Saturday and Sunday</h3>
-    <p>Registration opens at 9 and at 10 we start with opening, keynotes and lightning talks in room <code>H1058</code>, 1st floor in the back of the "<em>Hauptgebaude</em>" building. Registration is in front of the keynote room so follow the signs in that direction! After lunch (outside the building, at one of the food trucks) the hacking rooms open. You can find the workshops in the meeting room <code>H3004</code>, with the Nextcloud coding for beginners session in room <code>H3006</code></p>
-    <p>See the <a href="https://conf.nextcloud.com/conference/NextcloudConference2016/schedule">conference program here</a>.</p>
+    <h3>Conference days - Friday, Saturday and Sunday</h3>
+    <p>Registration opens at 9 and at 9:30 we start with opening, keynotes and lightning talks in room <code>MA004</code>, right in the main hall of the "<em>Mathematiks</em>" building. Registration is in front of the keynote room so you can't miss it! In the weekend lunch is outside at a food truck, during the week it is in the University Mensa at the top floor of the Mathematics building.</p>
+    <p>In the afternoon on Saturday and Sunday you can find the workshops on the 1st floor where we also have some food and drinks during breaks, room <code>MA141</code>. The hacking rooms on the 5th floor are open after lunch, too, so you won't be bored!</p>
+    <p>Find the <a href="https://nextcloud.com/blog/focus-friday-during-the-nextcloud-conference/">Friday program here</a> and the <a href="https://eventyay.com/e/de88e486/schedule/">Saturday/Sunday program (still in motion) here.</a></p>
     <h3>Hackathon</h3>
-    <p>Friday, registration is at the hacking rooms and opens at 9. Walk from entry to the stairs on the right and follow the signs! This is where we are:</p>
+    <p>During the rest of the week (Tue-Thur and Mon/Tue) we have our usual hackathon taking place on the 5th floor of the Mathematics building. This is where we are:</p>
     <ul>
-	<li>3th floor:
+	<li>5th floor:
 		<ul>
-		<li><a href="https://nextcloud.com/blog/friday-focus-enterprise-use-of-nextcloud-at-the-conference/" target="_blank">Friday Focus</a> room <code>H3004</code></li>
-		<li>food-and-drink room <code>H3008</code></li>
-		<li>the hacking rooms: <code>H3006</code> (<em>starters room</em>), <code>H3007</code></li>
+		<li>Meeting room <code>MA541</code></li>
+		<li>Food-and-drink room <code>MA544</code></li>
+		<li>Hacking rooms: <code>MA548</code> (<em>starters room</em>), <code>MA549</code>, <code>MA545</code></li>
 	</ul></li>
-	<li>Of course, coding has to be supported with some calories. In the Mathematik building across the street, all the way to the top and up the stairs one more floor you will find your daily lunch (with your food ticket!) and a great view over the city in the University Math Mensa. During weekdays you can also find a cafe in the main building as well as one in the front of the Mathematics building.</li>
+	<li>Of course, coding has to be supported with some calories. In the same building, all the way to the top and up the stairs one more floor you will find your daily lunch (with your food ticket!) and a great view over the city in the University Math Mensa. During weekdays you can also find a cafe at the front of the Mathematics building, the Mathe cafe.</li>
     </ul>
+    <p>Note that carrying your wristband is mandatory, especially after 18:00 and on the weekend!</p>
     </div>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/map.jpg" style="width: 100%" alt="front door" class="aligncenter" />
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/rooms-5.jpg" style="width: 100%" alt="5th floor" class="aligncenter" />
 </div>
 
 <div class="row conf-info">
     <div class="col-md-6">
       <h2>Connectivity and power</h2>
-    <p>We have got wifi and many rooms are equipped with a (limited) number of network cables.</p>
+    <p>We have wifi and the hacking rooms are equipped with a number of network cables.</p>
     <p>At registration, you sign up for a guest account. Use the how-tos here to connect:</p>
     <ul>
         <li><a href="https://www.tubit.tu-berlin.de/wlan/">in German</a></li>
@@ -49,9 +49,9 @@
     <div class="col-md-6">
     <h2>Getting stuff done and finding who to talk to</h2>
     <p>New to Nextcloud, looking for an introduction or answers about APIs, infrastructure or a pull request?
-    <br/> <strong>ask your questions in the <em>starters room</em> <code>H3006</code></strong></p>
-    <p>If you want to make sure a certain topic is discussed, make an agreement with the relevant people and book the meeting room. Find a white board on which to do so outside of the meeting room, <code>H3004</code>.</p>
-    <p>If you are looking for some non-hacking entertainment in Berlin, <a href="https://help.nextcloud.com">check our forum discussion on fun stuff to do</a>!</p>
+    <br/> <strong>ask your questions in the <em>starters room</em> <code>MA548</code></strong></p>
+    <p>If you want to make sure a certain topic is discussed, make an agreement with the relevant people and book the meeting room. Find a scheduling paper on which to do so outside of the meeting room, <code>MA541</code>.</p>
+    <p>If you are looking for some non-hacking entertainment in Berlin, <a href="https://help.nextcloud.com/t/social-cultural-activity-at-nc-conf-2017/19825">check our forum discussion on fun stuff to do</a>!</p>
     </div>
 </div>
 
@@ -66,11 +66,11 @@
     </ul>
     We always have vegetarian and meat options.</p></div>
     <div class="col-md-6">
-    <p>We have a wide variety of free drinks and some fruit and snacks in room <code>H3008</code>.</p>
-    <p>During the hackathon we have free bread with a variety of things to put on it in room <code>H3008</code> - make your own dinner. Restaurants can be found at <a href="http://www.openstreetmap.org/?mlat=52.50602&mlon=13.32248#map=18/52.50602/13.32248">Savignyplatz</a>, a 10 minute walk from the venue.</p>
-    <p>During weekdays, there is an open cafe at the entree of the "<em>Mathematik</em>" building and one in the Hauptgebaude itself.</p>
+    <p>We have a wide variety of free drinks and some fruit and snacks in room <code>MA544</code>.</p>
+    <p>During the hackathon we have free bread with a variety of things to put on it in the drink room - make your own dinner. Restaurants can be found at <a href="http://www.openstreetmap.org/?mlat=52.50602&mlon=13.32248#map=18/52.50602/13.32248">Savignyplatz</a>, a 10 minute walk from the venue.</p>
+    <p>During weekdays, there is a cafe at the entree of the "<em>Mathematik</em>" building.</p>
     </div>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/room_307.jpg" style="width: 100%" alt="front door" class="aligncenter" />
+    <!--<img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/room_307.jpg" style="width: 100%" alt="front door" class="aligncenter" />-->
 </div>
 
 <div class="row conf-info">
@@ -79,7 +79,7 @@
 	<ul>
 	    <ol>1. We will record video and audio. If you are unhappy with that, please let me know and I will make sure not to publish your video. I will not publish without permission anyway, I will let you review them before I put them out.</ol>
 	    <ol>2. For the Friday talks and the workshops:<br />
-		    Room 3005 has space for about 50 people and we will have a projector ("beamer" for you Germans). If the room is too small we can move to one of the other rooms which have space for 100 people, but I suspect we will be good as most sessions are mostly interesting for a subset of our contributors.</ol>
+		    Room MA141 has space for about 150 people and we will have a projector ("beamer" for you Germans).</ol>
 	<ol>3. for the lightning talks:<br />
 	<ul>
 		<li>time</br>
@@ -88,11 +88,11 @@
 		You can do slides, but they have to be in PDF and you have to get them to me Friday morning the latest. Please, aim for Thursday! I will put them all in one slide deck, in the right order, so we go from one presentation to the next without looking up files or opening folders or changing laptops.</li>
 		</li>
 		<li>Process<br />
-		Saturday, you should sit in the right order in the front row. The \'right order\' is the one in the lightning talk track:
+		Saturday/Sunday, you should sit in the talk order in the front row. <!--The \'right order\' is the one in the lightning talk track:
 		<ul>
 			<li><a href="https://conf.nextcloud.com/conference/NextcloudConference2016/program/proposal/53">Saturday</a></li>
 			<li><a href="https://conf.nextcloud.com/conference/NextcloudConference2016/program/proposal/54">Sunday</a></li>
-		</ul></li>
+		</ul>--></li>
 		<li>We start after the keynotes, so please sit there at the beginning of the event! That is at 10:00, at 11 the lightning talks start.</li>
 	</ul></ul>
 </div>
@@ -100,6 +100,6 @@
 
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
-    <h1 class="text-center">Have fun and build some awesome stuff!</h1>
+    <h1 class="text-center">Have fun and build awesome stuff!</h1>
     </div>
 </div>
