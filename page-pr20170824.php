0c7eaa14bab40e2410b2bc476e4221fb8b9a5644
Jos Poortvliet <jospoortvliet@gmail.com>
2017-08-24 11:22:40 +0200

    iOS app free
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20170824.php
@@ -0,0 +1,32 @@
+<div class="pr">
+<h1>Nextcloud iOS app Free for Download on App Store</h1>
+
+<p><strong>August 24, Berlin, Germany.</strong> Nextcloud GmbH, the leading global supplier of open source enterprise sync, share and communication technology, announces at the annual Nextcloud Conference that the Nextcloud iOS will be a free download from the Apple App store going forward. This move is part of a strong focus on enterprise business, with support contracts the main driver for Nextcloud GmbH's profitability.</p>
+
+<p><blockquote>Nextcloud aims to empower community to take back control over their data, not monetize them.</blockquote> said Frank Karlitschek, Managing Director of Nextcloud GmbH. <blockquote>Making the iOS app free, like the Android and desktop apps, fits in our strategy to focus on providing excellent enterprise support.</blockquote></p>
+
+<p>Late 2016 Nextcloud announced open sourcing the iOS app, putting the full code under the GPL license with an App Store exception in github. Tomorrow, August 25, the Nextcloud Conference Focus Friday will start with sessions around scalability, security and collaboration in large enterprise organizations. The conference team expects well over 150 attendees in the audience.</p>
+
+<p><blockquote>The Nextcloud iOS client has progressed tremendously in the last year</blockquote> said Marino Faggiana, iOS engineer at Nextcloud. <blockquote>removing the price tag means even more people have access to our private cloud solution!</blockquote></p>
+
+<p><strong>About Nextcloud</strong><br />
+Nextcloud offers an industry-leading fully open source solution for on-premise data handling and communication with an uncompromising focus on security and privacy and unprecedented scalability. Nextcloud brings together universal access to data with next-generation secure communication and collaboration capabilities under direct control of IT and integrated with existing compliant infrastructure. Nextcloud's open, modular architecture, emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">https://nextcloud.com</a> or follow <a href="https://twitter.com/nextclouders">@Nextclouders on Twitter.</a></p>
+
+<p><strong>Links:</strong><br />
+<ul>
+    <li><a href="https://nextcloud.com/about">About Nextcloud</a></li>
+    <li><a href="https://nextcloud.com/conf">Nextcloud Conference information</a></li>
+    <li><a href="https://nextcloud.com/blog/nextcloud-ios-app-open-sourced/">Blog about iOS app open sourced</a></li>
+    <li><a href="https://nextcloud.com/education">Information on Nextcloud in Education</a></li>
+    <li><a href="https://itunes.apple.com/us/app/nextcloud/id1125420102">iOS app in the Appstore</a></li>
+    <li><a href="https://github.com/nextcloud/ios">iOS app source code</a></li>
+</ul>
+<br />
+</p>
+
+<p><strong>Press contacts</strong></p>
+<p><strong>Nextcloud GmbH</strong><br />
+Jos Poortvliet<br />
+Head of Marketing<br />
+<a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
+</div>
