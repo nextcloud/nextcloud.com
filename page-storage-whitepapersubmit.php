a95887dc22c22427e8f15e640e13d4ec1d084109
Jos Poortvliet <jospoortvliet@gmail.com>
2017-09-11 13:37:36 +0200

    Make all errors look pretty
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-storage-whitepapersubmit.php
+++ page-storage-whitepapersubmit.php
@@ -1,71 +1,4 @@
-<?php
-if(isset($_POST['email'])) {
-   function died($error) {
-    // error code goes here
-        echo "We are very sorry, but there were error(s) found with the form you submitted: ";
-        echo "<br />";
-        echo $error."<br />";
-        echo "Please go back and fix these errors.<br />";
-        die();
-    }
-    // validation expected data exists
-    if(!isset($_POST['email'])) {
-        died('We can not find an email address. Did you fill in your email?'); }
-    $email_to = $_POST['email']; // required
-    $error_message = "";
-  if(RECAPTCHA_SECRET !== '' && isset($_POST['g-recaptcha-response'])) {
-    $url = 'https://www.google.com/recaptcha/api/siteverify';
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_SECRET, 'response' => $_POST['g-recaptcha-response'])));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $server_output = curl_exec($ch);
-
-    $server_output = json_decode($server_output, true);
-
-    curl_close($ch);
-
-    if (!isset($server_output['success']) || $server_output['success'] !== true) {
-      $error_message .= 'The captcha result was invalid.<br />';
-    }
-  } else {
-    $error_message .= 'Captcha code is missing.<br />';
-  }
-
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-        function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-// the app review mailing list address
-    $email_from = "sales@nextcloud.com";
-    $email_jos = "jos@nextcloud.com";
-	$email_subject = "Nextcloud Windows Network Drive and Sharepoint datasheet download";
-    $email_message .= "\n";
-    $email_message .= "Thank you for your interest in our external storage datasheets!"."\n\n";
-    $email_message .= "You can download the datasheets here:"."\n";
-    $email_message .= "https://nextcloud.com/wp-content/themes/next/assets/files/3NLkR6bd/wnd-datasheet.pdf"."\n";
-    $email_message .= "https://nextcloud.com/wp-content/themes/next/assets/files/3NLkR6bd/sharepoint-datasheet.pdf"."\n\n";
-    $email_message .= "Your feedback is welcome. If you have any questions or if you are interested in a demo, let us know!"."\n\n";
-    $email_message .= "Greetings,"."\n";
-    $email_message .= "The Nextcloud team";
-
-// create email headers
-    $headers = 'From: '.$email_from."\r\n".
-    'Reply-To: '.$email_from;
-// Send the email to the list
-    @mail($email_to, $email_subject, $email_message, $headers);
-// Second email to subscribe to the mailing list
-    @mail($email_jos, $email_subject, $email_to, $headers);
- ?>
-
-    <!-- success html here -->
-<head>
+ <head>
 <link href="<?php echo get_template_directory_uri(); ?>/assets/css/storage.css" rel="stylesheet">
 </head>
 <div class="background storage-background">
@@ -78,29 +11,97 @@ if(isset($_POST['email'])) {
 		</div>
 	</div>
 </div>
+
+
 <?php
-$email_to =  filter_var($email_to, FILTER_SANITIZE_EMAIL);
-if (filter_var($email_to, FILTER_VALIDATE_EMAIL)) {
-?>
-<section class="section--whitepaper">
-    <div class="container text-center">
-        <h3>Thank you for your interest in our data sheets</h3>
-        <p>The data sheets have been sent to <?php echo($email_to); ?>,</p>
-        <p>check your spam folder if you can not find it!</p>
-    </div>
-</section>
-<?php
-} else {
-?>
-<section class="section--whitepaper">
-    <div class="container text-center">
-        <h3>You did not enter a valid email address</h3>
-        <p>Use the back key to go to the previous page and enter your email address to receive your data sheets!</p>
-    </div>
-</section>
-<?php
-}
-?>
-<?php
+if(isset($_POST['email'])) {
+	function died($error) {
+		// error code goes here
+		?>
+		<section class="section--whitepaper">
+			<div class="container text-center">
+				<h3>Sorry, there was an error with the form you submitted</h3>
+				<p>The error(s) detected:<br/>
+					<?php
+					echo $error . "<br />";
+					?>
+				</p>
+				<p>Use the back key to go to the previous page and fix the
+					problem!</p>
+			</div>
+		</section>
+		<?php
+	}
+
+	// validation expected data exists
+	$error_message = "";
+
+	$email_to =  filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); // requires
+	if(!filter_var($email_to, FILTER_VALIDATE_EMAIL)) {
+		$error_message .= 'The email address supplied is invalid.<br />';
+	}
+
+	if (defined('RECAPTCHA_SECRET') && RECAPTCHA_SECRET !== '' && isset($_POST['g-recaptcha-response'])) {
+		$url = 'https://www.google.com/recaptcha/api/siteverify';
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_SECRET, 'response' => $_POST['g-recaptcha-response'])));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$server_output = curl_exec($ch);
+
+		$server_output = json_decode($server_output, true);
+
+		curl_close($ch);
+
+		if (!isset($server_output['success']) || $server_output['success'] !== true) {
+			$error_message .= 'The captcha result was invalid.<br />';
+		}
+	} else {
+		$error_message .= 'Captcha code is missing.<br />';
+	}
+
+	if (strlen($error_message) > 0) {
+		died($error_message);
+	} else {
+		function clean_string($string) {
+			$bad = array("content-type", "bcc:", "to:", "cc:", "href");
+			return str_replace($bad, "", $string);
+		}
+
+// the app review mailing list address
+		$email_from = "sales@nextcloud.com";
+		$email_jos = "jos@nextcloud.com";
+		$email_subject = "Nextcloud Windows Network Drive and Sharepoint datasheet download";
+		$email_message .= "\n";
+		$email_message .= "Thank you for your interest in our external storage datasheets!" . "\n\n";
+		$email_message .= "You can download the datasheets here:" . "\n";
+		$email_message .= "https://nextcloud.com/wp-content/themes/next/assets/files/3NLkR6bd/wnd-datasheet.pdf" . "\n";
+		$email_message .= "https://nextcloud.com/wp-content/themes/next/assets/files/3NLkR6bd/sharepoint-datasheet.pdf" . "\n\n";
+		$email_message .= "Your feedback is welcome. If you have any questions or if you are interested in a demo, let us know!" . "\n\n";
+		$email_message .= "Greetings," . "\n";
+		$email_message .= "The Nextcloud team";
+
+// create email headers
+		$headers = 'From: ' . $email_from . "\r\n" .
+			'Reply-To: ' . $email_from;
+// Send the email to the list
+		@mail($email_to, $email_subject, $email_message, $headers);
+// Second email to subscribe to the mailing list
+		@mail($email_jos, $email_subject, $email_to, $headers);
+	?>
+
+	<!-- success html here -->
+	<section class="section--whitepaper">
+		<div class="container text-center">
+			<h3>Thank you for your interest in our data sheets</h3>
+			<p>The data sheets have been sent to <?php echo($email_to); ?>,</p>
+			<p>check your spam folder if you can not find it!</p>
+		</div>
+	</section>
+	<?php
+	}
 }
 ?>
