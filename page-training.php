0941bf18e73c829e34f608d8c8e50d6ab7866e1c
Jos Poortvliet <jospoortvliet@gmail.com>
2017-09-20 13:07:19 +0200

    first draft

---
+++ page-training.php
@@ -0,0 +1,126 @@
+<head>
+<link type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/vendor/jquery-ui.css" rel="stylesheet" />
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/training.css" rel="stylesheet">
+<script>
+require(["require.config"], function() {
+	require(["pages/training", "modules/submenu", "bootstrap"])
+});
+</script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<div class="training-background">
+    <div class="container-fluid background">
+        <div class="container">
+			<div class="col-md-6 topheader">
+				<h1>Nextcloud Training</h1>
+				<h2>Workshops, keynotes and technical talks in a relaxed ambiance</p>
+			</div>
+		</div>
+	</div>
+<!--    <div class="container-fluid menu" id="menuAnchor">
+		<div class="container buttons">
+            <a href="#eduedition">education edition</a>
+			<a href="#easy">ease of use</a>
+			<a href="#scalability">scalability</a>
+			<a href="#control">control</a>
+		</div>
+	</div>-->
+</div>
+
+<section class="section--intro">
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+            <p class="section--paragraph text-center">The Nextcloud Training provides IT professionals the opportunity to define their own strategy to get the most out of Nextcloud. Meet Nextcloud experts, learn best practices and build up skills!</p>
+            <p class="section--paragraph text-center">Save the date: March X, 2018 in the center of Stuttgart</p>
+            <p class="section--paragraph text-center"><a href="" class="button button--blue button--arrow button--large">Sign up for more information</a></p>
+		</div>
+	</div>
+</div>
+</section>
+<div class="separator"></div>
+
+<section class="section--nextcloud">
+<div class="container">
+	<div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <p class="section--heading-1 section--text--center revealOnScroll">Business changes</p>
+            <p class="section--paragraph section--text--center revealOnScroll">Internal demand forces enterprises to embrace cloud-based file sync, share and collaboration platforms but the IT departments have to cope with massive legal and security risks.</p>
+            <p class="section--paragraph section--text--center revealOnScroll">Building your Private Cloud with Nextcloud is the solution.</p>
+            <p class="section--paragraph section--text--center revealOnScroll">Nextcloud is the most popular open source file share and collaboration solution, uniquely fusing the ease of use of consumer platforms with military grade security, reliability and scalability.</p>
+            <p class="section--paragraph section--text--center revealOnScroll">At our training day you get an opportunity to discuss changing demands in business and their impact on IT security. Our experts will help you get ahead of the curve and benefit from the opportunities while avoiding the downsides.</p>
+        </div>
+    </div>
+</div>
+</section>
+
+<section class="section--benefits benefits">
+<div class="container-fluid banner">
+    <div class="container">
+    <div class="col-md-6">
+        <p class="section--heading-1 section--text--center revealOnScroll">Benefits</p>
+        <p class="section--paragraph">
+        <ul>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> get the most out of Nextcloud for users and admins</li>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> learn best practices and unique skills from experts</li>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> speed up decision making, deployment, integration & development</li>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> meet the developers and decision makers of Nextcloud</li>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> meet other customers and partners</li>
+        </ul>
+        </p>
+    </div>
+    <img class="big-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/workshop2.jpg">
+    <img class="small-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/workshop2.jpg">
+    </div>
+</div>
+</section>
+
+<!--<div class="separator"></div>-->
+
+<section class="section--training">
+<div class="container">
+	<div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <p class="section--heading-1 section--text--center revealOnScroll">Program</p>
+            <p class="section--paragraph section--text--center revealOnScroll">The training will have a 2 day program with keynotes and multiple tracks of workshops and tech talks.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 revealOnScroll image--feature">
+            <ul class="revealOnScroll">
+                <li>Configuring external storage the Right Way, covering SMB, NFS, Windows Network Drive, SharePoint and Object Storage</li>
+                <li>Optimizing Security and using Encryption</li>
+                <li>Secure web conferencing and chat</li>
+                <li>Backup and Restore your Nextcloud instance</li>
+                <li>Configuring Collabora Online</li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <ul class="revealOnScroll">
+                <li>Installation, maintenance and basic troubleshooting of Nextcloud</li>
+                <li>Scaling up and improving performance of Nextcloud</li>
+                <li>Global scale: creating separation of data between data centers</li>
+                <li>Authentication with LDAP and SAML
+                <li>Getting the most out of sharing</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <img class="img-responsive featureimg" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/workshop3.jpg">
+        </div>
+        <div class="col-md-6">
+            <p class="section--paragraph  revealOnScroll">We will also cover branding, clients, versioning & quotas, full-text search, virus scanner, outlook integration and more. Stay tuned for the detailed schedule and list of speakers!</p>
+            <p class="section--paragraph  revealOnScroll">Visitors should bring a laptop with 8GB ram, 16GB storage, wlan, USB2 or 3, administrator rights and virtual box/VMware or Hyper-V set up and ready to go.</p>
+            <p class="section--paragraph  revealOnScroll">Basic Linux knowledge is required. All tech talks are done in English and German. Workshops are English.</p>
+         </div>
+    </div>
+    <p class="section--paragraph text-center"><a href="" class="button button--blue button--arrow button--large">Subscribe to stay updated</a></p>
+</div>
+</section>
+
+
+
+
+
+
