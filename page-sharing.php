b6fc27b395b95f261fc988fda4bb46960c07f131
Jos Poortvliet <jospoortvliet@gmail.com>
2017-09-26 20:26:20 +0200

    add app store sharing, monitoring and storage

--- page-sharing.php
+++ page-sharing.php
@@ -85,6 +85,29 @@ require(["require.config"], function() {
     </div>
 </section>
 
+<section class="section--appstore benefits">
+<div class="container-fluid banner">
+    <div class="container">
+    <div class="col-md-6">
+        <p class="section--heading-1 section--text--center revealOnScroll"><?php echo $l->t('Extend your cloud');?></p>
+        <p class="section--paragraph revealOnScroll"><?php echo $l->t('You can extend the functionality of your Nextcloud with extra features from the Nextcloud app store. Among the more than 100 apps you can find features that enhance sharing, including:');?></p>
+        <p class="section--paragraph">
+        <ul>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> <?php echo $l->t('Groupfolders allows admin-shared folders users can\'t remove');?></li>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> <?php echo $l->t('Activities for shared file downloads so you can track who downloads your shared files');?></li>
+        <li class="revealOnScroll"><i class="fa-check fa"></i> <?php echo $l->t('Easy sharing via email, Google+, Diaspora, Facebook and more social channels');?></li>
+        </ul>
+        </p>
+        <div class="text-center morebuttondiv">
+			<a href="https://apps.nextcloud.com" class="button button--large button--arrow revealOnScroll"><?php echo $l->t('Appstore');?> <i class="icon-arrow-circle-o-right icon"></i></a>
+        </div>
+    </div>
+    <img class="big-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/appstore-big.png">
+    <img class="small-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/appstore-big.png">
+    </div>
+</div>
+</section>
+
 <div id="federationScroll"></div>
 
 <section class="section--federation">
@@ -113,3 +136,5 @@ require(["require.config"], function() {
 	</div>
 </div>
 </section>
+
+
