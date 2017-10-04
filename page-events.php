da0c6e67e34bb930e020d586e78b6e7bf020031b
blizzz <blizzz@arthur-schiwon.de>
2017-10-04 13:46:41 +0200

    Ubucon got cancelled 😢

--- page-events.php
+++ page-events.php
@@ -164,11 +164,6 @@
 						<td>October 4</td>
 					</tr>
                     <tr>
-						<td><a  class="hyperlink" target="_blank" href="https://ubucon.de/2017/programm/#nextcloud">Ubucon Deutschland</a></td>
-						<td>Wolfsburg, Germany</td>
-						<td>October 13-15</td>
-					</tr>
-                    <tr>
 						<td><a  class="hyperlink" target="_blank" href="https://allthingsopen.org/speakers/frank-karlitschek/">All Things Open</a></td>
 						<td>RALEIGH, NC, USA</td>
 						<td>October 23-24</td>
