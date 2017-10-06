569039c43831ea79c7f634c51dcb3052cb6aee6a
Jos Poortvliet <jospoortvliet@gmail.com>
2017-10-06 19:08:44 +0200

    Remove Lato font

--- templates/head.php
+++ templates/head.php
@@ -13,7 +13,6 @@
 	<meta name="description" content="Nextcloud is an open source, self-hosted file sync and share and communication app platform. Access  &amp; sync your files, contacts, calendars  &amp; communicate and collaborate across your devices. You decide what happens with your data, where it is and who can access it!">
 	<meta name="theme-color" content="#0082c9">
 
-	<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i" rel="stylesheet">
 	<link href="<?php echo get_template_directory_uri(); ?>/assets/css/custom.css" rel="stylesheet">
 
