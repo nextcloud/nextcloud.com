9a5fb0441439a007bdf0fc0098334ba3286f9dcf
Jos Poortvliet <jospoortvliet@gmail.com>
2017-11-16 11:24:00 +0100

    add user management

---
+++ page-usermanagement.php
@@ -0,0 +1,151 @@
+<head>
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/usermanagement.css" rel="stylesheet">
+<script src="https://www.google.com/recaptcha/api.js?onload=CaptchaCallback&render=explicit" async defer></script>
+<script type="text/javascript">
+    var CaptchaCallback = function() {
+        grecaptcha.render('RecaptchaField1', {'sitekey' : '<?php echo RECAPTCHA_SITEKEY; ?>'});
+        grecaptcha.render('RecaptchaField2', {'sitekey' : '<?php echo RECAPTCHA_SITEKEY; ?>'});
+    };
+</script>
+</head>
+
+<div class="background usermanagement-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+				<h1><?php echo $l->t('User Management');?></h1>
+				<h2><?php echo $l->t('5 or 50 million users, handling accounts is easy.');?></p>
+			</div>
+		</div>
+	</div>
+</div>
+<section class="section--header">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Integrated account management');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Nextcloud features built in account management with optional two-factor authentication as well as Single-sign-on support and integration with external user directories like LDAP or IMAP, SMB, FTP and more.');?></p>
+		</div>
+	</div>
+</div>
+</section>
+<section class="section--builtin">
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 revealOnScroll image--floated">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/usermanagement.png"><img class="img-responsive featureimg" src="<?php bloginfo('template_directory'); ?>/assets/img/features/usermanagement.png" /></a>
+		</div>
+		<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Easy user management');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Through the built in user management, admins can create, modify, search and view user accounts. Name, disk quota, mail addresses and group membership can be handled and users can be given administrator privileges if needed.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('If local users are not enough, take advantage of support for one or more LDAP directories instead of or in addition to local users. Nextcloud can secure logins with two-factor authentication and supports single sign-on through SAML, OAuth2, Kerberos and others.');?></p>
+		</div>
+	</div>
+</div>
+</section>
+
+<div class="container-fluid quote">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2 featureblock revealOnScroll">
+				<p class=""><?php echo $l->t('Learn more about how Nextcloud helps you keep your data secure.');?></p>
+				<div class="text-center morebuttondiv">
+					<a href="/secure" class="button button--large button--arrow"><?php echo $l->t('Security in Nextcloud');?> <i class="icon-arrow-circle-o-right icon"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<section class="section--ldap">
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 revealOnScroll">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/ldap.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/ldap.png" class="img-responsive featureimg" /></a>
+		</div>
+		<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph__tittle"><?php echo $l->t('LDAP/Active Directory user management');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Nextcloud features a high performance, full featured LDAP application which lets users authenticate through their LDAP credentials. Group management, quotas and sharing permissions can be managed as usual. Nextcloud features LDAP group support, sharing, primary groups and easy configuration with auto-detection of LDAP attributes.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('For security reasons, the default LDAP configuration is fully read-only. A plugin infrastructure exists which can support writing to LDAP. Some apps have been developed which enable administrators to manage a LDAP server through Nextcloud. Creating, updating and removing LDAP configurations can optionally be done through an external, REST-based API.');?></p>
+			<p class="section--paragraph"><a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_user/user_auth_ldap.html"><?php echo $l->t('Find documentation here.');?></a></p>
+		</div>
+	</div>
+</div>
+</section>
+
+<section class="section--whitepaper quote">
+	<div class="container revealOnScroll">
+        <div class="row">
+			<div class="col-lg-8">
+                <h2 class="revealOnScroll"><?php echo $l->t('Get our LDAP datasheet');?></h2>
+                <form name="whitepaper" method="post" action="../ldap-whitepapersubmit">
+                    <p><label for="email"><?php echo $l->t('Download our free datasheet: <br /> LDAP user management');?><br>
+                    <td colspan="2" style="text-align:center">
+                    <div class="">
+                        <div id="RecaptchaField1"></div>
+                    </div>
+                    </td>
+                    <input class="mail" type="text" name="email" maxlength="80" placeholder="Enter your email"></label> <input class="button button--large" type="submit" value=" Get the datasheet "></p>
+                </form>
+            </div>
+        </div>
+        <img class="responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/whitepapers/ldap-thumbnail-banner.png"/>
+	</div>
+</section>
+
+<section class="section--more">
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 revealOnScroll image--floated">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/TOTP.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/TOTP.png" class="img-responsive featureimg" /></a>
+		</div>
+		<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Secure Authentication');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Nextcloud supports securing authentication through Nextcloud itself with a plugin-based two-factor authentication method. Supported are, among others, TOTP, SMS 2-factor and U2F. Upon activation by the system administrator, users have to activate 2FA in their personal settings.');?></p>
+			<p class="section--paragraph"><a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_user/two_factor-auth.html"><?php echo $l->t('Find documentation here.');?></a></p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 revealOnScroll">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/saml.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/saml.png" class="img-responsive featureimg" /></a>
+		</div>
+		<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Single-sign-on');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Nextcloud features a SSO & SAML app for easy Single-sign-on integration. SAML 2.0 is supported with OneLogin, Shibboleth and ADFS. The app also features authentication via environment variables, including support for Kerberos, OAuth2, OpenID and many other mechanisms provided by Apache modules.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('The native SAML integration negates the need for external software like Apache modules. Native SAML is compatible with all webservers and supports group memberships, flexible session management and app specific passwords.');?></p>
+			<p class="section--paragraph"><a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_server/sso_configuration.html"><?php echo $l->t('Find documentation here.');?></a></p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 revealOnScroll image--floated">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/gluu.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/gluu.png" class="img-responsive featureimg" /></a>
+		</div>
+		<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Other user backends');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Nextcloud can work with a number of other backends for authentication and user handling: IMAP, SMB and FTP. These can be used to integrate Nextcloud more easily in company infrastructure. On the app store, more authentication and user backends can be found, like integration with Gluu.');?></p>
+			<p class="section--paragraph"><a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_user/user_auth_ftp_smb_imap.html"><?php echo $l->t('Find documentation here');?></a> and <a class="hyperlink" href="https://apps.nextcloud.com"><?php echo $l->t('our app store here.');?></p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 revealOnScroll">
+			<a href="<?php bloginfo('template_directory'); ?>/assets/img/features/api.png"><img src="<?php bloginfo('template_directory'); ?>/assets/img/features/api.png" class="img-responsive featureimg" /></a>
+		</div>
+		<div class="col-md-6 revealOnScroll">
+			<p class="section--paragraph__tittle"><?php echo $l->t('Automated or manual provisioning');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Nextcloud offers an easy to use, REST based provisioning API to create and configure user accounts. It is possible to create, edit, delete and query user attributes, query, set and remove groups, set quota and query total storage used in Nextcloud.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('The API enables an admin to query for active Nextcloud applications, application info, and to enable or disable an app remotely. HTTP requests can be used via a Basic Auth header to easily integrate workflows in existing tools and infrastructure.');?></p>
+			<p class="section--paragraph"><a class="hyperlink" href="<?php echo $DOCUMENTATION_ADMIN; ?>configuration_user/user_provisioning_api.html"><?php echo $l->t('Find documentation here.');?></a></p>
+		</div>
+	</div>
+</div>
+</section>
