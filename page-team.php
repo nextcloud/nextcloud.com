bdbe90e296091014ba75afbe441cd252c1cd12b2
Jos Poortvliet <jospoortvliet@gmail.com>
2017-12-10 23:39:13 +0100

    add images
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-team.php
+++ page-team.php
@@ -300,7 +300,7 @@
 		</div>
         <div class="col-md-4">
 			<div class="teamp">
-				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
+				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/dogan.jpg" />
 				<h2 class="teamname">Doğan Can Uçar</h2>
 				<div class="teammeta">
 					<h3 class="teamtitle">Software Engineer</h3>
@@ -469,7 +469,7 @@
 		</div>
 		<div class="col-md-4">
 			<div class="teamp">
-				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
+				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/tomaz.jpg" />
 				<h2 class="teamname">Tomaz Canabrava</h2>
 				<div class="teammeta">
 					<h3 class="teamtitle">Software Engineer</h3>
