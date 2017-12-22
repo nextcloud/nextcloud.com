401398b8b13a25ac6ab8f42197f0a423ba033576
Jos Poortvliet <jospoortvliet@gmail.com>
2017-12-22 10:40:20 +0100

    add section on verticals to be included on other pages

---
+++ verticals.php
@@ -0,0 +1,30 @@
+<!--<section class="section--vertical">
+<div class="container">
+    <div class="row">
+        <h2 class="text-center revealOnScroll"><?php echo $l->t('Perfect for you');?></h2>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 revealOnScroll">
+            <p class="section--paragraph text-center"><?php echo $l->t('Nextcloud is committed to provide technology that is a perfect fit for your organization!');?></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 col-md-offset-3 revealOnScroll">
+			<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/headers/healthcare-small.jpg" /></a>
+			<div class="row text-center"><p class="section--paragraph__tittle"><?php echo $l->t('in Healthcare');?></p>
+			<p class="section--paragraph text-center"><?php echo $l->t('Nextcloud provides the highest security for protected health information.');?></p></div>
+			<div class="text-center morebuttondiv">
+                <a href="/healthcare" class="button button--blue button--large button--arrow"><?php echo $l->t('learn more');?> <i class="icon-arrow-circle-o-right icon"></i></a>
+            </div>
+		</div>
+		<div class="col-md-3 revealOnScroll">
+			<img src="<?php bloginfo('template_directory'); ?>/assets/img/headers/education-small.jpg" class="img-responsive" />
+			<div class="row text-center"><p class="section--paragraph__tittle"><?php echo $l->t('in Education');?></p>
+			<p class="section--paragraph text-center"><?php echo $l->t('Nextcloud has unique features for research and academic institutions.');?></p></div>
+			<div class="text-center morebuttondiv">
+                <a href="/education" class="button button--blue button--large button--arrow"><?php echo $l->t('learn more');?> <i class="icon-arrow-circle-o-right icon"></i></a>
+            </div>
+		</div>
+    </div>
+</div>
+</section>-->
