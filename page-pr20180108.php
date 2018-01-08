69cc2a3afc506d5cfcaa166276dfdafcb9575888
Jos Poortvliet <jospoortvliet@gmail.com>
2018-01-08 13:02:55 +0100

    add press release
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20180108.php
@@ -0,0 +1,39 @@
+<div class="pr">
+<h1>100K app downloads in 2017 shows strong growth of self-hosted online office market</h1>
+
+<p>January 8 2018, Stuttgart. Nextcloud, the globally leading supplier of open source enterprise sync, share and communication technology, and Collabora Productivity, the driving force behind putting LibreOffice in the Cloud, announce reaching over 100K downloads of the Collabora Nextcloud app in one year, showing strong growth of self-hosted online office market. Other metrics support this trend as well, Collabora has seen over two million docker image downloads of CODE, giving interested developers an easy way to get early access to the very latest feature additions and updates to LibreOffice Online. Some of these servers host  thousands of users at at customers like RegioIT in Germany and North-West University in South Africa.</p>
+
+
+<p><blockquote>"This impressive number of installations over the last year shows the strong interest in both Nextcloud and collaborative editing among private cloud users"</blockquote> said Michael Meeks, General Manager at Collabora Productivity.
+<blockquote>"Nextcloud‘s strong focus on ease of use combined with a wealth of rich business functionality makes them a vital partner"</blockquote></p>
+
+<p>On December 21, Collabora has announced their latest release of Collabora Online Development Edition – CODE 3.0, which, for the very first time brings full-feature editing dialogs, familiar from the PC LibreOffice version to the browser. Developers and home users are encouraged to update, try it out and get involved with the latest developments – however for enterprise production users are recommended to continue to use Collabora Online 2.1.5 or wait for the next release – due soon.</p>
+
+<p><blockquote>"Self hosted cloud solutions allow customers to reap the benefits of online collaboration while remaining in control over their data"</blockquote> said Andreas Rode, Head of Sales at Nextcloud GmbH. <blockquote>"Collabora Online is an excellent choice for enterprises wanting a powerful office suite for confidential documents on-premise or for Hosting and Cloud businesses who wish to include online Office Suite functionality into their service offering."</blockquote>
+</p>
+
+<p>Nextcloud uptake has been particularly strong among the security-sensitive sector of the wider Enterprise File Sync and Share Market, like businesses in finance, insurance, government and healthcare. Unique capabilities like the End-to-end Encryption in Nextcloud 13 as well as secure communication allow companies to ease GDPR compliance and dramatically improve data security in a world with growing legal and hacking threaths.</p>
+
+<p><strong>About Collabora</strong><br />
+<p>Collabora Productivity is the driving force behind putting LibreOffice in the Cloud, providing a range of products and consulting to enterprise and government. Powered by the largest team of certified LibreOffice engineers in the world, it is a leading contributor to the LibreOffice codebase and community. Collabora Office for Desktop and Collabora Online provide a business-hardened office suite with long-term, multi-platform support. Collabora Productivity is a division of Collabora, the global software consultancy dedicated to providing the benefits of Open Source to the commercial world, specialising in mobile, automotive and consumer electronics industries. For more information, <a href="https://www.collaboraoffice.com">visit www.collaboraoffice.com,</a> or follow <a href="https://twitter.com/CollaboraOffice">@CollaboraOffice on Twitter,</a></p>
+
+<p><strong>About Nextcloud</strong></br />
+Nextcloud offers the industry-leading fully open source solution for on-premise data handling and communication with an uncompromising focus on security and privacy and unprecedented scalability. Nextcloud brings together universal access to data with next-generation secure communication and collaboration capabilities under direct control of IT and integrated with existing compliant infrastructure. Nextcloud's open, modular architecture, emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">https://nextcloud.com</a> or follow <a href="https://twitter.com/nextclouders">@Nextclouders on Twitter.</a></p>
+
+<p><strong>Links:</strong><br />
+About Nextcloud: <a href="https://nextcloud.com/about">nextcloud.com/about</a><br />
+Collabora Online on Nextcloud: <a href="https://nextcloud.com/collaboraonline">nextcloud.com/collaboraonline</a><br />
+<br />
+</p>
+<p><strong>Press contacts</strong>
+Nextcloud GmbH<br />
+Jos Poortvliet<br />
+Kronenstraße 22A<br />
+D-70173 Stuttgart, Germany<br />
+e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a><br /><br />
+
+Collabora Productivity Limited<br />
+Jona Azizaj<br />
+e-mail: <a href="mailto:jona.azizaj@collabora.com">jona.azizaj@collabora.com
+</p>
+</div>
