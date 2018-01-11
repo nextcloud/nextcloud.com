19bb2349aef23fb553b0d855a5cba34b0f868904
Jos Poortvliet <jospoortvliet@gmail.com>
2018-01-11 11:52:52 +0100

    remove f-droid
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-talk.php
+++ page-talk.php
@@ -207,19 +207,15 @@
         <h2 class="text-center"><?php echo $l->t('Get the mobile apps now!');?></h2>
         <div class="container">
             <div class="row">
-                <div class="col-md-8 col-md-offset-2 featureblock revealOnScroll">
+                <div class="col-md-6 col-md-offset-3 featureblock revealOnScroll">
                     <div class="row text-center">
-                        <div class="col-sm-4 mobileclientbuttons">
+                        <div class="col-sm-6 mobileclientbuttons">
                             <a target="_blank" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_IOS_TALK; ?>">
                             <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/appstore.png"></a>
                         </div>
-                        <div class="col-sm-4">
+                        <div class="col-sm-6">
                             <a target="_blank" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_ANDROID_TALK; ?>">
                             <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/googleplay.png"></a>
-                        </div>
-                        <div class="col-sm-4">
-                            <a target="_blank" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_FDROID_TALK; ?>">
-                            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/fdroid.png"></a>
                         </div>
                     </div>
                 </div>
