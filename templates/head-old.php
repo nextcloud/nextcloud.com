b98e18161096db2531970e4bb794a027bc0658c5
Jos Poortvliet <jospoortvliet@gmail.com>
2018-01-22 22:12:01 +0100

    add social media tags fixes #671
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- templates/head-old.php
+++ templates/head-old.php
@@ -1,17 +1,7 @@
 <!DOCTYPE html>
 <html class="no-js" itemscope itemtype="http://schema.org/Organization" <?php language_attributes(); ?>>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php wp_title('-', true, 'right'); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta itemprop="name" content="Nextcloud">
-	<meta itemprop="description" content="Nextcloud is an open source, self-hosted file sync and share and communication app platform. Access  &amp; sync your files, contacts, calendars  &amp; communicate and collaborate across your devices. You decide what happens with your data, where it is and who can access it!">
-	<meta name="description" content="Nextcloud is an open source, self-hosted file sync and share and communication app platform. Access  &amp; sync your files, contacts, calendars  &amp; communicate and collaborate across your devices. You decide what happens with your data, where it is and who can access it!">
-	<meta itemprop="image" content="https://nextcloud.com/wp-content/themes/nextcloudorgnew/assets/img/home/ncsplash.png">
-	<meta name="theme-color" content="#0082c9">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/common/favicon.png" />
-	<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/img/common/favicon.png" />
+    <?php require get_template_directory().'/templates/metadata.php';?>
 
 	<!-- New styles -->
 	<link href="<?php echo get_template_directory_uri(); ?>/assets/css/old/header.css" rel="stylesheet">
@@ -25,7 +15,7 @@
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/old/font-awesome.css"/>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/old/styles.css"/>
 	<script type="text/javascript">
-		var templateUrl = '<?= get_bloginfo('template_url'); ?>';
+		var templateUrl = '<?php get_bloginfo('template_url'); ?>';
 	</script>
 
 	<?php wp_head(); ?>
