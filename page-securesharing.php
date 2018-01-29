6ae79d3b280a5d9058bc8bb9372518fb53b05b00
Jos Poortvliet <jospoortvliet@gmail.com>
2018-01-29 22:31:40 +0100

    update the securesharing page
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-securesharing.php
+++ page-securesharing.php
@@ -1,5 +1,5 @@
 <head>
-<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/secure.css" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/securesharing.css" rel="stylesheet">
 </head>
 <div class="background security-background">
 	<div class="container">
@@ -25,33 +25,93 @@
 <section>
 <div class="container">
 	<div class="row revealOnScroll">
-			<p class="section--paragraph">Forrester urges senior corporate management and IT professionals to shift away from ad-hoc solutions like email, outdated technologies like FTP or customer-grade file sync and share. Key requirements for a solution include:
-			<ul>
-				<li>Enterprise-grade security, with governance and policies to protect valuable content and code</li>
-				<li>Ease of use, working in familiar apps such as email to avoid disruption and adoption challenges</li>
-				<li>APIs for integration with engineering, support desk, and other essential work processes</li>
-			</ul>
-			The goal should be to streamline productivity without sacrificing visibility into the distribution of digital knowledge assets.</p>
+        <p class="section--paragraph">Forrester urges senior corporate management and IT professionals to shift away from ad-hoc solutions like email, outdated technologies like FTP or customer-grade file sync and share like Dropbox. Key requirements they identify for a solution include:
+        <ul>
+            <li>Enterprise-grade security, with governance and policies to protect valuable content and code</li>
+            <li>Ease of use, working in familiar apps such as email to avoid disruption and adoption challenges</li>
+            <li>APIs for integration with engineering, support desk, and other essential work processes</li>
+        </ul>
+        The goal should be to streamline productivity without sacrificing visibility into the distribution of digital knowledge assets.</p>
 	</div>
+</div>
+</section>
+<section class="section--compliance">
+<div class="container-fluid banner">
+    <div class="container">
+    <div class="row revealOnScroll">
+        <div class="col-md-6">
+            <h3>Concerns about new technologies</h3>
+            <p class="section--paragraph">A key concern cited by 72% of the respondents is the creation of new content silos. Having to migrate data and manage it in a new location, the goals of simplifying work flows while strengthening governance will not be achieved.</p>
+            <h3>Current 'solution': email</h3>
+            <p class="section--paragraph">Over half of the teams managed by respondents is reported to use public, consumer grade file sharing services and email to share code, patches and other intellectual property within the organization as well as with customers. Forrester notes that these solutions neither protect intellectual property and competitive advantage but also offer little control or rights management, nor audit and tracking controls.
+        </div>
+        <img class="big-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/email.jpeg">
+        <img class="small-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/email-small.jpeg">
+    </div>
+    </div>
+</div>
+</section>
+<section>
+<div class="container">
 	<div class="row revealOnScroll">
-		<h3>Concerns</h3>
-		<p class="section--paragraph">With 72% of the respondents agreeing with the statement, a key concern cited is the creation of new content silos. Having to migrate data and manage it in a new location, the goals of simplifying work flows while strengthening governance will not be achieved.</p>
-		<h3>Current 'solution': email</h3>
-		<p class="section--paragraph">Over half of the teams managed by respondents is reported to use public, consumer grade file sharing services and email to share code, patches and other intellectual property within the organization as well as with customers. Forrester notes that these solutions neither protect intellectual property and competitive advantage but also offer little control or rights management, nor audit and tracking controls.
-		<h2>Real solutions</h2>
-		<p class="section--paragraph">By providing businesses an <a class="hyperlink" href="https://nextcloud.com/blog/8-ways-businesses-benefit-from-purchasing-open-source/">open source</a>, <a class="hyperlink" href="https://nextcloud.com/?p=1886">self hosted</a> file access, sync and share solution with powerful integration in existing infrastructure and storage solutions, Nextcloud makes data available to users wherever it is. Its familiar, easy to use interface allow users to work efficiently and be confident everybody has access to the same, latest version of data. Its enterprise capabilities ensure IT maintains <a class="hyperlink" href="/workflow">full control</a> over sharing, retention and availability of data within and across the boundaries of the organization. Learn more about <a class="hyperlink" href="/features">Nextcloud's features here.</a></p>
-		<h3>Wherever it is</h3>
-		<p class="section--paragraph">Modern organizations have data in a wide variety of silos like Sharepoint, Windows Network Drives, FTP sites and (often without knowledge of management) Dropbox or Google Drive. Nextcloud acts as a <a class="hyperlink" href="/storage">single file access, sync and share layer</a> across all these storage systems, integrating with enterprise native account handling like LDAP or SAML.</p>
-		<h3>Under IT's control</h3>
-		<p class="section--paragraph">No matter the storage solution used, Nextcloud can <a class="hyperlink" href="/workflow">ensure corporate policies are followed.</a> Sharing can be restricted through the file firewall, filtering on user properties like location or group, or file properties like (automatically or manually) assigned tags, file type and more. With storage transparent to users, generic data can remain where it is while IT can make sure that business critical or compliance-relevant information is stored on internal infrastructure and does not leave the company. Use our secure <a href="/secure-drop">File Drop</a> to let customers upload files to you in a easy and secure way!</p>
-		<h3>With uniquely layered encryption</h3>
-		<p class="section--paragraph">Nextcloud offers a supremely easy to use, well integrated file sync and share solution with an uniquely layered approach to security. Together with the support for nearly any storage technology, it features server-side encryption and offers the ability to employ <a href="/endtoend">end-to-end, client side encryption</a> for a subset of user files. Rather than making it an either-or choice, your business can rely on utmost security where it is needed but keep collaborative online document editing and full-text search on the server over the rest of the stored data.</p>
+        <div class="col-md-3 revealOnScroll image--feature">
+            <img class="img-responsive acess__anywhere" src="<?php echo get_template_directory_uri(); ?>/assets/img/common/access-anywhere.png" alt="access on any device" >
+        </div>
+        <div class="col-md-9">
+            <h2>Real solutions</h2>
+            <p class="section--paragraph">By providing businesses a <a class="hyperlink" href="https://nextcloud.com/?p=1886">self hosted</a> file access, sync and share solution with powerful integration in existing infrastructure and storage solutions, Nextcloud makes data available to users wherever it is. No need for new storage solutions or moving all data over. Easy integration and quick deployment brings organizations immediately in a compliant, secure state.</p>
+            <p class="section--paragraph">Its familiar, easy to use interface on web, mobile and desktop allows users to work efficiently and be confident everybody has access to the same, latest version of data. Its enterprise capabilities ensure IT maintains <a class="hyperlink" href="/workflow">full control</a> over sharing, retention and availability of data within and across the boundaries of the organization. Learn more about <a class="hyperlink" href="/features">Nextcloud's features here.</a></p>
+		</div>
+    </div>
+</div>
+</section>
+<section>
+<div class="container">
+    <div class="row revealOnScroll">
+        <div class="col-md-12">
+            <h3>Wherever it is</h3>
+            <p class="section--paragraph">Modern organizations have data in a wide variety of silos like Sharepoint, Windows Network Drives, FTP sites and (often without knowledge of management) Dropbox or Google Drive. Nextcloud acts as a <a class="hyperlink" href="/storage">single file access, sync and share layer</a> across all these storage systems, integrating with enterprise native account handling like LDAP or SAML.</p>
+		</div>
+    </div>
+</div>
+</section>
+<section>
+<div class="container">
+    <div class="row revealOnScroll">
+        <div class="col-md-4 revealOnScroll image--floated  image--feature">
+            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/firewall.png" alt="Nextcloud File Firewall" >
+        </div>
+	    <div class="col-md-8">
+            <h3>Under IT's control</h3>
+            <p class="section--paragraph">No matter the storage solution used, Nextcloud can <a class="hyperlink" href="/workflow">ensure corporate policies are followed.</a> Sharing can be restricted through the file firewall, filtering on user properties like location or group, or file properties like (automatically or manually) assigned tags, file type and more. With storage transparent to users, generic data can remain where it is while IT can make sure that business critical or compliance-relevant information is stored on internal infrastructure and does not leave the company. Use our secure <a href="/secure-drop">File Drop</a> to let customers upload files to you in a easy and secure way!</p>
+   		</div>
+    </div>
+</div>
+</section>
+<section>
+<div class="container">
+    <div class="row revealOnScroll">
+        <div class="col-md-6 revealOnScroll image--feature">
+            <img class="img-responsive acess__anywhere" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/endtoend-android2-nw.png" alt="access on any device" >
+        </div>
+	    <div class="col-md-6">
+            <h3>With uniquely layered encryption</h3>
+            <p class="section--paragraph">Nextcloud offers a supremely easy to use, well integrated file sync and share solution with an uniquely layered approach to security. Together with the support for nearly any storage technology, it features server-side encryption and offers the ability to employ <a href="/endtoend">end-to-end, client side encryption</a> for a subset of user files. Rather than making it an either-or choice, your business can rely on utmost security where it is needed but keep collaborative online document editing and full-text search on the server over the rest of the stored data.</p>
+		</div>
+    </div>
+</div>
+</section>
+<section>
+<div class="container">
+    <div class="row revealOnScroll">
+        <div class="col-md-12">
+            <h2>Designed for the real world</h2>
+            <p class="section--paragraph">Users can access data on a FTP drive shared with another company; while data stored on an external object storage could be encrypted using server-side encryption so the storage provider has no way to compromise their security.</p>
 
-        <p class="section--paragraph">Users can access data on a FTP drive shared with another company; while data stored on an external object storage could be encrypted using server-side encryption so the storage provider has no way to compromise their security.</p>
+            <p class="section--paragraph">Meanwhile, files from the HR department are always enforced to be end-to-end encrypted on their clients, protecting their files from even the worst case of a full server breach.</p>
 
-        <p class="section--paragraph">Meanwhile, files from the HR department are always enforced to be end-to-end encrypted on their clients, protecting their files from even the worst case of a full server breach.</p>
-
-        <p class="section--paragraph">The layered approach to security makes sure users have the maximum amount of productivity in their web interface while making sure sensitive data receives the ultimate protection, client-to-client. Keeping it all in a single solution simplifies management and avoids mistakes from users or IT departments that might cause costly data leaks.</p>
+            <p class="section--paragraph">The layered approach to security makes sure users have the maximum amount of productivity in their web interface while making sure sensitive data receives the ultimate protection, client-to-client. Keeping it all in a single solution simplifies management and avoids mistakes from users or IT departments that might cause costly data leaks.</p>
+        </div>
 	</div>
 </div>
 </section>
@@ -67,6 +127,3 @@
 		</div>
 	</div>
 </div>
-
-
-
