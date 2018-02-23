8541fed2799ab5461b75195dc7936f62b779e50e
Jos Poortvliet <jospoortvliet@gmail.com>
2018-02-23 16:01:50 +0100

    storage
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-storage.php
+++ page-storage.php
@@ -43,13 +43,14 @@
         <div class="row">
             <div class="col-lg-8">
                 <h2 class="revealOnScroll"><?php echo $l->t('Get our datasheets');?></h2>
-                <form name="whitepaper" method="post" action="../storage-whitepapersubmit">
+                <form name="whitepaper" method="post" action="<?php echo get_template_directory_uri()."/mautic-submit.php" ?>">
                     <p><label for="email"><?php echo $l->t('Download our free datasheets about<br /> Windows Network Drive and Sharepoint');?><br>
                     <td colspan="2">
                     <div class="">
                         <div class="g-recaptcha" data-sitekey="<?php echo RECAPTCHA_SITEKEY; ?>"></div>
                     </div>
                     </td>
+                    <input type="hidden" name="segmentId" value="8">
                     <input class="mail" type="text" name="email" maxlength="80" placeholder="Enter your email"></label> <input class="button button--large" type="submit" value=" Get the datasheet "></p>
                 </form>
             </div>
