e669f3bbec24d517c693705ecbb4ee440eda4014
Jos Poortvliet <jospoortvliet@gmail.com>
2018-03-02 19:50:11 +0100

    more licensing info
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-homepage.php
+++ page-homepage.php
@@ -12,9 +12,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 toptext ">
-				<a class="announcement" href="https://nextcloud.com/blog/nextcloud-13-brings-secure-file-sync-and-collaboration-to-the-next-level">
+				<a class="announcement" href="https://nextcloud.com/blog/how-nextcloud-protects-your-business-from-license-uncertainty/">
 				<span class="type">News</span>
-				<span class="message"><strong>Nextcloud 13 is out</strong>: The best gets even better!</span>
+				<span class="message"><strong>Nextcloud protects your business</strong> from license uncertainty!</span>
 				<?php echo file_get_contents(get_template_directory_uri()."/assets/img/next.svg"); ?>
 				</a>
 				<h1 class="jumbotron--heading-1"><?php echo $l->t('Nextcloud - Protecting your data');?></h1>
