551e5fc353f08c247db2aed37756db309387ca9b
Jurrie Overgoor <github@jurr.org>
2018-03-13 11:08:23 +0100

    Fix minor typo in page-changelog.php

--- page-changelog.php
+++ page-changelog.php
@@ -4,7 +4,7 @@
 <p>We recommend you follow our <a href="<?php echo home_url('release-channels') ?>">Release Channels</a> to always have the latest and most secure Nextcloud Server version which fits your needs.</p>
 Go directly to the latest maintenance release of:
 <ul>
-<li><a href="#latest12">Nextcloud 13</a></li>
+<li><a href="#latest13">Nextcloud 13</a></li>
 <li><a href="#latest12">Nextcloud 12</a></li>
 <li><a href="#latest11">Nextcloud 11</a></li>
 <li><a href="#latest10">Nextcloud 10 (unsupported!)</a></li>
