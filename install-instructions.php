fd70f80aa82186f6a60199f53f803cfb8ff96075
Jos Poortvliet <jospoortvliet@gmail.com>
2018-03-30 14:46:26 +0200

    implement feedback from Marius
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- install-instructions.php
+++ install-instructions.php
@@ -83,34 +83,34 @@
                                 <div class="col-md-6">
                                     <h5><?php echo $l->t('Home user appliance');?></h5>
                                     <p><?php echo $l->t('<a target="_blank" href="https://www.techandme.se/">Tech and Me</a> maintains a VM designed to be the easiest way for less technical users to get Nextcloud up and running. It builds on Ubuntu Linux and is fully set up and configured with a secure SSL/TLS connection.');?></p>
-                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://www.techandme.se/nextcloud-vm/">home user appliance</a>');?></p>
+                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://www.techandme.se/nextcloud-vm/">Get home user appliance</a>');?></p>
                                     <p><?php echo $l->t('Find <a class="hyperlink" href="https://github.com/nextcloud/vm">source here</a>.');?></p>
                                 </div>
                                 <div class="col-md-6">
                                     <h5><?php echo $l->t('Enterprise appliance');?></h5>
                                     <p><?php echo $l->t('Nextcloud GmbH maintains a free appliance built on the Univention Corporate Server (UCS). It includes LDAP and user management as well as optional Collabora Online integration.');?></p>
-                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://www.univention.com/products/univention-app-center/app-catalog/nextcloud/">enterprise appliance</a>');?></p>
+                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://www.univention.com/products/univention-app-center/app-catalog/nextcloud/">Get enterprise appliance</a>');?></p>
                                     <p><?php echo $l->t('Find <a class="hyperlink" href="https://github.com/nextcloud/univention-app">source here</a>.');?></p>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-6">
-                                    <h5><?php echo $l->t('Docker');?></h5>
+                                    <h5><?php echo $l->t('Docker image');?></h5>
                                     <p><?php echo $l->t('Several Nextcloud community members maintain a Docker image. It supports a wide range of architectures and releases, features Apache/FPM, NGINX, various databases and more.');?></p>
-                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://hub.docker.com/_/nextcloud/">Docker image</a>');?></p>
+                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://hub.docker.com/_/nextcloud/">Get Docker image</a>');?></p>
                                     <p><?php echo $l->t('Find <a class="hyperlink" href="https://github.com/nextcloud/docker">source here</a>.');?></p>
                                 </div>
                                 <div class="col-md-6">
-                                    <h5><?php echo $l->t('Snap');?></h5>
-                                    <p><?php echo $l->t('Canonical and the Nextcloud community maintain a Nextcloud Snap, including release channels and quick and easy deployment for easy home use.');?></p>
-                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://uappexplorer.com/snap/ubuntu/nextcloud">Snap image</a>');?></p>
+                                    <h5><?php echo $l->t('Snap package');?></h5>
+                                    <p><?php echo $l->t('<a class="hyperlink" href="https://canonical.com">Canonical</a> and the Nextcloud community maintain a Nextcloud Snap, including release channels and quick and easy deployment for easy home use.');?></p>
+                                    <p><?php echo $l->t('<a class="btn btn-lg btn-primary" href="https://uappexplorer.com/snap/ubuntu/nextcloud">Get Snap package</a>');?></p>
                                     <p><?php echo $l->t('Find <a class="hyperlink" href="https://github.com/nextcloud/nextcloud-snap">source here</a>.');?></p>
                                 </div>
                             </div>
 						</div>
 						<div class="col-md-4">
 							<div class="thumbnail">
-								<img style="width:100%" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/VMwelcome.png" alt="Nextcloud VM" />
+								<img style="width:100%" src="<?php echo get_template_directory_uri(); ?>/assets/img/features/VMwelcome.png" alt="Nextcloud home user VM" />
 							</div>
 							<p><?php echo $l->t('<small><strong>Security note:</strong></br>To receive information about updates and security issues, we recommend a subscription to our low-traffic <a class="hyperlink" href="https://newsletter.nextcloud.com/?p=subscribe&id=1">newsletter</a>.</small>');?></p>
 							<p><?php echo $l->t('<small><strong>Release channels:</strong></br>We offer');?> <a class="hyperlink" href="<?php echo home_url('release-channels') ?>"><?php echo $l->t('Release Channels</a> with production, stable, beta and daily-branches. This gives you the opportunity to choose your balance between stability and features.</small> Most of our Appliances support these release channels or let you fix on specific major versions.');?></p>
