92ddfa0b127021fb2e00a1f034c365af90b57de2
Jos Poortvliet <jospoortvliet@gmail.com>
2018-05-22 22:45:20 +0200

    add GDPR and RH press releases
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20180523-1.php
@@ -0,0 +1,41 @@
+<div class="pr">
+<h1>Nextcloud releases GDPR Compliance kit for on-premises collaboration solution</h1>
+
+<p>Stuttgart - May 23, 2018 - Nextcloud GmbH, the globally leading supplier of self-hosted productivity solutions, announces the availability of a GDPR compliance kit for Nextcloud customers. On-premises solutions offer the most friction-less way of coming into compliance with privacy regulations and this kit offers Nextcloud customers tools and documentation to make compliance a checklist-affair. Specific features in two Nextcloud apps and an update, a GDPR compliance steps walk-through and a detailed account of data handling in Nextcloud with instructions on how to extract, modify and delete data as required by law, make up the package.</p>
+
+<p><blockquote>GDPR compliance is a major concern for many of our customers. Our GDPR Compliance Kit essentially takes these concerns away with regards to the file handling, collaboration and communication capabilities as offered by Nextcloud.</blockquote> stated Andreas Rode, head of sales.</p>
+
+<p>The GDPR requires businesses and other organizations who handle data from private users to offer clarity about how they use this data as well as a way to access, rectify or delete it. When dealing with a public cloud vendor, data leaves the control sphere of the business and a Data Processing Agreement needs to be signed with the cloud vendor. The business then has to ensure processes are set up to deal with GDPR related requests to be compliant. Keeping data in-house by self hosting negates the need for dealing with one more external party, keeping the whole process in-house and simplifying compliance.</p>
+
+<p>Today, Nextcloud also announces a partnership with Red Hat to offer full-stack in-house storage and solutions to help customers with GDPR compliance.</p>
+
+<p>The Nextcloud GDPR compliance kit offers businesses who host a Nextcloud server apps that help their GDPR compliance, depending on their specific circumstances:
+<ul>
+<li>The Imprint update to the theming app enables businesses to show a link to a legal notice or privacy policy on login page</li>
+<li>The Delete Account app enables businesses to offer users an easy way to delete their account as required under the GDPR</li>
+<li>The Data Request App adds a way for users to request data, changes or account deletion from their user settings.</li>
+</ul></p>
+Moreover, Nextcloud offers customers access to a GPDR compliance checklist and a nearly 20 page detailed GPDR admin manual indicating where data can be found on a typical Nextcloud server and how to handle requests for extracting, rectifying and deleting data.</p>
+
+<p><blockquote>The GDPR Compliance Kit dealt with all our concerns. The global nature of the research community with frequent collaboration with European researchers and students requires global compliance awareness and an on-premises solution backed by the expertise of Nextcloud GmbH gives us the assurances we need.</blockquote>
+noted Hans Erasmus, Junior Infrastructure Architect at the North-West University in South Africa.</p>
+
+<p>The apps and GDPR checklist are released to the general public while customers have access to the full compliance kit including GDPR admin manual. The apps can be found on the Nextcloud app store while the GDPR checklist can be downloaded from our website.</p>
+
+<p><strong>Links:</strong><br />
+Announcement blog: <a href="https://nextcloud.com/blog/nextcloud-releases-gdpr-compliance-kit-for-on-premises-collaboration-solution">nextcloud.com/blog/nextcloud-releases-gdpr-compliance-kit-for-on-premises-collaboration-solution</a><br />
+About Nextcloud: <a href="https://nextcloud.com/about">nextcloud.com/about</a><br />
+Nextcloud and RedHat partnership announcement: <a href="https://nextcloud.com/press/pr20180523-2">nextcloud.com/press/pr20180523-2</a><br />
+nextcloud app store: <a href="https://apps.nextcloud.com">apps.nextcloud.com</a><br />
+White paper page: <a href="https://nextcloud.com/whitepapers">nextcloud.com/whitepapers</a><br />
+User account deletion app: <a href="https://apps.nextcloud.com/apps/drop_account">apps.nextcloud.com/apps/drop_account</a><br />
+Data request app: <a href="https://apps.nextcloud.com/apps/data_request">apps.nextcloud.com/apps/data_request</a><br />
+<br />
+</p>
+<p><strong>Press contacts</strong><br />
+Nextcloud GmbH<br />
+Jos Poortvliet<br />
+Hauptmannsreute 44A<br />
+D-70192 Stuttgart, Germany<br />
+e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
+</div>
