18c4d04b5e901b487a1c40346a8a5fba2e2bc9ea
Jos Poortvliet <jospoortvliet@gmail.com>
2018-05-24 16:31:34 +0200

    add local forkawesome font, remove external jquery
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-compare.php
+++ page-compare.php
@@ -1,6 +1,5 @@
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/old/jquery-321.min.js"></script>
 <script>
 	require(["require.config"], function() {
 		require(["pages/compare", "jquery"])
