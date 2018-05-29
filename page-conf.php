7589704dae30bcc9a66b58ab1d3d0c16303eafa3
Jos Poortvliet <jospoortvliet@gmail.com>
2018-05-29 21:07:30 +0200

    update for CfP and registration
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-conf.php
+++ page-conf.php
@@ -1,5 +1,6 @@
 <head>
 <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/conf.css" rel="stylesheet">
+
 <script>
 	require(["require.config"], function() {
 		require(["bootstrap"])
@@ -11,7 +12,7 @@
 		<div class="container">
 			<div class="col-md-6 topheader">
 				<h1>Nextcloud Conference 2018</h1>
-				<h2  class="revealOnScroll">August 23-30, Technical University Berlin</h2>
+				<h2  class="revealOnScroll">A week of collaboration, coding and fun!</h2>
 			</div>
 		</div>
 	</div>
@@ -20,16 +21,16 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-8 col-sm-offset-2">
-<!-- 			<img class="conf-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/conf-logo.svg" alt="logo" /> -->
+			<img class="conf-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/grouppic2017-2.jpg" alt="logo"/>
 <!-- 			<h2 class="revealOnScroll"></h2> -->
-<!-- 			<p class="section--paragraph__tittle revealOnScroll"></p> -->
-			<p class="section--paragraph revealOnScroll">Annually bringing the global Nextcloud Contributor Community together for a week of coding, design, discussion, talks & fun.</p>
-<!-- 			<p class="section--paragraph revealOnScroll"><a href="https://eventyay.com/e/de88e486/" class="btn btn-lg btn-primary">Register for free</a> <a href="https://eventyay.com/e/de88e486/cfs/" class="btn btn-lg btn-primary">Submit a talk <i class="icon-arrow-circle-o-right icon"></i></a></p> -->
+			<p class="section--paragraph__tittle">August 23-30, Technical University Berlin</p>
+			<p class="section--paragraph">Annually bringing the global Nextcloud Contributor Community together for a week of coding, design, discussion, talks & fun.</p>
+			<p class="section--paragraph"><a href="https://eventyay.com/e/77d26f89/" class="btn btn-lg btn-primary">Register for free</a> <a href="https://eventyay.com/e/77d26f89/cfs/" class="btn btn-lg btn-primary">Submit a talk <i class="icon-arrow-circle-o-right icon"></i></a></p>
+<!-- 			<p class="section--paragraph revealOnScroll"><a href="https://eventyay.com/e/77d26f89/" class="btn btn-lg btn-primary">Register for free</a> <a href="https://eventyay.com/e/77d26f89/cfs/" class="btn btn-lg btn-primary">Submit a talk <i class="icon-arrow-circle-o-right icon"></i></a></p> -->
 <!-- 			<p class="section--paragraph revealOnScroll"><a href="https://nextcloud.com/blog/lightning-talks-and-workshops-at-the-nextcloud-conference/" class="hyperlink" >See the program</a></p> -->
 <!-- 			<p class="section--paragraph">Find <a class="hyperlink" href="<?php echo home_url('confinfo') ?>">practical information for visitors here</a></p> -->
 <!--             <p class="section--paragraph"><a class="hyperlink" href="https://player.cloud.wowza.com/hosted/rpfqrcyp/player.html">Follow the talks streamed online</a></p> -->
-                <p class="section--paragraph"><a href="https://nextcloud.com/blog/significant-growth-at-nextcloud-conference-brought-150-customers-colleagues-and-friends-together/" class="button button--blue button--large button--arrow">See the conf report on 2017</a></p>
-                <p class="section--paragraph">Save the date: August 23-30 2018<br/> Stay tuned for more information!</p>
+<!--                 <p class="section--paragraph">Save the date: August 23-30 2018<br/> Stay tuned for more information!</p> -->
 		</div>
 	</div>
 </section>
@@ -63,9 +64,10 @@
 				<li>Intro to the Android app dev process, why you should join the design team and how we do performance testing</li>
 				<li>Status updates on the Calendar,  Theming and iOS apps</li>
 			</ul></p>
-			<p class="section--paragraph revealOnScroll">Stay tuned for more information! We'll open registration later this year and expect our call for papers early 2018.</p>
+<!-- 			<p class="section--paragraph revealOnScroll">Stay tuned for more information! We'll open registration later this year and expect our call for papers early 2018.</p> -->
 <!-- 			<p class="section--paragraph revealOnScroll">On Friday the 24th we have a <a class="hyperlink" href="https://nextcloud.com/blog/focus-friday-during-the-nextcloud-conference/">special program with a focus on enterprise and large scale Nextcloud usage.</a></p> -->
-<!-- 			<p class="section--paragraph revealOnScroll">Check out <a class="hyperlink" href="https://eventyay.com/e/de88e486/schedule/">the full program</a>!</p> -->
+<!-- 			<p class="section--paragraph revealOnScroll">Check out <a class="hyperlink" href="https://eventyay.com/e/77d26f89/schedule/">the full program</a>!</p> -->
+			<p class="section--paragraph revealOnScroll">Submit your session ideas in our <a class="hyperlink" href="https://eventyay.com/e/77d26f89/cfs/">call for papers</a> now!</p>
 <!--             <p class="section--paragraph"><a class="hyperlink" href="https://player.cloud.wowza.com/hosted/rpfqrcyp/player.html">Follow the talks streamed online</a></p> -->
 		</div>
 	</div>
@@ -73,44 +75,53 @@
 </div>
 </section>
 
-<!--
-
-
 <section class="section--call-for-papers">
 <div class="container">
 	<div class="row text-center">
 		<p class="section--paragraph__tittle revealOnScroll">We have opened our call for papers!</p>
 		<p class="section--paragraph revealOnScroll">You're invited to contribute your thoughts to our event.</p>
 		<div class="text-center morebuttondiv revealOnScroll">
-			<a href="https://conf.nextcloud.com/conference/NextcloudConference2017/program/proposal/new" class="button button--large button--arrow">Submit a talk <i class="icon-arrow-circle-o-right icon"></i></a>
+			<a href="https://eventyay.com/e/77d26f89/cfs/" class="button button--large button--blue button--arrow">Submit a talk <i class="icon-arrow-circle-o-right icon"></i></a>
 		</div>
 	</div>
 </div>
-</section>-->
+</section>
+
+<section class="section--enterprise">
+	<div class="container-fluid">
+			<div class="container">
+			<h2 class="section--heading-1 revealOnScroll">For enterprise users</h2>
+			<p class="section--paragraph revealOnScroll">Join our enterprise training day Monday August 27.<br>
+			Save the date and stay tuned for more details!</p>
+<!-- 			<p class="section--paragraph revealOnScroll"><a class="hyperlink" href="https://nextcloud.com/blog/focus-friday-during-the-nextcloud-conference/">See the Friday Focus program in our blog!</a></p> -->
+<!-- 			<p class="section--paragraph revealOnScroll"><a class="button button--blue button--arrow button--large revealOnScroll" href="coming" role="button">Know all the features</a></p> -->
+			</div>
+	</div>
+</section>
 
 <section class="section--details">
 <div class="container">
 	<div class="row">
-		<div class="col-sm-5">
+		<div class="col-md-6">
 			<!--<p class="section--paragraph revealOnScroll">You can also RSVP on <a href="https://www.facebook.com/events/1610744752475847/" target="_blank">Facebook</a>, <a href="http://www.meetup.com/nextcloud-berlin/events/224095898/" target="_blank">Meetup.com</a> or <a href="https://plus.google.com/b/107072838776436530705/events/c2dlfpjfd4600hlt9oh747prlb8" target="_blank">Google+</a>!</p>-->
 			<h2  class="revealOnScroll">Location</h2>
 			<p class="section--paragraph revealOnScroll">Our conference will take place in Berlin at the <em>Technische Universität Berlin</em>, in the "<a class="hyperlink" href="https://www.math.tu-berlin.de/" target="_blank">Institut für Mathematik</a>" building, main entree at Strasse des 17. Juni 136, 10623 Berlin.</p>
 			<iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=13.32431048154831%2C52.51259881205445%2C13.328194320201874%2C52.51417400875078&amp;layer=mapnik&amp;marker=52.513385601296605%2C13.326252400875092" style="border: 1px solid black"></iframe><br/><small><a href="http://www.openstreetmap.org/?mlat=52.51339&amp;mlon=13.32625#map=19/52.51339/13.32625">View Larger Map</a></small>
 		</div>
-		<div class="col-sm-5 col-sm-offset-2">
+		<div class="col-md-6">
 			<p class="section--paragraph revealOnScroll"><a href="<?php echo get_template_directory_uri(); ?>/assets/img/conference/talks.jpg" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/talks_wee.jpg" style="width: 95%" alt="talk" class="aligncenter" /></a></p>
 			<h2  class="revealOnScroll">Entree</h2>
-			<p class="section--paragraph revealOnScroll">The Nextcloud Contributor Conference will be entirely open and free. Registration will open later this year! <!--We only ask you to <a class="hyperlink" href="https://eventyay.com/e/de88e486/" target="_blank">register</a> to help us plan better for the event--></p>
+			<p class="section--paragraph revealOnScroll">The Nextcloud Contributor Conference will be entirely open and free. We only ask you to <a class="hyperlink" href="https://eventyay.com/e/77d26f89/" target="_blank">register</a> to help us plan better for the event</p>
 		</div>
 	</div>
 	<div class="row revealOnScroll">
-		<div class="col-sm-5  ">
+		<div class="col-md-6">
 			<p class="section--paragraph revealOnScroll"><a href="<?php echo get_template_directory_uri(); ?>/assets/img/conference/drinks.jpg" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/drinks_wee.jpg" style="width: 95%" alt="group photo" class="aligncenter" /></a></p>
 			<h2  class="revealOnScroll">Code of conduct</h2>
 			<p class="section--paragraph revealOnScroll">We want everybody to feel welcome at the event. Please be mindful of our <a class="hyperlink" href="https://nextcloud.com/community/code-of-conduct/">code of conduct</a></p>
 			<p class="section--paragraph revealOnScroll">The conference is an open space which means potential issues. Please be sure that you are comfortable with that. Of course, you can always ask people not to take pictures or video of you but note that there will be video recordings of the talks.</p>
 		</div>
-		<div class="col-sm-5 col-sm-offset-2">
+		<div class="col-md-6">
 			<h2 class="conf-acc">Berlin and accommodation</h2>
 			<p class="section--paragraph revealOnScroll">We gathered some hotel options for you! Be quick, last year many hotels already had no space left in May...</p>
 			<ul>
@@ -124,17 +135,6 @@
 	</div>
 </div>
 </section>
-
-<!-- <section class="section--fridayfocus"> -->
-<!-- 	<div class="container-fluid"> -->
-<!-- 			<div class="container"> -->
-<!-- 			<h2 class="section--heading-1 revealOnScroll">Friday Focus</h2> -->
-<!-- 			<p class="section--paragraph revealOnScroll">Friday August 25 we focus on large enterprise and educational installations of Nextcloud. Participate in workshops to get in-depth knowledge of Nextcloud, find out how to scale your installation efficiently and contribute your ideas.</p> -->
-<!-- 			<p class="section--paragraph revealOnScroll"><a class="hyperlink" href="https://nextcloud.com/blog/focus-friday-during-the-nextcloud-conference/">See the Friday Focus program in our blog!</a></p> -->
-	<!-- 		<p class="section--paragraph revealOnScroll"><a class="button button--blue button--arrow button--large revealOnScroll" href="coming" role="button">Know all the features</a></p> -->
-<!-- 			</div> -->
-<!-- 	</div> -->
-<!-- </section> -->
 
 <section class="section--about">
 <div class="container">
