1009b11a8aaeab0ccf71ca2abf9a6f695cb9fb67
Jos Poortvliet <jospoortvliet@gmail.com>
2018-06-29 14:07:55 +0200

    improve wording, add CCPA
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-gdpr.php
+++ page-gdpr.php
@@ -18,7 +18,8 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-            <p class="section--paragraph text-center"><?php echo $l->t('The GDPR makes organizations liable for any violations of user privacy and deviations from a high data security standard. Any business-to-consumer company, but also many B2B enterprises will have to ensure they have their data under control.');?></p>
+            <p class="section--paragraph text-center"><?php echo $l->t('The GDPR (General Data Protection Regulation) makes organizations liable for any violations of user privacy and deviations from a high data security standard.');?></p>
+            <p class="section--paragraph text-center"><?php echo $l->t('Legislation like the CCPA (California Consumer Privacy Act of 2018) brings similar regulation to other countries.');?></p>
             <p class="section--paragraph text-center"><?php echo $l->t('The self-hosted Nextcloud solution simplifies compliance, decreasing business risk and costs.');?></p>
 		</div>
 	</div>
