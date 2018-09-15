43eacced3bfef77f2c85c72f271956dd02a1b2fe
Jos Poortvliet <jospoortvliet@gmail.com>
2018-09-15 13:06:49 +0200

    few fixes

--- page-pr20180917.php
+++ page-pr20180917.php
@@ -21,15 +21,15 @@ said Gabriel Engel, CEO and founder of Rocket.Chat.
 
 <p>Rocket.Chat and Nextcloud are the perfect combination for those looking for an on-prem, 100% open source, and highly secure collaboration solution.</p>
 
-<strong>About Nextcloud</strong><br>
+<p><strong>About Nextcloud</strong><br>
 Nextcloud offers the industry-leading, fully open source, self-hosted Content Collaboration Platform, combining the easy user interface of consumer-grade cloud solutions with the security and compliance measures enterprises need. Nextcloud brings together universal access to data through mobile, desktop and web interfaces with next-generation, on-premise secure communication and collaboration features like real-time document editing, chat and video calls, putting them under direct control of IT and integrated with existing infrastructure.<br/>
-Nextcloud's easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">https://nextcloud.com</a> or follow <a href="https://twitter.com/Nextclouders">@Nextclouders</a> on Twitter.
+Nextcloud's easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">https://nextcloud.com</a> or follow <a href="https://twitter.com/Nextclouders">@Nextclouders</a> on Twitter.</p>
 
-<strong>About Rocket.Chat</strong><br>
+<p><strong>About Rocket.Chat</strong><br>
 Rocket.Chat is the leading open source team chat collaboration platform that allows users to communicate securely in real-time on web, desktop or mobile and to customize their interface with a range of plugins, themes and integrations with other key software. With the launch of <a href="https://rocket.chat/2018/08/31/introducing-rocket-chat-marketplace/">Rocket.Chat Marketplace</a> in September 2018, users can build, browse and download apps on Rocket.Chat's newest app ecosystem.
 <br/>By opting for Rocket.Chat, users also benefit from a range of features including audio and video conferencing, guest access, screen and file sharing, LiveChat, LDAP Group Sync, two-factor authentication (2FA), E2E encryption, SSO, dozens of OAuth providers and unlimited users guests, channels, messages, searches and files. Users can set up Rocket.Chat on cloud or by hosting their own servers on-premises.
 <br/>With more than 700 developer-contributors and over 19.8k stars on Github, Rocket.Chat has the largest and most active community of chat developers in the open source communication sector and has won multiple awards since being founded in 2015, such as a 2016 Bossie Award for Best Open Source Application and first prize in the 2017 edition of All Things Open’s Startup Competition.
-To learn more, visit <a href="https://rocket.chat.com">the website</a>, check out the <a href="https://github.com/RocketChat/Rocket.Chat">GitHub repo</a> or follow <a href="https://twitter.com/RocketChat">@RocketChat</a> on Twitter.
+To learn more, visit <a href="https://rocket.chat.com">the website</a>, check out the <a href="https://github.com/RocketChat/Rocket.Chat">GitHub repo</a> or follow <a href="https://twitter.com/RocketChat">@RocketChat</a> on Twitter.</p>
 
 <p><strong>Links:</strong><br />
 <ul>
@@ -42,20 +42,18 @@ Nextcloud GmbH<br />
 Jos Poortvliet<br />
 Hirschstrasse 26<br />
 D-70173 Stuttgart, Germany<br />
-e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
-Nextcloud GmbH<br />
-Jos Poortvliet<br />
-Hirschstrasse 26<br />
-D-70173 Stuttgart, Germany<br />
-e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
+e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></br />
+<br />
+Rocket.Chat<br />
 Isabella Russell<br />
 e-mail: <a href="mailto:press@rocket.chat">press@rocket.chat</a></p>
 </div>
 
 <div class="row">
-<div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/rocketchat-nextcloud.jpg"><img class="img-responsive size-medium" src="https://nextcloud.com/media/rocketchat-nextcloud.jpg" alt="" /></a>Rocket.Chat CEO and founder Gabriel Engel with Nextcloud CEO and founder Frank Karlitschek</div></div>
+<div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/rocketchat-nextcloud.jpg"><img class="img-responsive" src="https://nextcloud.com/media/rocketchat-nextcloud.jpg" alt="" /></a>Rocket.Chat CEO and founder Gabriel Engel with Nextcloud CEO and founder Frank Karlitschek</div></div>
 <div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/uploading.png"><img class="size-medium img-responsive" src="https://nextcloud.com/media/uploading.png" alt="" /></a> Uploading a file</div></div>
-<div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/adding-server.png"><img class="img-responsive size-medium" src="https://nextcloud.com/media/adding-server.png" alt="" /></a> Adding a server</div></div>
+<div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/adding-server.png"><img class="img-responsive" src="https://nextcloud.com/media/adding-server.png" alt="" /></a> Adding a server</div></div>
 </div>
-
-<img alt="rocket.chat x nextcloud integration demo" src="http://g.recordit.co/TYL5rObv2D.gif"/>
+<div class="row">
+<div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/big_integration_gif.gif"><img class="img-responsive" alt="rocket.chat x nextcloud integration demo" src="https://nextcloud.com/media/big_integration_gif.gif" /></a> Adding a server</div></div>
+</div>
