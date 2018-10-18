be49dda066b10ca14d927fcd3f862428dde6eeb8
Jos Poortvliet <jospoortvliet@gmail.com>
2018-10-18 11:06:16 +0200

    minor text fixes to collabora PR
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-pr20181018.php
+++ page-pr20181018.php
@@ -2,7 +2,7 @@
 <h1>Nextcloud and Collabora bring deeper integration to document collaboration</h1>
 <h3>Integration adds previews, image insertion and sidebar with comments and sharing options</h3>
 
-<p><em>UK/Germany October 18 2018.</em> The Nextcloud and Collabora teams announce they have developed deeper integration, setting the stage for easier and more efficient browser based collaboration for teams who need to create and share documents, spreadsheets, presentations and more. The improvements allow direct insertion of images from Nextcloud in documents, showing other people who are editing the file and integrating the Nextcloud file sidebar with Collabora Online. The sidebar gives users access to the sharing settings, comments, earlier file versions and the file activity overview.</p>
+<p><em>UK/Germany, October 18 2018.</em> The Nextcloud and Collabora teams announce they have developed deeper integration, setting the stage for easier and more efficient browser based collaboration for teams who need to create and share documents, spreadsheets, presentations and more. The improvements allow direct insertion of images from Nextcloud in documents, showing other people who are editing the file and integrating the Nextcloud file sidebar with Collabora Online. The sidebar gives users access to the sharing settings, comments, earlier file versions and the file activity overview.</p>
 
 <blockquote>These great improvements, and the tighter integration with Collabora Online provides productivity and efficiency benefits for our mutual customers,</blockquote>
 
@@ -26,7 +26,7 @@ Nextcloud's easy and quick deployment, open, modular architecture and emphasis o
 <p><strong>About Collabora Productivity</strong><br>
 Collabora Productivity is the driving force behind putting LibreOffice in the Cloud, providing a range of products and consulting to enterprise and government. Powered by the largest team of certified LibreOffice engineers in the world, it is a leading contributor to the LibreOffice codebase and community. Collabora Office for Desktop and Collabora Online provide a business-hardened office suite with long-term, multi-platform support. Collabora Productivity is a division of Collabora, the global software consultancy dedicated to providing the benefits of Open Source to the commercial world, specializing in mobile, automotive and consumer electronics industries. For more information, visit <a href="https://www.collaboraoffice.com">www.collaboraoffice.com</a> or follow <a href="https://twitter.com/collaboraoffice">@CollaboraOffice</a> on Twitter.</p>
 
-<p><strong>Links:</strong><br />
+<p><strong>Links:</strong>
 <ul>
 <li>Nextcloud: <a href="https://nextcloud.com">nextcloud.com</a></li>
 <li>Collabora Productivity: <a href="https://www.collaboraoffice.com">www.collaboraoffice.com</a></li>
@@ -48,7 +48,7 @@ e-mail: <a href="mailto:press@collabora.com">press@collabora.com</a></p>
 <div class="row">
 <div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/Collabora-insert-image.png"><img class="img-responsive size-medium" src="https://nextcloud.com/media/Collabora-insert-image.png" alt="" /></a> You can now insert images directly from your Nextcloud.</div></div>
 <div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/Collabora-two-others.png"><img class="size-medium img-responsive" src="https://nextcloud.com/media/Collabora-two-others.png" alt="" /></a> See avatars of other editors</div></div>
-<div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/Collabora-sharing.png"><img class="img-responsive size-medium" src="https://nextcloud.com/media/Collabora-sharing.png" alt="" /></a> Sharing sidebar</div></div>
+<div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/Collabora-sharing.png"><img class="img-responsive size-medium" src="https://nextcloud.com/media/Collabora-sharing.png" alt="" /></a> Access the sharing sidebar</div></div>
 </div>
 <div class="row">
 <div class="col-md-4"><div class="thumbnail wp-caption alignnone" style="width: 100%"><a href="https://nextcloud.com/media/Collabora-comments.png"><img class="img-responsive size-medium" src="https://nextcloud.com/media/Collabora-comments.png" alt="" /></a> View and reply to comments</div></div>
