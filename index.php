540cd41b55edbf91efad2a53dabcba6507737a6e
Jos Poortvliet <jospoortvliet@gmail.com>
2018-10-26 21:08:25 +0200

    fix blogs overview
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<head>
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/blogroll.css" rel="stylesheet">
+<!--<script>
+	require(["require.config"], function() {
+		require(["modules/youtubePlayer", "modules/submenu"])
+	});
+</script>-->
+</head>
 
+<div class="background blogroll-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+				<h1><?php echo $l->t('<span class="avoidwrap">Nextcloud News</span>');?><a href="/blogfeed" class="rss-button"><img style="vertical-align: top;" width=35 class="img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/social/rss.png"></img></a></h1>
+				<h2><?php echo $l->t('What is going on in and around Nextcloud?');?></h2>
+			</div>
+		</div>
+	</div>
+</div>
+<section class="section--old-blogs">
+<div class="container">
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'roots'); ?>
@@ -19,3 +38,5 @@
     </ul>
   </nav>
 <?php endif; ?>
+</div>
+</section>
