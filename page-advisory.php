5e632e7f182723558086bfb31a7c0f4a45941959
Morris Jobke <hey@morrisjobke.de>
2018-10-30 12:15:38 +0100

    Add surrounding container in advisory page
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- page-advisory.php
+++ page-advisory.php
@@ -1,7 +1,15 @@
 <?php
 $identifier = strtolower(str_replace('.', '', $_GET['id']));
 if(file_exists(__DIR__.'/advisories/'.$identifier.'.php')) {
-	include(__DIR__.'/advisories/'.$identifier.'.php');
+	?>
+	<section class="content">
+		<div class="container">
+			<?php
+			include(__DIR__.'/advisories/'.$identifier.'.php');
+			?>
+		</div>
+	</section>
+	<?php
 } else {
 	get_template_part('404');
 }
