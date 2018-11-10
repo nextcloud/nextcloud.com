6085d2b538be8399037d109420075d42802f293d
Jos Poortvliet <jospoortvliet@gmail.com>
2018-11-10 12:38:00 +0100

    update yt vid
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-outlook.php
+++ page-outlook.php
@@ -32,8 +32,8 @@
 				<div class="col-md-1">
 				</div>
 				<div class="col-md-10">
-				<div data-type="youtube" data-video-id="tHzX453t1H4"></div>
-				<iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/tHzX453t1H4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<div data-type="youtube" data-video-id="GPkzeM6jqKE"></div>
+				<iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/GPkzeM6jqKE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 				</div>
 				<div class="col-md-6 col-md-offset-3">
 					<p class="section--paragraph text-center"><?php echo $l->t('The Nextcloud Secure Sharing Outlook Add-in enables Nextcloud customers to easily and securely send files, folders or upload links to others from within Microsoft Outlook.<br>The Add-in can replace attachments, automatically uploading files to Nextcloud and inserting a secure link in the email. It also makes it easy for users to provide others with a secure file upload link.');?></p>
