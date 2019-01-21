7d58107d67dc145a3aa9a4f22112194630d94323
Morris Jobke <hey@morrisjobke.de>
2019-01-21 16:47:02 +0100

    Update page-outdated-php-5-6.php
    Co-Authored-By: jospoortvliet <jos@opensuse.org>

--- page-outdated-php-5-6.php
+++ page-outdated-php-5-6.php
@@ -18,7 +18,7 @@
 		<div class="col-md-8 col-md-offset-2">
 			<p class="section--paragraph__tittle"><?php echo $l->t('Update for your Nextcloud');?></p>
 			<p class="section--paragraph"><?php echo $l->t('Dear Nextcloud user,');?></p>
-			<p class="section--paragraph"><?php echo $l->t('Our update check has noticed that your server is running PHP 5.6 or older. Nextcloud releases newer than version 13 do not support these PHP releases. We no longer provide public bug and security fixes to Nextcloud 13.');?></p>
+			<p class="section--paragraph"><?php echo $l->t('Our update check has noticed that your server is running PHP 5.6. Nextcloud releases newer than version 13 do not support these PHP releases. We no longer provide public bug and security fixes to Nextcloud 13.');?></p>
 			<p class="section--paragraph"><?php echo $l->t('Running a Nextcloud release without security and stability updates puts your data at risk. We strongly recommend to upgrade to Nextcloud 14 at the earliest convenience. This version requires at least PHP 7.2 on your server.');?></p>
 			<p class="section--paragraph"><?php echo $l->t('If you are a customer and can not upgrade to Nextcloud 14, contact the Nextcloud support team for access to our long term support for Nextcloud 13. We are dedicated to accommodating your needs.');?></p>
 			<p class="section--paragraph"><?php echo $l->t('We strongly recommend all other users to install a newer PHP version and upgrade Nextcloud to a more recent, maintained version. Once you upgraded PHP on your server, Nextcloud will inform you about possible updates. Just go to the admin settings and check the version section.');?></p>
