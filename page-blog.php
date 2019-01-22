ef93bccb787c38334382295369cf58931e9278bf
Jos Poortvliet <jospoortvliet@gmail.com>
2019-01-22 11:07:43 +0100

    fix blog
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-blog.php
+++ page-blog.php
@@ -1,6 +1,6 @@
 <!-- This is the postgrid page -->
 <head>
-<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/blogroll.css?v=1" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/blogroll.css?v=2" rel="stylesheet">
 <!--<script>
 	require(["require.config"], function() {
 		require(["modules/youtubePlayer", "modules/submenu"])
@@ -18,11 +18,52 @@
 		</div>
 	</div>
 </div>
-<section class="section--grid">
+<section class="section--blogroll">
 <div class="container">
-<?php echo do_shortcode("[post_grid id='3635']"); ?>
+<div class="row">
+    <div class="text-center">
+        <?php get_search_form(); ?>
+    </div>
+</div>
+    <?php if (!have_posts()) : ?>
+        <div class="alert alert-warning">
+            <?php _e('Sorry, no results were found.', 'roots'); ?>
+        </div>
+    <?php endif; ?>
+<div class="row">
+    <?php $i = 1; ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <?php if ($i % 3 == 0) { ?> <!-- for every three posts we have to put in a row... -->
+            <div class="row">
+                <div class="col-md-4">
+                    <?php get_template_part('templates/content', get_post_format()); ?>
+                </div>
+            </div>
+        <?php } else { ?> <!-- no row needed... -->
+            <div class="col-md-4">
+                <?php get_template_part('templates/content', get_post_format()); ?>
+            </div>
+        <?php }  ?>
+        <?php $i++; ?>
+    <?php endwhile; ?>
+</div>
+    <?php if ($wp_query->max_num_pages > 1) : ?>
+    <nav class="post-nav">
+        <ul class="pager">
+        <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+        <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+        </ul>
+    </nav>
+    <?php endif; ?>
 </div>
 </section>
+
+<!--  old grid  -->
+<!--<section class="section--grid">
+<div class="container">
+<?php // echo do_shortcode("[post_grid id='3635']"); ?>
+</div>
+</section>-->
 
 <section class="section--newsletter">
     <div class="container-fluid quote">
@@ -39,3 +80,15 @@
         </div>
     </div>
 </section>
+
+
+
+<!-- This is the blog list. -->
+<head>
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/blogroll.css" rel="stylesheet">
+<!--<script>
+	require(["require.config"], function() {
+		require(["modules/youtubePlayer", "modules/submenu"])
+	});
+</script>-->
+</head>
