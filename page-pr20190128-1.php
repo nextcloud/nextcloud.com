d04c56d42fc0e545a5189cfec1c43cf13cd98752
Jos Poortvliet <jospoortvliet@gmail.com>
2019-01-26 00:47:25 +0100

    add PRs
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20190128-1.php
@@ -0,0 +1,64 @@
+<head>
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/pressrelease.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/vendor/dsgvo-video-embed.min.css">
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/dsgvo-video-embed.min.js"></script>
+
+<div class="background generic-background">
+    <img class="pattern" src="<?php echo get_template_directory_uri(); ?>/assets/img/common/empty1x1.png" alt="">
+    <div class="container">
+        <div class="col-md-6 topheader">
+            <h1>Press release</h1>
+        </div>
+    </div>
+</div>
+
+<section class="section--pressrelease">
+<div class="container">
+<div class="row">
+<div class="pr">
+<h1>Nextcloud momentum continues amid industry awareness of GDPR and data leak risks, doubles customer base in 2018</h1>
+<h3>Major new customers all over the world prove strategic focus on collaboration right</h3>
+
+<p><em>Stuttgart, January 28 2018</em> - Nextcloud continues its stellar momentum in the self-hosted enterprise file exchange and content collaboration market, more than doubling its customer base and gaining major customers like the German Federal Government, cities like Bern and Geneva and dozens of manufacturing, finance, health, education and research organizations across the world in need of secure collaboration solutions. Growing organically as a European, 100% employee-owned business, Nextcloud GmbH delivers on the needs of more integrated communication and collaboration technology the market exceedingly demands.</p>
+
+<blockquote>Strong execution in our sales team has more than doubled our customer base and we added three more team members in the last few months</blockquote> said Andreas Rode, head of sales at Nextcloud GmbH. <blockquote>Customer demand in secure, self-hosted productivity solutions continues to grow and we look to expand our team further to provide the market with the best possible solution.</blockquote>
+
+<p>A year with an unprecedented amount of press and political coverage of privacy violations and the introduction of the GDPR have brought Nextcloud to the attention of a larger audience as the easiest way to regain control over sensitive data. With website traffic nearly doubling to over 300.000 unique visitors per month, the interest in Nextcloud was overwhelming and coverage by major outlets like Le Monde in France and Der Spiegel in Germany helped draw a new audience.</p>
+
+<blockquote>Every one of the last four months of 2018 set new a record for website visitors</blockquote> recounts Jos Poortvliet, head of marketing at Nextcloud GmbH. <blockquote>Our forums saw thousands of active users every month post 150-200 messages per day, we had 500 new twitter followers every month, our conference grew by a third to over 200 people - 2018 showed stunning growth for Nextcloud.</blockquote>
+
+<p>The Nextcloud ecosystem has grown to be the largest in the self-hosted productivity market with well over 200 apps built by over 150 different private developers and businesses. In the Android app store users can now find 17 Android apps which interact with or are developed exclusively for Nextcloud, like an SMS app, three bookmark handling apps, two password managers, a Deck Kanban app, several notes apps and more. Over 500 individuals contributed to the Nextcloud code base during 2018. The launch of Nextcloud Include with funding from the Reinhard von König Preis for innovation received by Nextcloud founder Frank Karlitschek brings support for growing diversity and creativity in the community. Integration announcements with third party technologies like Rocket.Chat, Moodle, Storj.io, Mastodon and others make it easier for customers to deeply integrate Nextcloud in their infrastructure.</p>
+
+<p>Major releases in 2018 of Nextcloud 13, 14 and 15 delivered unique capabilities including granular end-to-end encryption in the clients, a self-hosted audio/video chat solution with Nextcloud Talk and the unique Video Verification security feature. Nextcloud was made accessible to an even larger audience thanks to Simple Signup which integrates easy signing up to a Nextcloud instance at a Nextcloud Provider in the Nextcloud clients, and was used by thousands of people in the first few months.</p>
+
+<blockquote>Our focus on building innovative technology while delivering customers direct access to top engineering knowledge has enabled us to continue grow and service a quickly expanding customer base. Nextcloud has established itself as the defacto standard solution for self-hosted productivity,</blockquote> said Frank Karlitschek, founder and CEO of Nextcloud GmbH. <blockquote>With the tech preview announcement today of Virtual Drive integration in the Nextcloud Desktop Client and our Customer Advisory Board, Nextcloud continues to lead the market in innovation while delivering value to customers.</blockquote>
+
+<p>Today, Nextcloud also announced the upcoming Virtual Drive technology in the Nextcloud Desktop Client and the establishment of a Customer Advisory Board to help align development with the needs of its customers. The Customer Advisory Board will be officially launched at the upcoming Enterprise Day in Stuttgart, March 2019.</p>
+
+<p><strong>About Nextcloud</strong><br>
+Nextcloud offers the industry-leading, fully open source, self-hosted Content Collaboration Platform, combining the easy user interface of consumer-grade cloud solutions with the security and compliance measures enterprises need. Nextcloud brings together universal access to data through mobile, desktop and web interfaces with next-generation, on-premise secure communication and collaboration features like real-time document editing, chat and video calls, putting them under direct control of IT and integrated with existing infrastructure.<br/>
+Nextcloud's easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">nextcloud.com</a> or follow <a href="https://twitter.com/Nextclouders">@Nextclouders</a> on Twitter.</p>
+
+<p><strong>Links:</strong>
+<ul>
+<li>Blog coverage <a class="hyperlink" href="https://nextcloud.com/blog/nextcloud-momentum-continues-amid-industry-awareness-of-gdpr-and-data-leak-risks-doubles-customer-base-in-2018/">nextcloud.com/blog/nextcloud-momentum-continues-amid-industry-awareness-of-gdpr-and-data-leak-risks-doubles-customer-base-in-2018/</a></li>
+<li>More information about Nextcloud <a class="hyperlink" href="https://nextcloud.com/about">nextcloud.com/about</a></li>
+<li>Nextcloud enterprise solutions <a class="hyperlink" href="https://nextcloud.com/enterprise">nextcloud.com/enterprise</a></li>
+<li>Nextcloud Client information <a class="hyperlink" href="https://nextcloud.com/clients">nextcloud.com/clients</a></li>
+<li>a Virtual Drive for Nextcloud Desktop Client: <a class="hyperlink" href="https://nextcloud.com/blog/nextcloud-introduces-virtual-drive-in-desktop-client-to-simplify-desktop-integration/">nextcloud.com/blog/nextcloud-introduces-virtual-drive-in-desktop-client-to-simplify-desktop-integration/</a></li>
+<li>Announcing the Nextcloud Customer Advisory Board: <a class="hyperlink" href="https://nextcloud.com/blog/nextcloud-launches-customer-advisory-board/">nextcloud.com/blog/nextcloud-launches-customer-advisory-board/</a></li>
+<li>Enterprise Day <a class="hyperlink" href="https://nextcloud.com/training">nextcloud.com/training/</a></li>
+</ul>
+
+<p><strong>Press contact</strong><br />
+Nextcloud GmbH<br />
+Jos Poortvliet<br />
+Hirschstrasse 26<br />
+D-70173 Stuttgart, Germany<br />
+e-mail: <a href="mailto:pr@nextcloud.com">pr@nextcloud.com</a></p>
+
+</div>
+</div>
+</div>
+</section>
+
