d5af03c644e786358e695fed7fbf5b9798720a34
Jos Poortvliet <jospoortvliet@gmail.com>
2019-03-10 00:20:15 +0100

    prepare to add members
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-advisory-board.php
+++ page-advisory-board.php
@@ -34,84 +34,63 @@
     </div>
 </section>
 
-
-
 <section class="section--benefits benefits">
 <div class="container-fluid banner">
     <div class="container">
-        <div class="col-md-6">
-            <p class="section--heading-1 section--text--center revealOnScroll">Meeting opportunities</p>
-            <p class="section--paragraph revealOnScroll">Several yearly face to face meetings and conference calls provide the platform for improved alignment between the needs of our customers and our development direction.</p>
-            <ul>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Quarterly conference calls reviewing the roadmap and new market developments</li>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Discussion and dinner the day before the bi-annual Nextcloud Enterprise Day</li>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Regular information about on what is planned for future releases</li>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Other direct contact opportunities with Nextcloud senior management through the year</li>
-            </ul>
-            <p class="section--paragraph revealOnScroll">Every Premium Subscription Nextcloud customer gets a seat on the Advisory board.</p>
+        <div class="row">
+            <div class="col-md-6">
+                <p class="section--heading-1 section--text--center revealOnScroll">Meeting opportunities</p>
+                <p class="section--paragraph revealOnScroll">Several yearly face to face meetings and conference calls provide the platform for the best alignment between the needs of our customers and our development direction.</p>
+                <ul>
+                <li class="revealOnScroll"><i class="fa-check fa"></i> Quarterly conference calls reviewing the roadmap and new market developments</li>
+                <li class="revealOnScroll"><i class="fa-check fa"></i> Discussion and dinner the day before the bi-annual Nextcloud Enterprise Day</li>
+                <li class="revealOnScroll"><i class="fa-check fa"></i> Other direct contact opportunities with Nextcloud senior management through the year</li>
+                </ul>
+                <p class="section--paragraph revealOnScroll">Every Premium Subscription Nextcloud customer is offered a seat on the Advisory board.</p>
+            </div>
+        <img class="big-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting.jpg">
+        <img class="small-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting-square.jpg">
         </div>
-    <img class="big-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting.jpg">
-    <img class="small-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting-square.jpg">
     </div>
 </div>
 </section>
 
-<!-- <section class="section--quote">
+<section class="section--memberquotes">
 <div class="container">
-    <div class="row feature-row">
-        <div class="col-md-6 revealOnScroll">
-            <a><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/history/frank.jpg" alt="in action"/></a>
-        </div>
-        <div class="col-md-6 revealOnScroll feature--block">
-            <blockquote>staying in close contact with Nextcloud management has benefitted the TU greatly, enabling features like End-to-end Encryption and much improved SAML support to be developed in alignment with our needs</blockquote>
-            <p class="section--paragraph">- Dr. Thomas Hildmann, TU Berlin</p>
-        </div>
-    </div>
-    <div class="row feature-row">
-        <div class="col-md-6 image--floated revealOnScroll">
-            <a><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/history/frank.jpg" alt="in action"/></a>
-        </div>
-        <div class="col-md-6 revealOnScroll feature--block">
-            <blockquote>staying in close contact with Nextcloud management has benefitted the TU greatly, enabling features like End-to-end Encryption and much improved SAML support to be developed in alignment with our needs</blockquote>
-            <p class="section--paragraph">- Dr. Thomas Hildmann, TU Berlin</p>
-        </div>
-    </div>
-    <div class="row feature-row">
-        <div class="col-md-6 revealOnScroll">
-            <a><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/history/frank.jpg" alt="in action"/></a>
-        </div>
-        <div class="col-md-6 revealOnScroll feature--block">
-            <blockquote>staying in close contact with Nextcloud management has benefitted the TU greatly, enabling features like End-to-end Encryption and much improved SAML support to be developed in alignment with our needs</blockquote>
-            <p class="section--paragraph">- Dr. Thomas Hildmann, TU Berlin</p>
-        </div>
-    </div>
-    <div class="row feature-row">
-        <div class="col-md-6 image--floated revealOnScroll">
-            <a><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/history/frank.jpg" alt="in action"/></a>
-        </div>
-        <div class="col-md-6 revealOnScroll feature--block">
-            <blockquote>staying in close contact with Nextcloud management has benefitted the TU greatly, enabling features like End-to-end Encryption and much improved SAML support to be developed in alignment with our needs</blockquote>
-            <p class="section--paragraph">- Dr. Thomas Hildmann, TU Berlin</p>
+    <p class="section--heading-1 section--text--center">Membership</p>
+    <p class="section--paragraph section--text--center">Our board currently has over 20 members, some of whom prefer to remain private.</p>
+    <div class="row">
+        <div class="col-md-3 col-md-xs-6">
+            <div class="picture">
+                <div class="pic-container">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/history/frank.jpg" alt="in action"/>
+                </div>
+            </div>
+            <p class="section--paragraph__tittle text-center">firstname lastname</p>
+            <p class="section--paragraph text-center">position, company</p>
+            <blockquote>quote quote</blockquote>
         </div>
     </div>
 </div>
-</section>  -->
+</section>
 
 <section class="section--benefits benefits">
 <div class="container-fluid banner">
-    <div class="container">
-        <div class="col-md-6 col-md-offset-6">
-            <p class="section--heading-1 section--text--center revealOnScroll">Why participate?</p>
-            <p class="section--paragraph revealOnScroll">Several yearly face to face meetings and conference calls provide the platform for improved alignment between the needs of our customers and our development direction.</p>
-            <ul>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Gain unique insights in upcoming technologies</li>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Share needs and ideas from your business</li>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Hear what challenges others in the market face</li>
-            <li class="revealOnScroll"><i class="fa-check fa"></i> Help Nextcloud determine development priorities</li>
-            </ul>
-            <p class="section--paragraph revealOnScroll">Be part of building a vision for secure file exchange and teamwork in organizations of the future!</p>
+    <div class="row">
+        <div class="container">
+            <div class="col-md-6 col-md-offset-6">
+                <p class="section--heading-1 section--text--center revealOnScroll">Why participate?</p>
+                <p class="section--paragraph revealOnScroll">Several yearly face to face meetings and conference calls provide the platform for improved alignment between the needs of our customers and our development direction.</p>
+                <ul>
+                <li class="revealOnScroll"><i class="fa-check fa"></i> Gain unique insights in upcoming technologies</li>
+                <li class="revealOnScroll"><i class="fa-check fa"></i> Share needs and ideas from your business</li>
+                <li class="revealOnScroll"><i class="fa-check fa"></i> Hear what challenges others in the market face</li>
+                <li class="revealOnScroll"><i class="fa-check fa"></i> Help Nextcloud determine development priorities</li>
+                </ul>
+                <p class="section--paragraph revealOnScroll">Be part of building a vision for secure file exchange and teamwork in organizations of the future!</p>
+            </div>
+        <img class="big-img-invert" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting2.jpg">
+        <img class="small-img-invert" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting2-square.jpg">
         </div>
-    <img class="big-img-invert" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting2.jpg">
-    <img class="small-img-invert" src="<?php echo get_template_directory_uri(); ?>/assets/img/headers/meeting2-square.jpg">
     </div>
 </section>
