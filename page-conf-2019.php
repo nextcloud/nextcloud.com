4f898d67e038380d4b725facbe20ce847dc79cf4
Jos Poortvliet <jospoortvliet@gmail.com>
2019-07-16 10:28:07 +0200

    updated early bird date
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-conf-2019.php
+++ page-conf-2019.php
@@ -27,7 +27,7 @@
 		<div class="col-sm-8 col-sm-offset-2">
 			<p class="section--paragraph__tittle">September 14-15, Technical University Berlin</p>
 			<p class="section--paragraph">Annually bringing the global Nextcloud Contributor Community together for a week of coding, design, discussion, talks & fun.</p>
-			<p>The event costs €20, and until June 31st we offer €15 Early bird tickets! Contributors get a free ticket - just <a href="mailto:events@nextcloud.com?subject=%5BContributor%20ticket%5D%20YOUR%20NAME&body=Link%20to%20your%20contribution" class="hyperlink">shoot us an email</a> with a link to your contribution(s) on github, transifex or otherwise! We provide catered lunch at the conference, you can find some hotel options further down the page.</a></p>
+			<p>The event costs €20, and until July 31st we offer €15 Early bird tickets! Contributors get a free ticket - just <a href="mailto:events@nextcloud.com?subject=%5BContributor%20ticket%5D%20YOUR%20NAME&body=Link%20to%20your%20contribution" class="hyperlink">shoot us an email</a> with a link to your contribution(s) on github, transifex or otherwise! We provide catered lunch at the conference, you can find some hotel options further down the page.</a></p>
 			<p class="section--paragraph"><a href="https://eventyay.com/e/70d8ad61/" class="button button--arrow button--large button--blue">Get your ticket</a> <!--<a href="https://nextcloud.com/livestream" class="button button--arrow button--large button--blue">Live stream </a>--> <a href="https://eventyay.com/e/70d8ad61/cfs/" class="button button--arrow button--large button--blue">Submit a talk</a></p>
 <!--             <p class="section--paragraph">The 2018 conference is over, <a classy="hyperlink" href="https://nextcloud.com/blog/nextcloud-conference-2018-millions-of-nec-routers-nextcloud-14-and-simple-signup/">Read our report!</a></p> -->
             <div class="button button--white button--dropdown">
@@ -260,7 +260,7 @@
 		<div class="col-md-6">
 			<p class="section--paragraph revealOnScroll"><a href="<?php echo get_template_directory_uri(); ?>/assets/img/conference/talks.jpg" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/talks_wee.jpg" style="width: 95%" alt="talk" class="aligncenter" /></a></p>
 			<h2  class="revealOnScroll">Entree</h2>
-			<p class="section--paragraph revealOnScroll"><p><a href="https://eventyay.com/e/70d8ad61/" class="hyperlink">The event costs €20,</a> and until June 31st we offer €15 Early bird tickets! Contributors get a free ticket - just <a href="mailto:events@nextcloud.com?subject=%5BContributor%20ticket%5D%20YOUR%20NAME&body=Link%20to%20your%20contribution" class="hyperlink">shoot us an email</a> with a link to your contribution(s) on github, transifex or otherwise! We provide catered lunch at the conference, you can find some hotel options further down the page.</a></p>
+			<p class="section--paragraph revealOnScroll"><p><a href="https://eventyay.com/e/70d8ad61/" class="hyperlink">The event costs €20,</a> and until July 31st we offer €15 Early bird tickets! Contributors get a free ticket - just <a href="mailto:events@nextcloud.com?subject=%5BContributor%20ticket%5D%20YOUR%20NAME&body=Link%20to%20your%20contribution" class="hyperlink">shoot us an email</a> with a link to your contribution(s) on github, transifex or otherwise! We provide catered lunch at the conference, you can find some hotel options further down the page.</a></p>
 		</div>
 	</div>
 	<div class="row revealOnScroll">
