c7878b8cf4b62ed0c970fa7574ac720d2e727950
Jos Poortvliet <jospoortvliet@gmail.com>
2019-07-22 09:13:06 +0200

    Revert "piwik out, matomo in"
    This reverts commit 835fa88093c3b80f8b42ded30da7ad1e448b2e4e.

--- templates/piwik-tracking.php
+++ templates/piwik-tracking.php
@@ -1,21 +1,21 @@
-<!-- Matomo -->
+<!-- Piwik -->
 <script type="text/javascript">
-  var _paq = window._paq || [];
+  var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setCookieDomain", "*.nextcloud.com"]);
   _paq.push(["setDomains", ["*.nextcloud.com"]]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="https://stats.nextcloud.com/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    var u="//stats.nextcloud.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="https://stats.nextcloud.com/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Matomo Code -->
+<noscript><p><img src="//stats.nextcloud.com/piwik.php?idsite=1&rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 <!-- Mautic -->
 <script>
     (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
