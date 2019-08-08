91d938f97bb73bb7b8fa7ccfad0ec884967eed72
Jos Poortvliet <jospoortvliet@gmail.com>
2019-08-08 18:08:02 +0200

    improve sharing page
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-sharing.php
+++ page-sharing.php
@@ -95,9 +95,10 @@ require(["require.config"], function() {
                     <li><?php echo $l->t('Share with individual users, groups or create custom groups with the');?> <a class="hyperlink" href="https://apps.nextcloud.com/apps/circles">Circles app.</a></li>
                     <li><?php echo $l->t('Share photo galleries');?></li>
                     <li><?php echo $l->t('Share public links read-only, secure view (no download/print/copy-paste) or with editing capabilities');?></li>
-                    <ul><li><?php echo $l->t('Virtual data rooms with comprehensive restrictions on office documents like only allowing to fill in forms, watermarking the view, allowing only comments, enforcing change tracking and more are possible as well. Contact Nextcloud sales for options. ');?></li></ul>
                     <li><?php echo $l->t('Attach notes to a share, either to a Nextcloud user or to a public link');?></li>
                     <li><?php echo $l->t('Share directly to users on other Nextcloud, ownCloud or Pydio servers (see below: <a class="hyperlink" href="#federation">federation</a>)');?></li>
+                    <li><?php echo $l->t('Extensive Virtual Data Room restrictions like watermarking, ability to limit documents to filling in forms or blocking download or printing, are available. Learn more on our Virtual Data Room page.');?>
+                    <a class="button button--blue" href="<?php echo home_url('secure-drop') ?>"><?php echo $l->t('Virtual Data Rooms');?></a></li>
                 </ul>
             </div>
         </div>
