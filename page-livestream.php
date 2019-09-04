b1ea25bad3c7457a43b83e220d567bfa57e52bf9
Jos Poortvliet <jospoortvliet@gmail.com>
2019-09-04 20:43:02 +0200

    prepare for streaming!
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-livestream.php
+++ page-livestream.php
@@ -11,7 +11,7 @@
 	<div class="background conf-background">
 		<div class="container">
 			<div class="col-md-6 topheader">
-				<h1>Nextcloud Conference 2018</h1>
+				<h1>Nextcloud Conference 2019</h1>
 				<h2  class="revealOnScroll">A week of collaboration, coding and fun!</h2>
 			</div>
 		</div>
@@ -21,13 +21,14 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-8 col-sm-offset-2">
-			<img class="conf-img revealOnScroll" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/grouppic2017-2.jpg" alt="logo"/>
+<!-- 			<img class="conf-img revealOnScroll" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/grouppic2018.jpg" alt="logo"/> -->
 <!-- 			<h2 class="revealOnScroll"></h2> -->
 			<p class="section--paragraph__tittle">stay tuned for our live stream!</p>
-			<p class="section--paragraph">Starting Saturday morning, August 25, ~10:00 Berlin time.</p>
+			<p class="section--paragraph">Starting Saturday morning, September14, ~10:00 Berlin time.</p>
 			<div class="row text-center">
-                <a class="button button--large button--blue button--arrow" href="https://nextcloud.com/schedule/schedule.html">View the full schedule here</a>
-                <a class="button button--large button--blue button--arrow" href="https://nextcloud.com/schedule/speakers.html">See the speakers</a>
+                <a class="button button--large button--blue button--arrow" href="https://nextcloud.com/conf/schedule">View the full schedule here</a>
+                <a class="button button--large button--blue button--arrow" href="https://nextcloud.com/conf/speakers">See the speakers</a>
+                <a class="button button--large button--blue button--arrow" href="https://nextcloud.com/conf/sessions">See all the talks</a>
             </div>
 		</div>
 	</div>
