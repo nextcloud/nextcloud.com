d8332c61c98c1fe5da078c7a5470052950c90026
Jos Poortvliet <jospoortvliet@gmail.com>
2019-09-23 21:25:19 +0200

    fix invisible button
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-talk.php
+++ page-talk.php
@@ -48,7 +48,7 @@
         <div class="col-md-6 col-md-offset-3">
             <p class="text-center"><?php echo $l->t('Regain full control over the communication in your organization right now.');?></p>
             <div class="text-center morebuttondiv">
-                <a href="<?php echo home_url('buy') ?>" class="button button--large button--arrow"><?php echo $l->t('Contact us');?> <i class="icon-arrow-circle-o-right icon"></i></a>
+                <a href="<?php echo home_url('buy') ?>" class="button button--large button--arrow button--blue"><?php echo $l->t('Contact us');?> <i class="icon-arrow-circle-o-right icon"></i></a>
             </div>
         </div>
     </div>
