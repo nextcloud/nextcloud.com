3b6753d3d2a66b0728c022235f470ecf61bc59e7
Jos Poortvliet <jospoortvliet@gmail.com>
2020-04-08 10:59:59 +0200

    em
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-ionos.php
+++ page-ionos.php
@@ -211,7 +211,7 @@ imagedestroy($image);
                     <h3><?php echo $l->t('Your order');?></h3>
                     <hr>
                     <p><label for="users"><?php echo $l->t('Number of users, at a price of 9.50 euro per user per month.');?><br />
-                    <?php echo $l->t('Special pricing for education, government and larger numbers on request in Notes section.');?><br />
+                    <em><?php echo $l->t('Special pricing for education, government and larger numbers on request in Notes section.');?></em><br />
                     <select name="users" onChange="setUsers()">
                         <?php
                             for ($i=1; $i<=200; $i++)
