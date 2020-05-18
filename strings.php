4a3506b0d7bf365804ad362eef1e80b7847189ed
Jos Poortvliet <jospoortvliet@gmail.com>
2020-05-18 11:53:10 +0200

    RC2 is here!
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- strings.php
+++ strings.php
@@ -65,9 +65,9 @@ $DOWNLOAD_SERVER_PACKAGES_DAILY = 'https://download.nextcloud.com/download/repos
 
 // Testing
 $DOWNLOAD_SERVER_PACKAGES_TESTING = 'https://download.nextcloud.com/server/prereleases/';
-$SERVER_TESTING_VERSION ='18 beta 3'; // nothing to test -> leave it empty
-$DOWNLOAD_SERVER_TAR_TESTING ='https://download.nextcloud.com/server/prereleases/nextcloud-18.0.0beta3.tar.bz2';
-$DOWNLOAD_SERVER_ZIP_TESTING ='https://download.nextcloud.com/server/prereleases/nextcloud-18.0.0beta3.zip';
+$SERVER_TESTING_VERSION ='19 RC 2'; // nothing to test -> leave it empty
+$DOWNLOAD_SERVER_TAR_TESTING ='https://download.nextcloud.com/server/prereleases/nextcloud-19.0.0RC2.tar.bz2';
+$DOWNLOAD_SERVER_ZIP_TESTING ='https://download.nextcloud.com/server/prereleases/nextcloud-19.0.0RC2.zip';
 
 // web installer
 $DOWNLOAD_SERVER_WEB_INSTALLER = 'https://download.nextcloud.com/server/installer/setup-nextcloud.php';
