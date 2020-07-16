7b62a9e5f205b8e0b9f19d399eb0ab6f62aefecd
Jos Poortvliet <jospoortvliet@gmail.com>
2020-07-16 18:27:14 +0200

    more
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-conf-2020.php
+++ page-conf-2020.php
@@ -1,5 +1,5 @@
 <head>
-<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/conf.css?v=6" rel="stylesheet">
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/conf.css?v=7" rel="stylesheet">
 <meta itemprop="image" content="<?php echo get_template_directory_uri(); ?>/assets/img/conference/grouppic2019-small.jpg">
 <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/assets/img/conference/grouppic2019-small.jpg">
 <meta name="twitter:image:src" content="<?php echo get_template_directory_uri(); ?>/assets/img/conference/grouppic2019-small.jpg">
