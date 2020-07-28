c11c0bd791cb92a34438cdceaee08067f30c002c
Jos Poortvliet <jospoortvliet@gmail.com>
2020-07-28 09:10:13 +0200

    Add press release
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20200728.php
@@ -0,0 +1,59 @@
+<head>
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/pressrelease.css" rel="stylesheet">
+<meta itemprop="image" content="https://nextcloud.com/media/Forms.png">
+<meta name="twitter:image" content="https://nextcloud.com/media/Forms.png">
+<meta name="twitter:image:src" content="https://nextcloud.com/media/Forms.png">
+<meta property="og:image" content="https://nextcloud.com/media/Forms.png">
+</head>
+<div class="background generic-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+                <h1>Press release</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="section--pressrelease">
+<div class="container">
+<div class="row">
+<div class="pr">
+<h1>Nextcloud introduces Forms to protect privacy-sensitive survey data</h1>
+<p><em>July 28, 2020, Stuttgart</em> - Stuttgart. Nextcloud GmbH, the company behind the worlds&#39; most deployed on-premises content collaboration platform, announces the immediate availability of Nextcloud Forms. Similar to Google Forms, this app makes it easy to create surveys for anything from market research or customer feedback to planning an office party. Privacy is an issue with surveys, as a third of people who abandon a form do so for privacy and security reasons. With Nextcloud Forms, data remains secure on the Nextcloud server and the privacy of respondents is respected.</p>
+<blockquote>Surveys are very useful in a wide range of scenarios, often containing private data. A tool for creating a variety of forms that protects the privacy of those surveyed and surveying, while being easy to install and use, is exactly what the market needs.</blockquote>
+<p>-- Frank Karlitschek, CEO of Nextcloud GmbH</p>
+<p>The Forms app offers the ability to choose from a range of question types such as checkboxes, multiple choice, dropdowns, short answer and long text.</p>
+<p>Forms can be shared via link to anyone, or even directly sent to specific users on the Nextcloud or to the whole Nextcloud instance. For improved security it is possible to set an expiration date. There is no limitation on the amount of forms or questions users can create nor the links they can share or people who can reply. Forms is responsive and will look good on any device or screen.</p>
+<p>Responses are visualized either as summary statistics with charts which make it easy to analyze the results, or as the individual responses if users need to dig deeper or have a small number of respondents. The export format is compatible with the format used by Google Forms to work with any tool or workflow designed for integration with that.</p>
+<p>The Nextcloud Forms app is available for easy one-click installation in the Nextcloud app store.</p>
+<h2>Links</h2>
+<ul>
+<li>About Nextcloud: <a href="https://nextcloud.com/about/">https://nextcloud.com/about/</a></li>
+<li>Nextcloud Forms app on our app store: <a href="https://apps.nextcloud.com/apps/forms">https://apps.nextcloud.com/apps/forms</a></li>
+<li>Source repository: <a href="https://github.com/nextcloud/forms">https://github.com/nextcloud/forms</a></li>
+<li>Roadmap: <a href="https://github.com/nextcloud/forms/milestones">https://github.com/nextcloud/forms/milestones</a></li>
+<li>Screenshots: <a href="https://github.com/nextcloud/forms/tree/master/screenshots">https://github.com/nextcloud/forms/tree/master/screenshots</a></li>
+<li>Survey statistics <a href="https://wpforms.com/online-form-statistics-facts/">https://wpforms.com/online-form-statistics-facts/</a></li>
+</ul>
+<h2>Press contacts</h2>
+<p>Jos Poortvliet<br />
+Phone: +49 (0) 171 121 7528<br />
+jos.poortvliet<a href="mailto:pr@nextcloud.com">@nextcloud.com</a></p>
+<h2>Graphics</h2>
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="https://nextcloud.com/media/forms1.png"><img src="https://nextcloud.com/media/forms1.png" alt="Forms in action" class="img-responsive" /></a><br />Creating a form
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://nextcloud.com/media/forms2.png"><img src="https://nextcloud.com/media/forms2.png" alt="Forms in action" class="img-responsive" /></a><br />Answering a survey
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://nextcloud.com/media/forms3.png"><img src="https://nextcloud.com/media/forms3.png" alt="Forms in action" class="img-responsive" /></a><br />Results example
+    </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
