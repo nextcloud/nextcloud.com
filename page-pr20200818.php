0a25b53927c7137b9fac2b64fce749164de6ee10
Jos Poortvliet <jospoortvliet@gmail.com>
2020-08-18 09:04:19 +0200

    add press release
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20200818.php
@@ -0,0 +1,86 @@
+<head>
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/pressrelease.css" rel="stylesheet">
+<meta itemprop="image" content="https://nextcloud.com/media/end2end-encryption.png">
+<meta name="twitter:image" content="https://nextcloud.com/media/end2end-encryption.png">
+<meta name="twitter:image:src" content="https://nextcloud.com/media/end2end-encryption.png">
+<meta property="og:image" content="https://nextcloud.com/media/end2end-encryption.png">
+</head>
+<div class="background generic-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+                <h1>Press release</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="section--pressrelease">
+<div class="container">
+<div class="row">
+<div class="pr">
+<h1>Production ready end-to-end encryption and new user interface for desktop client 3.0</h1>
+<p><em>August 18, 2020, Stuttgart</em> - Nextcloud GmbH, the company behind the worlds’ most deployed on-premises content collaboration platform, announces the immediate availability of the Nextcloud Desktop Client v3.0. This new release marks the availability of our end-to-end encryption technology across our mobile and desktop clients and the introduction of a new user interface with a deeper integration of Nextcloud Hub in the desktop experience of users.</p>
+<blockquote>
+<p>Earlier this year, Nextcloud Hub delivered a new generation of collaboration technology to the on-premises cloud market. With the release today, we bring more of this experience to the desktop client while adding the long-anticipated end-to-end encryption feature.</p>
+</blockquote>
+<p>-- Frank Karlitschek, CEO of Nextcloud GmbH</p>
+<h2>Bringing Nextcloud Hub to the desktop</h2>
+<p>The new interface for the desktop client makes Talk and other apps on the Nextcloud server easier to access for desktop users. A click on the icon in the system tray pops up a new menu, showing a list of events on the server with on top user account information, a link to Files, Talk and a button that brings a list of other apps. From the list of server activity, users can directly access the sharing settings of a file. In the file manager, a right-click on a document now gives the option to edit directly in the online office document editor in Nextcloud. New is also the ability for users to add a note for the recipient of a file share in the share dialog.</p>
+<h2>Delivering Enterprise-ready end-to-end encryption to the clients</h2>
+<p>To augment the existing storage and in-transit encryption options, Nextcloud introduces today production ready end-to-end encryption to its clients.</p>
+<p>The desktop client and mobile clients offer a per-folder capability to enable local encryption whereby all files as well as their names and metadata are securely encrypted. Advanced key handling with Cryptographic Identity Protection in the form of server signed certificates facilitates easily adding new devices. In a future release, the scheme will allow for secure, end-to-end encrypted sharing with other users. Nextcloud supports an optional offline administrator recovery key and allows a complete audit log. The encryption design allows for the deployment of a secure HSM to issue certificates to users in enterprises.</p>
+<blockquote>
+<p>Organizations that demand the utmost security need a true enterprise solution that doesn’t require users to manually exchange encryption keys and long, complex passwords or share large, encrypted volumes. Nextcloud is first to market with an integrated, secure technology to keep a subset of highly sensitive files cryptographically secure even in the worst case of an undetected, full server breach.</p>
+</blockquote>
+<p>-- Roeland Douma, Security lead at Nextcloud</p>
+<p>The Nextcloud end-to-end encryption feature is designed in such way that the server never has access to unencrypted files or keys, nor does server-provided code ever handle unencrypted data which could provide avenues for compromise. There is no need for user interaction or extra work and key sharing is seamlessly handled by the server, facilitating completely intuitive syncing.</p>
+<p>The end-to-end encryption is supported in the latest releases of our Android and iOS Files clients as well as the desktop client v3.</p>
+<h2>About Nextcloud</h2>
+<p>Nextcloud Hub is the industry-leading, fully open source, on-premises team productivity platform, combining the easy user interface of consumer-grade cloud solutions with the security and compliance measures enterprises need. Nextcloud Hub brings together universal access to data through mobile, desktop and web interfaces with next-generation, on-premise secure communication and collaboration features like real-time document editing, chat and video calls, putting them under direct control of IT and integrated with existing infrastructure.</p>
+<p>Nextcloud’s easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com/">nextcloud.com</a> or follow @<a href="https://twitter.com/Nextclouders">Nextclouders</a> on Twitter.</p>
+<h2>Links</h2>
+<ul>
+<li>About Nextcloud: <a href="https://nextcloud.com/about/" class="uri">https://nextcloud.com/about/</a></li>
+<li>Announcement blog: <a href="https://nextcloud.com/blog/production-ready-end-to-end-encryption-and-new-user-interface-arrive-with-nextcloud-desktop-client-3-0/" class="uri">https://nextcloud.com/blog/production-ready-end-to-end-encryption-and-new-user-interface-arrive-with-nextcloud-desktop-client-3-0/</a></li>
+<li>Nextcloud end-to-end encryption app on our app store: <a href="https://apps.nextcloud.com/apps/end_to_end_encryption" class="uri">https://apps.nextcloud.com/apps/end_to_end_encryption</a></li>
+<li>Details and end-to-end encryption design whitepaper download: <a href="https://nextcloud.com/endtoend/" class="uri">https://nextcloud.com/endtoend/</a></li>
+<li>More about the various encryption solutions in Nextcloud: <a href="https://nextcloud.com/blog/encryption-in-nextcloud/" class="uri">https://nextcloud.com/blog/encryption-in-nextcloud/</a></li>
+<li>Press brief: <a href="https://cloud.nextcloud.com/s/Zy4qrRTLi5rni2T" class="uri">https://cloud.nextcloud.com/s/Zy4qrRTLi5rni2T</a></li>
+<li>Screenshots and videos below</li>
+</ul>
+<h2>Press contacts</h2>
+<p>Jos Poortvliet<br />
+Phone: +49 (0) 171 121 7528<br />
+jos.poortvliet<a href="mailto:pr@nextcloud.com">@nextcloud.com</a></p>
+<h2>Graphics</h2>
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="https://nextcloud.com/media/edit-menu.gif"><img src="https://nextcloud.com/media/edit-menu.gif" alt="Editing document" class="img-responsive" /></a><br />Right-click to edit a file <small><a href="https://youtu.be/NPjG64_A4NU">(video on YouTube)</a></small>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://nextcloud.com/media/new-menu.gif"><img src="https://nextcloud.com/media/new-menu.gif" alt="New menu" class="img-responsive" /></a><br />The new menu in action <small><a href="https://www.youtube.com/watch?v=1ZEPQmleikg">(video on YouTube)</a></small>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://nextcloud.com/media/end-to-end.gif"><img src="https://nextcloud.com/media/end-to-end.gif" alt="end-to-end encryption" class="img-responsive" /></a><br />End-to-end encryption in action <small><a href="https://youtu.be/S9_lZtumCmk">(video on YouTube)</a></small>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <a href="https://nextcloud.com/media/accounts-3.13.jpg"><img class="img-responsive" src="https://nextcloud.com/media/accounts-3.13.jpg" alt="accounts" /></a><br />New search bar, accounts view in Android 3.13
+    </div>
+    <div class="col-md-3">
+        <a href="https://nextcloud.com/media/sidebar-3.13.jpg"><img class="img-responsive" src="https://nextcloud.com/media/sidebar-3.13.jpg" alt="sidebar" /></a><br />Sidebar view in Android 3.13
+    </div>
+    <div class="col-md-3">
+        <a href="https://nextcloud.com/media/dark-mode-3.13.jpg"><img class="img-responsive" src="https://nextcloud.com/media/dark-mode-3.13.jpg" alt="dark mode" /></a><br />Dark mode in Android 3.13
+    </div>
+    <div class="col-md-3">
+        <a target="_blank" href="https://nextcloud.com/media/end2end-encryption.png"><img src="https://nextcloud.com/media/end2end-encryption.png" alt="Editing document" class="img-responsive" /></a><br />End to end encryption infographic
+    </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
