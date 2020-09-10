3d7ef76c10eb73d342a326cd9470f44293791042
Jos Poortvliet <jospoortvliet@gmail.com>
2020-09-10 18:32:53 +0200

    add a trial request form
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-trial.php
@@ -0,0 +1,162 @@
+<head>
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/buy.css?v=2" rel="stylesheet">
+<!-- <script src='https://www.google.com/recaptcha/api.js'></script> -->
+<script>
+	require(["require.config"], function() {
+		require(["modules/submenu"])
+	});
+</script>
+</head>
+<section class="buy-hero-section background buy-background second-menu">
+    <div class="container background">
+        <div class="row">
+            <div class="col-md-6 topheader">
+                <h1><?php echo $l->t('Contact us for a quote');?></h1>
+                <h2><?php echo $l->t('Our sales team will help you determine the best solution for your business needs');?></h2>
+            </div>
+        </div>
+    </div>
+	<div class="container-fluid menu" id="menuAnchor">
+		<div class="container buttons">
+            <a class="button button--blue" href="<?php echo home_url('faq') ?>"><?php echo $l->t('FAQ');?></a>
+			<a class="button button--blue" href="<?php echo home_url('enterprise/order') ?>"><?php echo $l->t('Order online');?></a>
+			<a class="button button--blue" href="<?php echo home_url('buy') ?>"><?php echo $l->t('get a quote');?></a>
+			<a class="button button--blue" href="<?php echo home_url('pricing') ?>"><?php echo $l->t('pricing plans');?></a>
+			<a class="button button--blue" href="<?php echo home_url('enterprise') ?>"><?php echo $l->t('enterprise offering');?></a>
+		</div>
+	</div>
+</section>
+
+<?php
+
+$int1 = random_int(0, 15);
+$int2 = random_int(0, 50);
+$salt = bin2hex(random_bytes(5));
+$hash = hash('sha256', $salt . ($int1 + $int2));
+$checksum = $salt . ':' . $hash;
+
+$image = imagecreate(100, 20);
+$background_color = imagecolorallocate($image, 255, 255, 255);
+$text_color = imagecolorallocate($image, 0, 0, 0);
+imagestring($image, 5, 3, 2, $int1 . ' + ' . $int2, $text_color);
+
+ob_start();
+imagepng($image);
+$imagestring = ob_get_contents();
+ob_end_clean();
+imagedestroy($image);
+?>
+<section class="section--welcome">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2><?php echo $l->t('See how Nextcloud works for you');?></h2>
+            <p><?php echo $l->t('To determine if Nextcloud is fit for your use case, we offer trails of Nextcloud Enterprise on a hosted instance as well as trial licenses for testing on your own infrastructure.');?></p>
+            <p><?php echo $l->t('Our Nextcloud Enterprise build is pre-configured, optimized and hardened for the special needs of large scale, production-critical enterprise deployments. Backed by our subscription and support, it provides you with the best possible Nextcloud experience, ');?></p>
+        </div>
+	</div>
+</div>
+</section>
+<section class="section--contact-form">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h3><?php echo $l->t('Contact us for an enterprise trial');?></h3>
+            <p><?php echo $l->t('Please answer the questions below so we can best help you with a realistic setup.');?><br /> <?php echo $l->t('Mandatory questions are marked with a *');?></p>
+        </div>
+	</div>
+    <form name="trial" method="post" action="../trialsubmit/">
+        <div class="row">
+            <div class="col-md-12">
+                <p><label for="yourname"><?php echo $l->t('Your name');?>*<br>
+                <input  type="text" name="yourname" maxlength="60" size="60"></label></p>
+                <p><label for="email">Email*<br>
+                <input  type="text" name="email" maxlength="80" size="60"></label></p>
+                <p><label for="organization"><?php echo $l->t('Organization');?>*<br>
+                <input  type="text" name="organization" maxlength="100" size="60" placeholder="<?php echo $l->t('Name of your organization');?>"></label></p>
+                <p><label for="role"><?php echo $l->t('Your role');?><br>
+                <input  type="text" name="role" maxlength="100" size="60" placeholder="<?php echo $l->t('Your job title');?>"></label></p>
+                <p><label for="phone"><?php echo $l->t('Phone number');?><br>
+                <input  type="text" name="phone" maxlength="40" size="60" placeholder="<?php echo $l->t('Please include country code (00 or +XX)');?>"></label></p>
+                <p><label for="users"><?php echo $l->t('Number of users over next 12 months (estimation)');?>*<br>
+                <input  type="text" name="users" maxlength="80" size="60" placeholder="<?php echo $l->t('Our trial license will cover that number of users.');?>"></label></p>
+                <p><label for="hosted-or-onprem"><?php echo $l->t('Would you like a hosted trial or a license for your own on-premises Nextcloud Enterprise setup?');?><br /> <small><?php echo $l->t("Note that you will be able to connect your organizations' LDAP and storage to the hosted trial, if you like.");?></small><br>
+                <select name="hosted-or-onprem">
+                    <option value="hosted"><?php echo $l->t('Hosted');?></option>
+                    <option value="onpremises"><?php echo $l->t('On-premises');?></option>
+                </select></label></p>
+                <p><label for="need-setup-help"><?php echo $l->t('Would you require assistance to set up the service or to design/review the architecture?');?><br>
+                <select name="need-setup-help">
+                    <option value="unsure"><?php echo $l->t('Not sure');?></option>
+                    <option value="yes"><?php echo $l->t('Yes');?></option>
+                    <option value="no"><?php echo $l->t('No');?></option>
+                </select></label></p>
+                <p><?php echo $l->t('Our sales engineers will also be available to help you set things up and test, if there is any such need.');?><br /><?php echo $l->t(' A more extensive proof-of-concept with our help is of course also possible, just let us know!');?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h3><?php echo $l->t('Your needs');?></h3>
+                <p><?php echo $l->t('Below you can optionally indicate additional services you would like to test. We will include test versions or licenses of these.');?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <p><label for="clustering"><?php echo $l->t('Are you or do you think you will be using any sort of clustering for the application, database or storage?');?><br>
+                <select name="clustering">
+                    <option value="unsure"><?php echo $l->t('Not sure');?></option>
+                    <option value="yes"><?php echo $l->t('Yes');?></option>
+                    <option value="no"><?php echo $l->t('No');?></option>
+                </select></label></p>
+                <p><label for="collabora"><?php echo $l->t('Would you be interested in editing office documents online?');?><br>
+                <select name="collabora">
+                    <option value="unsure"><?php echo $l->t('Not sure');?></option>
+                    <option value="yes"><?php echo $l->t('Yes');?></option>
+                    <option value="no"><?php echo $l->t('No');?></option>
+                </select></label></p>
+            </div>
+            <div class="col-md-6">
+                <p><label for="webconferencing"><?php echo $l->t('Would you be interested in secure webconferencing and audio and video calls?');?><br>
+                <select name="webconferencing">
+                    <option value="unsure"><?php echo $l->t('Not sure');?></option>
+                    <option value="yes"><?php echo $l->t('Yes');?></option>
+                    <option value="no"><?php echo $l->t('No');?></option>
+                </select></label></p>
+                <p><label for="outlook"><?php echo $l->t('Would you be interested in our Secure Sharing add-in for Outlook?');?><br>
+                <select name="outlook">
+                    <option value="unsure"><?php echo $l->t('Not sure');?></option>
+                    <option value="yes"><?php echo $l->t('Yes');?></option>
+                    <option value="no"><?php echo $l->t('No');?></option>
+                </select></label></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p><label for="comments"><?php echo $l->t('Any more details, questions or information?');?><br />
+                <textarea name="comments" maxlength="2000" cols="80" rows="8" placeholder="<?php echo $l->t('Let us know how we can help you!');?>"></textarea></label></p>
+                <p>
+                    <label for="partner"><?php echo $l->t('Can we allow one of our certified local partners reach out to you with an offer if we feel they can service you more appropriately?');?>* <br />
+                    <select name="partner">
+                        <option value="yes"><?php echo $l->t('Yes');?></option>
+                        <option value="no"><?php echo $l->t('No');?></option>
+                    </select></label><br />
+                    <a href="<?php echo home_url('partners') ?>" class="hyperlink"><?php echo $l->t('See a list of our partners here.');?></a>
+                </p>
+                <p><input type="checkbox" id="gdprcheck" name="gdprcheck" value="gdprchecked"><label for="gdprcheck"> <?php echo $l->t('I agree with the Nextcloud privacy policy and understand my data will be processed so Nextcloud can reach out to me.');?>*<br /></p>
+                <td colspan="2" style="text-align:center">
+                <div class="g-recaptcha" data-sitekey="<?php echo RECAPTCHA_SITEKEY; ?>"></div>
+                <p><label for="captcha"><?php echo $l->t('Please calculate the following sum');?>* <span></span><br>
+                <img src="data:image/png;base64,<?php echo base64_encode($imagestring); ?>"><br>
+                <input  type="text" name="captcha" maxlength="20" size="20" placeholder="13"></label></p>
+                <input  type="hidden" name="checksum" value="<?php echo $checksum;?>">
+                <input type="submit" value=" Submit inquiry " class="button button--blue">
+            </div>
+        </div>
+    </form>
+    <div class="row">
+        <div class="col-md-12">
+            <p><?php echo $l->t('We respect your privacy! If you fill in this form, we will reach out to send you an offer and might also contact you with relevant information like a local event or workshop we are organizing. We never sell your data - any mails you get will be from us, or, if you agreed to be brought in contact with them, one of our Nextcloud service partners.');?> <a href="<?php echo home_url('privacy') ?>" class="hyperlink"><?php echo $l->t('See our privacy policy here.');?></a></p>
+        </div>
+    </div>
+</div>
+</section>
---
+++ page-trialsubmit.php
@@ -0,0 +1,172 @@
+<head>
+<script>
+	require(["require.config"], function() {
+		require(["pages/enterprise"])
+	});
+</script>
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/enterprise.css" rel="stylesheet">
+</head>
+<section class="enterprise-hero-section second-menu">
+	<div class="container-fluid background">
+		<div class="container">
+			<div class="col-md-6 topheader">
+				<h1><?php echo $l->t('Get support from the source');?></h1>
+				<h2><?php echo $l->t('The best expertise whenever you need it!');?></h2>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php
+if(isset($_POST['email'])) {
+   function died($error) {
+   	// error code goes here
+    ?>
+            <section class="section--whitepaper">
+                <div class="container text-center">
+                    <h3>Sorry, there was an error with the form you submitted</h3>
+                    <p>Did you fill in all mandatory fields?<br />
+                    The error(s) detected include:<br/>
+                        <?php
+                        echo $error . "<br />";
+                        ?>
+                    </p>
+                    <p>Use the back key to go to the previous page and fix the
+                        issue!</p>
+                </div>
+            </section>
+            <?php
+        }
+    // validation expected data exists
+    if(!isset($_POST['yourname']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['organization']) ||
+        !isset($_POST['users']) ||
+        !isset($_POST['checksum']) ||
+        !isset($_POST['captcha'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted - did you fill in all mandatory fields?'); }
+    $yourname = $_POST['yourname']; // required
+    $organization= $_POST['organization']; // required
+    $phone = $_POST['phone']; // required
+    $email_from = $_POST['email']; // required
+    $users = $_POST['users']; // required
+    $role = $_POST['role'];
+    $comments = $_POST['comments'];
+    $needsetuphelp = $_POST['need-setup-help'];
+    $hostedoronprem = $_POST['hosted-or-onprem'];
+    $clustering = $_POST['clustering'];
+    $collabora = $_POST['collabora'];
+    $webconferencing = $_POST['webconferencing'];
+    $outlook = $_POST['outlook'];
+    $partner = $_POST['partner'];
+    $checksum = $_POST['checksum']; // required
+    $gdprcheck = $_POST['gdprcheck'];
+    $captcha = $_POST['captcha'];
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,10}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The email address you entered does not appear to be valid.<br />';
+  }
+    $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$yourname)) {
+    $error_message .= 'The name you entered does not appear to be valid.<br />';
+  }
+  if(!($gdprcheck=="gdprchecked")) {
+    $error_message .= 'You did not agree with our privacy policy so we would not be allowed to read and reply to your inquiry.<br />';
+  }
+  if (strlen($checksum) !== 75 || !strpos($checksum, ':')) {
+        $error_message .= 'The checksum is not valid.<br />';
+    } else {
+        list($salt, $expectedHash) = explode(':', $checksum, 2);
+        $hash = hash('sha256', $salt . $captcha);
+
+        if ($hash !== $expectedHash) {
+            $error_message .= 'The captcha result you entered does not appear to be correct.<br />';
+        }
+    }
+    $string_exp = "/^((\+|00)\d{1,3})?(\d+|\s+)+\d$/";
+//   if(!preg_match($string_exp,$phone)) {
+//     $error_message .= 'The phone number you entered does not appear to be valid, did you add a country code like +49?<br />';
+//     }
+//   if(RECAPTCHA_SECRET !== '' && isset($_POST['g-recaptcha-response'])) {
+//     $url = 'https://www.google.com/recaptcha/api/siteverify';
+//     $ch = curl_init();
+//
+//     curl_setopt($ch, CURLOPT_URL, $url);
+//     curl_setopt($ch, CURLOPT_POST, 1);
+//     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_SECRET, 'response' => $_POST['g-recaptcha-response'])));
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//
+//     $server_output = curl_exec($ch);
+//
+//     $server_output = json_decode($server_output, true);
+//
+//     curl_close($ch);
+//
+//     if (!isset($server_output['success']) || $server_output['success'] !== true) {
+//       $error_message .= 'The captcha result was invalid.<br />';
+//     }
+//   } else {
+//     $error_message .= 'Captcha code is missing.<br />';
+//   }
+
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  } else {
+        function clean_string($string) {
+            $bad = array("content-type","bcc:","to:","cc:","href");
+            $string = str_replace($bad,"",$string);
+            return htmlspecialchars($string);
+        }
+// the app review mailing list address
+//     $email_to = "sales@nextcloud.com";
+    $email_message = "Trial request form details below.\n\n";
+    $email_to = "sales@nextcloud.com";
+	$email_subject = "Nextcloud Enterprise Trial: ".clean_string($organization);
+    $email_message .= "Name: ".clean_string($yourname)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Phone number: ".clean_string($phone)."\n";
+    $email_message .= "Organization: ".clean_string($organization)."\n";
+    $email_message .= "Role: ".clean_string($role)."\n";
+    $email_message .= "How many users do you expect in 12 months? ".clean_string($users)."\n";
+    $email_message .= "Would you require assistance to set up the service or to design/review the architecture? ".clean_string($needsetuphelp)."\n";
+    $email_message .= "Do you need a hosted or on-premises trial? ".clean_string($hostedoronprem)."\n";
+    $email_message .= "Are you using any sort of clustering for the application, database or storage? ".clean_string($clustering)."\n";
+    $email_message .= "Would you be interested in editing office documents online? ".clean_string($collabora)."\n";
+    $email_message .= "Would you be interested in secure webconferencing and audio and video calls? ".clean_string($webconferencing)."\n";
+    $email_message .= "Would you be interested in our Secure Sharing add-in for Outlook? ".clean_string($outlook)."\n";
+    $email_message .= "Can we hand over your data to a partner? ".clean_string($partner)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+
+// create email headers
+    $headers = 'From: no-reply@nextcloud.com'."\r\n".
+    'Reply-To: '.$email_from."\r\n" .
+    'Content-Type: text/plain; charset=UTF-8'."\r\n" .
+    'Cc: '.$email_from;
+// store in log
+    $data = [
+        'to' => $email_to,
+        'subject' => $email_subject,
+        'message' => $email_message,
+        'headers' => $headers,
+    ];
+    file_put_contents('/var/log/sales-leads.txt', json_encode($data) . PHP_EOL, FILE_APPEND | LOCK_EX);
+// Send the email to the list
+    @mail($email_to, $email_subject, $email_message, $headers);
+// Second email to subscribe to the mailing list
+//     @mail("frank@nextcloud.org", "website form", "website form", $headers);
+ ?>
+
+
+  <!-- success html here -->
+    <section class="section--whitepaper">
+        <div class="container text-center">
+            <h3>Thank you for contacting us</h3>
+            <p>We received your message and will contact you on <?php echo($email_from); ?>,</p>
+            <p>check your inbox for a reply in the next 2-3 working days.</p>
+        </div>
+    </section>
+	<?php
+	}
+}
+?>
