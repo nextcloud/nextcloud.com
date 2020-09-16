b935b4d556dd0a92f938d85794826e12fad2b824
Jos Poortvliet <jospoortvliet@gmail.com>
2020-09-16 13:55:23 +0200

    menu fix for mobile
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -145,7 +145,9 @@ require get_template_directory() . '/strings.php';
 				</li>
             </ul>
             <ul class="right-buttons">
-                <li class="nav__section"><a class="nav__label button button--white"><?php echo $l->t('Get Nextcloud'); ?></a>
+                <li class="nav__section">
+                <a class="nav__label get-nc-button button button--white"><?php echo $l->t('Get Nextcloud'); ?></a>
+                <a class="nav__label get-nc-nobutton"><?php echo $l->t('Get Nextcloud'); ?></a>
                     <ul class="nav__links ">
                         <li class="nav__item">
                             <a href="<?php echo home_url('install/#instructions-server'); ?>">
