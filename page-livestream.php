add7de7d23af976338082dfa284fca4cd9df06d9
Jos Poortvliet <jospoortvliet@gmail.com>
2020-09-28 19:46:58 +0200

    update livestream page
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-livestream.php
+++ page-livestream.php
@@ -16,8 +16,8 @@
 	<div class="background conf-background">
 		<div class="container">
 			<div class="col-md-6 topheader">
-				<h1>Nextcloud Conference 2019</h1>
-				<h2>A week of collaboration, coding and fun!</h2>
+				<h1>Nextcloud Conference 2020</h1>
+				<h2>Virtual space@#HomeOffice</h2>
 			</div>
 		</div>
 	</div>
@@ -26,11 +26,12 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-8 col-sm-offset-2">
+            <p class="section--paragraph">Join from wherever you are - the event is taking place as a live online event space 🛰👾</a></p>
 <!-- 			<img class="conf-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/grouppic2018.jpg" alt="logo"/> -->
 <!-- 			<h2></h2> -->
-			<!--<h3 class="section--paragraph__title">stay tuned for our live stream!</h3>
-			<p class="section--paragraph">Starting Saturday morning, September14, ~10:00 Berlin time.</p>-->
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/494b_gEVgOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<h3 class="section--paragraph__title">stay tuned for our live stream!</h3>
+			<p class="section--paragraph">Starting Saturday morning, October 3, ~10:00 Berlin time.</p>-->
+<!-- 			<iframe width="560" height="315" src="https://www.youtube.com/embed/494b_gEVgOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
 			<div class="row text-center">
                 <a class="button button--large button--blue button--arrow" href="https://nextcloud.com/conf/schedule">View the full schedule here</a>
                 <a class="button button--large button--blue button--arrow" href="https://nextcloud.com/conf/speakers">See the speakers</a>
