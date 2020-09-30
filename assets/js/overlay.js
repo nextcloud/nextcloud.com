a376c6ab1d3ad41f549af50d9196635c1e16c57c
Marco Ambrosini <marcoambrosini@pm.me>
2020-09-30 16:11:03 +0200

    Fix click outside
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -2,9 +2,9 @@ define(['jquery',],
 	function ($) {
 		$(document).ready(function() {
 			$('.overlay').hide();
-			$('.overlay-trigger').on('click', function(e) {
-				e.preventDefault();
-				$.get(e.target.href, function(r) { 
+			$('.overlay-trigger').on('click', function(event) {
+				event.preventDefault();
+				$.get(event.target.href, function(r) { 
 					if (r) {
 						$('.overlay-content').append(r);
 						$('.hide-in-overlay').hide();
@@ -12,11 +12,19 @@ define(['jquery',],
 					}
 				});
 			});
+			// Close the overlay when clicking on the close button
 			$('.overlay-close').on('click', function() {
 				$('.overlay').fadeOut('800');
 			});
+			/** Click outside */
+			// Stop event propagation when clicking inside the content
+			$('.overlay-content').click(function(event) {
+				event.stopPropagation();
+			});
+			// Close the overlay when clicking outside
 			$('.overlay').on('click', function() {
 				$('.overlay').fadeOut('800');
+				// Remove the content
 				$('overlay-content').empty();
 			});
 		});
