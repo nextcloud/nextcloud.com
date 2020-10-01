a38580d9f604fb8dd356075cd93f97090de88972
Marco Ambrosini <marcoambrosini@pm.me>
2020-10-01 14:34:08 +0200

    Hide the overlay at first load
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- overlay.php
+++ overlay.php
@@ -1,7 +1,6 @@
-<div class="overlay">
+<div class="overlay" style="display: none ">
 	<div class="overlay-wrapper">
 		<div class="overlay-content"></div>
 		<button class="overlay-close"><div class="icon-close"></div></button>
 	</div>
-	
 </div>
