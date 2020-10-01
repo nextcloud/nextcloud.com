d489906cb35ad674ef5a6a2a051b10d267237bcd
Marco Ambrosini <marcoambrosini@pm.me>
2020-10-01 14:54:21 +0200

    Add docs
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,3 +1,22 @@
+/**
+ * Overlay functionality
+ * 
+ * This functionality allows to open a linked page in an overlay dialog, instead
+ * of routing to the clicked link. 
+ * 
+ * Usage:
+ * - open the php file of the page in which you want the overlay to appear 
+ * - Include this line at the bottom of the page (be sure to veryfy the 
+ * path to overlay.php):
+ * `<?php require get_template_directory().'/overlay.php'; ?>`
+ * - Add the class `overlay-trigger` to each <a> element that should open the 
+ * overlay instead of routing to a different page. Note: the <a> elements 
+ * must have a valid href attribute
+ * - If there's anything you want to hide in the overlayed page, add the class 
+ * `hide-in-overlay` to the html tags are to be hidden.
+ * 
+ */
+
 define(['jquery',],
 	function ($) {
 		$(document).ready(function() {
