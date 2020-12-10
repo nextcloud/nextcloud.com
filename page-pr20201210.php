f16887b4173882dcf1305dfc6e92b6035040dbd8
Jos Poortvliet <jospoortvliet@gmail.com>
2020-12-10 15:03:03 +0100

    add SuiteCRM PR
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20201210.php
@@ -0,0 +1,65 @@
+<head>
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/pressrelease.css" rel="stylesheet">
+<meta itemprop="image" content="https://github.com/eneiluj/integration_suitecrm/raw/master/img/screenshot1.jpg">
+<meta name="twitter:image" content="https://github.com/eneiluj/integration_suitecrm/raw/master/img/screenshot1.jpg">
+<meta name="twitter:image:src" content="https://github.com/eneiluj/integration_suitecrm/raw/master/img/screenshot1.jpg">
+<meta property="og:image" content="https://github.com/eneiluj/integration_suitecrm/raw/master/img/screenshot1.jpg">
+</head>
+<div class="background generic-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+                <h1>Press release</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="section--pressrelease">
+<div class="container">
+<div class="row">
+<div class="pr">
+<h1>SalesAgility and Nextcloud announce SuiteCRM integration collaboration</h1>
+
+<p><em>December 10, 2020</em> - Pioneering CRM provider SalesAgility, has partnered with content collaboration platform, Nextcloud GmbH, bringing together two leading open source communities to create an integrated user experience, giving businesses full control of their data.</p>
+
+<p>The first phase of the integration with SalesAgility’s flagship SuiteCRM solution provides Nextcloud users a new comprehensive dashboard widget. This displays upcoming activities from SuiteCRM and receives notifications from inside Nextcloud. Users are also able to directly search on key SuiteCRM modules including Account, Contacts and Leads.</p>
+
+<p>Both companies strongly believe in putting the customer and user firmly in control. SuiteCRM and Nextcloud are released under open source licenses allowing users to protect their digital sovereignty and have the freedom to host on-premise or opt for cloud hosting.</p>
+
+<p>Dale Murray, CEO at SalesAgility said: “Our vision is to transform the CRM market by creating a powerful, flexible and user focused open-source solution. SuiteCRM provides users with a central repository for all customer data enabling key insights that can be used to enhance and nurture business relationships. By building relationships with technologies such as Nextcloud we are not only enhancing the productivity of our customers but working with communities who share our vision and value of giving customers full control over their data and systems.”
+
+<p>Frank Karlitschek, Founder and CEO at Nextcloud GmbH said: “Our customers are looking for a great user experience from their content collaboration platform, empowering users to access and work with their data and communicate wherever they are. Nextcloud enterprises optimize the productivity of their teams while keeping data secure and integrating with a variety of other technologies. The integration with SuiteCRM enables our users to access crucial customer data directly from within their collaboration environment. Connecting in such a way with fellow open source communities multiplies the benefits of open ecosystems for our mutual customers.”</p>
+
+<p>The integration app is available from the Nextcloud app store and is available for the recently released Nextcloud 20 software. Customers interested in this, the SuiteCRM and Nextcloud integration, can contact SalesAgility or Nextcloud GmbH to find out more.</p>
+
+
+
+<h2>About Nextcloud</h2>
+<p>Nextcloud Hub is the industry-leading, fully open source, on-premises team productivity platform, combining the easy user interface of consumer-grade cloud solutions with the security and compliance measures enterprises need. Nextcloud Hub brings together universal access to data through mobile, desktop and web interfaces with next-generation, on-premise secure communication and collaboration features like real-time document editing, chat and video calls, putting them under direct control of IT and integrated with existing infrastructure.</p>
+<p>Nextcloud’s easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">nextcloud.com</a> or follow <a href="https://twitter.com/Nextclouders">https://twitter.com/Nextclouders</a> on Twitter.</p>
+
+<h2>About SalesAgility</p>
+
+<p>SalesAgility are experts in delivering open source Customer Relationship Management (CRM) solutions worldwide to organisations of all sizes, on time and within budget.</p>
+
+<p>In 2013, SalesAgility launched its core open source CRM solution, SuiteCRM, and has since launched other key open source CRM offerings, including SuiteCRM:OnDemand and SuiteASSURED. It’s innovative technology offers clients a crucial 360-degree view of their customer base, enabling them to understand customer habits and tailor marketing activity accordingly. SalesAgility offers a more tailored and bespoke service to its clients at a lower cost than its competitors and is currently 96% more cost effective than its biggest competitor, Salesforce.</p>
+
+<p>SalesAgility is a truly open source business, with transparency, flexibility and collaboration at its core, and has ambitious plans to become the world’s most adopted CRM system.</p>
+
+<h2>Links</h2>
+<ul>
+<li><a href="https://nextcloud.com/about">About Nextcloud</a></li>
+<li><a href="https://apps.nextcloud.com/apps/integration_suitecrm">SuiteCRM integration in Nextcloud app store</a></li>
+<li><a href="https://suitecrm.com/">SuiteCRM</a></li>
+<li><a href="https://salesagility.com/">SalesAgility</a></li>
+</ul>
+<h2>Graphics</h2>
+<ul>
+<li><a href="https://github.com/eneiluj/integration_suitecrm/raw/master/img/screenshot1.jpg">Screenshot of SuiteCRM Dashboard widget in Nextcloud</a></li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+
