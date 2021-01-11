9b5832a618d1b345405511dc68f70fa23b46367c
Jos Poortvliet <jospoortvliet@gmail.com>
2021-01-11 11:20:03 +0100

    add Telekom press info
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20210111.php
@@ -0,0 +1,139 @@
+<head>
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/pressrelease.css" rel="stylesheet">
+<meta itemprop="image" content="https://nextcloud.com/media/Telekom-and-Nextcloud-small.png">
+<meta name="twitter:image" content="https://nextcloud.com/media/Telekom-and-Nextcloud-small.png">
+<meta name="twitter:image:src" content="https://nextcloud.com/media/Telekom-and-Nextcloud-small.png">
+<meta property="og:image" content="https://nextcloud.com/media/Telekom-and-Nextcloud-small.png">
+</head>
+<div class="background generic-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+                <h1>Press release</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="section--pressrelease">
+<div class="container">
+<div class="row">
+<div class="pr">
+<p><a href="#german">Deutsche Version unter</a></p>
+<h1>Telekom and Nextcloud offer Collaboration Platform</h1>
+<h3>    • Managed Service for Enterprises in Europe<br />
+    • Focus on European Data Sovereignty and GDPR compliance</h3>
+<p><em>Stuttgart, January 11, 2021</em> - Deutsche Telekom and Nextcloud GmbH announce the immediate availability of an enterprise-ready, Europe-hosted <a href="https://open-telekom-cloud.com/en/solutions/digital-workplace">content collaboration platform.</a> With the quickly growing demand for online productivity technologies that preserve the digital sovereignty of their users and a European-wide push for technology providers to get together and deliver those, the joint offering provides the scaleable and comprehensive digital collaboration space businesses, educational organizations governments need to maintain productivity during the Covid crisis and after. </p>
+<p>The managed solution is based on Nextcloud hub, delivering on the needs of the collaboration of modern organizations with secure data and document exchange with online editing capabilities, chat and video conferencing as well as task and calendar management. Users can access data through the browser, desktop and mobile application interfaces and data is hosted in Deutsche Telekom's’ most modern data centers in the EU to guarantee the scalability, reliability and security enterprises require.</p>
+<h3>Support of GAIA X initiative</h3>
+<p>Says Hagen Rickmann, Managing Director Business Customers at Telekom Deutschland:</p>
+<blockquote>2021 will be a year of rebuilding our economies and the second wave of Covid-19 has further accelerated the need for remote collaboration, with many employees working from home. At the same time, the increase frequency of digital attacks and risks from privacy violations have increased awareness of security and digital sovereignty. There is a clear demand from organizations and policy makers for better protection of customers' data, more transparency around security and data storage and respect for privacy from cloud vendors. In response, Telekom and Nextcloud have partnered up to provide an open source, 100% EU hosted, transparent and privacy respecting collaboration platform.</blockquote>
+<p>Both Deutsche Telekom and Nextcloud GmbH support the GAIA-X initiative for building a data infrastructure based on European standards. Data on the joint collaboration platform never leave strict German jurisdiction and users do not have to worry about data compliance issues.</p>
+
+<h3>Solution based on Open Source</h3>
+<p>Deutsche Telekom, as a trusted partner for a broad variety of leading ICT services from a single source, serves any kind of organization with a resilient cloud platform as well as individual build and run management with fixed service templates and price schemes and a rewarded service model.</p>
+<p>he Open Source nature of the Nextcloud Hub platform gives customers unprecedented control and flexibility compared to typical Saas offerings, enabling deep integration in existing infrastructure as well as extension and adaption of the product. The use of open source and open standards also ensures transparency, offering customers and third parties the ability to verify the functionality and security of the product.</p>
+<p>Says Frank Karlitschek, CEO and Founder of Nextcloud GmbH:</p>
+<blockquote>With more and more European firms, government organizations and educational institutes using cloud services we see a constantly growing demand for European data sovereignty. In a data-driven economy we need platforms that guarantee our sovereignty to and innovation and develop new business models. Together with Deutsche Telekom Nextcloud offers a credible alternative with major benefits in terms of management by trust, scalability and adaptability.</blockquote>
+<p>The GDPR-compliant, the managed platform is available for immediate deployment for customers. More information: <a href="https://open-telekom-cloud.com/en/solutions/digital-workplace">https://open-telekom-cloud.com/en/solutions/digital-workplace</a></p>
+
+<h2>Abut Deutsche Telekom and T-Systems</h2>
+<p>About Deutsche Telekom: <a href="https://www.telekom.com/en/company/at-a-glance">Deutsche Telekom at a glance</a><br />
+About T-Systems: <a href="https://www.t-systems.com/de/en/about-t-systems/company/company-profile-198840">T-Systems company profile</a></p>
+
+<h2>About Nextcloud</h2>
+<p>Nextcloud Hub is the industry-leading, fully open source, on-premises team productivity platform, combining the easy user interface of consumer-grade cloud solutions with the security and compliance measures enterprises need. Nextcloud Hub brings together universal access to data through mobile, desktop and web interfaces with next-generation, on-premise secure communication and collaboration features like real-time document editing, chat and video calls, putting them under direct control of IT and integrated with existing infrastructure.</p>
+<p>Nextcloud’s easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com">nextcloud.com</a> or follow <a href="https://twitter.com/Nextclouders">https://twitter.com/Nextclouders</a> on Twitter.</p>
+
+<h2>Further information for the media</h2>
+<p><a href="http://www.telekom.com/medien">www.telekom.com/medien</a><br />
+<a href="http://www.telekom.com/fotos">www.telekom.com/fotos</a><br />
+<a href="http://www.twitter.com/deutschetelekom">www.twitter.com/deutschetelekom</a><br />
+<a href="http://www.facebook.com/deutschetelekom">www.facebook.com/deutschetelekom</a><br />
+<a href="http://www.telekom.com/blog">www.telekom.com/blog</a><br />
+<a href="http://www.youtube.com/deutschetelekom">www.youtube.com/deutschetelekom</a><br />
+<a href="http://www.instagram.com/deutschetelekom">www.instagram.com/deutschetelekom</a><br />
+<a href="https://www.nextcloud.com/press">https://www.nextcloud.com/press</a><br />
+<a href="https://www.instagram.com/nextclouders">https://www.instagram.com/nextclouders</a><br />
+<a href="https://twitter.com/nextclouders">https://twitter.com/nextclouders</a><br />
+<a href="https://www.facebook.com/Nextclouders/">https://www.facebook.com/Nextclouders/</a></p>
+
+<h2>Press contact</h2>
+<p>Deutsche Telekom AG<br />
+Corporate Communications<br />
+
+Tel.: 0228 181 – 49494<br />
+Email: media@telekom.de</p>
+<p><strong>Nextcloud GmbH</strong><br />
+Jos Poortvliet<br />
+Phone: +49 (0) 171 121 7528<br />
+<a href="mailto:jos.poortvliet@nextcloud.com" class="email">jos.poortvliet@nextcloud.com</a></p>
+<h2>Graphics</h2>
+<ul>
+<li><a href="https://nextcloud.com/media/Telekom-and-Nextcloud-small.pngg">Launch graphic</a></li>
+</ul>
+
+
+<a name="german"></a>
+<h1>Telekom und Nextcloud bieten Collaboration-Lösung</h1>
+<h3>    • Gemanagtes Angebot für Unternehmen in Europa<br />
+    • Europäische Datensouveränität und DSGVO-Konformität im Mittelpunkt</h3>
+<p><em>Stuttgart, 11. Januar, 2021</em> - Die Deutsche Telekom und Nextcloud GmbH bieten Unternehmen ab sofort eine rein europäische <a href="https://open-telekom-cloud.com/de/loesungen/digital-workplace">Collaboration-Plattform</a> an. Die gemanagte Lösung erfüllt die Anforderungen der Datenschutzgrundverordnung (DSGVO) für sicheren Daten- und Dokumentenaustausch mit Online-Bearbeitungsfunktionen. Sie basiert auf Nextcloud Hub und enthält auch Chat und Videokonferenzen sowie Aufgaben- und Kalendermanagement. Die Daten werden in den modernsten Rechenzentren der Telekom in der EU gehostet. Das gewährleistet Skalierbarkeit, Zuverlässigkeit und Sicherheit. </p>
+<p>Die Nachfrage nach Online-Produktivitätstechnologien, die die digitale Souveränität ihrer Nutzer bewahren, wächst schnell. Unternehmen, Bildungseinrichtungen und Regierungen benötigen einen skalierbaren und umfassenden Raum für digitale Zusammenarbeit, um die Produktivität während der Covid-Krise und danach aufrechtzuerhalten. </p>
+<blockquote>2021 wird ein Jahr des Wiederaufbaus unserer Volkswirtschaften sein,</blockquote>
+<p>erklärt Hagen Rickmann, Geschäftsführer Geschäftskunden der Telekom Deutschland. „</p>
+<blockquote>Die Covid-19-Pandemie unterstreicht die Notwendigkeit der Remote-Zusammenarbeit, viele Mitarbeiter arbeiten von zu Hause aus. “ Gleichzeitig hätten zunehmende digitale Angriffe das Bewusstsein für Sicherheit und digitale Souveränität erhöht. „Es gibt eine klare Forderung von Unternehmen und politischen Entscheidungsträgern nach einem besseren Schutz der Kundendaten, mehr Transparenz in Bezug auf Sicherheit und Datenspeicherung</blockquote>
+<p>fügte Rickmann hinzu</p>
+<blockquote>Deshalb haben sich die Telekom und Nextcloud zusammengeschlossen, um eine Open-Source-Plattform für Zusammenarbeit anzubieten, die zu 100 % in der EU gehostet wird, transparent ist und die Daten schützt.</blockquote>
+<p>Die Deutsche Telekom und Nextcloud GmbH unterstützen die GAIA-X-Initiative zum Aufbau einer auf europäischen Standards basierenden Dateninfrastruktur. Die Daten auf der gemeinsamen Kollaborationsplattform verlassen nie den strengen deutschen Rechtsraum und die Nutzer müssen sich nicht um Fragen der Daten-Compliance kümmern.</p>
+<p>Die Telekom als vertrauenswürdiger Partner für ein breites Spektrum an führenden ICT-Services versorgt Unternehmen jeder Größe mit ihrer Open Telekom Cloud. Dazu gehören individuelles Build & Run-Management mit festen Service-Templates und Preislisten sowie ein prämiertes Service-Modell.</p>
+<p>Die auf Open Source basierende Nextcloud-Hub-Plattform gibt Kunden eine noch nie dagewesene Kontrolle und Flexibilität im Vergleich zu typischen Saas-Angeboten und ermöglicht eine tiefe Integration in die bestehende Infrastruktur sowie die Erweiterung und Anpassung des Produkts. Die Verwendung von Open Source und offenen Standards sorgt zudem für Transparenz und bietet Kunden und Dritten die Möglichkeit, die Funktionalität und Sicherheit des Produkts zu überprüfen.</p>
+<blockquote>Da immer mehr europäische Unternehmen, Regierungsorganisationen und Bildungseinrichtungen Cloud-Dienste nutzen, sehen wir eine stetig wachsende Nachfrage nach europäischer Datensouveränität. In einer datengesteuerten Wirtschaft brauchen wir Plattformen, die unsere Souveränität für Innovation und die Entwicklung neuer Geschäftsmodelle garantieren. Gemeinsam mit der Deutschen Telekom bietet Nextcloud eine glaubwürdige Alternative mit großen Vorteilen in Bezug auf die Verwaltung durch Vertrauen, Skalierbarkeit und Anpassungsfähigkeit</blockquote>
+<p>betonte Frank Karlitschek, CEO und Gründer der Nextcloud GmbH.</p>
+<p>Mehr Infos unter zur Collaboration-Plattform: <br /><a href="https://open-telekom-cloud.com/de/loesungen/digital-workplace">https://open-telekom-cloud.com/de/loesungen/digital-workplace</a></p>
+
+<h2>Über die Deutsche Telekom und T-Systems</h2>
+<p>Über die Deutsche Telekom: <a href="https://www.telekom.com/de/konzern/konzernprofil">Deutsche Telekom Konzernprofil</a><br />
+Über T-Systems: <a href="https://www.t-systems.com/de/de/ueber-uns/unternehmen/unternehmensprofil-187388">T-Systems Unternehmensprofil</a></p>
+
+<h2>Über Nextcloud GmbH</h2>
+<p>Nextcloud bietet die branchenführende Open-Source-Softwarelösung für on-premises Datenverarbeitung und -kommunikation, die die einfache Benutzeroberfläche von Consumer-Grade-Cloud-Lösungen mit den für Unternehmen erforderlichen Sicherheits- und Compliance-Maßnahmen kombiniert. Nextcloud vereint den universellen Zugriff auf Daten über mobile, Desktop- und Web-Interfaces mit innovativen, sicheren Kommunikations- und Kollaborationsfunktionen wie Dokumentenbearbeitung in Echtzeit, Chat und Videoanrufe, die unter direkter Kontrolle der IT und in die bestehende Infrastruktur integriert werden.</p>
+
+<p>Mit seiner einfachen und schnellen Bereitstellung, offenen, modularen Architektur und dem Fokus auf Sicherheits- und fortschrittlichen Föderationsfunktionen, ermöglicht Nextcloud modernen Unternehmen, ihre vorhandenen Dateispeicheranlagen innerhalb und außerhalb der Grenzen ihres Unternehmens zu optimieren.</p>
+
+<p>Für weitere Informationen besuchen Sie <a href="https://nextcloud.com">nextcloud.com<a/> oder folgen Sie <a href="https://twitter.com/nextclouders">@Nextclouders</a> auf Twitter.</p>
+
+<h2>Weitere Informationen für Medienvertreter</h2>
+<p><a href="http://www.telekom.com/medien">www.telekom.com/medien</a><br />
+<a href="http://www.telekom.com/fotos">www.telekom.com/fotos</a><br />
+<a href="http://www.twitter.com/deutschetelekom">www.twitter.com/deutschetelekom</a><br />
+<a href="http://www.facebook.com/deutschetelekom">www.facebook.com/deutschetelekom</a><br />
+<a href="http://www.telekom.com/blog">www.telekom.com/blog</a><br />
+<a href="http://www.youtube.com/deutschetelekom">www.youtube.com/deutschetelekom</a><br />
+<a href="http://www.instagram.com/deutschetelekom">www.instagram.com/deutschetelekom</a><br />
+<a href="https://www.nextcloud.com/press">https://www.nextcloud.com/press</a><br />
+<a href="https://www.instagram.com/nextclouders">https://www.instagram.com/nextclouders</a><br />
+<a href="https://twitter.com/nextclouders">https://twitter.com/nextclouders</a><br />
+<a href="https://www.facebook.com/Nextclouders/">https://www.facebook.com/Nextclouders/</a></p>
+
+<h2>Presse contact</h2>
+<p>Deutsche Telekom AG<br />
+Corporate Communications<br />
+
+Tel.: 0228 181 – 49494<br />
+Email: media@telekom.de</p>
+<p><strong>Nextcloud GmbH</strong><br />
+Jos Poortvliet<br />
+Phone: +49 (0) 171 121 7528<br />
+<a href="mailto:jos.poortvliet@nextcloud.com" class="email">jos.poortvliet@nextcloud.com</a></p>
+<h2>Graphics</h2>
+<ul>
+<li><a href="https://nextcloud.com/media/Telekom-and-Nextcloud-small.pngg">Launch graphic</a></li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+
+
+
