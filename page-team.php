e5df2417fbd90ce3bbd6eff3e85421f3fef5ca93
Jos Poortvliet <jospoortvliet@gmail.com>
2021-02-19 14:42:03 +0100

    add img for Alexa
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-team.php
+++ page-team.php
@@ -308,7 +308,7 @@
         </div>
         <div class="col-md-4">
             <div class="teamp">
-                <img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
+                <img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/alexandra.jpg" />
                 <h2 class="teamname">	Alexandra Boehm</h2>
                 <div class="teammeta">
                     <h3 class="teamtitle">Accountant</h3>
