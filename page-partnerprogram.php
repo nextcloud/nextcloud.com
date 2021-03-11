5262d40bfd5bc7851dca3962e78f83dae064ff1a
Jos Poortvliet <jospoortvliet@gmail.com>
2021-03-11 09:39:42 +0100

    draft pp
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-partnerprogram.php
@@ -0,0 +1,336 @@
+<head>
+<meta itemprop="image" content="<?php echo get_template_directory_uri(); ?>/assets/img/features/search_moodle_course_module.png">
+<meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/assets/img/features/search_moodle_course_module.png">
+<meta name="twitter:image:src" content="<?php echo get_template_directory_uri(); ?>/assets/img/features/search_moodle_course_module.png">
+<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/img/features/search_moodle_course_module.png">
+<meta name=”Description” content="Integrated email communication in Nextcloud.">
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/partnerprogram.css" rel="stylesheet">
+</head>
+
+<section class="hero-section">
+	<div class="background partnerprogram-background">
+		<div class="container">
+			<div class="col-md-6 topheader">
+				<h1><?php echo $l->t('Partner program');?></h1>
+				<h2><?php echo $l->t('Become part of the success story');?></h2>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section--overview">
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/mobile.svg" /></div>
+			<h3 class="section--paragraph__title text-center"><?php echo $l->t('Provide trust');?></h3>
+			<p class="section--paragraph text-center"><?php echo $l->t('A Nextcloud partnership shows customers they can trust your expertise and know it is backed by the full engineering power of Nextcloud.');?></p>
+		</div>
+		<div class="col-md-4">
+			<div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/permissions.svg" /></div>
+			<h3 class="section--paragraph__title text-center"><?php echo $l->t('Go to market together');?></h3>
+			<p class="section--paragraph text-center"><?php echo $l->t('Promote, market and sell Nextcloud together. Organize and attend events with us, aqcuire leads, participate in tenders and close deals.');?></p>
+		</div>
+		<div class="col-md-4">
+			<div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/storage.svg" /></div>
+			<h3 class="section--paragraph__title text-center"><?php echo $l->t('Level up');?></h3>
+			<p class="section--paragraph text-center"><?php echo $l->t('Provide training to your team or customers, access our technical support and learn from our experts how to optimize large-scale deployments.');?></p>
+		</div>
+	</div>
+</div>
+</section>
+
+
+<section class="section--register quote">
+	<div class="container">
+       <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <p class="text-center"><?php echo $l->t('Get started!');?></p>
+            <div class="text-center morebuttondiv">
+                <a href="https://cloud.nextcloud.com/apps/forms/wyWbxHRYEFqGDW6z" class="button button--large button--arrow"><?php echo $l->t('Apply as partner');?> <i class="icon-arrow-circle-o-right icon"></i></a>
+            </div>
+        </div>
+	</div>
+</section>
+
+<section class="section--benefits">
+<div class="container-fluid">
+    <div class="container">
+        <h2 class="text-center"><?php echo $l->t('Partner benefits');?></h2>
+        <div class="row">
+            <div class="col-sm-4">
+                <p class="section--paragraph">
+                <ul>
+                    <li> <?php echo $l->t('commission on Nextcloud sales');?></li>
+                    <li> <?php echo $l->t('joint marketing');?></li>
+                    <li> <?php echo $l->t('lead sharing');?></li>
+                </ul></p>
+            </div>
+            <div class="col-sm-4">
+                <p class="section--paragraph">
+                <ul>
+                    <li> <?php echo $l->t('sales and technical training');?></li>
+                    <li> <?php echo $l->t('get and manage certifications');?></li>
+                    <li> <?php echo $l->t('joint annual BDR plan');?></li>
+                </ul></p>
+            </div>
+            <div class="col-sm-4">
+                <ul>
+                    <li> <?php echo $l->t('marketing materials');?></li>
+                    <li> <?php echo $l->t('event collaboration');?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<section class="section--comparison">
+    <div class="container">
+        <div class="row">
+         <div class="col-md-8 col-md-offset-2">
+            <h2 class="text-center"><?php echo $l->t('Partner levels');?></h2>
+<!--                 <h3 class="section--paragraph__title"><?php echo $l->t('Deployment and control with MDM and MSI');?></h3> -->
+<!--                 <p class="section--paragraph"><?php echo $l->t('Nextcloud has four partner levels which depend on revenue targets..');?></p> -->
+<!--                 <p class="section--paragraph"><?php echo $l->t('Here is a complete overview of the Nextcloud branded client options.');?></p> -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <td>
+                                    <p><strong><small>Benefit</small></strong></p>
+                                </td>
+                                <td>
+                                    <p><strong><small>Bronze partner</small></strong></p>
+                                </td>
+                                <td>
+                                    <p><strong><small>Silver partner</small></strong></p>
+                                </td>
+                                <td>
+                                    <p><strong><small>Gold partner</small></strong></p>
+                                </td>
+                                <td>
+                                    <p><strong><small>Platinum partner</small></strong></p>
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><small>Sales</small></td>
+                                <td>
+<!--                                     <p>-</p> -->
+                                </td>
+                                <td>
+<!--                                     <p>-</p> -->
+                                </td>
+                                <td>
+<!--                                     <p>-</p> -->
+                                </td>
+                                <td>
+<!--                                     <p><i class="fa fa-check" aria-hidden="true"></i></p> -->
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Lead sharing</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>Opportunistic</p>
+                                </td>
+                                <td>
+                                    <p>Regional</p>
+                                </td>
+                                <td>
+                                    <p>Exclusive</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Technical training</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Dedicated partner manager</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Dedicated account manager</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Dedicated sales engineer</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Marketing</small></td>
+                                <td>
+<!--                                     <p>-</p> -->
+                                </td>
+                                <td>
+<!--                                     <p>-</p> -->
+                                </td>
+                                <td>
+<!--                                     <p>-</p> -->
+                                </td>
+                                <td>
+<!--                                     <p><i class="fa fa-check" aria-hidden="true"></i></p> -->
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Social media support</small></td>
+                                <td>
+                                    <p>Yearly</p>
+                                </td>
+                                <td>
+                                    <p>Quarterly</p>
+                                </td>
+                                <td>
+                                    <p>Monthly</p>
+                                </td>
+                                <td>
+                                    <p>Weekly</p>
+                                </td>
+                            </tr>
+                            <tr>
+                            <tr>
+                                <td><small>Partner page presence</small></td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Announcement blog</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                                <td><small>Marketing onboarding</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Marketing calls</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>Yearly</p>
+                                </td>
+                                <td>
+                                    <p>Quarterly</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><small>Campaign collaboration</small></td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p>-</p>
+                                </td>
+                                <td>
+                                    <p><i class="fa fa-check" aria-hidden="true"></i></p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="section--register quote">
+	<div class="container">
+       <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <p class="text-center"><?php echo $l->t('Get started!');?></p>
+            <div class="text-center morebuttondiv">
+                <a href="https://cloud.nextcloud.com/apps/forms/wyWbxHRYEFqGDW6z" class="button button--large button--arrow"><?php echo $l->t('Apply as partner');?> <i class="icon-arrow-circle-o-right icon"></i></a>
+            </div>
+        </div>
+	</div>
+</section>
