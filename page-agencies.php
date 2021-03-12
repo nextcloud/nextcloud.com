20fc8f882f9a8f774a201bfbd6b59b00c13697fb
Jos Poortvliet <jospoortvliet@gmail.com>
2021-03-12 13:14:53 +0100

    add ARD/ZDF to agencies page
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-agencies.php
+++ page-agencies.php
@@ -91,7 +91,36 @@ require(["require.config"], function() {
 
 
 
-
+<section class="section--whitepaper quote">
+	<div class="container ">
+        <div class="row">
+			<div class="col-lg-8">
+                <h2 class=""><?php echo $l->t('How the German Radio and Television transfers terrabytes of data in and out of Nextcloud');?></h2>
+                <form name="whitepaper" method="post" action="<?php echo home_url('whitepaper-submit') ?>">
+                    <p><label for="email"><?php echo $l->t('Download our free case study: <br /> IVZ, Germany’s public radio and television deploys Nextcloud with ONLYOFFICE for efficient file exchange and online content collaboration');?><br>
+                    <td colspan="2" style="text-align:center">
+                    <div class="">
+                        <div class="g-recaptcha" data-sitekey="<?php echo RECAPTCHA_SITEKEY; ?>"></div>
+                    </div>
+                    </td>
+                    <input type="hidden" name="segmentId" value="93">
+                    <input type="hidden" name="firstname" value="">
+                    <input type="hidden" name="requesttime" value="<?php echo time(); ?>">
+                    <input class="mail" type="text" name="email" maxlength="80" placeholder="Enter your email"></label>
+                    <div class="newsletter">
+                        <input type="hidden" name="newsletter" value="0" />
+<!--                         <input type="checkbox" name="newsletter" value="1"> <small>Sign me up for the Nextcloud newsletter</small><br/> -->
+                        <input type="hidden" name="moreinfo" value="0" />
+                        <input type="checkbox" name="moreinfo" value="1"> <small><?php echo $l->t('Subscribe me to the monthly Nextcloud newsletter');?></small><br/>
+                        <small><?php echo $l->t('See our');?> <a class="hyperlink" href="<?php echo home_url('privacy') ?>"><?php echo $l->t('privacy policy');?></a></small>
+                    </div>
+                    <input class="button button--large" type="submit" value=" Get the case study "></p>
+                </form>
+            </div>
+        </div>
+        <img class="responsive" src="<?php bloginfo('template_directory'); ?>/assets/img/whitepapers/ivz-thumbnail-banner.png"/>
+	</div>
+</section>
 
 
 
