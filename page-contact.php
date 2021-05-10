166b6a5d449fb82bceb2eff044f363af7c0b08fd
Jos Poortvliet <jospoortvliet@gmail.com>
2021-05-10 14:27:01 +0200

    Update contact form to new style
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-contact.php
+++ page-contact.php
@@ -6,56 +6,104 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 topheader">
-                <h1><?php echo $l->t('Contact Nextcloud');?></h1>
+                <h1><?php echo $l->t('Contact us');?></h1>
             </div>
         </div>
     </div>
 </section>
-<section class="section--welcome">
+
+<section class="section--contact-form">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <p><?php echo $l->t('If you have technical questions or need support,');?> <a class="hyperlink" href="<?php echo home_url('support') ?>"><?php echo $l->t('check out the options on our support page.</a>');?>
-            <p><?php echo $l->t('As a home user, best use our forums for questions.');?> <a class="hyperlink" href="https://help.nextcloud.com" target="_blank"><?php echo $l->t('Find our forums here.');?></a></p>
-            <p><?php echo $l->t('If you have questions about what Nextcloud can do for you,');?> <a class="hyperlink" href="<?php echo home_url('search') ?>" target="_blank"><?php echo $l->t('search our website.');?></a></p>
-            </div>
-        <div class="col-md-8 col-md-offset-2">
-            <h1><?php echo $l->t('Contact us');?></h1>
-            <p><?php echo $l->t('If you have any questions or comments on the website or have a business question, complete the form below and we\'ll get back to you!<br/><strong>If you need technical help, please do not use this form.</strong> If you are a customer, <a href="https://portal.nextcloud.com">use our support portal</a> and if not');?> <a class="hyperlink" href="<?php echo home_url('support') ?>"><?php echo $l->t('see here for support options.');?></a></p>
-            <p><?php echo $l->t('Support requests using this form <strong>will be ignored!</strong>');?></p>
-            <p><?php echo $l->t('If you want to test out Nextcloud Enterprise or get a quote for use in your organization, use the quote form for a quicker response.');?>  <a class="hyperlink" href="<?php echo home_url('buy') ?>"><?php echo $l->t('Find the quote form here,');?></a> <a class="hyperlink" href="<?php echo home_url('trial') ?>"><?php echo $l->t('or ask directly for a trial of Nextcloud Enterprise here.');?></a> <?php echo $l->t('If you want to become a partner,');?> <a class="hyperlink" href="<?php echo home_url('partnerprogram') ?>"><?php echo $l->t('see here.');?></a></p>
-            <p><?php echo $l->t('If you have a question related to an user account signed through <strong>Simple Signup</strong>, please contact the provider where you signed up. Nextcloud has NO information from you, not even what provider you are with, so there is nothing we can do to help you.');?></p>
+            <p><?php echo $l->t('If you have any general questions or comments, complete the form below and we\'ll get back to you!');?></p>
+            <p><?php echo $l->t('We usually reply within two weeks. For a faster reply, use one of the specific forms below.');?></p>
+            <ul>
+                <li><strong><?php echo $l->t('Technical questions or support');?>.</strong> <a class="hyperlink" href="<?php echo home_url('support') ?>"><?php echo $l->t('Check out the options on our support page.');?></a> <br /><?php echo $l->t('Support requests through the contact form will not get any response.');?>
+                <li><strong><?php echo $l->t('Test out Nextcloud Enterprise or get a quote');?>.</strong> <a class="hyperlink" href="<?php echo home_url('buy') ?>"><?php echo $l->t('Find the quote form here');?></a> and ask directly for a <a class="hyperlink" href="<?php echo home_url('trial') ?>"><?php echo $l->t('trial of Nextcloud Enterprise here.');?></a></li>
+                <li><strong><?php echo $l->t('Become a Nextcloud partner');?>.</strong> <a class="hyperlink" href="<?php echo home_url('partnerprogram') ?>"><?php echo $l->t('See the form here.');?></a></li>
+                <li><strong><?php echo $l->t('Issues with your Nextcloud account');?>.</strong> <?php echo $l->t('Please contact the provider where you signed up. Nextcloud does no hosting and does not know where you host your data, so we can not help you.');?></li>
+            </ul>
         </div>
     </div>
 </div>
-
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-2">
+            <h2><?php echo $l->t('Contact form');?></h2>
             <form name="contact" method="post" action="contactsubmit/">
-            <div class="row">
-                <div class="col-md-8">
-                <?php echo $l->t('* mandatory question');?>
-                    <p><label for="yourname"><?php echo $l->t('Your name');?>*<br>
-                    <input  type="text" name="yourname" maxlength="60" size="60"></label></p>
-                    <p><label for="email">Email*<br>
-                    <input  type="text" name="email" maxlength="80" size="60"></label></p>
-                    <p><label for="organization"><?php echo $l->t('Organization');?>*<br>
-                    <input  type="text" name="organization" maxlength="100" size="60" placeholder="<?php echo $l->t('Name of your organization');?>"></label></p>
-                    <p><label for="role"><?php echo $l->t('Your role');?>*<br>
-                    <input  type="text" name="role" maxlength="100" size="60" placeholder="<?php echo $l->t('Your job title');?>"></label></p>
-                    <p><label for="phone"><?php echo $l->t('Phone number');?>*<br>
-                    <input  type="text" name="phone" maxlength="40" size="60" placeholder="<?php echo $l->t('Please include country code (00 or +XX)');?>"></label></p>
-                    <p><label for="comments"><?php echo $l->t('Your message');?>*<br />
-                    <textarea  name="comments" maxlength="2000" cols="80" rows="8" placeholder="<?php echo $l->t('Let us know how we can help you!');?>"></textarea></label></p>
-                    <p><input type="checkbox" id="gdprcheck" name="gdprcheck" value="gdprchecked"><label for="gdprcheck"> <?php echo $l->t('I agree with the Nextcloud privacy policy and understand my data will be processed so Nextcloud can reach out to me.');?>*<br /></p>
-                    <input type="submit" value=" Submit ">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <p><label for="yourname"><?php echo $l->t('Your name');?></label></p>
+                    </div>
+                    <div class="col-lg-7">
+                        <input  type="text" name="yourname" maxlength="60" size="60">
+                    </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <p><label for="email">Email</label></p>
+                    </div>
+                    <div class="col-lg-7">
+                        <input  type="text" name="email" maxlength="80" size="60">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <br /> <span> </span>
+                    </div>
+                    <div class="col-lg-7">
+                   <?php echo $l->t('If your question is business-related:');?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <p><label for="organization"><?php echo $l->t('Organization');?></label></p>
+                    </div>
+                    <div class="col-lg-7">
+                        <input  type="text" name="organization" maxlength="100" size="60" placeholder="<?php echo $l->t('Name of your organization');?>">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <p><label for="phone"><?php echo $l->t('Phone number');?></label></p>
+                    </div>
+                    <div class="col-lg-7">
+                        <input  type="text" name="phone" maxlength="40" size="60" placeholder="<?php echo $l->t('Please include country code (00 or +XX)');?>">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <p><label for="comments"><?php echo $l->t('Your message');?></label></p>
+                    </div>
+                    <div class="col-lg-7">
+                        <textarea  name="comments" maxlength="2000" cols="60" rows="8" placeholder="<?php echo $l->t('Let us know how we can help you!');?>"></textarea>
+                        <strong><?php echo $l->t('Reminder: support questions through this form will get ignored.');?></strong>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <br /> <span> </span>
+                    </div>
+                    <div class="col-lg-7">
+                        <p><input type="checkbox" id="gdprcheck" name="gdprcheck" value="gdprchecked"><label for="gdprcheck"> <?php echo $l->t('I agree with the Nextcloud privacy policy and understand my data will be processed so Nextcloud can reach out to me.');?> <br /><a class="hyperlink" href="<?php echo home_url('privacy') ?>" class="hyperlink"><?php echo $l->t('See our privacy policy here.');?></a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <br /> <span> </span>
+                    </div>
+                    <div class="col-lg-7">
+                    <input class="button button--blue" type="submit" value=" Submit ">
+                    </div>
+                </div>
             </form>
-            <p><?php echo $l->t('We respect your privacy! If you fill in this form, we will reach out to send you a reply. We never sell your data - any mails you get will be from us.');?> <a class="hyperlink" href="<?php echo home_url('privacy') ?>" class="hyperlink"><?php echo $l->t('See our privacy policy here.');?></a></p>
         </div>
     </div>
+</section>
+
+<section class="section--address">
+<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
         <h2><?php echo $l->t('Visit us!');?></h2>
