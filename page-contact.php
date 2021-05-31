241238b135ae94965d3204ceaa63df0898df6b53
Jos Poortvliet <jospoortvliet@gmail.com>
2021-05-31 10:10:03 +0200

    move contact form title
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-contact.php
+++ page-contact.php
@@ -29,8 +29,12 @@
 </div>
 <div class="container">
     <div class="row">
-        <div class="col-md-10">
+        <div class="col-md-10 col-md-offset-2">
             <h2><?php echo $l->t('Contact form');?></h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-10">
             <form name="contact" method="post" action="contactsubmit/">
                 <div class="row">
                     <div class="col-lg-4">
