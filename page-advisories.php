890542103f935dd003f53cc6b8e7058997be9f98
Lukas Reschke <lukas@statuscode.ch>
2021-06-01 13:17:10 +0200

    Move security advisories to GitHub (#1488)
    * Move security advisories to GitHub
    
    The advisory process takes quite some time at the moment, and I'd like to save time on that.
    
    What does this change mean:
    
    1. Someone opening http://nextcloud.com/security/advisories/ will be redirected to the GitHub advisories page
    2. The old advisories will still all be accessible.
    
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>
    
    * Close PHP tag
    
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- page-advisories.php
+++ page-advisories.php
@@ -1,36 +1,6 @@
-<head>
-    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/generic.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/vendor/dsgvo-video-embed.min.css">
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/dsgvo-video-embed.min.js"></script>
+<?php
 
-<div class="background generic-background">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 topheader">
-                <h1><?php echo $l->t('Security Advisories');?></h1>
-            </div>
-        </div>
-    </div>
-</div>
+header("Location: https://github.com/nextcloud/security-advisories/security/advisories");
+exit();
+?>
 
-<section class="content">
-<div class="container">
-<div class="row page-content-header">
-	<div class="col-md-3 col-md-offset-9">
-		<form role="form" action="/security/advisory/" method="get">
-			<input name="id" type="text" class="form-control" placeholder="Advisory identifier">
-			<button type="submit" class="btn btn-default">Go</button>
-		</form>
-	</div>
-</div>
-<div class="sub-nav"><?php echo $l->t('<a href="/security">Security page</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="/security/threat-model">Threat Model</a>');?></div>
-<div style="margin-top: 20px;"><p><?php echo $l->t('You can <a href="/advisories/advisories.rss">follow our advisories via RSS</a>.');?></p></div>
-<div class="row" style="
-    margin-top: 20px;
-">
-	<div class="col-md-12">
-		<?php get_template_part('advisories/full-list');?>
-	</div>
-</div>
-</div>
-</section>
