7408410a8b5c3685b1931906dff2d6cf586b98a0
Jos Poortvliet <jospoortvliet@gmail.com>
2021-06-03 17:32:15 +0200

    move captcha to right
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-contact.php
+++ page-contact.php
@@ -109,20 +109,23 @@
                         <p><input type="checkbox" id="gdprcheck" name="gdprcheck" value="gdprchecked"><label for="gdprcheck"> <?php echo $l->t('I agree with the Nextcloud privacy policy and understand my data will be processed so Nextcloud can reach out to me.');?> <br /><a class="hyperlink" href="<?php echo home_url('privacy') ?>" class="hyperlink"><?php echo $l->t('See our privacy policy here.');?></a></p>
                     </div>
                 </div>
-                <p>
-                    <?php
-                        require_once realpath(dirname(__FILE__)) . '/lib/captcha.php';
-                    ?>
-                    <label for="captcha"><?php echo $l->t('Please enter the following captcha');?>* <span></span><br>
-                        <img src="data:image/png;base64,<?php echo base64_encode(GetCaptcha()); ?>" /><br>
-                        <input  type="text" name="captcha" maxlength="20" size="20" placeholder="13"></label>
-                </p>
                 <div class="row">
                     <div class="col-lg-4">
                         <br /> <span> </span>
                     </div>
                     <div class="col-lg-7">
-                    <input class="button button--blue" type="submit" value=" Submit ">
+                        <?php require_once realpath(dirname(__FILE__)) . '/lib/captcha.php'; ?>
+                        <p><label for="captcha"><?php echo $l->t('Please enter the following captcha');?></label> <span></span><br>
+                        <img src="data:image/png;base64,<?php echo base64_encode(GetCaptcha()); ?>" /></p>
+                        <p><input  type="text" name="captcha" maxlength="20" size="20" placeholder="eXaMpLe15"></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <br /> <span> </span>
+                    </div>
+                    <div class="col-lg-7">
+                        <input class="button button--blue" type="submit" value=" Submit ">
                     </div>
                 </div>
             </form>
