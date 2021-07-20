500cebd80490c478b6780553277fe9936149da1a
Daniel Hansson <github@hanssonit.se>
2021-07-20 11:10:50 +0200

    update line-up (#1511)
    * update line-up
    
    will add a new pic as well soon
    
    * Delete nuc10side.jpeg
    
    * Add files via upload
    
    * change asset
    
    * Delete asuspn51back.png
    
    * add compressed file

--- page-devices.php
+++ page-devices.php
@@ -35,13 +35,13 @@ require(["require.config"], function() {
         <div class="col-lg-4">
             <div class="block-container">
                 <div class="text-center">
-                    <a href="https://shop.hanssonit.se/product-category/nextcloud/home-sme-server/"><img src="<?php bloginfo('template_directory'); ?>/assets/img/devices/nuc10side.jpeg" class="text-center featureimg" /></a>
+                    <a href="https://shop.hanssonit.se/product-category/nextcloud/home-sme-server/"><img src="<?php bloginfo('template_directory'); ?>/assets/img/devices/asuspn51back.png" class="text-center featureimg" /></a>
                 </div>
                 <div class="col-md-12">
                     <h3 class="section--paragraph__title"><?php echo $l->t('Nextcloud Home/SME Server');?></h3>
                     <p class="section--paragraph"><em><?php echo $l->t('Get your own self-hosted cloud server!');?></em></p>
                     <p class="section--paragraph"><?php echo $l->t('The Nextcloud Home/SME Server is a Nextcloud server pre-configured with Nextcloud and ready to boot once delivered. Just as the Nextcloud VM it includes easy to setup apps like Collabora, OnlyOffice, Talk and more.');?></p>
-                    <p class="section--paragraph"><?php echo $l->t('Updates are managed with a easy-to-run update script which updates the whole server in just one single command. The server is based on a powerful Intel 64-bit CPU (10:th-gen), 500 GB M2 PCIe SSD + 2/4 TB HDD/SSD, and 16/32 GB RAM.');?></p>
+                    <p class="section--paragraph"><?php echo $l->t('Updates are managed with a easy-to-run update script which updates the whole server in just one single command. The server is based on a powerful AMD Ryzen 5500U 64-bit CPU (6 cores, 12 threads), 500 GB M2 PCIe SSD + 2/4 TB HDD/SSD, and 16/32 GB RAM.');?></p>
                     <p class="section--paragraph"><?php echo $l->t('The Nextcloud Home/SME server is made by the team behind the Nextcloud VM, and aims to be the easiest way to setup your own cloud - now available as a bare-bone server!');?></p>
                     <div class="devices-tags">
                         <span>Managed</span>
