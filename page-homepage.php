3bf65879013456fbe4ea3755294006e11291f8cd
Jos Poortvliet <jospoortvliet@gmail.com>
2021-07-20 11:21:40 +0200

    remove player.js
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-homepage.php
+++ page-homepage.php
@@ -63,7 +63,7 @@
 <div class="container">
 	<div class="row">
         <div class="col-md-10 col-md-offset-1 video">
-            <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/571637197?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Nextcloud 22 introduction and overview"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+            <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/571637197?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Nextcloud 22 introduction and overview"></iframe></div>
 		</div>
     </div>
 	<div class="row">
