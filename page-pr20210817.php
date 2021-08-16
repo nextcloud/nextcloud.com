2e29a3760764907bf149d51ff193cda6f1a54fda
Jos Poortvliet <jospoortvliet@gmail.com>
2021-08-16 11:36:03 +0200

    add press release
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20210817.php
@@ -0,0 +1,89 @@
+<head>
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/pressrelease.css" rel="stylesheet">
+    <meta itemprop="image" content="https://nextcloud.com/media/Digital-platform-Nextcloud-logo-GAIA-X.png">
+    <meta name="twitter:image" content="https://nextcloud.com/media/Digital-platform-Nextcloud-logo-GAIA-X.png">
+    <meta name="twitter:image:src" content="https://nextcloud.com/media/Digital-platform-Nextcloud-logo-GAIA-X.png">
+    <meta property="og:image" content="https://nextcloud.com/media/Digital-platform-Nextcloud-logo-GAIA-X.png">
+</head>
+<div class="background generic-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+                <h2>Press release</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="section--pressrelease">
+<div class="container">
+<div class="row">
+<div class="pr">
+<p><a href="#german">Deutsche Version unter</a></p>
+<h1>Gaia-X proves independence from US big tech, picks EU based collaboration platform</h1>
+<p><em>Gaia-X doubles down on the original mission of independence from foreign Hyperscalers and shows tangible results</em></p>
+<p><em>Berlin, August 17 2021</em> - Nextcloud GmbH, the company behind the worlds’ most deployed on-premises content collaboration platform announces collaboration with the GAIA-X project, is now the official digital collaboration platform for GAIA-X.</p>
+<blockquote>As a large and diversified European endeavor, Gaia-X was looking for a flexible, functionally complete, and European-based collaboration platform that integrates well with other tools. Nextcloud facilitates extensive cross-organisational collaboration for us in a secure and compliant way.</blockquote>
+<p><em>– Francesco Bonfiglio, CEO of Gaia-X AISBL</em></p>
+<p>GAIA-X was imagined as a federated data infrastructure ecosystem for Europe. More than 250 members already came on board and decided to join forces around the same common objectives and endeavors of GAIA-X. Europe’s GAIA-X challenges the Big Tech cloud dominance and gives European companies more control over their data. In other words, it is Europe’s plan to reclaim data sovereignty, the driving motivation behind the project.</p>
+<p>The joining of US hyperscalers Amazon Web Services, Google, and Microsoft called into question whether EU’s Gaia-X cloud platform depends on the participation of these foreign giants and makes unclear how Europe can maintain control of its digital future if it’s reliant on foreign companies for technology.</p>
+<p>After a long selection process, GAIA-X board bypassed the products from the hyperscalers and selected a European-developed and hosted collaboration platform. The GAIA-X members will collaborate on the next generation, independent and federated infrastructure for Europe using Nextcloud, the most popular on-premises content collaboration platform, hosted at Europe’s largest hosting provider, IONOS.</p>
+<blockquote>The choice for Nextcloud by GAIA-X shows Europe already has a strong alternative to the products of foreign hyperscalers. As a platform already used by tens of millions of users in government, education, finance and health care, Nextcloud is a proven platform that offers strong, real-time collaboration in a secure, compliant and privacy-respecting manner.</blockquote>
+<p>-- Frank Karlitschek, CEO and Founder of Nextcloud</p>
+<p>The new digital collaboration platform Nextcloud is now up and running and the first members are onboarded. Every member already has read-only access to <a href="https://community.gaia-x.eu/" class="hyperlink">https://community.gaia-x.eu/</a>.</p>
+<p>The first Extraordinary General Assembly of the Gaia-X AISBL  already took place in June 2021 to nominate the new Board of Directors by using Nextcloud Polls as a voting system to establish the first Board of Directors for the next two years.</p>
+<h2>About Nextcloud</h2>
+<p>Nextcloud Hub is the industry-leading, fully open source, on-premises team productivity platform, combining the easy user interface of consumer-grade cloud solutions with the security and compliance measures enterprises need. Nextcloud Hub brings together universal access to data through mobile, desktop and web interfaces with next-generation, on-premise secure communication and collaboration features like real-time document editing, chat and video calls, putting them under direct control of IT and integrated with existing infrastructure.</p>
+<p>Nextcloud’s easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a class="hyperlink" href="https://nextcloud.com">nextcloud.com</a> or follow <a class="hyperlink" href="https://twitter.com/Nextclouders">@Nextclouders</a> on Twitter.</p>
+
+<h2>Links</h2>
+<ul>
+    <li><a href="https://nextcloud.com/about">About Nextcloud</a></li>
+    <li><a href="https://www.gaia-x.eu">About GAIA-X</a></li>
+    <li><a href="https://cloud.nextcloud.com/s/TS4FXRcKsEy4H8A">Graphics</a></li>
+</ul>
+<h2>Screenshots</h2>
+<ul>
+    <li><a href="https://nextcloud.com/media/Frank_Karlitschek.jpg">Frank Karlitschek, CEO at Nextcloud GmbH</a></li>
+    <li><a href="https://nextcloud.com/media/Digital-platform-Nextcloud-logo-GAIA-X.png">Gaia-X login screen with Nextcloud logo</a></li>
+</ul>
+
+<h2>Press contacts</h2>
+<p>Jos Poortvliet<br />
+Marketing Director<br />
+Phone: +49 (0) 171 121 7528<br />
+pr@nextcloud.com</p>
+<hr />
+
+
+<a name="german"></a>
+
+<h1>TBD</h1>
+
+<h2>Über Nextcloud</h2>
+<p>Nextcloud Hub ist die branchenführende, vollständig quelloffene Plattform für die Team-Produktivität vor Ort, die die einfache Benutzeroberfläche von Cloud-Lösungen für Privatanwender mit den Sicherheits- und Compliance-Maßnahmen kombiniert, die Unternehmen benötigen. Nextcloud Hub vereint den universellen Zugriff auf Daten über Mobil-, Desktop- und Webschnittstellen mit sicheren Kommunikations- und Kollaborationsfunktionen der nächsten Generation vor Ort, wie Dokumentenbearbeitung in Echtzeit, Chat und Videoanrufe, und stellt sie unter die direkte Kontrolle der IT-Abteilung und integriert in die bestehende Infrastruktur.</p>
+
+<p>Die einfache und schnelle Bereitstellung von Nextcloud, die offene, modulare Architektur und der Schwerpunkt auf Sicherheit und fortschrittlichen Federation-Funktionen ermöglichen es modernen Unternehmen, ihre bestehenden Dateispeicher innerhalb und über die Grenzen ihrer Organisation hinaus zu nutzen. Für weitere Informationen besuchen Sie <a href="https://nextcloud.com">nextcloud.com</a> oder folgen Sie <a href="https://twitter.com/Nextclouders">https://twitter.com/Nextclouders</a> auf Twitter.</p>
+<h2>Links</h2>
+<ul>
+<li><a href="https://nextcloud.com/about">Über Nextcloud</a></li>
+<li><a href="https://www.gaia-x.eu">Über GAIA-X</a></li>
+<li><a href="https://cloud.nextcloud.com/s/TS4FXRcKsEy4H8A">Graphics</a></li>
+</ul>
+<h2>Screenshots</h2>
+<ul>
+<li><a href="https://nextcloud.com/media/Frank_Karlitschek.jpg">Frank Karlitschek, CEO at Nextcloud GmbH</a></li>
+<li><a href="https://nextcloud.com/media/Digital-platform-Nextcloud-logo-GAIA-X.png">Gaia-X login screen with Nextcloud logo</a></li>
+</ul>
+<h2>Presse Kontact</h2>
+<p>Jos Poortvliet<br />
+Marketing Director<br />
+Phone: +49 (0) 171 121 7528<br />
+pr@nextcloud.com</p>
+</div>
+</div>
+</div>
+</section>
+
+
+
