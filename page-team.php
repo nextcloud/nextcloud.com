c99c78ddd3d1b668d3d2f67e767b3c38e357e1ea
Jos Poortvliet <jospoortvliet@gmail.com>
2021-10-05 09:08:54 +0200

    Add Aleksandra (#1555)
    * Add Aleksandra
    
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>
    
    * Add gh link for Aleksandra
    
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-team.php
+++ page-team.php
@@ -493,6 +493,19 @@
 				<p class="teamdescription">Alex has a master's degree in computer science. He worked in the gaming industry on well-known game franchises doing C++ and ActionScript but also freelanced while traveling through South East Asia in a variety of front-end and back-end work with JS, and even more with C++ and Qt. He's into cyberpunk and 80's heavy metal.</p>
 			</div>
 		</div>
+        <div class="col-md-4">
+            <div class="teamp">
+            <img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/aleksandra.jpg" />
+				<h2 class="teamname">Aleksandra Lazarević</h2>
+				<div class="teammeta">
+					<h3 class="teamtitle">Software engineer</h3>
+					<h4 class="teamexpertise">Talk iOS, Swift</h4>
+					<h4 class="teamlocation">Lives in Belgrade, Serbia</h4>
+					<p class="teamsocial"><a href="https://github.com/alazarevic5"><i class="fa-github fa"></i></a></p>
+				</div>
+				<p class="teamdescription">Aleksandra has finished her master studies in University in Belgrade for Information Technology. She has been developing iOS mobile applications for several years. In addition to application development, she has experience in teaching.</p>
+			</div>
+        </div>
 		<div class="col-md-4">
             <div class="teamp">
                 <img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
