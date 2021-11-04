4e8652efa79d140906276e7a5010c789d19199a4
Jos Poortvliet <jospoortvliet@gmail.com>
2021-11-04 17:01:43 +0100

    add PR eID
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

---
+++ page-pr20211104.php
@@ -0,0 +1,79 @@
+<head>
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/pressrelease.css" rel="stylesheet">
+<!--    <meta itemprop="image" content="https://nextcloud.com/media/backup-app-settings-1.png">
+    <meta name="twitter:image" content="https://nextcloud.com/media/backup-app-settings-1.png">
+    <meta name="twitter:image:src" content="https://nextcloud.com/media/backup-app-settings-1.png">
+    <meta property="og:image" content="https://nextcloud.com/media/backup-app-settings-1.png">-->
+</head>
+<div class="background generic-background">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 topheader">
+                <h2>Press release</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="section--pressrelease">
+<div class="container">
+<div class="row">
+<div class="pr">
+<p><a href="#german">Deutsche Version unter</a></p>
+<h1>German government supports authentication with passport in Nextcloud</h1>
+<p><em>A digital boost for Europe as the collaboration platform for Gaia-X makes it possible to use European identity card for access</em></p>
+<p><em>Berlin, October 20 2021</em> - Nextcloud GmbH, the company behind the worlds’ most deployed on-premises content collaboration platform announces support for European e-ID identity card authentication, developed on behalf of the German Federal Office for Information Security. Citizens are now able to log into the European digital sovereign communication and collaboration platform used by Gaia-X and many others.</p>
+<p>To date, organizations in Europe have largely relied on proprietary, foreign collaboration platforms due to a lack of viable alternatives. This leads to dependency and a quasi-monopoly position for mostly US and China-based software and service providers while also introducing data protection and privacy concerns. Nextcloud supports businesses, governments and individuals in data protection-compliant digitalization, providing a complete, modern solution for communication and collaboration. Nextcloud has been proven in tens of thousands of organizations with tens of millions of users across the globe and is widely adopted by businesses and government organizations across Europe.</p>
+<p>The German eID Card (“Personalausweis”), which has been certified at the highest possible level of assurance with the <a href="https://eID.AS/">eIDAS</a>-Regulation, can be used by all citizens for electronic identification (eID) and for strong pseudonymous authentication on the Internet. Until now, this has been used in a range of special applications mostly in the German government sector.</p>
+<p>With the “eID-Login” App for Nextcloud developed by ecsec GmbH on behalf of the Federal Office for Information Security (Bundesamt für Sicherheit in der Informationstechnik, BSI), it is now possible to use the German eID Card in Nextcloud for strong authentication. The technology is published under an <a href="https://github.com/ecsec/eID-Login/Nextcloud">Open Source</a> license and available at no cost. This makes Nextcloud an even more viable platform for use in e-government or data protection critical use cases.</p>
+<blockquote>
+<p>It is great to see the German governments’ interest in supporting Nextcloud as the first popular collaboration and cloud solution with support for the German eID Card for strong authentication and identification. This enhances its strength s a platform already used by tens of millions of users in government, education, finance and health care for real-time collaboration in a secure, compliant and privacy-respecting manner.</p>
+</blockquote>
+<p>-- Frank Karlitschek, CEO and Founder of Nextcloud</p>
+<p>The early and consequent consideration of relevant security aspects according to the <a href="https://zenodo.org/record/4502152#.YLoP3qgzZjE">„Security by Design“</a> principle and the publication of the “eID-Login” App for Nextcloud as <a href="https://github.com/ecsec/eID-Login/Nextcloud">Open Source</a> ensure that a very high level of trustworthiness is achieved. Thanks to the <a href="https://skidentity.de/">SkIDentity</a> service, which is provided without fee for the project, the (German) eID card can now be used free of charge for strong authentication in Nextcloud.</p>
+<h2>About Nextcloud GmbH</h2>
+<p>Nextcloud Hub is the industry-leading, fully open-source, on-premises team productivity platform and Germany’s number one collaboration solution. It combines the easy user interface of consumer-grade cloud solutions with the security and compliance measures enterprises need. Nextcloud Hub brings together universal access to data through mobile, desktop and web interfaces with next-generation, on-premise secure communication and collaboration features like real-time document editing, chat and video calls, putting them under the direct control of IT and integrated with existing infrastructure.</p>
+<p>Nextcloud’s easy and quick deployment, open, modular architecture and emphasis on security and advanced federation capabilities enable modern enterprises to leverage their existing file storage assets within and across the borders of their organization. For more information, visit <a href="https://nextcloud.com/">nextcloud.com</a> or follow <a href="https://twitter.com/Nextclouders">@Nextclouders</a> on Twitter.</p>
+<h2>Links</h2>
+<ul>
+<li>About Nextcloud: <a href="https://nextcloud.com/about" class="hyperlink">https://nextcloud.com/about</a></li>
+<li>About eIDAS : <a href="https://www.eid.as" class="hyperlink">https://www.eid.as</a></li>
+</ul>
+<h2>Press contacts</h2>
+<p>Jos Poortvliet<br />
+Marketing Director<br />
+Phone: +49 (0) 171 121 7528<br />
+pr@nextcloud.com</p>
+<hr />
+<a id="german"></a>
+<h3>German version</h3>
+<h1>Deutsche Regierung unterstützt die Nextcloud Authentifizierung mit dem Personalausweis</h1>
+<p><em>Ein digitaler Schub für Europa: Die Kollaborationsplattform Nextcloud für Gaia-X und andere Platformen, ermöglicht den Zugang mit Hilfe des europäischen Personalausweises.</em></p>
+<p>Berlin, 4. November 2021 - Die Nextcloud GmbH, das Unternehmen hinter der weltweit am weitesten verbreiteten On-Premise-Content-Collaboration-Plattform, gibt die Unterstützung der Authentifizierung mit dem europäischen Personalausweis bekannt, die im Auftrag des Bundesamts für Sicherheit in der Informationstechnik entwickelt wurde. Damit können sich Bürgerinnen und Bürger in die von Gaia-X genutzte europäische Plattform für digital souveräne Kommunikation und Zusammenarbeit einloggen.</p>
+<p>Bislang haben sich Organisationen in Europa mangels zuverlässiger Alternativen weitgehend auf proprietäre, ausländische Kollaborationsplattformen verlassen. Das führt zu Abhängigkeiten und einem Quasi-Monopol meist amerikanischer und chinesischer Software- und Serviceanbieter und wirft Fragen des Datenschutzes und der Privatsphäre auf. Nextcloud unterstützt Unternehmen, Behörden und Privatpersonen bei der datenschutzkonformen Digitalisierung und bietet eine umfassende, moderne Lösung für Kommunikation und Zusammenarbeit. Nextcloud hat sich bereits in zahlreichen Organisationen mit Millionen von Nutzern weltweit bewährt und wird von Unternehmen und Behörden in ganz Europa eingesetzt.</p>
+<p>Der nach der eIDAS-Verordnung auf höchstem Sicherheitsniveau zertifizierte deutsche eID-Ausweis (Personalausweis) kann von allen Bürgern zur elektronischen Identifikation (eID) und Authentifizierung im Internet genutzt werden. Bislang wird dies in einer Reihe von Spezialanwendungen vor allem im deutschen Behördenbereich genutzt.</p>
+<p>Mit der von der ecsec GmbH im Auftrag des Bundesamtes für Sicherheit in der Informationstechnik (BSI) entwickelten App “eID-Login” für Nextcloud*,* ist es nun möglich, den deutschen eID-Ausweis in Nextcloud zur Authentifizierung zu nutzen. Die Technologie wird unter einer Open-Source-Lizenz veröffentlicht und ist kostenlos verfügbar. Dies macht Nextcloud zu einer Plattform für den Einsatz in E-Government- oder datenschutzkritischen Anwendungsfällen.</p>
+<blockquote>
+<p>Wir freuen uns sehr über das Interesse der deutschen Regierung, Nextcloud als beliebte Kollaborations- und Cloud-Lösung mit den deutschen Personalausweises für starke Authentifizierung und Identifizierung zu unterstützen. Dies unterstreicht die Stärke der Plattform, die bereits von Millionen von Anwendern in Behörden, im Bildungs-, Finanz- und Gesundheitswesen für eine sichere und datenschutzkonforme Zusammenarbeit in Echtzeit genutzt wird.</p>
+</blockquote>
+<p>-- <em>Frank Karlitschek, CEO und Gründer von Nextcloud</em></p>
+<p>Die frühzeitige und konsequente Berücksichtigung relevanter Sicherheitsaspekte nach dem Security by Design”-Prinzip und die Freigabe der eID-Login”-App für Nextcloud als Open Source sorgen dafür, dass ein sehr hohes Maß an Vertrauenswürdigkeit erreicht wird. Dank des für das Projekt kostenfrei zur Verfügung gestellten SkIDentity-Dienstes kann der (deutsche) eID-Ausweis für die Authentifizierungmit Nextcloud genutzt werden.</p>
+<h2>Über Nextcloud GmbH</h2>
+<p>Nextcloud bietet <a href="https://nextcloud.com/blog/first-in-germany-again/">die branchenführende</a> Open-Source-Softwarelösung für on-premises Datenverarbeitung und -kommunikation, die die einfache Benutzeroberfläche von Consumer-Grade-Cloud-Lösungen mit den für Unternehmen erforderlichen Sicherheits- und Compliance-Maßnahmen kombiniert. Nextcloud vereint den universellen Zugriff auf Daten über mobile, Desktop- und Web-Interfaces mit innovativen, sicheren Kommunikations- und Kollaborationsfunktionen wie Dokumentenbearbeitung in Echtzeit, Chat und Videoanrufe, die unter direkter Kontrolle der IT und in die bestehende Infrastruktur integriert werden.</p>
+<p>Mit seiner einfachen und schnellen Bereitstellung, offenen, modularen Architektur und dem Fokus auf Sicherheits- und fortschrittlichen Föderationsfunktionen, ermöglicht Nextcloud modernen Unternehmen, ihre vorhandenen Dateispeicheranlagen innerhalb und außerhalb der Grenzen ihres Unternehmens zu optimieren. Für weitere Informationen besuchen Sie <a href="https://nextcloud.com">nextcloud.com</a> oder folgen Sie <span class="citation" data-cites="Nextclouders">[@Nextclouders]</span>(https://twitter.com/Nextclouders) auf Twitter.</p>
+<h2>Links</h2>
+<ul>
+<li>Über Nextcloud: <a href="https://nextcloud.com/about" class="hyperlink">https://nextcloud.com/about</a></li>
+<li>Über eIDAS : <a href="https://www.eid.as" class="hyperlink">https://www.eid.as</a></li>
+</ul>
+<h2>Press contacts</h2>
+<p>Jos Poortvliet<br />
+Marketing Director<br />
+Phone: +49 (0) 171 121 7528<br />
+pr@nextcloud.com</p>
+
+</div>
+</div>
+</div>
+</section>
+<?php require get_template_directory().'/overlay.php'; ?>
