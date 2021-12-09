0fd3c15ffbacccd2245605680a3acb5b2f138057
Jos Poortvliet <jospoortvliet@gmail.com>
2021-12-09 19:41:44 +0100

    update photos
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-team.php
+++ page-team.php
@@ -482,7 +482,7 @@
 		</div>
 		<div class="col-md-4">
 			<div class="teamp">
-				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
+				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/alvaro.jpg" />
 				<h2 class="teamname">Álvaro Brey Vilas</h2>
 				<div class="teammeta">
 					<h3 class="teamtitle">Software Engineer</h3>
@@ -808,7 +808,7 @@
 		</div>
 		<div class="col-md-4">
 			<div class="teamp">
-				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
+				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/nimisha.jpg" />
 				<h2 class="teamname">Nimisha Vijay</h2>
 				<div class="teammeta">
 					<h3 class="teamtitle">Designer</h3>
