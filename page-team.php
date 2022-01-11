807036188904dfd2a350a89e4a7ff6b5f3f9cdd4
Jos Poortvliet <jospoortvliet@gmail.com>
2022-01-11 18:48:42 +0100

    Fix image Nikola
    Signed-off-by: Jos Poortvliet <jospoortvliet@gmail.com>

--- page-team.php
+++ page-team.php
@@ -887,7 +887,7 @@
 		</div>
 		<div class="col-md-4">
 			<div class="teamp">
-				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/nimisha.jpg" />
+				<img class="img-responsive img-circle teamimg" alt="photo" src="<?php bloginfo('template_directory'); ?>/assets/img/people/person.jpg" />
 				<h2 class="teamname">Nikola Gladovic</h2>
 				<div class="teammeta">
 					<h3 class="teamtitle">Software engineer</h3>
