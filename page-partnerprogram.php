7b2dad527736fe3117af2e99a95ae7c22423e7f9
hipinspire <mb@hipinspire.com>
2022-01-19 16:28:37 +0100

    update

--- page-partnerprogram.php
+++ page-partnerprogram.php
@@ -1,64 +1,65 @@
 <head>
-<link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/partnerprogram.css?v=3" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/partnerprogram.css?v=3" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/pages/partners.min.css" rel="stylesheet">
 </head>
 
 <section class="hero-section">
-	<div class="background partnerprogram-background">
-		<div class="container">
-			<div class="col-md-6 topheader">
-				<h1>Partner program</h1>
-				<h2>Become part of the success story</h2>
-			</div>
-		</div>
-	</div>
+    <div class="background partnerprogram-background">
+        <div class="container">
+            <div class="col-md-6 topheader">
+                <h1>Partner program</h1>
+                <h2>Become part of the success story</h2>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section class="section--intro">
-<div class="container">
-	<div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <p class="section--paragraph text-center">With 100% year over year organic growth and well over 400 enterprise customers, Nextcloud GmbH is a sustainable and professional business offering what your customers need: a reliable, secure, on-premises collaboration platform.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <p class="section--paragraph text-center">With 100% year over year organic growth and well over 400 enterprise customers, Nextcloud GmbH is a sustainable and professional business offering what your customers need: a reliable, secure, on-premises collaboration platform.</p>
 
-            <p class="section--paragraph text-center">Join us on our growth path to increase revenue for your own business and give your customers the certainty that you are working with the market leading firm in on-premises productivity.</p>
+                <p class="section--paragraph text-center">Join us on our growth path to increase revenue for your own business and give your customers the certainty that you are working with the market leading firm in on-premises productivity.</p>
+            </div>
         </div>
-	</div>
-</div>
+    </div>
 </section>
 
 <section class="section--overview">
-<div class="container">
-	<div class="row">
-		<div class="col-md-4">
-			<div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/mobile.svg" /></div>
-			<h3 class="section--paragraph__title text-center">Provide trust</h3>
-			<p class="section--paragraph text-center">A Nextcloud partnership shows customers they can trust your expertise and know it is backed by the full engineering power of Nextcloud.</p>
-		</div>
-		<div class="col-md-4">
-			<div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/permissions.svg" /></div>
-			<h3 class="section--paragraph__title text-center">Go to market together</h3>
-			<p class="section--paragraph text-center">Promote, market and sell Nextcloud together. Organize and attend events with us, acquire leads, participate in tenders and close deals.</p>
-		</div>
-		<div class="col-md-4">
-			<div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/storage.svg" /></div>
-			<h3 class="section--paragraph__title text-center">Level up</h3>
-			<p class="section--paragraph text-center">Provide training to your team or customers, access our technical support and learn from our experts how to optimize large-scale deployments.</p>
-		</div>
-	</div>
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/mobile.svg" /></div>
+                <h3 class="section--paragraph__title text-center">Provide trust</h3>
+                <p class="section--paragraph text-center">A Nextcloud partnership shows customers they can trust your expertise and know it is backed by the full engineering power of Nextcloud.</p>
+            </div>
+            <div class="col-md-4">
+                <div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/permissions.svg" /></div>
+                <h3 class="section--paragraph__title text-center">Go to market together</h3>
+                <p class="section--paragraph text-center">Promote, market and sell Nextcloud together. Organize and attend events with us, acquire leads, participate in tenders and close deals.</p>
+            </div>
+            <div class="col-md-4">
+                <div class="icon text-center"><img src="<?php bloginfo('template_directory'); ?>/assets/img/icons/storage.svg" /></div>
+                <h3 class="section--paragraph__title text-center">Level up</h3>
+                <p class="section--paragraph text-center">Provide training to your team or customers, access our technical support and learn from our experts how to optimize large-scale deployments.</p>
+            </div>
+        </div>
+    </div>
 </section>
 
 
 <section class="section--register quote">
-	<div class="container">
-       <div class="row">
+    <div class="container">
+        <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <p class="section--paragraph__title text-center">Get started!</p>
                 <p class="section--paragraph text-center">You know your customer. We know our product.<br /> Let's work together for the best results!</p>
-            <div class="text-center morebuttondiv">
-                <a href="<?php echo home_url('partnerapply') ?>" class="button button--large button--arrow">Apply as partner <i class="icon-arrow-circle-o-right icon"></i></a>
+                <div class="text-center morebuttondiv">
+                    <a href="<?php echo home_url('partnerapply') ?>" class="button button--large button--arrow">Apply as partner <i class="icon-arrow-circle-o-right icon"></i></a>
+                </div>
             </div>
         </div>
-	</div>
 </section>
 
 <section class="section--logos">
@@ -73,19 +74,19 @@
 </section>
 
 <section class="section--benefits">
-<div class="container-fluid">
-    <div class="container">
+    <div class="container-fluid">
+        <div class="container">
             <h2 class="text-center">Partner benefits</h2>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <p class="section--paragraph">Nextcloud works with a variety of partners, building on, providing services around and complementing our product. The Nextcloud partner program is designed to ensure that our partners receive the highest quality training, support, and enablement in the industry. We recognize that you, our partners, are critical to our growth initiatives and ultimate objective: to deliver innovative technology that is beautiful, functional, and easy to use.</p>
-                <br />
-                <p class="section--paragraph">As partner you benefit from:</p>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <p class="section--paragraph">Nextcloud works with a variety of partners, building on, providing services around and complementing our product. The Nextcloud partner program is designed to ensure that our partners receive the highest quality training, support, and enablement in the industry. We recognize that you, our partners, are critical to our growth initiatives and ultimate objective: to deliver innovative technology that is beautiful, functional, and easy to use.</p>
+                    <br />
+                    <p class="section--paragraph">As partner you benefit from:</p>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4 col-md-offset-2">
-<!--                 <ul> -->
+            <div class="row">
+                <div class="col-md-4 col-md-offset-2">
+                    <!--                 <ul> -->
                     <li> Lead sharing</li>
                     <li> Sales and technical training</li>
                     <li> Get and manage certifications</li>
@@ -94,9 +95,9 @@
                     <li> Access to Nextcloud Enterprise and Portal resources</li>
                     <li> Joint marketing</li>
                     <li> Marketing materials</li>
-<!--                 </ul> -->
-            </div>
-            <div class="col-md-4">
+                    <!--                 </ul> -->
+                </div>
+                <div class="col-md-4">
                     <li> Event collaboration</li>
                     <li> Nextcloud logo use</li>
                     <li> Partner logo & position on partner page</li>
@@ -105,22 +106,116 @@
                     <li> Kick-backs</li>
                     <li> Deal registration discount</li>
                     <li> Access to NFR licenses</li>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </section>
 
-
-
-<section class="section--register quote">
-	<div class="container">
-       <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <p class="text-center">Get started today.</p>
-            <div class="text-center morebuttondiv">
-                <a href="<?php echo home_url('partnerapply') ?>" class="button button--large button--arrow">Apply as partner <i class="icon-arrow-circle-o-right icon"></i></a>
+<section class="dist-partners-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="the-title">
+                    <h3>Distribution Partners</h3>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="partners-holder">
+                    <div class="partner2-col">
+                        <div class="partner-box">
+                            <div class="partner-logo">
+                                <img src="<?php bloginfo('template_directory'); ?>/assets/img/partners/corewin.png" alt="CoreWin logo" title="CoreWin"/>
+                            </div>
+                            <div class="partner-text">
+                                <h4>CoreWin</h4>
+                                <p>CoreWin is a distributor of software with a focus on process optimization, information security, data analysis and consulting.
+                                CoreWin’s team speaks English fluently, that’s why company works directly with software vendors to provide a quick response to all inquiries.
+                                CoreWin also provides the first level of technical support for all products. Being a distributor, CoreWin carries out pre-sales and technical consultations, as well as provide implementation services.
+                                CoreWin – first Core, then Win.</p>
+                            </div>
+                            <ul class="partner-info">
+                                <li>Distributor</li>
+                                <li><a href="http://www.corewin.com.ua" target="_blank">Go to website</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="partner2-col">
+                        <div class="partner-box">
+                            <div class="partner-logo">
+                                <img src="<?php bloginfo('template_directory'); ?>/assets/img/partners/ebertlang.png" alt="EBERTLANG logo" title="EBERTLANG"/>
+                            </div>
+                            <div class="partner-text">
+                                <h4>EBERTLANG Distribution GmbH</h4>
+                                <p>EBERTLANG is a leading value-added distributor of infrastructure software. The company offers a broad portfolio of solutions, including IT security, backup & storage and managed services. With more than 25 years of experience and two locations in Wetzlar (Germany) and Pfäffikon (Switzerland), EBERTLANG has built a tremendous network of more than 19,000 registered reseller partners, who have access to comprehensive training programs, a highly qualified technical support team, and an automated partner portal.</p>
+                            </div>
+                            <ul class="partner-info">
+                                <li>Distributor</li>
+                                <li><a href="https://www.ebertlang.com" target="_blank">Go to website</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="partner2-col">
+                        <div class="partner-box">
+                            <div class="partner-logo">
+                                <img src="<?php bloginfo('template_directory'); ?>/assets/img/partners/fgts.png" alt="FactorGroup logo" title="FactorGroup"/>
+                            </div>
+                            <div class="partner-text">
+                                <h4>FactorGroup</h4>
+                                <p>Factor group specializes in supplying the telecommunications industry with the world's leading equipment and technological solutions, offering consulting services, pre-design and design works, pre-sale advice, equipment installation and commissioning support, employee training, as well as after-sales service, product versions updates, equipment repair and replacement. Factor group executives have expertise in networking and information technologies market in Russia and the CIS countries since 1991.</p>
+                            </div>
+                            <ul class="partner-info">
+                                <li>Distributor</li>
+                                <li><a href="http://fgts.ru" target="_blank">Go to website</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="partner2-col">
+                        <div class="partner-box">
+                            <div class="partner-logo">
+                                <img src="<?php bloginfo('template_directory'); ?>/assets/img/partners/hancom.png" alt="Hancom Incorporate logo" title="Hancom Incorporate"/>
+                            </div>
+                            <div class="partner-text">
+                                <h4>Hancom Incorporate</h4>
+                                <p>With more than 30 years in productivity applications, HANCOM has been providing a powerful productivity solution optimized for collaboration in diverse workplaces. We deliver a consistent, professional-grade experience and supports essential office applications - word processor, spreadsheet, presentation and PDF tool across multiple platforms including Windows OS, Mobile and Web to offer boundless office productivity environments.</p>
+                            </div>
+                            <ul class="partner-info">
+                                <li>Distributor</li>
+                                <li><a href="http://www.hancom.com" target="_blank">Go to website</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="partner2-col">
+                        <div class="partner-box">
+                            <div class="partner-logo">
+                                <img src="<?php bloginfo('template_directory'); ?>/assets/img/partners/prianto.png" alt="Prianto logo" title="Prianto Labs"/>
+                            </div>
+                            <div class="partner-text">
+                                <h4>Prianto PPM GmbH</h4>
+                                <p>
+                                    Prianto PPM GmbH is an independant part of the Prianto Group, which was spun off from Prianto GmbH in July 2019. Prianto PPM is aimed at system houses that want to purchase a wide variety of software products easily and at competitive prices from a single source. The business will be operated centrally from Germany for the whole of Europe. These include electronic data exchange, individualized price lists and a high level of compliance and security.    
+                                </p>
+                            </div>
+                            <ul class="partner-info">
+                                <li>Distributor</li>
+                                <li><a href="http://www.prianto.com/ppm" target="_blank">Go to website</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-	</div>
+    </div>
+</section>
+
+<section class="section--register quote">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <p class="text-center">Get started today.</p>
+                <div class="text-center morebuttondiv">
+                    <a href="<?php echo home_url('partnerapply') ?>" class="button button--large button--arrow">Apply as partner <i class="icon-arrow-circle-o-right icon"></i></a>
+                </div>
+            </div>
+        </div>
 </section>
